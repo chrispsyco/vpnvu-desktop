mullvad/mullvadvpn-app

Test if OpenVPN is respawned by daemon

mullvad-tests/tests/connection.rs
@@ -3,6 +3,7 @@
 extern crate mullvad_tests;
 extern crate mullvad_types;
 
+use std::fs;
 use std::time::Duration;
 
 use mullvad_tests::{wait_for_file, DaemonRunner};
@@ -41,6 +42,30 @@ fn spawns_openvpn() {
 
     rpc_client.set_account(Some("123456".to_owned())).unwrap();
     rpc_client.connect().unwrap();
+
+    wait_for_file(&openvpn_command_line_file, Duration::from_secs(5));
+
+    assert!(openvpn_command_line_file.exists());
+}
+
+#[test]
+fn respawns_openvpn_if_it_crashes() {
+    let mut daemon = DaemonRunner::spawn();
+    let mut rpc_client = daemon.rpc_client().unwrap();
+    let openvpn_command_line_file = daemon.mock_openvpn_command_line_file();
+
+    assert!(!openvpn_command_line_file.exists());
+
+    rpc_client.set_account(Some("123456".to_owned())).unwrap();
+    rpc_client.connect().unwrap();
+
+    wait_for_file(&openvpn_command_line_file, Duration::from_secs(5));
+
+    // Stop OpenVPN by removing the command_line file
+    fs::remove_file(&openvpn_command_line_file)
+        .expect("Failed to remove the mock OpenVPN command-line file");
+
+    assert!(!openvpn_command_line_file.exists());
 
     wait_for_file(&openvpn_command_line_file, Duration::from_secs(5));
 

chrispsyco/vpnvu-desktop

mullvad-tests/tests/connection.rs
@@ -4,6 +4,7 @@ extern crate mullvad_ipc_client;
 extern crate mullvad_tests;
 extern crate mullvad_types;
 
+use std::fs;
 use std::sync::mpsc;
 use std::time::Duration;
 
@@ -26,6 +27,27 @@ fn spawns_openvpn() {
 
     rpc_client.set_account(Some("123456".to_owned())).unwrap();
     rpc_client.connect().unwrap();
+
+    wait_for_file_write_finish(&openvpn_args_file, Duration::from_secs(5));
+
+    assert!(openvpn_args_file.exists());
+}
+
+#[test]
+fn respawns_openvpn_if_it_crashes() {
+    let mut daemon = DaemonRunner::spawn();
+    let mut rpc_client = daemon.rpc_client().unwrap();
+    let openvpn_args_file = daemon.mock_openvpn_args_file();
+
+    assert!(!openvpn_args_file.exists());
+
+    rpc_client.set_account(Some("123456".to_owned())).unwrap();
+    rpc_client.connect().unwrap();
+
+    wait_for_file_write_finish(&openvpn_args_file, Duration::from_secs(5));
+
+    // Stop OpenVPN by removing the mock OpenVPN arguments file
+    fs::remove_file(&openvpn_args_file).expect("Failed to remove the mock OpenVPN arguments file");
 
     wait_for_file_write_finish(&openvpn_args_file, Duration::from_secs(5));
 