mullvad/mullvadvpn-app

Expose autoconnect option in CLI

mullvad-cli/src/cmds/mod.rs
@@ -4,6 +4,9 @@ use Command;
 mod account;
 pub use self::account::Account;
 
+mod autoconnect;
+pub use self::autoconnect::Autoconnect;
+
 mod status;
 pub use self::status::Status;
 
@@ -29,6 +32,7 @@ pub use self::version::Version;
 pub fn get_commands() -> HashMap<&'static str, Box<Command>> {
     let commands: Vec<Box<Command>> = vec![
         Box::new(Account),
+        Box::new(Autoconnect),
         Box::new(Status),
         Box::new(Connect),
         Box::new(Disconnect),

mullvad-ipc-client/src/lib.rs
@@ -155,8 +155,20 @@ impl DaemonRpcClient {
         self.call("get_account_data", &[account])
     }
 
+    pub fn set_allow_lan(&mut self, allow_lan: bool) -> Result<()> {
+        self.call("set_allow_lan", &[allow_lan])
+    }
+
     pub fn get_allow_lan(&mut self) -> Result<bool> {
         self.call("get_allow_lan", &NO_ARGS)
+    }
+
+    pub fn set_autoconnect(&mut self, autoconnect: bool) -> Result<()> {
+        self.call("set_autoconnect", &[autoconnect])
+    }
+
+    pub fn get_autoconnect(&mut self) -> Result<bool> {
+        self.call("get_autoconnect", &NO_ARGS)
     }
 
     pub fn get_current_location(&mut self) -> Result<GeoIpLocation> {
@@ -189,10 +201,6 @@ impl DaemonRpcClient {
 
     pub fn set_account(&mut self, account: Option<AccountToken>) -> Result<()> {
         self.call("set_account", &[account])
-    }
-
-    pub fn set_allow_lan(&mut self, allow_lan: bool) -> Result<()> {
-        self.call("set_allow_lan", &[allow_lan])
     }
 
     pub fn set_openvpn_mssfix(&mut self, mssfix: Option<u16>) -> Result<()> {

chrispsyco/vpnvu-desktop

mullvad-cli/src/cmds/mod.rs
@@ -4,6 +4,9 @@ use Command;
 mod account;
 pub use self::account::Account;
 
+mod auto_connect;
+pub use self::auto_connect::AutoConnect;
+
 mod status;
 pub use self::status::Status;
 
@@ -29,6 +32,7 @@ pub use self::version::Version;
 pub fn get_commands() -> HashMap<&'static str, Box<Command>> {
     let commands: Vec<Box<Command>> = vec![
         Box::new(Account),
+        Box::new(AutoConnect),
         Box::new(Status),
         Box::new(Connect),
         Box::new(Disconnect),

mullvad-ipc-client/src/lib.rs
@@ -155,8 +155,20 @@ impl DaemonRpcClient {
         self.call("get_account_data", &[account])
     }
 
+    pub fn set_allow_lan(&mut self, allow_lan: bool) -> Result<()> {
+        self.call("set_allow_lan", &[allow_lan])
+    }
+
     pub fn get_allow_lan(&mut self) -> Result<bool> {
         self.call("get_allow_lan", &NO_ARGS)
+    }
+
+    pub fn set_auto_connect(&mut self, auto_connect: bool) -> Result<()> {
+        self.call("set_auto_connect", &[auto_connect])
+    }
+
+    pub fn get_auto_connect(&mut self) -> Result<bool> {
+        self.call("get_auto_connect", &NO_ARGS)
     }
 
     pub fn get_current_location(&mut self) -> Result<GeoIpLocation> {
@@ -189,10 +201,6 @@ impl DaemonRpcClient {
 
     pub fn set_account(&mut self, account: Option<AccountToken>) -> Result<()> {
         self.call("set_account", &[account])
-    }
-
-    pub fn set_allow_lan(&mut self, allow_lan: bool) -> Result<()> {
-        self.call("set_allow_lan", &[allow_lan])
     }
 
     pub fn set_openvpn_mssfix(&mut self, mssfix: Option<u16>) -> Result<()> {