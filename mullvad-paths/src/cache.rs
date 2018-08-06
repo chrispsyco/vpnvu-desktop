mullvad/mullvadvpn-app

use dirs instead of app_dirs

mullvad-paths/src/cache.rs
@@ -1,33 +1,28 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the cache directory pointed to by `MULLVAD_CACHE_DIR`, or the default
 /// one if that variable is unset.
 pub fn cache_dir() -> Result<PathBuf> {
-    let dir = get_cache_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_cache_dir)
 }
 
 fn get_cache_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_CACHE_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_cache_dir(),
+        None => user_cache_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(target_os = "linux")]
-fn get_default_cache_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/var/cache/mullvad-daemon"))
-}
-
-#[cfg(any(target_os = "macos", windows))]
-fn get_default_cache_dir() -> Result<PathBuf> {
-    Ok(::app_dirs::get_app_root(
-        ::app_dirs::AppDataType::UserCache,
-        &::metadata::APP_INFO,
-    )?)
+fn user_cache_dir() -> Result<PathBuf> {
+    #[cfg(target_os = "linux")]
+    {
+        Ok(PathBuf::from("/var/cache"))
+    }
+    #[cfg(any(target_os = "macos", windows))]
+    {
+        ::dirs::cache_dir().ok_or_else(|| ::ErrorKind::FindDirError.into())
+    }
 }

mullvad-paths/src/logs.rs
@@ -1,31 +1,29 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the logging directory pointed to by `MULLVAD_LOG_DIR`, or the default
 /// one if that variable is unset.
 pub fn log_dir() -> Result<PathBuf> {
-    let dir = get_log_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_log_dir)
 }
 
 /// Get the logging directory, but don't try to create it.
 pub fn get_log_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_LOG_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_log_dir(),
+        None => user_log_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(unix)]
-fn get_default_log_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/var/log/mullvad-daemon"))
-}
-
-#[cfg(windows)]
-fn get_default_log_dir() -> Result<PathBuf> {
-    ::get_program_data_dir()
+fn user_log_dir() -> Result<PathBuf> {
+    #[cfg(unix)]
+    {
+        Ok(PathBuf::from("/var/log"))
+    }
+    #[cfg(windows)]
+    {
+        ::get_allusersprofile_dir()
+    }
 }

mullvad-paths/src/settings.rs
@@ -1,33 +1,28 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the settings directory pointed to by `MULLVAD_SETTINGS_DIR`, or the default
 /// one if that variable is unset.
 pub fn settings_dir() -> Result<PathBuf> {
-    let dir = get_settings_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_settings_dir)
 }
 
 fn get_settings_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_SETTINGS_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_settings_dir(),
+        None => user_settings_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(unix)]
-fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/etc/mullvad-daemon"))
-}
-
-#[cfg(windows)]
-fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(::app_dirs::get_app_root(
-        ::app_dirs::AppDataType::UserData,
-        &::metadata::APP_INFO,
-    )?)
+fn user_settings_dir() -> Result<PathBuf> {
+    #[cfg(unix)]
+    {
+        Ok(PathBuf::from("/etc"))
+    }
+    #[cfg(windows)]
+    {
+        ::dirs::data_local_dir().ok_or_else(|| ::ErrorKind::FindDirError.into())
+    }
 }

chrispsyco/vpnvu-desktop

mullvad-paths/src/cache.rs
@@ -1,33 +1,28 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the cache directory pointed to by `MULLVAD_CACHE_DIR`, or the default
 /// one if that variable is unset.
 pub fn cache_dir() -> Result<PathBuf> {
-    let dir = get_cache_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_cache_dir)
 }
 
 fn get_cache_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_CACHE_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_cache_dir(),
+        None => get_default_cache_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(target_os = "linux")]
 fn get_default_cache_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/var/cache/mullvad-daemon"))
-}
-
-#[cfg(any(target_os = "macos", windows))]
-fn get_default_cache_dir() -> Result<PathBuf> {
-    Ok(::app_dirs::get_app_root(
-        ::app_dirs::AppDataType::UserCache,
-        &::metadata::APP_INFO,
-    )?)
+    #[cfg(target_os = "linux")]
+    {
+        Ok(PathBuf::from("/var/cache"))
+    }
+    #[cfg(any(target_os = "macos", windows))]
+    {
+        ::dirs::cache_dir().ok_or_else(|| ::ErrorKind::FindDirError.into())
+    }
 }

mullvad-paths/src/logs.rs
@@ -1,31 +1,29 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the logging directory pointed to by `MULLVAD_LOG_DIR`, or the default
 /// one if that variable is unset.
 pub fn log_dir() -> Result<PathBuf> {
-    let dir = get_log_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_log_dir)
 }
 
 /// Get the logging directory, but don't try to create it.
 pub fn get_log_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_LOG_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_log_dir(),
+        None => get_default_log_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(unix)]
 fn get_default_log_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/var/log/mullvad-daemon"))
-}
-
-#[cfg(windows)]
-fn get_default_log_dir() -> Result<PathBuf> {
-    ::get_program_data_dir()
+    #[cfg(unix)]
+    {
+        Ok(PathBuf::from("/var/log"))
+    }
+    #[cfg(windows)]
+    {
+        ::get_allusersprofile_dir()
+    }
 }

mullvad-paths/src/settings.rs
@@ -1,33 +1,28 @@
-use {ErrorKind, Result, ResultExt};
+use Result;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 /// Creates and returns the settings directory pointed to by `MULLVAD_SETTINGS_DIR`, or the default
 /// one if that variable is unset.
 pub fn settings_dir() -> Result<PathBuf> {
-    let dir = get_settings_dir()?;
-    fs::create_dir_all(&dir).chain_err(|| ErrorKind::CreateDirFailed(dir.clone()))?;
-    Ok(dir)
+    ::create_and_return(get_settings_dir)
 }
 
 fn get_settings_dir() -> Result<PathBuf> {
     match env::var_os("MULLVAD_SETTINGS_DIR") {
         Some(path) => Ok(PathBuf::from(path)),
-        None => get_default_settings_dir(),
+        None => get_default_settings_dir().map(|dir| dir.join(::PRODUCT_NAME)),
     }
 }
 
-#[cfg(unix)]
 fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(PathBuf::from("/etc/mullvad-daemon"))
-}
-
-#[cfg(windows)]
-fn get_default_settings_dir() -> Result<PathBuf> {
-    Ok(::app_dirs::get_app_root(
-        ::app_dirs::AppDataType::UserData,
-        &::metadata::APP_INFO,
-    )?)
+    #[cfg(unix)]
+    {
+        Ok(PathBuf::from("/etc"))
+    }
+    #[cfg(windows)]
+    {
+        ::dirs::data_local_dir().ok_or_else(|| ::ErrorKind::FindDirError.into())
+    }
 }