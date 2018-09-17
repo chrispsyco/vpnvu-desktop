mullvad/mullvadvpn-app

Implement support for resolvconf

talpid-core/src/security/linux/dns/mod.rs
@@ -1,8 +1,10 @@
+mod resolvconf;
 mod static_resolv_conf;
 mod systemd_resolved;
 
 use std::net::IpAddr;
 
+use self::resolvconf::Resolvconf;
 use self::static_resolv_conf::StaticResolvConf;
 use self::systemd_resolved::SystemdResolved;
 
@@ -14,12 +16,14 @@ error_chain! {
     }
 
     links {
+        Resolvconf(resolvconf::Error, resolvconf::ErrorKind);
         StaticResolvConf(static_resolv_conf::Error, static_resolv_conf::ErrorKind);
         SystemdResolved(systemd_resolved::Error, systemd_resolved::ErrorKind);
     }
 }
 
 pub enum DnsSettings {
+    Resolvconf(Resolvconf),
     StaticResolvConf(StaticResolvConf),
     SystemdResolved(SystemdResolved),
 }
@@ -28,6 +32,7 @@ impl DnsSettings {
     pub fn new() -> Result<Self> {
         SystemdResolved::new()
             .map(DnsSettings::SystemdResolved)
+            .or_else(|_| Resolvconf::new().map(DnsSettings::Resolvconf))
             .or_else(|_| StaticResolvConf::new().map(DnsSettings::StaticResolvConf))
             .chain_err(|| ErrorKind::NoDnsSettingsManager)
     }
@@ -36,6 +41,7 @@ impl DnsSettings {
         use self::DnsSettings::*;
 
         match *self {
+            Resolvconf(ref mut resolvconf) => resolvconf.set_dns(interface, servers)?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.set_dns(servers)?,
             SystemdResolved(ref mut systemd_resolved) => {
                 systemd_resolved.set_dns(interface, servers)?
@@ -49,6 +55,7 @@ impl DnsSettings {
         use self::DnsSettings::*;
 
         match *self {
+            Resolvconf(ref mut resolvconf) => resolvconf.reset()?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.reset()?,
             SystemdResolved(ref mut systemd_resolved) => systemd_resolved.reset()?,
         }

chrispsyco/vpnvu-desktop

talpid-core/src/security/linux/dns/mod.rs
@@ -1,28 +1,42 @@
+mod resolvconf;
 mod static_resolv_conf;
 
 use std::net::IpAddr;
 
+use self::resolvconf::Resolvconf;
 use self::static_resolv_conf::StaticResolvConf;
 
 error_chain! {
+    errors {
+        NoDnsSettingsManager {
+            description("No DNS settings manager detected")
+        }
+    }
+
     links {
+        Resolvconf(resolvconf::Error, resolvconf::ErrorKind);
         StaticResolvConf(static_resolv_conf::Error, static_resolv_conf::ErrorKind);
     }
 }
 
 pub enum DnsSettings {
+    Resolvconf(Resolvconf),
     StaticResolvConf(StaticResolvConf),
 }
 
 impl DnsSettings {
     pub fn new() -> Result<Self> {
-        Ok(DnsSettings::StaticResolvConf(StaticResolvConf::new()?))
+        Resolvconf::new()
+            .map(DnsSettings::Resolvconf)
+            .or_else(|_| StaticResolvConf::new().map(DnsSettings::StaticResolvConf))
+            .chain_err(|| ErrorKind::NoDnsSettingsManager)
     }
 
-    pub fn set_dns(&mut self, servers: Vec<IpAddr>) -> Result<()> {
+    pub fn set_dns(&mut self, interface: &str, servers: Vec<IpAddr>) -> Result<()> {
         use self::DnsSettings::*;
 
         match self {
+            Resolvconf(ref mut resolvconf) => resolvconf.set_dns(interface, servers)?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.set_dns(servers)?,
         }
 
@@ -33,6 +47,7 @@ impl DnsSettings {
         use self::DnsSettings::*;
 
         match self {
+            Resolvconf(ref mut resolvconf) => resolvconf.reset()?,
             StaticResolvConf(ref mut static_resolv_conf) => static_resolv_conf.reset()?,
         }
 