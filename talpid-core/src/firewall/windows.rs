mullvad/mullvadvpn-app

Add {v4,v6}_gateway fields to TunnelMetadata

talpid-core/src/firewall/windows.rs
@@ -139,7 +139,7 @@ impl Firewall {
     ) -> Result<()> {
         trace!("Applying 'connected' firewall policy");
         let ip_str = Self::widestring_ip(&endpoint.address.ip());
-        let gateway_str = Self::widestring_ip(&tunnel_metadata.gateway.into());
+        let gateway_str = Self::widestring_ip(&tunnel_metadata.v4_gateway.into());
 
         let tunnel_alias =
             WideCString::new(tunnel_metadata.interface.encode_utf16().collect::<Vec<_>>()).unwrap();

talpid-types/src/net/wireguard.rs
@@ -3,7 +3,7 @@ use ipnetwork::IpNetwork;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::{
     fmt,
-    net::{IpAddr, SocketAddr},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
 };
 
 
@@ -19,7 +19,8 @@ pub struct TunnelParameters {
 pub struct ConnectionConfig {
     pub tunnel: TunnelConfig,
     pub peer: PeerConfig,
-    pub gateway: IpAddr,
+    pub v4_gateway: Ipv4Addr,
+    pub v6_gateway: Option<Ipv6Addr>,
 }
 
 impl ConnectionConfig {

chrispsyco/vpnvu-desktop

talpid-core/src/firewall/windows.rs
@@ -141,7 +141,7 @@ impl Firewall {
     ) -> Result<()> {
         trace!("Applying 'connected' firewall policy");
         let ip_str = Self::widestring_ip(&endpoint.address.ip());
-        let gateway_str = Self::widestring_ip(&tunnel_metadata.gateway.into());
+        let gateway_str = Self::widestring_ip(&tunnel_metadata.ipv4_gateway.into());
 
         let tunnel_alias =
             WideCString::new(tunnel_metadata.interface.encode_utf16().collect::<Vec<_>>()).unwrap();

talpid-types/src/net/wireguard.rs
@@ -3,7 +3,7 @@ use ipnetwork::IpNetwork;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::{
     fmt,
-    net::{IpAddr, SocketAddr},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr},
 };
 
 use rand::RngCore;
@@ -21,7 +21,8 @@ pub struct TunnelParameters {
 pub struct ConnectionConfig {
     pub tunnel: TunnelConfig,
     pub peer: PeerConfig,
-    pub gateway: IpAddr,
+    pub ipv4_gateway: Ipv4Addr,
+    pub ipv6_gateway: Option<Ipv6Addr>,
 }
 
 impl ConnectionConfig {