mullvad/mullvadvpn-app

Fix custom relay settings schema in daemon-rpc.ts

gui/src/shared/daemon-rpc-types.ts
@@ -98,7 +98,8 @@ export type ConnectionConfig =
           addresses: string[];
           endpoint: string;
         };
-        gateway: string;
+        ipv4_gateway: string;
+        ipv6_gateway?: string;
       };
     };
 

chrispsyco/vpnvu-desktop

gui/src/shared/daemon-rpc-types.ts
@@ -90,15 +90,16 @@ export type ConnectionConfig =
   | {
       wireguard: {
         tunnel: {
-          private_key: string;
+          privateKey: string;
           addresses: string[];
         };
         peer: {
-          public_key: string;
+          publicKey: string;
           addresses: string[];
           endpoint: string;
         };
-        gateway: string;
+        ipv4Gateway: string;
+        ipv6Gateway?: string;
       };
     };
 