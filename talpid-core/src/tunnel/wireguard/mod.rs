mullvad/mullvadvpn-app

Change route manager to be active rather than passive

talpid-core/src/tunnel/wireguard/mod.rs
@@ -3,7 +3,8 @@
 use self::config::Config;
 use super::{TunnelEvent, TunnelMetadata};
 use crate::routing;
-use std::{io, path::Path, sync::mpsc};
+
+use std::{collections::HashMap, io, path::Path, sync::mpsc};
 
 pub mod config;
 mod ping_monitor;
@@ -53,7 +54,7 @@ pub struct WireguardMonitor {
     /// Tunnel implementation
     tunnel: Box<dyn Tunnel>,
     /// Route manager
-    router: routing::RouteManager,
+    route_handle: routing::RouteManagerHandle,
     /// Callback to signal tunnel events
     event_callback: Box<dyn Fn(TunnelEvent) + Send + Sync + 'static>,
     close_msg_sender: mpsc::Sender<CloseMsg>,
@@ -67,17 +68,21 @@ impl WireguardMonitor {
         on_event: F,
     ) -> Result<WireguardMonitor> {
         let tunnel = Box::new(WgGoTunnel::start_tunnel(&config, log_path)?);
-        let router = routing::RouteManager::new().map_err(Error::SetupRoutingError)?;
+        let iface_name = tunnel.get_interface_name();
+        let route_handle = routing::RouteManagerHandle::new(
+            Self::get_routes(iface_name, &config),
+            &mut tokio_executor::DefaultExecutor::current(),
+        )
+        .map_err(Error::SetupRoutingError)?;
         let event_callback = Box::new(on_event.clone());
         let (close_msg_sender, close_msg_receiver) = mpsc::channel();
-        let mut monitor = WireguardMonitor {
+        let monitor = WireguardMonitor {
             tunnel,
-            router,
+            route_handle,
             event_callback,
             close_msg_sender,
             close_msg_receiver,
         };
-        monitor.setup_routing(&config)?;
 
         let metadata = monitor.tunnel_metadata(&config);
         let iface_name = monitor.tunnel.get_interface_name().to_string();
@@ -101,7 +106,6 @@ impl WireguardMonitor {
             let _ = close_sender.send(CloseMsg::PingErr);
         });
 
-
         Ok(monitor)
     }
 
@@ -121,9 +125,7 @@ impl WireguardMonitor {
         // Clear routes manually - otherwise there will be some log spam since the tunnel device
         // can be removed before the routes are cleared, which automatically clears some of the
         // routes that were set.
-        if let Err(e) = self.router.delete_routes() {
-            log::error!("Failed to remove a route from the routing table - {}", e);
-        }
+        self.route_handle.stop();
 
         if let Err(e) = self.tunnel.stop() {
             log::error!("Failed to stop tunnel - {}", e);
@@ -132,40 +134,44 @@ impl WireguardMonitor {
         wait_result
     }
 
-    fn setup_routing(&mut self, config: &Config) -> Result<()> {
-        let iface_name = self.tunnel.get_interface_name();
-        let mut routes: Vec<_> = config
+    fn get_routes(
+        iface_name: &str,
+        config: &Config,
+    ) -> HashMap<ipnetwork::IpNetwork, crate::routing::NetNode> {
+        let node = routing::Node::device(iface_name.to_string());
+        let mut routes: HashMap<_, _> = config
             .peers
             .iter()
             .flat_map(|peer| peer.allowed_ips.iter())
             .cloned()
-            .map(|allowed_ip| {
-                routing::Route::new(allowed_ip, routing::NetNode::Device(iface_name.to_string()))
+            .flat_map(|allowed_ip| {
+                if allowed_ip.prefix() == 0 {
+                    if allowed_ip.is_ipv4() {
+                        vec![
+                            ("0.0.0.0/1".parse().unwrap(), node.clone().into()),
+                            ("128.0.0.0/1".parse().unwrap(), node.clone().into()),
+                        ]
+                    } else {
+                        vec![
+                            ("8000::/1".parse().unwrap(), node.clone().into()),
+                            ("::/1".parse().unwrap(), node.clone().into()),
+                        ]
+                    }
+                } else {
+                    vec![(allowed_ip, node.clone().into())]
+                }
             })
             .collect();
 
-        // To survive network roaming, we should listen for new routes and reapply them
-        // here - probably would need RouteManager be extended. Or maybe RouteManager can deal
-        // with it on it's own
-        let default_node = self
-            .router
-            .get_default_route_node()
-            .map_err(Error::SetupRoutingError)?;
-
         // route endpoints with specific routes
         for peer in config.peers.iter() {
-            let default_route = routing::Route::new(
+            routes.insert(
                 peer.endpoint.ip().into(),
-                routing::NetNode::Address(default_node),
+                routing::NetNode::DefaultNode.into(),
             );
-            routes.push(default_route);
         }
 
-        let required_routes = routing::RequiredRoutes { routes };
-
-        self.router
-            .add_routes(required_routes)
-            .map_err(Error::SetupRoutingError)
+        routes
     }
 
     fn tunnel_metadata(&self, config: &Config) -> TunnelMetadata {
@@ -188,7 +194,6 @@ enum CloseMsg {
 pub struct CloseHandle {
     chan: mpsc::Sender<CloseMsg>,
 }
-
 
 impl CloseHandle {
     pub fn close(&mut self) {

chrispsyco/vpnvu-desktop

talpid-core/src/tunnel/wireguard/mod.rs
@@ -3,7 +3,7 @@
 use self::config::Config;
 use super::{TunnelEvent, TunnelMetadata};
 use crate::routing;
-use std::{io, path::Path, sync::mpsc};
+use std::{collections::HashMap, io, path::Path, sync::mpsc};
 
 pub mod config;
 mod ping_monitor;
@@ -53,7 +53,7 @@ pub struct WireguardMonitor {
     /// Tunnel implementation
     tunnel: Box<dyn Tunnel>,
     /// Route manager
-    router: routing::RouteManager,
+    route_handle: routing::RouteManager,
     /// Callback to signal tunnel events
     event_callback: Box<dyn Fn(TunnelEvent) + Send + Sync + 'static>,
     close_msg_sender: mpsc::Sender<CloseMsg>,
@@ -67,17 +67,21 @@ impl WireguardMonitor {
         on_event: F,
     ) -> Result<WireguardMonitor> {
         let tunnel = Box::new(WgGoTunnel::start_tunnel(&config, log_path)?);
-        let router = routing::RouteManager::new().map_err(Error::SetupRoutingError)?;
+        let iface_name = tunnel.get_interface_name();
+        let route_handle = routing::RouteManager::new(
+            Self::get_routes(iface_name, &config),
+            &mut tokio_executor::DefaultExecutor::current(),
+        )
+        .map_err(Error::SetupRoutingError)?;
         let event_callback = Box::new(on_event.clone());
         let (close_msg_sender, close_msg_receiver) = mpsc::channel();
-        let mut monitor = WireguardMonitor {
+        let monitor = WireguardMonitor {
             tunnel,
-            router,
+            route_handle,
             event_callback,
             close_msg_sender,
             close_msg_receiver,
         };
-        monitor.setup_routing(&config)?;
 
         let metadata = monitor.tunnel_metadata(&config);
         let iface_name = monitor.tunnel.get_interface_name().to_string();
@@ -101,7 +105,6 @@ impl WireguardMonitor {
             let _ = close_sender.send(CloseMsg::PingErr);
         });
 
-
         Ok(monitor)
     }
 
@@ -121,9 +124,7 @@ impl WireguardMonitor {
         // Clear routes manually - otherwise there will be some log spam since the tunnel device
         // can be removed before the routes are cleared, which automatically clears some of the
         // routes that were set.
-        if let Err(e) = self.router.delete_routes() {
-            log::error!("Failed to remove a route from the routing table - {}", e);
-        }
+        self.route_handle.stop();
 
         if let Err(e) = self.tunnel.stop() {
             log::error!("Failed to stop tunnel - {}", e);
@@ -132,40 +133,44 @@ impl WireguardMonitor {
         wait_result
     }
 
-    fn setup_routing(&mut self, config: &Config) -> Result<()> {
-        let iface_name = self.tunnel.get_interface_name();
-        let mut routes: Vec<_> = config
+    fn get_routes(
+        iface_name: &str,
+        config: &Config,
+    ) -> HashMap<ipnetwork::IpNetwork, crate::routing::NetNode> {
+        let node = routing::Node::device(iface_name.to_string());
+        let mut routes: HashMap<_, _> = config
             .peers
             .iter()
             .flat_map(|peer| peer.allowed_ips.iter())
             .cloned()
-            .map(|allowed_ip| {
-                routing::Route::new(allowed_ip, routing::NetNode::Device(iface_name.to_string()))
+            .flat_map(|allowed_ip| {
+                if allowed_ip.prefix() == 0 {
+                    if allowed_ip.is_ipv4() {
+                        vec![
+                            ("0.0.0.0/1".parse().unwrap(), node.clone().into()),
+                            ("128.0.0.0/1".parse().unwrap(), node.clone().into()),
+                        ]
+                    } else {
+                        vec![
+                            ("8000::/1".parse().unwrap(), node.clone().into()),
+                            ("::/1".parse().unwrap(), node.clone().into()),
+                        ]
+                    }
+                } else {
+                    vec![(allowed_ip, node.clone().into())]
+                }
             })
             .collect();
 
-        // To survive network roaming, we should listen for new routes and reapply them
-        // here - probably would need RouteManager be extended. Or maybe RouteManager can deal
-        // with it on it's own
-        let default_node = self
-            .router
-            .get_default_route_node()
-            .map_err(Error::SetupRoutingError)?;
-
         // route endpoints with specific routes
         for peer in config.peers.iter() {
-            let default_route = routing::Route::new(
+            routes.insert(
                 peer.endpoint.ip().into(),
-                routing::NetNode::Address(default_node),
+                routing::NetNode::DefaultNode.into(),
             );
-            routes.push(default_route);
         }
 
-        let required_routes = routing::RequiredRoutes { routes };
-
-        self.router
-            .add_routes(required_routes)
-            .map_err(Error::SetupRoutingError)
+        routes
     }
 
     fn tunnel_metadata(&self, config: &Config) -> TunnelMetadata {
@@ -188,7 +193,6 @@ enum CloseMsg {
 pub struct CloseHandle {
     chan: mpsc::Sender<CloseMsg>,
 }
-
 
 impl CloseHandle {
     pub fn close(&mut self) {