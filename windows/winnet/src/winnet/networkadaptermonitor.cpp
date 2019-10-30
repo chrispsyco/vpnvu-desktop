mullvad/mullvadvpn-app

Refactor NetMonitor

windows/winnet/src/winnet/networkadaptermonitor.cpp
@@ -1,0 +1,328 @@
+#include "stdafx.h"
+
+#include "networkadaptermonitor.h"
+#include <libcommon/memory.h>
+#include <sstream>
+#include <cstring>
+
+using namespace std::placeholders;
+
+
+NetworkAdapterMonitor::NetworkAdapterMonitor(
+	std::shared_ptr<common::logging::ILogSink> logSink,
+	UpdateSinkType updateSink,
+	FilterType filter,
+	std::shared_ptr<IDataProvider> dataProvider
+)
+	: m_logSink(logSink)
+	, m_updateSink(updateSink)
+	, m_filter(filter)
+	, m_dataProvider(dataProvider)
+{
+	//
+	// Initialize adapters
+	//
+	
+	MIB_IF_TABLE2 *table;
+
+	const auto status = m_dataProvider->getIfTable2(&table);
+
+	THROW_UNLESS(NO_ERROR, status, "Acquire network interface table");
+
+	common::memory::ScopeDestructor sd;
+
+	sd += [this, table]()
+	{
+		m_dataProvider->freeMibTable(table);
+	};
+
+	for (ULONG i = 0; i < table->NumEntries; ++i)
+	{
+		m_adapters[table->Table[i].InterfaceLuid.Value] = table->Table[i];
+
+		if (filter(table->Table[i]))
+		{
+			m_filteredAdapters.push_back(table->Table[i]);
+		}
+	}
+
+	//
+	// Send initial notification
+	//
+
+	if (!m_filteredAdapters.empty())
+	{
+		m_updateSink(m_filteredAdapters, nullptr, UpdateType::Add);
+	}
+
+	//
+	// Listen to adapter events
+	//
+
+	const auto statusCb = dataProvider->notifyIpInterfaceChange(
+		AF_UNSPEC,
+		Callback,
+		static_cast<void*>(this),
+		FALSE,
+		&m_notificationHandle
+	);
+
+	THROW_UNLESS(NO_ERROR, statusCb, "Register interface change notification");
+}
+
+NetworkAdapterMonitor::NetworkAdapterMonitor(
+	std::shared_ptr<common::logging::ILogSink> logSink
+	, UpdateSinkType updateSink
+	, FilterType filter
+) : NetworkAdapterMonitor(logSink, updateSink, filter, std::make_shared<SystemDataProvider>())
+{
+}
+
+NetworkAdapterMonitor::~NetworkAdapterMonitor()
+{
+	m_dataProvider->cancelMibChangeNotify2(m_notificationHandle);
+}
+
+bool NetworkAdapterMonitor::hasIPv4Interface(NET_LUID luid) const
+{
+	MIB_IPINTERFACE_ROW iprow = { 0 };
+	iprow.InterfaceLuid = luid;
+	iprow.Family = AF_INET;
+
+	const auto status = m_dataProvider->getIpInterfaceEntry(&iprow);
+
+	if (NO_ERROR == status)
+	{
+		return true;
+	}
+	else if (ERROR_NOT_FOUND != status)
+	{
+		common::error::Throw("Resolve IPv4 interface", status);
+	}
+	return false;
+}
+
+bool NetworkAdapterMonitor::hasIPv6Interface(NET_LUID luid) const
+{
+	MIB_IPINTERFACE_ROW iprow = { 0 };
+	iprow.InterfaceLuid = luid;
+	iprow.Family = AF_INET6;
+
+	const auto status = m_dataProvider->getIpInterfaceEntry(&iprow);
+
+	if (NO_ERROR == status)
+	{
+		return true;
+	}
+	else if (ERROR_NOT_FOUND != status)
+	{
+		common::error::Throw("Resolve IPv6 interface", status);
+	}
+	return false;
+}
+
+std::vector<MIB_IF_ROW2>::iterator NetworkAdapterMonitor::findFilteredAdapter(const MIB_IF_ROW2 &adapter)
+{
+	return std::find_if(m_filteredAdapters.begin(), m_filteredAdapters.end(), [&adapter](const MIB_IF_ROW2 &elem)
+	{
+		return elem.InterfaceLuid.Value == adapter.InterfaceLuid.Value;
+	});
+}
+
+MIB_IF_ROW2 NetworkAdapterMonitor::getIfEntry(NET_LUID luid) const
+{
+	MIB_IF_ROW2 rowOut;
+	rowOut.InterfaceLuid = luid;
+	const auto status = m_dataProvider->getIfEntry2(&rowOut);
+
+	if (NO_ERROR == status)
+	{
+		return rowOut;
+	}
+
+	std::stringstream ss;
+
+	ss << "GetIfEntry2() failed for LUID 0x" << std::hex << rowOut.InterfaceLuid.Value
+		<< " in NetworkAdapterMonitor::getIfEntry(), error: 0x" << status;
+
+	throw std::runtime_error(ss.str());
+}
+
+void NetworkAdapterMonitor::callback(const MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE)
+{
+	std::scoped_lock<std::mutex> processingLock(m_processingMutex);
+
+	MIB_IF_ROW2 iface = getIfEntry(hint->InterfaceLuid);
+	
+	bool adapterEnabled = NET_IF_ADMIN_STATUS_UP == iface.AdminStatus
+		&& (hasIPv4Interface(iface.InterfaceLuid)
+			|| hasIPv6Interface(iface.InterfaceLuid));
+
+	const auto adapterIt = m_adapters.find(hint->InterfaceLuid.Value);
+
+	if (adapterEnabled)
+	{
+		//
+		// Check if the adapter has been added or updated
+		//
+
+		bool fieldsChanged;
+		
+		if (m_adapters.end() == adapterIt)
+		{
+			const auto pair = m_adapters.emplace(
+				iface.InterfaceLuid.Value,
+				iface
+			);
+			fieldsChanged = true;
+		}
+		else
+		{
+			//
+			// Only send an Update event if the fields have changed
+			//
+			fieldsChanged = std::memcmp(
+				&adapterIt->second,
+				&iface,
+				sizeof(MIB_IF_ROW2)
+			) != 0;
+
+			// update stored adapter
+			adapterIt->second = iface;
+		}
+
+		if (m_filter(iface))
+		{
+			//
+			// Report Add event if this is new
+			//
+			if (m_filteredAdapters.end() == findFilteredAdapter(iface))
+			{
+				m_filteredAdapters.push_back(iface);
+				m_updateSink(m_filteredAdapters, &iface, UpdateType::Add);
+			}
+			else if (fieldsChanged)
+			{
+				m_updateSink(m_filteredAdapters, &iface, UpdateType::Update);
+			}
+		}
+		else
+		{
+			//
+			// Synthesize a Delete event if we're no longer interested
+			// in this adapter
+			//
+			auto filteredIt = std::find_if(m_filteredAdapters.begin(), m_filteredAdapters.end(), [&iface](const MIB_IF_ROW2 &elem)
+			{
+				return elem.InterfaceLuid.Value == iface.InterfaceLuid.Value;
+			});
+
+			if (m_filteredAdapters.end() != filteredIt)
+			{
+				m_filteredAdapters.erase(filteredIt);
+				m_updateSink(
+					m_filteredAdapters,
+					&iface,
+					UpdateType::Delete
+				);
+			}
+		}
+	}
+	else
+	{
+		if (m_adapters.end() == adapterIt)
+		{
+			return;
+		}
+		
+		//
+		// Remove the adapter
+		//
+
+		m_adapters.erase(adapterIt);
+
+		auto filteredIt = std::find_if(m_filteredAdapters.begin(), m_filteredAdapters.end(), [&iface](const MIB_IF_ROW2 &elem)
+		{
+			return elem.InterfaceLuid.Value == iface.InterfaceLuid.Value;
+		});
+
+		if (m_filteredAdapters.end() != filteredIt)
+		{
+			m_filteredAdapters.erase(filteredIt);
+
+			if (m_filter(iface))
+			{
+				m_updateSink(
+					m_filteredAdapters,
+					&iface,
+					UpdateType::Delete
+				);
+			}
+		}
+	}
+}
+
+//static
+void __stdcall NetworkAdapterMonitor::Callback(void *context, MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType)
+{
+	auto inst = reinterpret_cast<NetworkAdapterMonitor *>(context);
+
+	try
+	{
+		inst->callback(hint, updateType);
+	}
+	catch (const std::exception &err)
+	{
+		inst->m_logSink->error(err.what());
+	}
+	catch (...)
+	{
+		inst->m_logSink->error("Unspecified error in NetworkAdapterMonitor::Callback()");
+	}
+}
+
+//
+// SystemDataProvider
+//
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::notifyIpInterfaceChange(
+	ADDRESS_FAMILY Family,
+	PIPINTERFACE_CHANGE_CALLBACK Callback,
+	PVOID CallerContext,
+	BOOLEAN InitialNotification,
+	HANDLE *NotificationHandle
+)
+{
+	return NotifyIpInterfaceChange(
+		Family,
+		Callback,
+		CallerContext,
+		InitialNotification,
+		NotificationHandle
+	);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::cancelMibChangeNotify2(HANDLE NotificationHandle)
+{
+	return CancelMibChangeNotify2(NotificationHandle);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIfEntry2(PMIB_IF_ROW2 Row)
+{
+	return GetIfEntry2(Row);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIfTable2(PMIB_IF_TABLE2 *Table)
+{
+	return GetIfTable2(Table);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row)
+{
+	return GetIpInterfaceEntry(Row);
+}
+
+void NetworkAdapterMonitor::SystemDataProvider::freeMibTable(PVOID Memory)
+{
+	FreeMibTable(Memory);
+}

windows/winnet/src/winnet/networkadaptermonitor.h
@@ -1,0 +1,129 @@
+#pragma once
+
+#include <libcommon/logging/ilogsink.h>
+#include <libcommon/error.h>
+#include <map>
+#include <winsock2.h>
+#include <ws2ipdef.h>
+#include <iphlpapi.h>
+#include <windows.h>
+#include <functional>
+#include <mutex>
+#include <vector>
+
+
+class NetworkAdapterMonitor
+{
+public:
+
+	enum class UpdateType
+	{
+		Add,
+		Delete,
+		Update
+	};
+
+	using FilterType = std::function<bool(const MIB_IF_ROW2 &adapter)>;
+
+	//
+	// An event may apply to a specific adapter, or it may apply to all adapters.
+	// In the latter case, 'adapter' will be set to nullptr.
+	//
+	using UpdateSinkType = std::function<void(const std::vector<MIB_IF_ROW2> &adapters, const MIB_IF_ROW2 *adapter, UpdateType updateType)>;
+
+	struct IDataProvider;
+	class SystemDataProvider;
+
+	NetworkAdapterMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink
+		, UpdateSinkType updateSink
+		, FilterType filter
+		, std::shared_ptr<IDataProvider> dataProvider
+	);
+	NetworkAdapterMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink
+		, UpdateSinkType updateSink
+		, FilterType filter
+	);
+	virtual ~NetworkAdapterMonitor();
+
+	NetworkAdapterMonitor(const NetworkAdapterMonitor &) = delete;
+	NetworkAdapterMonitor& operator=(const NetworkAdapterMonitor &) = delete;
+	NetworkAdapterMonitor(NetworkAdapterMonitor &&) = delete;
+	NetworkAdapterMonitor& operator=(NetworkAdapterMonitor &&) = delete;
+
+private:
+
+	std::mutex m_processingMutex;
+
+	std::shared_ptr<common::logging::ILogSink> m_logSink;
+	UpdateSinkType m_updateSink;
+	FilterType m_filter;
+
+	std::shared_ptr<IDataProvider> m_dataProvider;
+
+	MIB_IF_ROW2 getIfEntry(NET_LUID luid) const;
+
+	bool hasIPv4Interface(NET_LUID luid) const;
+	bool hasIPv6Interface(NET_LUID luid) const;
+
+	std::map<ULONG64, MIB_IF_ROW2> m_adapters;
+	std::vector<MIB_IF_ROW2> m_filteredAdapters;
+
+	std::vector<MIB_IF_ROW2>::iterator findFilteredAdapter(const MIB_IF_ROW2 &adapter);
+
+	HANDLE m_notificationHandle;
+	static void __stdcall Callback(void *context, MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType);
+	virtual void callback(const MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType);
+};
+
+
+struct NetworkAdapterMonitor::IDataProvider
+{
+	virtual ~IDataProvider() = 0
+	{
+	}
+
+	virtual DWORD notifyIpInterfaceChange(
+		ADDRESS_FAMILY Family,
+		PIPINTERFACE_CHANGE_CALLBACK Callback,
+		PVOID CallerContext,
+		BOOLEAN InitialNotification,
+		HANDLE *NotificationHandle
+	) = 0;
+	virtual DWORD cancelMibChangeNotify2(HANDLE NotificationHandle) = 0;
+
+	virtual DWORD getIfTable2(PMIB_IF_TABLE2 *Table) = 0;
+	virtual void freeMibTable(PVOID Memory) = 0;
+	
+	virtual DWORD getIfEntry2(PMIB_IF_ROW2 Row) = 0;
+	virtual DWORD getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row) = 0;
+};
+
+class NetworkAdapterMonitor::SystemDataProvider : public IDataProvider
+{
+public:
+
+	SystemDataProvider() = default;
+	virtual ~SystemDataProvider() = default;
+
+	SystemDataProvider(const SystemDataProvider&) = delete;
+	SystemDataProvider(SystemDataProvider&&) = delete;
+	SystemDataProvider& operator=(const SystemDataProvider&) = delete;
+	SystemDataProvider& operator=(const SystemDataProvider&&) = delete;
+
+	DWORD notifyIpInterfaceChange(
+		ADDRESS_FAMILY Family,
+		PIPINTERFACE_CHANGE_CALLBACK Callback,
+		PVOID CallerContext,
+		BOOLEAN InitialNotification,
+		HANDLE *NotificationHandle
+	) override;
+	DWORD cancelMibChangeNotify2(HANDLE NotificationHandle) override;
+
+	DWORD getIfTable2(PMIB_IF_TABLE2 *Table) override;
+	void freeMibTable(PVOID Memory) override;
+	
+	DWORD getIfEntry2(PMIB_IF_ROW2 Row) override;
+	DWORD getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row) override;
+};

windows/winnet/src/winnet/offlinemonitor.h
@@ -1,0 +1,40 @@
+#pragma once
+
+#include <libcommon/logging/ilogsink.h>
+#include <mutex>
+#include "networkadaptermonitor.h"
+
+class OfflineMonitor
+{
+public:
+
+	//
+	// Connectivity changed.
+	// true = connected, false = disconnected.
+	//
+	using Notifier = std::function<void(bool)>;
+
+	OfflineMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink,
+		Notifier notifier,
+		std::shared_ptr<NetworkAdapterMonitor::IDataProvider> dataProvider
+	);
+	OfflineMonitor(std::shared_ptr<common::logging::ILogSink> logSink, Notifier notifier);
+
+private:
+
+	std::shared_ptr<common::logging::ILogSink> m_logSink;
+	Notifier m_notifier;
+
+	bool m_connected;
+	NetworkAdapterMonitor m_netInterfaces;
+
+	void LogOfflineState();
+
+	NetworkAdapterMonitor& getAdapter()
+	{
+		return m_netInterfaces;
+	}
+
+	void callback(const std::vector<MIB_IF_ROW2> &adapters, const MIB_IF_ROW2 *adapter, NetworkAdapterMonitor::UpdateType type);
+};

chrispsyco/vpnvu-desktop

windows/winnet/src/winnet/networkadaptermonitor.cpp
@@ -1,0 +1,325 @@
+#include "stdafx.h"
+
+#include "networkadaptermonitor.h"
+#include <libcommon/memory.h>
+#include <sstream>
+#include <cstring>
+
+using namespace std::placeholders;
+
+
+NetworkAdapterMonitor::NetworkAdapterMonitor(
+	std::shared_ptr<common::logging::ILogSink> logSink,
+	UpdateSinkType updateSink,
+	FilterType filter,
+	std::shared_ptr<IDataProvider> dataProvider
+)
+	: m_logSink(logSink)
+	, m_notificationHandle(nullptr)
+	, m_updateSink(updateSink)
+	, m_filter(filter)
+	, m_dataProvider(dataProvider)
+{
+	//
+	// Initialize adapters
+	//
+	
+	MIB_IF_TABLE2 *table;
+
+	const auto status = m_dataProvider->getIfTable2(&table);
+
+	THROW_UNLESS(NO_ERROR, status, "Acquire network interface table");
+
+	common::memory::ScopeDestructor sd;
+
+	sd += [this, table]()
+	{
+		m_dataProvider->freeMibTable(table);
+	};
+
+	for (ULONG i = 0; i < table->NumEntries; ++i)
+	{
+		m_adapters[table->Table[i].InterfaceLuid.Value] = table->Table[i];
+
+		if (filter(table->Table[i]))
+		{
+			m_filteredAdapters.push_back(table->Table[i]);
+		}
+	}
+
+	//
+	// Send initial notification
+	//
+
+	if (!m_filteredAdapters.empty())
+	{
+		m_updateSink(m_filteredAdapters, nullptr, UpdateType::Add);
+	}
+
+	//
+	// Listen to adapter events
+	//
+
+	const auto statusCb = m_dataProvider->notifyIpInterfaceChange(
+		AF_UNSPEC,
+		Callback,
+		this,
+		FALSE,
+		&m_notificationHandle
+	);
+
+	THROW_UNLESS(NO_ERROR, statusCb, "Register interface change notification");
+}
+
+NetworkAdapterMonitor::NetworkAdapterMonitor(
+	std::shared_ptr<common::logging::ILogSink> logSink
+	, UpdateSinkType updateSink
+	, FilterType filter
+) : NetworkAdapterMonitor(logSink, updateSink, filter, std::make_shared<SystemDataProvider>())
+{
+}
+
+NetworkAdapterMonitor::~NetworkAdapterMonitor()
+{
+	if (nullptr != m_notificationHandle)
+	{
+		m_dataProvider->cancelMibChangeNotify2(m_notificationHandle);
+		m_notificationHandle = nullptr;
+	}
+}
+
+bool NetworkAdapterMonitor::hasIPv4Interface(NET_LUID luid) const
+{
+	MIB_IPINTERFACE_ROW iprow = { 0 };
+	iprow.InterfaceLuid = luid;
+	iprow.Family = AF_INET;
+
+	const auto status = m_dataProvider->getIpInterfaceEntry(&iprow);
+
+	if (NO_ERROR == status)
+	{
+		return true;
+	}
+	else if (ERROR_NOT_FOUND != status)
+	{
+		common::error::Throw("Resolve IPv4 interface", status);
+	}
+	return false;
+}
+
+bool NetworkAdapterMonitor::hasIPv6Interface(NET_LUID luid) const
+{
+	MIB_IPINTERFACE_ROW iprow = { 0 };
+	iprow.InterfaceLuid = luid;
+	iprow.Family = AF_INET6;
+
+	const auto status = m_dataProvider->getIpInterfaceEntry(&iprow);
+
+	if (NO_ERROR == status)
+	{
+		return true;
+	}
+	else if (ERROR_NOT_FOUND != status)
+	{
+		common::error::Throw("Resolve IPv6 interface", status);
+	}
+	return false;
+}
+
+std::vector<MIB_IF_ROW2>::iterator NetworkAdapterMonitor::findFilteredAdapter(const MIB_IF_ROW2 &adapter)
+{
+	return std::find_if(m_filteredAdapters.begin(), m_filteredAdapters.end(), [&adapter](const MIB_IF_ROW2 &elem)
+	{
+		return elem.InterfaceLuid.Value == adapter.InterfaceLuid.Value;
+	});
+}
+
+MIB_IF_ROW2 NetworkAdapterMonitor::getAdapter(NET_LUID luid) const
+{
+	MIB_IF_ROW2 rowOut;
+	rowOut.InterfaceLuid = luid;
+	const auto status = m_dataProvider->getIfEntry2(&rowOut);
+
+	if (NO_ERROR == status)
+	{
+		return rowOut;
+	}
+
+	std::stringstream ss;
+
+	ss << "GetIfEntry2() failed for LUID 0x" << std::hex << rowOut.InterfaceLuid.Value
+		<< " in NetworkAdapterMonitor::getAdapter(), error: 0x" << status;
+
+	throw std::runtime_error(ss.str());
+}
+
+void NetworkAdapterMonitor::callback(const MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE)
+{
+	MIB_IF_ROW2 iface = getAdapter(hint->InterfaceLuid);
+	
+	bool adapterEnabled = NET_IF_ADMIN_STATUS_UP == iface.AdminStatus
+		&& (hasIPv4Interface(iface.InterfaceLuid)
+			|| hasIPv6Interface(iface.InterfaceLuid));
+
+	const auto adapterIt = m_adapters.find(hint->InterfaceLuid.Value);
+
+	if (adapterEnabled)
+	{
+		//
+		// Check if the adapter has been added or updated
+		//
+
+		bool fieldsChanged;
+		
+		if (m_adapters.end() == adapterIt)
+		{
+			const auto pair = m_adapters.emplace(
+				iface.InterfaceLuid.Value,
+				iface
+			);
+			fieldsChanged = true;
+		}
+		else
+		{
+			//
+			// Only send an Update event if the fields have changed
+			//
+			fieldsChanged = std::memcmp(
+				&adapterIt->second,
+				&iface,
+				sizeof(MIB_IF_ROW2)
+			) != 0;
+
+			// update stored adapter
+			adapterIt->second = iface;
+		}
+
+		if (m_filter(iface))
+		{
+			//
+			// Report Add event if this is new
+			//
+			if (m_filteredAdapters.end() == findFilteredAdapter(iface))
+			{
+				m_filteredAdapters.push_back(iface);
+				m_updateSink(m_filteredAdapters, &iface, UpdateType::Add);
+			}
+			else if (fieldsChanged)
+			{
+				m_updateSink(m_filteredAdapters, &iface, UpdateType::Update);
+			}
+		}
+		else
+		{
+			//
+			// Synthesize a Delete event if we're no longer interested
+			// in this adapter
+			//
+			const auto filteredIt = findFilteredAdapter(iface);
+
+			if (m_filteredAdapters.end() != filteredIt)
+			{
+				m_filteredAdapters.erase(filteredIt);
+				m_updateSink(
+					m_filteredAdapters,
+					&iface,
+					UpdateType::Delete
+				);
+			}
+		}
+	}
+	else
+	{
+		if (m_adapters.end() == adapterIt)
+		{
+			return;
+		}
+		
+		//
+		// Remove the adapter
+		//
+
+		m_adapters.erase(adapterIt);
+
+		const auto filteredIt = findFilteredAdapter(iface);
+
+		if (m_filteredAdapters.end() != filteredIt)
+		{
+			m_filteredAdapters.erase(filteredIt);
+
+			if (m_filter(iface))
+			{
+				m_updateSink(
+					m_filteredAdapters,
+					&iface,
+					UpdateType::Delete
+				);
+			}
+		}
+	}
+}
+
+//static
+void __stdcall NetworkAdapterMonitor::Callback(void *context, MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType)
+{
+	auto inst = reinterpret_cast<NetworkAdapterMonitor *>(context);
+
+	try
+	{
+		inst->callback(hint, updateType);
+	}
+	catch (const std::exception &err)
+	{
+		inst->m_logSink->error(err.what());
+	}
+	catch (...)
+	{
+		inst->m_logSink->error("Unspecified error in NetworkAdapterMonitor::Callback()");
+	}
+}
+
+//
+// SystemDataProvider
+//
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::notifyIpInterfaceChange(
+	ADDRESS_FAMILY Family,
+	PIPINTERFACE_CHANGE_CALLBACK Callback,
+	PVOID CallerContext,
+	BOOLEAN InitialNotification,
+	HANDLE *NotificationHandle
+)
+{
+	return NotifyIpInterfaceChange(
+		Family,
+		Callback,
+		CallerContext,
+		InitialNotification,
+		NotificationHandle
+	);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::cancelMibChangeNotify2(HANDLE NotificationHandle)
+{
+	return CancelMibChangeNotify2(NotificationHandle);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIfEntry2(PMIB_IF_ROW2 Row)
+{
+	return GetIfEntry2(Row);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIfTable2(PMIB_IF_TABLE2 *Table)
+{
+	return GetIfTable2(Table);
+}
+
+DWORD NetworkAdapterMonitor::SystemDataProvider::getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row)
+{
+	return GetIpInterfaceEntry(Row);
+}
+
+void NetworkAdapterMonitor::SystemDataProvider::freeMibTable(PVOID Memory)
+{
+	FreeMibTable(Memory);
+}

windows/winnet/src/winnet/networkadaptermonitor.h
@@ -1,0 +1,126 @@
+#pragma once
+
+#include <libcommon/logging/ilogsink.h>
+#include <libcommon/error.h>
+#include <map>
+#include <winsock2.h>
+#include <ws2ipdef.h>
+#include <iphlpapi.h>
+#include <windows.h>
+#include <functional>
+#include <vector>
+
+
+class NetworkAdapterMonitor
+{
+public:
+
+	enum class UpdateType
+	{
+		Add,
+		Delete,
+		Update
+	};
+
+	using FilterType = std::function<bool(const MIB_IF_ROW2 &adapter)>;
+
+	//
+	// An event may apply to a specific adapter, or it may apply to all adapters.
+	// In the latter case, 'adapter' will be set to nullptr.
+	//
+	using UpdateSinkType = std::function<void(const std::vector<MIB_IF_ROW2> &adapters, const MIB_IF_ROW2 *adapter, UpdateType updateType)>;
+
+	struct IDataProvider;
+	class SystemDataProvider;
+
+	NetworkAdapterMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink
+		, UpdateSinkType updateSink
+		, FilterType filter
+		, std::shared_ptr<IDataProvider> dataProvider
+	);
+	NetworkAdapterMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink
+		, UpdateSinkType updateSink
+		, FilterType filter
+	);
+	~NetworkAdapterMonitor();
+
+	NetworkAdapterMonitor(const NetworkAdapterMonitor &) = delete;
+	NetworkAdapterMonitor& operator=(const NetworkAdapterMonitor &) = delete;
+	NetworkAdapterMonitor(NetworkAdapterMonitor &&) = delete;
+	NetworkAdapterMonitor& operator=(NetworkAdapterMonitor &&) = delete;
+
+private:
+
+	std::shared_ptr<common::logging::ILogSink> m_logSink;
+	UpdateSinkType m_updateSink;
+	FilterType m_filter;
+
+	std::shared_ptr<IDataProvider> m_dataProvider;
+
+	MIB_IF_ROW2 getAdapter(NET_LUID luid) const;
+
+	bool hasIPv4Interface(NET_LUID luid) const;
+	bool hasIPv6Interface(NET_LUID luid) const;
+
+	std::map<ULONG64, MIB_IF_ROW2> m_adapters;
+	std::vector<MIB_IF_ROW2> m_filteredAdapters;
+
+	std::vector<MIB_IF_ROW2>::iterator findFilteredAdapter(const MIB_IF_ROW2 &adapter);
+
+	HANDLE m_notificationHandle;
+	static void __stdcall Callback(void *context, MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType);
+	virtual void callback(const MIB_IPINTERFACE_ROW *hint, MIB_NOTIFICATION_TYPE updateType);
+};
+
+
+struct NetworkAdapterMonitor::IDataProvider
+{
+	virtual ~IDataProvider() = 0
+	{
+	}
+
+	virtual DWORD notifyIpInterfaceChange(
+		ADDRESS_FAMILY Family,
+		PIPINTERFACE_CHANGE_CALLBACK Callback,
+		PVOID CallerContext,
+		BOOLEAN InitialNotification,
+		HANDLE *NotificationHandle
+	) = 0;
+	virtual DWORD cancelMibChangeNotify2(HANDLE NotificationHandle) = 0;
+
+	virtual DWORD getIfTable2(PMIB_IF_TABLE2 *Table) = 0;
+	virtual void freeMibTable(PVOID Memory) = 0;
+	
+	virtual DWORD getIfEntry2(PMIB_IF_ROW2 Row) = 0;
+	virtual DWORD getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row) = 0;
+};
+
+class NetworkAdapterMonitor::SystemDataProvider : public IDataProvider
+{
+public:
+
+	SystemDataProvider() = default;
+	virtual ~SystemDataProvider() = default;
+
+	SystemDataProvider(const SystemDataProvider&) = delete;
+	SystemDataProvider(SystemDataProvider&&) = delete;
+	SystemDataProvider& operator=(const SystemDataProvider&) = delete;
+	SystemDataProvider& operator=(const SystemDataProvider&&) = delete;
+
+	DWORD notifyIpInterfaceChange(
+		ADDRESS_FAMILY Family,
+		PIPINTERFACE_CHANGE_CALLBACK Callback,
+		PVOID CallerContext,
+		BOOLEAN InitialNotification,
+		HANDLE *NotificationHandle
+	) override;
+	DWORD cancelMibChangeNotify2(HANDLE NotificationHandle) override;
+
+	DWORD getIfTable2(PMIB_IF_TABLE2 *Table) override;
+	void freeMibTable(PVOID Memory) override;
+	
+	DWORD getIfEntry2(PMIB_IF_ROW2 Row) override;
+	DWORD getIpInterfaceEntry(PMIB_IPINTERFACE_ROW Row) override;
+};

windows/winnet/src/winnet/offlinemonitor.h
@@ -1,0 +1,35 @@
+#pragma once
+
+#include <libcommon/logging/ilogsink.h>
+#include <mutex>
+#include "networkadaptermonitor.h"
+
+class OfflineMonitor
+{
+public:
+
+	//
+	// Connectivity changed.
+	// true = connected, false = disconnected.
+	//
+	using Notifier = std::function<void(bool)>;
+
+	OfflineMonitor(
+		std::shared_ptr<common::logging::ILogSink> logSink,
+		Notifier notifier,
+		std::shared_ptr<NetworkAdapterMonitor::IDataProvider> dataProvider
+	);
+	OfflineMonitor(std::shared_ptr<common::logging::ILogSink> logSink, Notifier notifier);
+
+private:
+
+	std::shared_ptr<common::logging::ILogSink> m_logSink;
+	Notifier m_notifier;
+
+	bool m_connected;
+	NetworkAdapterMonitor m_netAdapterMonitor;
+
+	void LogOfflineState();
+
+	void callback(const std::vector<MIB_IF_ROW2> &adapters, const MIB_IF_ROW2 *adapter, NetworkAdapterMonitor::UpdateType type);
+};