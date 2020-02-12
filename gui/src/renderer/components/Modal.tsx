mullvad/mullvadvpn-app

Add outdated version dialog when navigating to problem report

gui/src/renderer/components/Modal.tsx
@@ -1,4 +1,30 @@
 import * as React from 'react';
+import { Component, Styles, Text, View } from 'reactxp';
+import { colors } from '../../config.json';
+
+const styles = {
+  dialogBackground: Styles.createViewStyle({
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 14,
+    paddingRight: 14,
+  }),
+  dialog: Styles.createViewStyle({
+    backgroundColor: colors.darkBlue,
+    borderRadius: 11,
+    padding: 16,
+  }),
+  dialogWarning: Styles.createTextStyle({
+    fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 20,
+    color: colors.white80,
+  }),
+  dialogButtonContainer: Styles.createViewStyle({
+    marginTop: 16,
+  }),
+};
 
 export class ModalContent extends React.Component {
   public render() {
@@ -48,5 +74,27 @@ interface IModalContainerProps {
 export class ModalContainer extends React.Component<IModalContainerProps> {
   public render() {
     return <div style={{ position: 'relative', flex: 1 }}>{this.props.children}</div>;
+  }
+}
+
+interface IDialogProps {
+  message: string;
+  buttons: React.ReactNode[];
+}
+
+export class Dialog extends Component<IDialogProps> {
+  public render() {
+    return (
+      <View style={styles.dialogBackground}>
+        <View style={styles.dialog}>
+          <Text style={styles.dialogWarning}>{this.props.message}</Text>
+          {this.props.buttons.map((button, index) => (
+            <View key={index} style={styles.dialogButtonContainer}>
+              {button}
+            </View>
+          ))}
+        </View>
+      </View>
+    );
   }
 }

chrispsyco/vpnvu-desktop

gui/src/renderer/components/Modal.tsx
@@ -1,52 +1,92 @@
 import * as React from 'react';
+import { Component, Styles, Text, View } from 'reactxp';
+import { colors } from '../../config.json';
 
-export class ModalContent extends React.Component {
+const styles = {
+  modalAlertBackground: Styles.createViewStyle({
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 14,
+    paddingRight: 14,
+  }),
+  modalAlert: Styles.createViewStyle({
+    backgroundColor: colors.darkBlue,
+    borderRadius: 11,
+    padding: 16,
+  }),
+  modalAlertMessage: Styles.createTextStyle({
+    fontFamily: 'Open Sans',
+    fontSize: 16,
+    fontWeight: '500',
+    lineHeight: 20,
+    color: colors.white80,
+  }),
+  modalAlertButtonContainer: Styles.createViewStyle({
+    marginTop: 16,
+  }),
+};
+
+export const ModalContent: React.FC = ({ children }) => {
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}>
+      {children}
+    </div>
+  );
+};
+
+const ModalBackground: React.FC = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}>
+      {children}
+    </div>
+  );
+};
+
+export const ModalContainer: React.FC = ({ children }) => {
+  return <div style={{ position: 'relative', flex: 1 }}>{children}</div>;
+};
+
+interface IModalAlertProps {
+  message: string;
+  buttons: React.ReactNode[];
+}
+
+export class ModalAlert extends Component<IModalAlertProps> {
   public render() {
     return (
-      <div
-        style={{
-          position: 'absolute',
-          display: 'flex',
-          flexDirection: 'column',
-          flex: 1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}>
-        {this.props.children}
-      </div>
+      <ModalBackground>
+        <View style={styles.modalAlertBackground}>
+          <View style={styles.modalAlert}>
+            <Text style={styles.modalAlertMessage}>{this.props.message}</Text>
+            {this.props.buttons.map((button, index) => (
+              <View key={index} style={styles.modalAlertButtonContainer}>
+                {button}
+              </View>
+            ))}
+          </View>
+        </View>
+      </ModalBackground>
     );
-  }
-}
-
-export class ModalAlert extends React.Component {
-  public render() {
-    return (
-      <div
-        style={{
-          backgroundColor: 'rgba(0,0,0,0.5)',
-          position: 'absolute',
-          display: 'flex',
-          flexDirection: 'column',
-          flex: 1,
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-interface IModalContainerProps {
-  children?: React.ReactNode;
-}
-
-export class ModalContainer extends React.Component<IModalContainerProps> {
-  public render() {
-    return <div style={{ position: 'relative', flex: 1 }}>{this.props.children}</div>;
   }
 }