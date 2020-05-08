sway913/mullvadvpn-app

Scroll to expose expanded items in location list

gui/src/renderer/components/CityRow.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import { Component, Styles, Types, View } from 'reactxp';
 import { colors } from '../../config.json';
 import { compareRelayLocation, RelayLocation } from '../../shared/daemon-rpc-types';
@@ -18,6 +19,8 @@ interface IProps {
   expanded: boolean;
   onSelect?: (location: RelayLocation) => void;
   onExpand?: (location: RelayLocation, value: boolean) => void;
+  onWillExpand?: (buttonDimensions: DOMRect, additionalHeight: number) => void;
+  onWillCollapse?: () => void;
   children?: RelayRowElement | RelayRowElement[];
 }
 
@@ -30,6 +33,8 @@ const styles = {
 };
 
 export default class CityRow extends Component<IProps> {
+  private buttonRef = React.createRef<Cell.CellButton>();
+
   public static compareProps(oldProps: IProps, nextProps: IProps): boolean {
     if (React.Children.count(oldProps.children) !== React.Children.count(nextProps.children)) {
       return false;
@@ -70,6 +75,7 @@ export default class CityRow extends Component<IProps> {
     return (
       <View>
         <Cell.CellButton
+          ref={this.buttonRef}
           onPress={this.handlePress}
           disabled={!this.props.hasActiveRelays}
           selected={this.props.selected}
@@ -83,7 +89,15 @@ export default class CityRow extends Component<IProps> {
           {hasChildren && <ChevronButton onPress={this.toggleCollapse} up={this.props.expanded} />}
         </Cell.CellButton>
 
-        {hasChildren && <Accordion expanded={this.props.expanded}>{this.props.children}</Accordion>}
+        {hasChildren && (
+          <Accordion
+            expanded={this.props.expanded}
+            onWillExpand={this.onWillExpand}
+            onWillCollapse={this.props.onWillCollapse}
+            animationDuration={150}>
+            {this.props.children}
+          </Accordion>
+        )}
       </View>
     );
   }
@@ -98,6 +112,14 @@ export default class CityRow extends Component<IProps> {
   private handlePress = () => {
     if (this.props.onSelect) {
       this.props.onSelect(this.props.location);
+    }
+  };
+
+  private onWillExpand = (nextHeight: number) => {
+    const buttonNode = ReactDOM.findDOMNode(this.buttonRef.current);
+    if (buttonNode instanceof HTMLElement) {
+      const buttonDimensions = buttonNode.getBoundingClientRect();
+      this.props.onWillExpand?.(buttonDimensions, nextHeight);
     }
   };
 }

gui/src/renderer/components/CountryRow.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import { Component, Styles, Types, View } from 'reactxp';
 import { compareRelayLocation, RelayLocation } from '../../shared/daemon-rpc-types';
 import Accordion from './Accordion';
@@ -17,6 +18,8 @@ interface IProps {
   expanded: boolean;
   onSelect?: (location: RelayLocation) => void;
   onExpand?: (location: RelayLocation, value: boolean) => void;
+  onWillExpand?: (buttonDimensions: DOMRect, additionalHeight: number) => void;
+  onWillCollapse?: () => void;
   children?: CityRowElement | CityRowElement[];
 }
 
@@ -32,6 +35,8 @@ const styles = {
 };
 
 export default class CountryRow extends Component<IProps> {
+  private buttonRef = React.createRef<Cell.CellButton>();
+
   public static compareProps(oldProps: IProps, nextProps: IProps) {
     if (React.Children.count(oldProps.children) !== React.Children.count(nextProps.children)) {
       return false;
@@ -78,6 +83,7 @@ export default class CountryRow extends Component<IProps> {
     return (
       <View style={styles.container}>
         <Cell.CellButton
+          ref={this.buttonRef}
           style={styles.base}
           onPress={this.handlePress}
           disabled={!this.props.hasActiveRelays}
@@ -92,7 +98,15 @@ export default class CountryRow extends Component<IProps> {
           ) : null}
         </Cell.CellButton>
 
-        {hasChildren && <Accordion expanded={this.props.expanded}>{this.props.children}</Accordion>}
+        {hasChildren && (
+          <Accordion
+            expanded={this.props.expanded}
+            onWillExpand={this.onWillExpand}
+            onWillCollapse={this.props.onWillCollapse}
+            animationDuration={150}>
+            {this.props.children}
+          </Accordion>
+        )}
       </View>
     );
   }
@@ -107,6 +121,14 @@ export default class CountryRow extends Component<IProps> {
   private handlePress = () => {
     if (this.props.onSelect) {
       this.props.onSelect(this.props.location);
+    }
+  };
+
+  private onWillExpand = (nextHeight: number) => {
+    const buttonNode = ReactDOM.findDOMNode(this.buttonRef.current);
+    if (buttonNode instanceof HTMLElement) {
+      const buttonDimensions = buttonNode.getBoundingClientRect();
+      this.props.onWillExpand?.(buttonDimensions, nextHeight);
     }
   };
 }

gui/src/renderer/components/SelectLocation.tsx
@@ -44,6 +44,7 @@ interface ISelectLocationSnapshot {
 
 export default class SelectLocation extends Component<IProps> {
   private scrollView = React.createRef<CustomScrollbars>();
+  private heightAdditionRef = React.createRef<HeightAddition>();
   private selectedExitLocationRef = React.createRef<React.ReactInstance>();
   private selectedBridgeLocationRef = React.createRef<React.ReactInstance>();
 
@@ -128,27 +129,33 @@ export default class SelectLocation extends Component<IProps> {
               </NavigationBar>
               <View style={styles.container}>
                 <NavigationScrollbars ref={this.scrollView}>
-                  <View style={styles.content}>
-                    {this.props.locationScope === LocationScope.relay ? (
-                      <ExitLocations
-                        ref={this.exitLocationList}
-                        source={this.props.relayLocations}
-                        defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
-                        selectedValue={this.props.selectedExitLocation}
-                        selectedElementRef={this.selectedExitLocationRef}
-                        onSelect={this.onSelectExitLocation}
-                      />
-                    ) : (
-                      <BridgeLocations
-                        ref={this.bridgeLocationList}
-                        source={this.props.bridgeLocations}
-                        defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
-                        selectedValue={this.props.selectedBridgeLocation}
-                        selectedElementRef={this.selectedBridgeLocationRef}
-                        onSelect={this.onSelectBridgeLocation}
-                      />
-                    )}
-                  </View>
+                  <HeightAddition ref={this.heightAdditionRef}>
+                    <View style={styles.content}>
+                      {this.props.locationScope === LocationScope.relay ? (
+                        <ExitLocations
+                          ref={this.exitLocationList}
+                          source={this.props.relayLocations}
+                          defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
+                          selectedValue={this.props.selectedExitLocation}
+                          selectedElementRef={this.selectedExitLocationRef}
+                          onSelect={this.onSelectExitLocation}
+                          onWillExpand={this.onWillExpand}
+                          onWillCollapse={this.heightAdditionRef.current?.resetHeight}
+                        />
+                      ) : (
+                        <BridgeLocations
+                          ref={this.bridgeLocationList}
+                          source={this.props.bridgeLocations}
+                          defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
+                          selectedValue={this.props.selectedBridgeLocation}
+                          selectedElementRef={this.selectedBridgeLocationRef}
+                          onSelect={this.onSelectBridgeLocation}
+                          onWillExpand={this.onWillExpand}
+                          onWillCollapse={this.heightAdditionRef.current?.resetHeight}
+                        />
+                      )}
+                    </View>
+                  </HeightAddition>
                 </NavigationScrollbars>
               </View>
             </NavigationContainer>
@@ -219,4 +226,40 @@ export default class SelectLocation extends Component<IProps> {
       this.props.onSelectClosestToExit();
     }
   };
+
+  private onWillExpand = (buttonDimensions: DOMRect, additionalHeight: number) => {
+    buttonDimensions.height += additionalHeight;
+    this.heightAdditionRef.current?.addHeight(additionalHeight);
+    this.scrollView.current?.scrollIntoView(buttonDimensions);
+  };
+}
+
+interface IHeightAdditionProps {
+  children?: React.ReactNode;
+}
+
+class HeightAddition extends Component<IHeightAdditionProps> {
+  ref = React.createRef<HTMLDivElement>();
+
+  public addHeight(additionalHeight: number) {
+    this.resetHeight();
+    if (this.ref.current) {
+      this.minHeight = this.ref.current.offsetHeight + additionalHeight + 'px';
+    }
+  }
+
+  public resetHeight = () => {
+    this.minHeight = 'auto';
+  };
+
+  public render() {
+    return <div ref={this.ref}>{this.props.children}</div>;
+  }
+
+  private set minHeight(value: string) {
+    const element = this.ref.current;
+    if (element) {
+      element.style.minHeight = value;
+    }
+  }
 }

chrispsyco/vpnvu-desktop

gui/src/renderer/components/CityRow.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import { Component, Styles, Types, View } from 'reactxp';
 import { colors } from '../../config.json';
 import { compareRelayLocation, RelayLocation } from '../../shared/daemon-rpc-types';
@@ -18,6 +19,8 @@ interface IProps {
   expanded: boolean;
   onSelect?: (location: RelayLocation) => void;
   onExpand?: (location: RelayLocation, value: boolean) => void;
+  onWillExpand?: (locationRect: DOMRect, expandedContentHeight: number) => void;
+  onTransitionEnd?: () => void;
   children?: RelayRowElement | RelayRowElement[];
 }
 
@@ -30,6 +33,8 @@ const styles = {
 };
 
 export default class CityRow extends Component<IProps> {
+  private buttonRef = React.createRef<Cell.CellButton>();
+
   public static compareProps(oldProps: IProps, nextProps: IProps): boolean {
     if (React.Children.count(oldProps.children) !== React.Children.count(nextProps.children)) {
       return false;
@@ -70,6 +75,7 @@ export default class CityRow extends Component<IProps> {
     return (
       <View>
         <Cell.CellButton
+          ref={this.buttonRef}
           onPress={this.handlePress}
           disabled={!this.props.hasActiveRelays}
           selected={this.props.selected}
@@ -83,7 +89,15 @@ export default class CityRow extends Component<IProps> {
           {hasChildren && <ChevronButton onPress={this.toggleCollapse} up={this.props.expanded} />}
         </Cell.CellButton>
 
-        {hasChildren && <Accordion expanded={this.props.expanded}>{this.props.children}</Accordion>}
+        {hasChildren && (
+          <Accordion
+            expanded={this.props.expanded}
+            onWillExpand={this.onWillExpand}
+            onTransitionEnd={this.props.onTransitionEnd}
+            animationDuration={150}>
+            {this.props.children}
+          </Accordion>
+        )}
       </View>
     );
   }
@@ -98,6 +112,14 @@ export default class CityRow extends Component<IProps> {
   private handlePress = () => {
     if (this.props.onSelect) {
       this.props.onSelect(this.props.location);
+    }
+  };
+
+  private onWillExpand = (nextHeight: number) => {
+    const buttonNode = ReactDOM.findDOMNode(this.buttonRef.current);
+    if (buttonNode instanceof HTMLElement) {
+      const buttonRect = buttonNode.getBoundingClientRect();
+      this.props.onWillExpand?.(buttonRect, nextHeight);
     }
   };
 }

gui/src/renderer/components/CountryRow.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
 import { Component, Styles, Types, View } from 'reactxp';
 import { compareRelayLocation, RelayLocation } from '../../shared/daemon-rpc-types';
 import Accordion from './Accordion';
@@ -17,6 +18,8 @@ interface IProps {
   expanded: boolean;
   onSelect?: (location: RelayLocation) => void;
   onExpand?: (location: RelayLocation, value: boolean) => void;
+  onWillExpand?: (locationRect: DOMRect, expandedContentHeight: number) => void;
+  onTransitionEnd?: () => void;
   children?: CityRowElement | CityRowElement[];
 }
 
@@ -32,6 +35,8 @@ const styles = {
 };
 
 export default class CountryRow extends Component<IProps> {
+  private buttonRef = React.createRef<Cell.CellButton>();
+
   public static compareProps(oldProps: IProps, nextProps: IProps) {
     if (React.Children.count(oldProps.children) !== React.Children.count(nextProps.children)) {
       return false;
@@ -78,6 +83,7 @@ export default class CountryRow extends Component<IProps> {
     return (
       <View style={styles.container}>
         <Cell.CellButton
+          ref={this.buttonRef}
           style={styles.base}
           onPress={this.handlePress}
           disabled={!this.props.hasActiveRelays}
@@ -92,7 +98,15 @@ export default class CountryRow extends Component<IProps> {
           ) : null}
         </Cell.CellButton>
 
-        {hasChildren && <Accordion expanded={this.props.expanded}>{this.props.children}</Accordion>}
+        {hasChildren && (
+          <Accordion
+            expanded={this.props.expanded}
+            onWillExpand={this.onWillExpand}
+            onTransitionEnd={this.props.onTransitionEnd}
+            animationDuration={150}>
+            {this.props.children}
+          </Accordion>
+        )}
       </View>
     );
   }
@@ -107,6 +121,14 @@ export default class CountryRow extends Component<IProps> {
   private handlePress = () => {
     if (this.props.onSelect) {
       this.props.onSelect(this.props.location);
+    }
+  };
+
+  private onWillExpand = (nextHeight: number) => {
+    const buttonNode = ReactDOM.findDOMNode(this.buttonRef.current);
+    if (buttonNode instanceof HTMLElement) {
+      const buttonRect = buttonNode.getBoundingClientRect();
+      this.props.onWillExpand?.(buttonRect, nextHeight);
     }
   };
 }

gui/src/renderer/components/SelectLocation.tsx
@@ -44,6 +44,7 @@ interface ISelectLocationSnapshot {
 
 export default class SelectLocation extends Component<IProps> {
   private scrollView = React.createRef<CustomScrollbars>();
+  private spacePreAllocationViewRef = React.createRef<SpacePreAllocationView>();
   private selectedExitLocationRef = React.createRef<React.ReactInstance>();
   private selectedBridgeLocationRef = React.createRef<React.ReactInstance>();
 
@@ -128,27 +129,33 @@ export default class SelectLocation extends Component<IProps> {
               </NavigationBar>
               <View style={styles.container}>
                 <NavigationScrollbars ref={this.scrollView}>
-                  <View style={styles.content}>
-                    {this.props.locationScope === LocationScope.relay ? (
-                      <ExitLocations
-                        ref={this.exitLocationList}
-                        source={this.props.relayLocations}
-                        defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
-                        selectedValue={this.props.selectedExitLocation}
-                        selectedElementRef={this.selectedExitLocationRef}
-                        onSelect={this.onSelectExitLocation}
-                      />
-                    ) : (
-                      <BridgeLocations
-                        ref={this.bridgeLocationList}
-                        source={this.props.bridgeLocations}
-                        defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
-                        selectedValue={this.props.selectedBridgeLocation}
-                        selectedElementRef={this.selectedBridgeLocationRef}
-                        onSelect={this.onSelectBridgeLocation}
-                      />
-                    )}
-                  </View>
+                  <SpacePreAllocationView ref={this.spacePreAllocationViewRef}>
+                    <View style={styles.content}>
+                      {this.props.locationScope === LocationScope.relay ? (
+                        <ExitLocations
+                          ref={this.exitLocationList}
+                          source={this.props.relayLocations}
+                          defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
+                          selectedValue={this.props.selectedExitLocation}
+                          selectedElementRef={this.selectedExitLocationRef}
+                          onSelect={this.onSelectExitLocation}
+                          onWillExpand={this.onWillExpand}
+                          onTransitionEnd={this.spacePreAllocationViewRef.current?.reset}
+                        />
+                      ) : (
+                        <BridgeLocations
+                          ref={this.bridgeLocationList}
+                          source={this.props.bridgeLocations}
+                          defaultExpandedLocations={this.getExpandedLocationsFromSnapshot()}
+                          selectedValue={this.props.selectedBridgeLocation}
+                          selectedElementRef={this.selectedBridgeLocationRef}
+                          onSelect={this.onSelectBridgeLocation}
+                          onWillExpand={this.onWillExpand}
+                          onTransitionEnd={this.spacePreAllocationViewRef.current?.reset}
+                        />
+                      )}
+                    </View>
+                  </SpacePreAllocationView>
                 </NavigationScrollbars>
               </View>
             </NavigationContainer>
@@ -219,4 +226,39 @@ export default class SelectLocation extends Component<IProps> {
       this.props.onSelectClosestToExit();
     }
   };
+
+  private onWillExpand = (locationRect: DOMRect, expandedContentHeight: number) => {
+    locationRect.height += expandedContentHeight;
+    this.spacePreAllocationViewRef.current?.allocate(expandedContentHeight);
+    this.scrollView.current?.scrollIntoView(locationRect);
+  };
+}
+
+interface ISpacePreAllocationView {
+  children?: React.ReactNode;
+}
+
+class SpacePreAllocationView extends Component<ISpacePreAllocationView> {
+  private ref = React.createRef<HTMLDivElement>();
+
+  public allocate(height: number) {
+    if (this.ref.current) {
+      this.minHeight = this.ref.current.offsetHeight + height + 'px';
+    }
+  }
+
+  public reset = () => {
+    this.minHeight = 'auto';
+  };
+
+  public render() {
+    return <div ref={this.ref}>{this.props.children}</div>;
+  }
+
+  private set minHeight(value: string) {
+    const element = this.ref.current;
+    if (element) {
+      element.style.minHeight = value;
+    }
+  }
 }