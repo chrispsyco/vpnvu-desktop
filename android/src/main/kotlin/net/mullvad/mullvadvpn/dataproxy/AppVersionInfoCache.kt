mullvad/mullvadvpn-app

Use new version info in Android

android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/AppVersionInfoCache.kt
@@ -29,35 +29,19 @@ class AppVersionInfoCache(
 
     val latestStable
         get() = appVersionInfo?.latestStable
-    val latest
-        get() = appVersionInfo?.latest
+    val latestBeta
+        get() = appVersionInfo?.latestBeta
     val isSupported
         get() = appVersionInfo?.supported ?: true
 
     val isOutdated: Boolean
         get() {
-            if (showBetaReleases) {
-                return version != null && latest != null && latest != version
-            } else {
-                return version != null && latestStable != null && latestStable != version
-            }
+            return appVersionInfo?.suggestedUpgrade != null
         }
 
     val upgradeVersion: String?
         get() {
-            if (showBetaReleases) {
-                if (version == latest) {
-                    return null
-                } else {
-                    return latest
-                }
-            } else {
-                if (version == latestStable) {
-                    return null
-                } else {
-                    return latestStable
-                }
-            }
+            return appVersionInfo?.suggestedUpgrade
         }
 
     var onUpdate: (() -> Unit)? = null

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/dataproxy/AppVersionInfoCache.kt
@@ -27,38 +27,14 @@ class AppVersionInfoCache(
             }
         }
 
-    val latestStable
-        get() = appVersionInfo?.latestStable
-    val latest
-        get() = appVersionInfo?.latest
     val isSupported
         get() = appVersionInfo?.supported ?: true
 
-    val isOutdated: Boolean
-        get() {
-            if (showBetaReleases) {
-                return version != null && latest != null && latest != version
-            } else {
-                return version != null && latestStable != null && latestStable != version
-            }
-        }
+    val isOutdated
+        get() = appVersionInfo?.suggestedUpgrade != null
 
-    val upgradeVersion: String?
-        get() {
-            if (showBetaReleases) {
-                if (version == latest) {
-                    return null
-                } else {
-                    return latest
-                }
-            } else {
-                if (version == latestStable) {
-                    return null
-                } else {
-                    return latestStable
-                }
-            }
-        }
+    val upgradeVersion
+        get() = appVersionInfo?.suggestedUpgrade
 
     var onUpdate: (() -> Unit)? = null
         set(value) {