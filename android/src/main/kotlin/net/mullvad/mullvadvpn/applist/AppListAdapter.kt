zouguangxian/mullvadvpn-app

Load installed apps into adapter

android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListAdapter.kt
@@ -1,11 +1,26 @@
 package net.mullvad.mullvadvpn.applist
 
+import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.util.JobTracker
 
-class AppListAdapter : Adapter<AppListHolder>() {
+class AppListAdapter(context: Context) : Adapter<AppListHolder>() {
+    private val appList = ArrayList<ApplicationInfo>()
+    private val jobTracker = JobTracker()
+    private val packageManager = context.packageManager
+
+    init {
+        jobTracker.newBackgroundJob("populateAppList") {
+            populateAppList(context)
+        }
+    }
+
+    override fun getItemCount() = appList.size
+
     override fun onCreateViewHolder(parentView: ViewGroup, type: Int): AppListHolder {
         val inflater = LayoutInflater.from(parentView.context)
         val view = inflater.inflate(R.layout.app_list_item, parentView, false)
@@ -14,5 +29,15 @@ class AppListAdapter : Adapter<AppListHolder>() {
     }
 
     override fun onBindViewHolder(holder: AppListHolder, position: Int) {}
-    override fun getItemCount() = 0
+
+    private fun populateAppList(context: Context) {
+        val applications = context.packageManager.getInstalledApplications(0)
+
+        appList.clear()
+        appList.addAll(applications)
+
+        jobTracker.newUiJob("notifyAppListChanges") {
+            notifyItemRangeInserted(0, applications.size)
+        }
+    }
 }

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListAdapter.kt
@@ -1,18 +1,43 @@
 package net.mullvad.mullvadvpn.applist
 
+import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.util.JobTracker
 
-class AppListAdapter : Adapter<AppListItemHolder>() {
+class AppListAdapter(context: Context) : Adapter<AppListItemHolder>() {
+    private val appList = ArrayList<ApplicationInfo>()
+    private val jobTracker = JobTracker()
+    private val packageManager = context.packageManager
+
+    init {
+        jobTracker.newBackgroundJob("populateAppList") {
+            populateAppList(context)
+        }
+    }
+
+    override fun getItemCount() = appList.size
+
     override fun onCreateViewHolder(parentView: ViewGroup, type: Int): AppListItemHolder {
         val inflater = LayoutInflater.from(parentView.context)
         val view = inflater.inflate(R.layout.app_list_item, parentView, false)
 
-        return AppListItemHolder(view)
+        return AppListItemItemHolder(view)
     }
 
     override fun onBindViewHolder(holder: AppListItemHolder, position: Int) {}
-    override fun getItemCount() = 0
+
+    private fun populateAppList(context: Context) {
+        val applications = context.packageManager.getInstalledApplications(0)
+
+        appList.clear()
+        appList.addAll(applications)
+
+        jobTracker.newUiJob("notifyAppListChanges") {
+            notifyItemRangeInserted(0, applications.size)
+        }
+    }
 }