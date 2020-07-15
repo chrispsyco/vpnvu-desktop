zouguangxian/mullvadvpn-app

Show app icons in the app list

android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListAdapter.kt
@@ -25,7 +25,7 @@ class AppListAdapter(context: Context) : Adapter<AppListHolder>() {
         val inflater = LayoutInflater.from(parentView.context)
         val view = inflater.inflate(R.layout.app_list_item, parentView, false)
 
-        return AppListHolder(packageManager, view)
+        return AppListHolder(packageManager, jobTracker, view)
     }
 
     override fun onBindViewHolder(holder: AppListHolder, position: Int) {

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/applist/AppListAdapter.kt
@@ -24,7 +24,7 @@ class AppListAdapter(context: Context) : Adapter<AppListItemHolder>() {
         val inflater = LayoutInflater.from(parentView.context)
         val view = inflater.inflate(R.layout.app_list_item, parentView, false)
 
-        return AppListItemHolder(packageManager, view)
+        return AppListItemHolder(packageManager, jobTracker, view)
     }
 
     override fun onBindViewHolder(holder: AppListItemHolder, position: Int) {