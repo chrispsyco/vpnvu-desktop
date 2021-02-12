pingoow/mullvadvpn-app

Fix fragment transition animation Z-order

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ProblemReportFragment.kt
@@ -17,14 +17,14 @@ import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.ViewSwitcher
-import androidx.fragment.app.Fragment
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.CompletableDeferred
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.ui.fragments.FixAnimationFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-class ProblemReportFragment : Fragment() {
+class ProblemReportFragment : FixAnimationFragment() {
     private val jobTracker = JobTracker()
 
     private var showingEmail by observable(false) { _, oldValue, newValue ->

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceAwareFragment.kt
@@ -1,10 +1,10 @@
 package net.mullvad.mullvadvpn.ui
 
 import android.content.Context
-import androidx.fragment.app.Fragment
+import net.mullvad.mullvadvpn.ui.fragments.FixAnimationFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-abstract class ServiceAwareFragment : Fragment() {
+abstract class ServiceAwareFragment : FixAnimationFragment() {
     val jobTracker = JobTracker()
 
     open val isSecureScreen = false

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ViewLogsFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.fragment.app.Fragment
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.ui.fragments.FixAnimationFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-class ViewLogsFragment : Fragment() {
+class ViewLogsFragment : FixAnimationFragment() {
     private val jobTracker = JobTracker()
 
     private lateinit var problemReport: MullvadProblemReport

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ProblemReportFragment.kt
@@ -17,14 +17,14 @@ import android.widget.EditText
 import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.ViewSwitcher
-import androidx.fragment.app.Fragment
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.CompletableDeferred
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.ui.fragments.BaseFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-class ProblemReportFragment : Fragment() {
+class ProblemReportFragment : BaseFragment() {
     private val jobTracker = JobTracker()
 
     private var showingEmail by observable(false) { _, oldValue, newValue ->

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ServiceAwareFragment.kt
@@ -1,10 +1,10 @@
 package net.mullvad.mullvadvpn.ui
 
 import android.content.Context
-import androidx.fragment.app.Fragment
+import net.mullvad.mullvadvpn.ui.fragments.BaseFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-abstract class ServiceAwareFragment : Fragment() {
+abstract class ServiceAwareFragment : BaseFragment() {
     val jobTracker = JobTracker()
 
     open val isSecureScreen = false

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/ViewLogsFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.fragment.app.Fragment
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.ui.fragments.BaseFragment
 import net.mullvad.mullvadvpn.util.JobTracker
 
-class ViewLogsFragment : Fragment() {
+class ViewLogsFragment : BaseFragment() {
     private val jobTracker = JobTracker()
 
     private lateinit var problemReport: MullvadProblemReport