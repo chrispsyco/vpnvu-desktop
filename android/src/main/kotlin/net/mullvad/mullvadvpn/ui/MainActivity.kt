thesecurity-company/mullvadvpn-app

Wait until registration is complete

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/MainActivity.kt
@@ -19,6 +19,7 @@ import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.BuildConfig
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.service.Event
 import net.mullvad.mullvadvpn.service.MullvadVpnService
 import net.mullvad.talpid.util.EventNotifier
 
@@ -60,7 +61,14 @@ class MainActivity : FragmentActivity() {
                 }
 
                 serviceConnection = newConnection
-                serviceNotifier.notify(newConnection)
+
+                if (newConnection != null) {
+                    newConnection.dispatcher.registerHandler(Event.ListenerReady::class) { _ ->
+                        serviceNotifier.notify(newConnection)
+                    }
+                } else {
+                    serviceNotifier.notify(null)
+                }
 
                 if (shouldConnect) {
                     tryToConnect()

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/MainActivity.kt
@@ -19,6 +19,7 @@ import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.BuildConfig
 import net.mullvad.mullvadvpn.R
 import net.mullvad.mullvadvpn.dataproxy.MullvadProblemReport
+import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.service.MullvadVpnService
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 import net.mullvad.talpid.util.EventNotifier
@@ -61,7 +62,14 @@ class MainActivity : FragmentActivity() {
                 }
 
                 serviceConnection = newConnection
-                serviceNotifier.notify(newConnection)
+
+                if (newConnection != null) {
+                    newConnection.dispatcher.registerHandler(Event.ListenerReady::class) { _ ->
+                        serviceNotifier.notify(newConnection)
+                    }
+                } else {
+                    serviceNotifier.notify(null)
+                }
 
                 if (shouldConnect) {
                     tryToConnect()