mullvad/mullvadvpn-app

Allow registering event listener messengers

android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceEndpoint.kt
@@ -6,6 +6,8 @@ import net.mullvad.mullvadvpn.util.DispatchingHandler
 import net.mullvad.mullvadvpn.util.Intermittent
 
 class ServiceEndpoint(looper: Looper, intermittentDaemon: Intermittent<MullvadDaemon>) {
+    private val listeners = mutableListOf<Messenger>()
+
     internal val dispatcher = DispatchingHandler(looper) { message ->
         Request.fromMessage(message)
     }
@@ -13,6 +15,12 @@ class ServiceEndpoint(looper: Looper, intermittentDaemon: Intermittent<MullvadDa
     val messenger = Messenger(dispatcher)
 
     val settingsListener = SettingsListener(intermittentDaemon)
+
+    init {
+        dispatcher.registerHandler(Request.RegisterListener::class) { request ->
+            listeners.add(request.listener)
+        }
+    }
 
     fun onDestroy() {
         dispatcher.onDestroy()

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -6,11 +6,19 @@ import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Request
 
 class ServiceEndpoint(looper: Looper) {
+    private val listeners = mutableSetOf<Messenger>()
+
     internal val dispatcher = DispatchingHandler(looper) { message ->
         Request.fromMessage(message)
     }
 
     val messenger = Messenger(dispatcher)
+
+    init {
+        dispatcher.registerHandler(Request.RegisterListener::class) { request ->
+            listeners.add(request.listener)
+        }
+    }
 
     fun onDestroy() {
         dispatcher.onDestroy()