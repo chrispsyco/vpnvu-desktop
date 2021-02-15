mullvad/mullvadvpn-app

Create a `ServiceEndpoint` helper class

android/src/main/kotlin/net/mullvad/mullvadvpn/service/ServiceEndpoint.kt
@@ -1,0 +1,14 @@
+package net.mullvad.mullvadvpn.service
+
+import android.os.Looper
+import net.mullvad.mullvadvpn.util.DispatchingHandler
+
+class ServiceEndpoint(looper: Looper) {
+    internal val dispatcher = DispatchingHandler(looper) { message ->
+        Request.fromMessage(message)
+    }
+
+    fun onDestroy() {
+        dispatcher.onDestroy()
+    }
+}

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/ServiceEndpoint.kt
@@ -1,0 +1,15 @@
+package net.mullvad.mullvadvpn.service.endpoint
+
+import android.os.Looper
+import net.mullvad.mullvadvpn.ipc.DispatchingHandler
+import net.mullvad.mullvadvpn.ipc.Request
+
+class ServiceEndpoint(looper: Looper) {
+    internal val dispatcher = DispatchingHandler(looper) { message ->
+        Request.fromMessage(message)
+    }
+
+    fun onDestroy() {
+        dispatcher.onDestroy()
+    }
+}