shanbatla/mullvadvpn-app

Remove `AccountCache` from `ServiceInstance`

android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -59,7 +59,7 @@ class MullvadVpnService : TalpidVpnService() {
             oldInstance?.onDestroy()
 
             accountExpiryNotification = newInstance?.let { instance ->
-                AccountExpiryNotification(this, instance.daemon, instance.accountCache)
+                AccountExpiryNotification(this, instance.daemon, handler.accountCache)
             }
 
             serviceNotifier.notify(newInstance)
@@ -258,7 +258,6 @@ class MullvadVpnService : TalpidVpnService() {
             instance = ServiceInstance(
                 messenger,
                 daemon,
-                handler.accountCache,
                 connectionProxy,
                 customDns,
                 splitTunneling

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -58,7 +58,7 @@ class MullvadVpnService : TalpidVpnService() {
             oldInstance?.onDestroy()
 
             accountExpiryNotification = newInstance?.let { instance ->
-                AccountExpiryNotification(this, instance.daemon, instance.accountCache)
+                AccountExpiryNotification(this, instance.daemon, endpoint.accountCache)
             }
 
             serviceNotifier.notify(newInstance)
@@ -256,7 +256,6 @@ class MullvadVpnService : TalpidVpnService() {
                 endpoint.messenger,
                 daemon,
                 daemonInstance.intermittentDaemon,
-                endpoint.accountCache,
                 connectionProxy,
                 customDns,
                 splitTunneling