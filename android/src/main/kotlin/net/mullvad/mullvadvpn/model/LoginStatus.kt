mullvad/mullvadvpn-app

Create new `LoginStatus` type

android/src/main/kotlin/net/mullvad/mullvadvpn/model/LoginStatus.kt
@@ -1,0 +1,15 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.joda.time.DateTime
+
+@Parcelize
+data class LoginStatus(
+    val account: String,
+    val expiry: DateTime?,
+    val isNew: Boolean
+) : Parcelable {
+    val isExpired: Boolean
+        get() = expiry != null && expiry.isAfterNow()
+}

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/model/LoginStatus.kt
@@ -1,0 +1,15 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.joda.time.DateTime
+
+@Parcelize
+data class LoginStatus(
+    val account: String,
+    val expiry: DateTime?,
+    val isNewAccount: Boolean
+) : Parcelable {
+    val isExpired: Boolean
+        get() = expiry != null && expiry.isAfterNow()
+}