mullvad/mullvadvpn-app

Don't return account token when creating account

android/src/main/kotlin/net/mullvad/mullvadvpn/service/AccountCache.kt
@@ -51,11 +51,11 @@ class AccountCache(val settingsListener: SettingsListener) {
         }
     }
 
-    suspend fun createNewAccount(): String? {
+    suspend fun createNewAccount() {
         newlyCreatedAccount = true
         createdAccountExpiry = null
 
-        return availableDaemon.await().createNewAccount()
+        availableDaemon.await().createNewAccount()
     }
 
     suspend fun login(account: String) {

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/AccountCache.kt
@@ -50,11 +50,11 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
         }
     }
 
-    suspend fun createNewAccount(): String? {
+    suspend fun createNewAccount() {
         newlyCreatedAccount = true
         createdAccountExpiry = null
 
-        return daemon.await().createNewAccount()
+        daemon.await().createNewAccount()
     }
 
     suspend fun login(account: String) {