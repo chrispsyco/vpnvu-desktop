mullvad/mullvadvpn-app

Reduce exposed `SplitTunneling` service-side API

android/src/main/kotlin/net/mullvad/mullvadvpn/service/SplitTunneling.kt
@@ -18,11 +18,11 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
     private val excludedApps = HashSet<String>()
     private val preferences = context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE)
 
-    val onChange = EventNotifier<ArrayList<String>?>(null)
-
-    var enabled by observable(preferences.getBoolean(KEY_ENABLED, false)) { _, _, _ ->
+    private var enabled by observable(preferences.getBoolean(KEY_ENABLED, false)) { _, _, _ ->
         enabledChanged()
     }
+
+    val onChange = EventNotifier<ArrayList<String>?>(null)
 
     init {
         if (appListFile.exists()) {
@@ -37,13 +37,15 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
         endpoint.dispatcher.apply {
             registerHandler(Request.IncludeApp::class) { request ->
                 request.packageName?.let { packageName ->
-                    includeApp(packageName)
+                    excludedApps.remove(packageName)
+                    update()
                 }
             }
 
             registerHandler(Request.ExcludeApp::class) { request ->
                 request.packageName?.let { packageName ->
-                    excludeApp(packageName)
+                    excludedApps.add(packageName)
+                    update()
                 }
             }
 
@@ -52,25 +54,9 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
             }
 
             registerHandler(Request.PersistExcludedApps::class) { _ ->
-                persist()
+                appListFile.writeText(excludedApps.joinToString(separator = "\n"))
             }
         }
-    }
-
-    fun isAppExcluded(appPackageName: String) = excludedApps.contains(appPackageName)
-
-    fun excludeApp(appPackageName: String) {
-        excludedApps.add(appPackageName)
-        update()
-    }
-
-    fun includeApp(appPackageName: String) {
-        excludedApps.remove(appPackageName)
-        update()
-    }
-
-    fun persist() {
-        appListFile.writeText(excludedApps.joinToString(separator = "\n"))
     }
 
     fun onDestroy() {

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/SplitTunneling.kt
@@ -19,11 +19,11 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
     private val excludedApps = HashSet<String>()
     private val preferences = context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE)
 
-    val onChange = EventNotifier<List<String>?>(null)
-
-    var enabled by observable(preferences.getBoolean(KEY_ENABLED, false)) { _, _, _ ->
+    private var enabled by observable(preferences.getBoolean(KEY_ENABLED, false)) { _, _, _ ->
         enabledChanged()
     }
+
+    val onChange = EventNotifier<List<String>?>(null)
 
     init {
         if (appListFile.exists()) {
@@ -37,11 +37,13 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
 
         endpoint.dispatcher.apply {
             registerHandler(Request.IncludeApp::class) { request ->
-                includeApp(request.packageName)
+                excludedApps.remove(request.packageName)
+                update()
             }
 
             registerHandler(Request.ExcludeApp::class) { request ->
-                excludeApp(request.packageName)
+                excludedApps.add(request.packageName)
+                update()
             }
 
             registerHandler(Request.SetEnableSplitTunneling::class) { request ->
@@ -49,25 +51,9 @@ class SplitTunneling(context: Context, endpoint: ServiceEndpoint) {
             }
 
             registerHandler(Request.PersistExcludedApps::class) { _ ->
-                persist()
+                appListFile.writeText(excludedApps.joinToString(separator = "\n"))
             }
         }
-    }
-
-    fun isAppExcluded(appPackageName: String) = excludedApps.contains(appPackageName)
-
-    fun excludeApp(appPackageName: String) {
-        excludedApps.add(appPackageName)
-        update()
-    }
-
-    fun includeApp(appPackageName: String) {
-        excludedApps.remove(appPackageName)
-        update()
-    }
-
-    fun persist() {
-        appListFile.writeText(excludedApps.joinToString(separator = "\n"))
     }
 
     fun onDestroy() {