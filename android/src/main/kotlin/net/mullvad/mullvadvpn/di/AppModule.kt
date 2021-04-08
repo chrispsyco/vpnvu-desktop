mullvad/mullvadvpn-app

Add ServiceConnection scope. Fix SplitTunnelingViewmodel tests

android/src/main/kotlin/net/mullvad/mullvadvpn/di/AppModule.kt
@@ -1,10 +1,16 @@
 package net.mullvad.mullvadvpn.di
 
 import android.content.pm.PackageManager
+import android.os.Messenger
 import kotlinx.coroutines.Dispatchers
 import net.mullvad.mullvadvpn.applist.ApplicationsIconManager
 import net.mullvad.mullvadvpn.applist.ApplicationsProvider
-import net.mullvad.mullvadvpn.service.SplitTunneling
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageHandler
+import net.mullvad.mullvadvpn.service.ServiceInstance
+import net.mullvad.mullvadvpn.ui.MainActivity
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
+import net.mullvad.mullvadvpn.ui.serviceconnection.SplitTunneling
 import net.mullvad.mullvadvpn.viewmodel.SplitTunnelingViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,7 +20,6 @@ import org.koin.dsl.onClose
 
 val appModule = module {
 
-    single<SplitTunneling> { SplitTunneling(androidContext()) }
     single<PackageManager> { androidContext().packageManager }
     single<String> (named(SELF_PACKAGE_NAME)) { androidContext().packageName }
 
@@ -22,6 +27,15 @@ val appModule = module {
         viewModel { SplitTunnelingViewModel(get(), get(), Dispatchers.Default) }
         scoped { ApplicationsIconManager(get()) } onClose { it?.dispose() }
         scoped { ApplicationsProvider(get(), get(named(SELF_PACKAGE_NAME))) }
+    }
+
+    scope<ServiceConnection> {
+        scoped<ServiceConnection> { (service: ServiceInstance, mainActivity: MainActivity) ->
+            ServiceConnection(service, mainActivity)
+        }
+        scoped<SplitTunneling> { (messenger: Messenger, eventDispatcher: MessageHandler<Event>) ->
+            SplitTunneling(messenger, eventDispatcher)
+        }
     }
 }
 const val APPS_SCOPE = "APPS_SCOPE"

android/src/main/kotlin/net/mullvad/mullvadvpn/ipc/DispatchingHandler.kt
@@ -11,11 +11,11 @@ import kotlin.reflect.KClass
 class DispatchingHandler<T : Any>(
     looper: Looper,
     private val extractor: (Message) -> T?
-) : Handler(looper) {
+) : Handler(looper), MessageHandler<T> {
     private val handlers = HashMap<KClass<out T>, (T) -> Unit>()
     private val lock = ReentrantReadWriteLock()
 
-    fun <V : T> registerHandler(variant: KClass<V>, handler: (V) -> Unit) {
+    override fun <V : T> registerHandler(variant: KClass<V>, handler: (V) -> Unit) {
         lock.writeLock().withLock {
             handlers.put(variant) { instance ->
                 @Suppress("UNCHECKED_CAST")

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SplitTunneling.kt
@@ -2,11 +2,14 @@ package net.mullvad.mullvadvpn.ui.serviceconnection
 
 import android.os.Messenger
 import kotlin.properties.Delegates.observable
-import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageHandler
 import net.mullvad.mullvadvpn.ipc.Request
 
-class SplitTunneling(val connection: Messenger, eventDispatcher: DispatchingHandler<Event>) {
+class SplitTunneling(
+    private val connection: Messenger,
+    eventDispatcher: MessageHandler<Event>
+) {
     private var excludedApps: Set<String> = emptySet()
 
     var enabled by observable(false) { _, wasEnabled, isEnabled ->
@@ -28,15 +31,11 @@ class SplitTunneling(val connection: Messenger, eventDispatcher: DispatchingHand
 
     fun isAppExcluded(appPackageName: String): Boolean = excludedApps.contains(appPackageName)
 
-    fun excludeApp(appPackageName: String) {
+    fun excludeApp(appPackageName: String) =
         connection.send(Request.ExcludeApp(appPackageName).message)
-    }
 
-    fun includeApp(appPackageName: String) {
+    fun includeApp(appPackageName: String) =
         connection.send(Request.IncludeApp(appPackageName).message)
-    }
 
-    fun persist() {
-        connection.send(Request.PersistExcludedApps.message)
-    }
+    fun persist() = connection.send(Request.PersistExcludedApps.message)
 }

android/src/test/kotlin/net/mullvad/mullvadvpn/di/AppModuleTest.kt
@@ -1,0 +1,47 @@
+package net.mullvad.mullvadvpn.di
+
+import android.os.Messenger
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageHandler
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
+import net.mullvad.mullvadvpn.ui.serviceconnection.SplitTunneling
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
+import org.koin.core.scope.Scope
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import kotlin.test.assertEquals
+
+class AppModuleTest : KoinTest {
+
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        modules(appModule)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test_scope_linking() {
+        val appsScope: Scope = getKoin().createScope(APPS_SCOPE, named(APPS_SCOPE))
+        val serviceConnectionScope = getKoin().createScope<ServiceConnection>()
+
+        appsScope.linkTo(serviceConnectionScope)
+
+        val mockedMessenger = mockk<Messenger>()
+        val mockedEventMessageHandler = mockk<MessageHandler<Event>>(relaxed = true)
+        val serviceConnectionSplitTunneling = serviceConnectionScope.get<SplitTunneling>(
+            parameters = { parametersOf(mockedMessenger, mockedEventMessageHandler) }
+        )
+
+        assertEquals(appsScope.get<SplitTunneling>(), serviceConnectionSplitTunneling)
+    }
+}

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/di/AppModule.kt
@@ -1,10 +1,16 @@
 package net.mullvad.mullvadvpn.di
 
 import android.content.pm.PackageManager
+import android.os.Messenger
 import kotlinx.coroutines.Dispatchers
 import net.mullvad.mullvadvpn.applist.ApplicationsIconManager
 import net.mullvad.mullvadvpn.applist.ApplicationsProvider
-import net.mullvad.mullvadvpn.service.SplitTunneling
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageDispatcher
+import net.mullvad.mullvadvpn.service.ServiceInstance
+import net.mullvad.mullvadvpn.ui.MainActivity
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
+import net.mullvad.mullvadvpn.ui.serviceconnection.SplitTunneling
 import net.mullvad.mullvadvpn.viewmodel.SplitTunnelingViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,7 +20,6 @@ import org.koin.dsl.onClose
 
 val appModule = module {
 
-    single<SplitTunneling> { SplitTunneling(androidContext()) }
     single<PackageManager> { androidContext().packageManager }
     single<String> (named(SELF_PACKAGE_NAME)) { androidContext().packageName }
 
@@ -22,6 +27,15 @@ val appModule = module {
         viewModel { SplitTunnelingViewModel(get(), get(), Dispatchers.Default) }
         scoped { ApplicationsIconManager(get()) } onClose { it?.dispose() }
         scoped { ApplicationsProvider(get(), get(named(SELF_PACKAGE_NAME))) }
+    }
+
+    scope<ServiceConnection> {
+        scoped<ServiceConnection> { (service: ServiceInstance, mainActivity: MainActivity) ->
+            ServiceConnection(service, mainActivity)
+        } onClose { it?.onDestroy() }
+        scoped<SplitTunneling> { (messenger: Messenger, dispatcher: MessageDispatcher<Event>) ->
+            SplitTunneling(messenger, dispatcher)
+        }
     }
 }
 const val APPS_SCOPE = "APPS_SCOPE"

android/src/main/kotlin/net/mullvad/mullvadvpn/ipc/DispatchingHandler.kt
@@ -11,11 +11,11 @@ import kotlin.reflect.KClass
 class DispatchingHandler<T : Any>(
     looper: Looper,
     private val extractor: (Message) -> T?
-) : Handler(looper) {
+) : Handler(looper), MessageDispatcher<T> {
     private val handlers = HashMap<KClass<out T>, (T) -> Unit>()
     private val lock = ReentrantReadWriteLock()
 
-    fun <V : T> registerHandler(variant: KClass<V>, handler: (V) -> Unit) {
+    override fun <V : T> registerHandler(variant: KClass<V>, handler: (V) -> Unit) {
         lock.writeLock().withLock {
             handlers.put(variant) { instance ->
                 @Suppress("UNCHECKED_CAST")

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SplitTunneling.kt
@@ -2,11 +2,14 @@ package net.mullvad.mullvadvpn.ui.serviceconnection
 
 import android.os.Messenger
 import kotlin.properties.Delegates.observable
-import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageDispatcher
 import net.mullvad.mullvadvpn.ipc.Request
 
-class SplitTunneling(val connection: Messenger, eventDispatcher: DispatchingHandler<Event>) {
+class SplitTunneling(
+    private val connection: Messenger,
+    eventDispatcher: MessageDispatcher<Event>
+) {
     private var excludedApps: Set<String> = emptySet()
 
     var enabled by observable(false) { _, wasEnabled, isEnabled ->
@@ -28,15 +31,11 @@ class SplitTunneling(val connection: Messenger, eventDispatcher: DispatchingHand
 
     fun isAppExcluded(appPackageName: String): Boolean = excludedApps.contains(appPackageName)
 
-    fun excludeApp(appPackageName: String) {
+    fun excludeApp(appPackageName: String) =
         connection.send(Request.ExcludeApp(appPackageName).message)
-    }
 
-    fun includeApp(appPackageName: String) {
+    fun includeApp(appPackageName: String) =
         connection.send(Request.IncludeApp(appPackageName).message)
-    }
 
-    fun persist() {
-        connection.send(Request.PersistExcludedApps.message)
-    }
+    fun persist() = connection.send(Request.PersistExcludedApps.message)
 }

android/src/test/kotlin/net/mullvad/mullvadvpn/di/AppModuleTest.kt
@@ -1,0 +1,47 @@
+package net.mullvad.mullvadvpn.di
+
+import android.os.Messenger
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlin.test.assertEquals
+import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.MessageDispatcher
+import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
+import net.mullvad.mullvadvpn.ui.serviceconnection.SplitTunneling
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
+import org.koin.core.scope.Scope
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+
+class AppModuleTest : KoinTest {
+
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        modules(appModule)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun test_scope_linking() {
+        val appsScope: Scope = getKoin().createScope(APPS_SCOPE, named(APPS_SCOPE))
+        val serviceConnectionScope = getKoin().createScope<ServiceConnection>()
+
+        appsScope.linkTo(serviceConnectionScope)
+
+        val mockedMessenger = mockk<Messenger>()
+        val mockedEventMessageHandler = mockk<MessageDispatcher<Event>>(relaxed = true)
+        val serviceConnectionSplitTunneling = serviceConnectionScope.get<SplitTunneling>(
+            parameters = { parametersOf(mockedMessenger, mockedEventMessageHandler) }
+        )
+
+        assertEquals(appsScope.get<SplitTunneling>(), serviceConnectionSplitTunneling)
+    }
+}