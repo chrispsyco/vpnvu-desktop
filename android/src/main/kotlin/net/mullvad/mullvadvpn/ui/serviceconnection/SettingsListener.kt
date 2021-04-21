mullvad/mullvadvpn-app

Allow setting account with a request from UI side

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SettingsListener.kt
@@ -1,18 +1,24 @@
 package net.mullvad.mullvadvpn.ui.serviceconnection
 
+import android.os.Messenger
 import net.mullvad.mullvadvpn.model.DnsOptions
 import net.mullvad.mullvadvpn.model.RelaySettings
 import net.mullvad.mullvadvpn.model.Settings
 import net.mullvad.mullvadvpn.service.Event
+import net.mullvad.mullvadvpn.service.Request
 import net.mullvad.talpid.util.EventNotifier
 
-class SettingsListener(val eventDispatcher: EventDispatcher) {
+class SettingsListener(val connection: Messenger, eventDispatcher: EventDispatcher) {
     val accountNumberNotifier = EventNotifier<String?>(null)
     val dnsOptionsNotifier = EventNotifier<DnsOptions?>(null)
     val relaySettingsNotifier = EventNotifier<RelaySettings?>(null)
     val settingsNotifier = EventNotifier<Settings?>(null)
 
     private var settings by settingsNotifier.notifiable()
+
+    var account: String?
+        get() = accountNumberNotifier.latestEvent
+        set(value) { connection.send(Request.SetAccount(value).message) }
 
     init {
         eventDispatcher.registerHandler(Event.Type.SettingsUpdate) { event: Event.SettingsUpdate ->

chrispsyco/vpnvu-desktop

android/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/SettingsListener.kt
@@ -1,19 +1,25 @@
 package net.mullvad.mullvadvpn.ui.serviceconnection
 
+import android.os.Messenger
 import net.mullvad.mullvadvpn.ipc.DispatchingHandler
 import net.mullvad.mullvadvpn.ipc.Event
+import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.DnsOptions
 import net.mullvad.mullvadvpn.model.RelaySettings
 import net.mullvad.mullvadvpn.model.Settings
 import net.mullvad.talpid.util.EventNotifier
 
-class SettingsListener(eventDispatcher: DispatchingHandler<Event>) {
+class SettingsListener(val connection: Messenger, eventDispatcher: DispatchingHandler<Event>) {
     val accountNumberNotifier = EventNotifier<String?>(null)
     val dnsOptionsNotifier = EventNotifier<DnsOptions?>(null)
     val relaySettingsNotifier = EventNotifier<RelaySettings?>(null)
     val settingsNotifier = EventNotifier<Settings?>(null)
 
     private var settings by settingsNotifier.notifiable()
+
+    var account: String?
+        get() = accountNumberNotifier.latestEvent
+        set(value) { connection.send(Request.SetAccount(value).message) }
 
     init {
         eventDispatcher.registerHandler(Event.SettingsUpdate::class, ::handleNewEvent)