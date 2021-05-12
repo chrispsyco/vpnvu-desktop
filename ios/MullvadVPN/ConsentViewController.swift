nikitalita/mullvadvpn-app

HeaderBar: add divider

ios/MullvadVPN/ConsentViewController.swift
@@ -19,8 +19,8 @@ class ConsentViewController: UIViewController, RootContainment, SFSafariViewCont
         return .lightContent
     }
 
-    var preferredHeaderBarStyle: HeaderBarStyle {
-        return .transparent
+    var preferredHeaderBarPresentation: HeaderBarPresentation {
+        return HeaderBarPresentation(style: .transparent, showsDivider: false)
     }
 
     var prefersHeaderBarHidden: Bool {

chrispsyco/vpnvu-desktop

ios/MullvadVPN/ConsentViewController.swift
@@ -19,8 +19,8 @@ class ConsentViewController: UIViewController, RootContainment, SFSafariViewCont
         return .lightContent
     }
 
-    var preferredHeaderBarStyle: HeaderBarStyle {
-        return .default
+    var preferredHeaderBarPresentation: HeaderBarPresentation {
+        return HeaderBarPresentation(style: .default, showsDivider: false)
     }
 
     var prefersHeaderBarHidden: Bool {