HelloReboot/mullvadvpn-app

Propagate device state over service connection
Adds device state communication between the app and service with a
repository backed by a data source to add abstraction layers on the app
side.

Limitations:
* Expiration is not updated correctly.
* The login/creation flow has not been fully adapted to devices.

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/DeviceState.kt
@@ -1,0 +1,29 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class DeviceState : Parcelable {
+    @Parcelize
+    object UnknownDeviceState : DeviceState()
+
+    @Parcelize
+    data class DeviceRegistered(val deviceConfig: DeviceConfig) : DeviceState()
+
+    @Parcelize
+    object DeviceNotRegistered : DeviceState()
+
+    fun isUnknown(): Boolean {
+        return this is UnknownDeviceState
+    }
+
+    fun token(): String? {
+        return (this as? DeviceRegistered)?.deviceConfig?.token
+    }
+
+    companion object {
+        fun fromDeviceConfig(deviceConfig: DeviceConfig?): DeviceState {
+            return deviceConfig?.let { DeviceRegistered(it) } ?: DeviceNotRegistered
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/LaunchFragment.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.onSubscription
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.DeviceState
+import net.mullvad.mullvadvpn.ui.serviceconnection.DeviceRepository
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 
 class LaunchFragment : ServiceAwareFragment() {
-    private val hasAccountToken = CompletableDeferred<Boolean>()
-
-    override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
-        serviceConnection.settingsListener.accountNumberNotifier.subscribe(this) { accountToken ->
-            hasAccountToken.complete(accountToken != null)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,25 +27,27 @@ class LaunchFragment : ServiceAwareFragment() {
         return view
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        jobTracker.newUiJob("advanceToNextScreen") {
-            advanceToNextScreen()
-        }
-    }
-
     override fun onStop() {
         jobTracker.cancelJob("advanceToNextScreen")
-
         super.onStop()
     }
 
-    private suspend fun advanceToNextScreen() {
-        if (hasAccountToken.await()) {
-            advanceToConnectScreen()
-        } else {
-            advanceToLoginScreen()
+    override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
+        advanceToNextScreen(serviceConnection.deviceRepository)
+    }
+
+    private fun advanceToNextScreen(deviceRepository: DeviceRepository) {
+        jobTracker.newUiJob("advanceToNextScreen") {
+            deviceRepository.deviceState
+                .onSubscription { deviceRepository.refreshDeviceState() }
+                .first { state -> state.isUnknown().not() }
+                .let { deviceState ->
+                    when (deviceState) {
+                        is DeviceState.DeviceRegistered -> advanceToConnectScreen()
+                        is DeviceState.DeviceNotRegistered -> advanceToLoginScreen()
+                        else -> Unit
+                    }
+                }
         }
     }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/SettingsFragment.kt
@@ -8,9 +8,12 @@ import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.DeviceState
 import net.mullvad.mullvadvpn.ui.serviceconnection.AccountCache
 import net.mullvad.mullvadvpn.ui.serviceconnection.AppVersionInfoCache
+import net.mullvad.mullvadvpn.ui.serviceconnection.DeviceRepository
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 import net.mullvad.mullvadvpn.ui.widget.AccountCell
 import net.mullvad.mullvadvpn.ui.widget.AppVersionCell
@@ -26,10 +29,12 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
     private var active = false
 
     private var accountCache: AccountCache? = null
+    private var deviceRepository: DeviceRepository? = null
     private var versionInfoCache: AppVersionInfoCache? = null
 
     override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
         accountCache = serviceConnection.accountCache
+        deviceRepository = serviceConnection.deviceRepository
         versionInfoCache = serviceConnection.appVersionInfoCache
 
         if (active) {
@@ -39,6 +44,7 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
 
     override fun onNoServiceConnection() {
         accountCache = null
+        deviceRepository = null
         versionInfoCache = null
     }
 
@@ -102,9 +108,10 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
         versionInfoCache?.onUpdate = null
 
         accountCache?.apply {
-            onAccountNumberChange.unsubscribe(this@SettingsFragment)
             onAccountExpiryChange.unsubscribe(this@SettingsFragment)
         }
+
+        jobTracker.cancelAllJobs()
 
         super.onStop()
     }
@@ -116,12 +123,6 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
 
     private fun configureListeners() {
         accountCache?.apply {
-            onAccountNumberChange.subscribe(this@SettingsFragment) { account ->
-                jobTracker.newUiJob("updateLoggedInStatus") {
-                    updateLoggedInStatus(account != null)
-                }
-            }
-
             onAccountExpiryChange.subscribe(this@SettingsFragment) { expiry ->
                 jobTracker.newUiJob("updateAccountInfo") {
                     accountMenu.accountExpiry = expiry
@@ -129,6 +130,16 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
             }
 
             fetchAccountExpiry()
+        }
+
+        jobTracker.newUiJob("updateLoggedInStatus") {
+            deviceRepository?.let { repository ->
+                repository.deviceState
+                    .onEach { state -> if (state.isUnknown()) repository.refreshDeviceState() }
+                    .collect { device ->
+                        updateLoggedInStatus(device is DeviceState.DeviceRegistered)
+                    }
+            }
         }
 
         versionInfoCache?.onUpdate = {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/DeviceRepository.kt
@@ -1,0 +1,20 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted.Companion.Lazily
+import kotlinx.coroutines.flow.stateIn
+import net.mullvad.mullvadvpn.model.DeviceState
+
+class DeviceRepository(
+    private val dataSource: ServiceConnectionDeviceDataSource,
+    externalScope: CoroutineScope
+) {
+    val deviceState = dataSource.deviceStateUpdates
+        .stateIn(
+            externalScope,
+            Lazily,
+            DeviceState.UnknownDeviceState
+        )
+
+    fun refreshDeviceState() = dataSource.refreshDevice()
+}

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/DeviceState.kt
@@ -1,0 +1,29 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class DeviceState : Parcelable {
+    @Parcelize
+    object InitialState : DeviceState()
+
+    @Parcelize
+    data class DeviceRegistered(val deviceConfig: DeviceConfig) : DeviceState()
+
+    @Parcelize
+    object DeviceNotRegistered : DeviceState()
+
+    fun isInitialState(): Boolean {
+        return this is InitialState
+    }
+
+    fun token(): String? {
+        return (this as? DeviceRegistered)?.deviceConfig?.token
+    }
+
+    companion object {
+        fun fromDeviceConfig(deviceConfig: DeviceConfig?): DeviceState {
+            return deviceConfig?.let { DeviceRegistered(it) } ?: DeviceNotRegistered
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/LaunchFragment.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.onSubscription
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.DeviceState
+import net.mullvad.mullvadvpn.ui.serviceconnection.DeviceRepository
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 
 class LaunchFragment : ServiceAwareFragment() {
-    private val hasAccountToken = CompletableDeferred<Boolean>()
-
-    override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
-        serviceConnection.settingsListener.accountNumberNotifier.subscribe(this) { accountToken ->
-            hasAccountToken.complete(accountToken != null)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,25 +27,27 @@ class LaunchFragment : ServiceAwareFragment() {
         return view
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        jobTracker.newUiJob("advanceToNextScreen") {
-            advanceToNextScreen()
-        }
-    }
-
     override fun onStop() {
         jobTracker.cancelJob("advanceToNextScreen")
-
         super.onStop()
     }
 
-    private suspend fun advanceToNextScreen() {
-        if (hasAccountToken.await()) {
-            advanceToConnectScreen()
-        } else {
-            advanceToLoginScreen()
+    override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
+        advanceToNextScreen(serviceConnection.deviceRepository)
+    }
+
+    private fun advanceToNextScreen(deviceRepository: DeviceRepository) {
+        jobTracker.newUiJob("advanceToNextScreen") {
+            deviceRepository.deviceState
+                .onSubscription { deviceRepository.refreshDeviceState() }
+                .first { state -> state.isInitialState().not() }
+                .let { deviceState ->
+                    when (deviceState) {
+                        is DeviceState.DeviceRegistered -> advanceToConnectScreen()
+                        is DeviceState.DeviceNotRegistered -> advanceToLoginScreen()
+                        else -> Unit
+                    }
+                }
         }
     }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/SettingsFragment.kt
@@ -8,9 +8,12 @@ import android.widget.ImageButton
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.DeviceState
 import net.mullvad.mullvadvpn.ui.serviceconnection.AccountCache
 import net.mullvad.mullvadvpn.ui.serviceconnection.AppVersionInfoCache
+import net.mullvad.mullvadvpn.ui.serviceconnection.DeviceRepository
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 import net.mullvad.mullvadvpn.ui.widget.AccountCell
 import net.mullvad.mullvadvpn.ui.widget.AppVersionCell
@@ -26,10 +29,12 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
     private var active = false
 
     private var accountCache: AccountCache? = null
+    private var deviceRepository: DeviceRepository? = null
     private var versionInfoCache: AppVersionInfoCache? = null
 
     override fun onNewServiceConnection(serviceConnection: ServiceConnection) {
         accountCache = serviceConnection.accountCache
+        deviceRepository = serviceConnection.deviceRepository
         versionInfoCache = serviceConnection.appVersionInfoCache
 
         if (active) {
@@ -39,6 +44,7 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
 
     override fun onNoServiceConnection() {
         accountCache = null
+        deviceRepository = null
         versionInfoCache = null
     }
 
@@ -102,9 +108,10 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
         versionInfoCache?.onUpdate = null
 
         accountCache?.apply {
-            onAccountNumberChange.unsubscribe(this@SettingsFragment)
             onAccountExpiryChange.unsubscribe(this@SettingsFragment)
         }
+
+        jobTracker.cancelAllJobs()
 
         super.onStop()
     }
@@ -116,12 +123,6 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
 
     private fun configureListeners() {
         accountCache?.apply {
-            onAccountNumberChange.subscribe(this@SettingsFragment) { account ->
-                jobTracker.newUiJob("updateLoggedInStatus") {
-                    updateLoggedInStatus(account != null)
-                }
-            }
-
             onAccountExpiryChange.subscribe(this@SettingsFragment) { expiry ->
                 jobTracker.newUiJob("updateAccountInfo") {
                     accountMenu.accountExpiry = expiry
@@ -129,6 +130,16 @@ class SettingsFragment : ServiceAwareFragment(), StatusBarPainter, NavigationBar
             }
 
             fetchAccountExpiry()
+        }
+
+        jobTracker.newUiJob("updateLoggedInStatus") {
+            deviceRepository?.let { repository ->
+                repository.deviceState
+                    .onEach { state -> if (state.isInitialState()) repository.refreshDeviceState() }
+                    .collect { device ->
+                        updateLoggedInStatus(device is DeviceState.DeviceRegistered)
+                    }
+            }
         }
 
         versionInfoCache?.onUpdate = {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/DeviceRepository.kt
@@ -1,0 +1,20 @@
+package net.mullvad.mullvadvpn.ui.serviceconnection
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted.Companion.Lazily
+import kotlinx.coroutines.flow.stateIn
+import net.mullvad.mullvadvpn.model.DeviceState
+
+class DeviceRepository(
+    private val dataSource: ServiceConnectionDeviceDataSource,
+    externalScope: CoroutineScope
+) {
+    val deviceState = dataSource.deviceStateUpdates
+        .stateIn(
+            externalScope,
+            Lazily,
+            DeviceState.InitialState
+        )
+
+    fun refreshDeviceState() = dataSource.refreshDevice()
+}