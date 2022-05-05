wilbernight/mullvadvpn-app

Hide private device type in `mullvad-daemon`

mullvad-daemon/src/device/api.rs
@@ -1,13 +1,11 @@
 use std::pin::Pin;
 
 use futures::{future::FusedFuture, Future};
-use mullvad_types::{
-    device::{Device, DeviceData},
-    wireguard::WireguardData,
-};
+use mullvad_types::{device::Device, wireguard::WireguardData};
 
-use super::{Error, ResponseTx};
-pub struct CurrentApiCall {
+use super::{Error, InnerDeviceConfig, ResponseTx};
+
+pub(crate) struct CurrentApiCall {
     current_call: Option<Call>,
 }
 
@@ -20,7 +18,7 @@ impl CurrentApiCall {
         self.current_call = None;
     }
 
-    pub fn set_login(&mut self, login: ApiCall<DeviceData>, tx: ResponseTx<()>) {
+    pub fn set_login(&mut self, login: ApiCall<InnerDeviceConfig>, tx: ResponseTx<()>) {
         self.current_call = Some(Call::Login(login, Some(tx)));
     }
 
@@ -89,7 +87,7 @@ impl FusedFuture for CurrentApiCall {
 type ApiCall<T> = Pin<Box<dyn Future<Output = Result<T, Error>> + Send>>;
 
 enum Call {
-    Login(ApiCall<DeviceData>, Option<ResponseTx<()>>),
+    Login(ApiCall<InnerDeviceConfig>, Option<ResponseTx<()>>),
     TimerKeyRotation(ApiCall<WireguardData>),
     OneshotKeyRotation(ApiCall<WireguardData>),
     Validation(ApiCall<Device>),
@@ -119,8 +117,8 @@ impl futures::Future for Call {
     }
 }
 
-pub enum ApiResult {
-    Login(Result<DeviceData, Error>, ResponseTx<()>),
+pub(crate) enum ApiResult {
+    Login(Result<InnerDeviceConfig, Error>, ResponseTx<()>),
     Rotation(Result<WireguardData, Error>),
     Validation(Result<Device, Error>),
 }

mullvad-daemon/src/device/service.rs
@@ -4,12 +4,12 @@ use chrono::{DateTime, Utc};
 use futures::future::{abortable, AbortHandle};
 use mullvad_types::{
     account::{AccountToken, VoucherSubmission},
-    device::{Device, DeviceData, DeviceId, InnerDevice},
+    device::{Device, DeviceId},
     wireguard::WireguardData,
 };
 use talpid_types::net::wireguard::PrivateKey;
 
-use super::Error;
+use super::{Error, InnerDevice, InnerDeviceConfig};
 use mullvad_api::{
     availability::ApiAvailabilityHandle,
     rest::{self, Error as RestError, MullvadRestHandle},
@@ -42,14 +42,14 @@ impl DeviceService {
     /// Generate a new device for a given token
     pub fn generate_for_account(
         &self,
-        token: AccountToken,
-    ) -> impl Future<Output = Result<DeviceData, Error>> + Send {
+        account_token: AccountToken,
+    ) -> impl Future<Output = Result<InnerDeviceConfig, Error>> + Send {
         let private_key = PrivateKey::new_from_random();
         let pubkey = private_key.public_key();
 
         let proxy = self.proxy.clone();
         let api_handle = self.api_availability.clone();
-        let token_copy = token.clone();
+        let token_copy = account_token.clone();
         async move {
             let (device, addresses) = retry_future_n(
                 move || proxy.create(token_copy.clone(), pubkey.clone()),
@@ -60,28 +60,30 @@ impl DeviceService {
             .await
             .map_err(map_rest_error)?;
 
-            Ok(DeviceData {
-                token,
-                device: InnerDevice::from(device),
-                wg_data: WireguardData {
-                    private_key,
-                    addresses,
-                    created: Utc::now(),
-                },
+            Ok(InnerDeviceConfig {
+                account_token,
+                device: InnerDevice::try_from_device(
+                    device,
+                    WireguardData {
+                        private_key,
+                        addresses,
+                        created: Utc::now(),
+                    },
+                )?,
             })
         }
     }
 
     pub async fn generate_for_account_with_backoff(
         &self,
-        token: AccountToken,
-    ) -> Result<DeviceData, Error> {
+        account_token: AccountToken,
+    ) -> Result<InnerDeviceConfig, Error> {
         let private_key = PrivateKey::new_from_random();
         let pubkey = private_key.public_key();
 
         let proxy = self.proxy.clone();
         let api_handle = self.api_availability.clone();
-        let token_copy = token.clone();
+        let token_copy = account_token.clone();
         let (device, addresses) = retry_future(
             move || api_handle.when_online(proxy.create(token_copy.clone(), pubkey.clone())),
             should_retry_backoff,
@@ -90,14 +92,16 @@ impl DeviceService {
         .await
         .map_err(map_rest_error)?;
 
-        Ok(DeviceData {
-            token,
-            device: InnerDevice::from(device),
-            wg_data: WireguardData {
-                private_key,
-                addresses,
-                created: Utc::now(),
-            },
+        Ok(InnerDeviceConfig {
+            account_token,
+            device: InnerDevice::try_from_device(
+                device,
+                WireguardData {
+                    private_key,
+                    addresses,
+                    created: Utc::now(),
+                },
+            )?,
         })
     }
 

mullvad-daemon/src/migrations/device.rs
@@ -8,14 +8,10 @@
 
 use super::{v5::MigrationData, MigrationComplete};
 use crate::{
-    device::{self, DeviceService},
+    device::{self, DeviceService, InnerDevice, InnerDeviceConfig},
     DaemonEventSender, InternalDaemonEvent,
 };
-use mullvad_types::{
-    account::AccountToken,
-    device::{DeviceData, InnerDevice},
-    wireguard::WireguardData,
-};
+use mullvad_types::{account::AccountToken, wireguard::WireguardData};
 use std::time::Duration;
 use talpid_core::mpsc::Sender;
 use talpid_types::ErrorExt;
@@ -61,51 +57,56 @@ pub(crate) fn generate_device(
 
 async fn cache_from_wireguard_key(
     service: DeviceService,
-    token: AccountToken,
+    account_token: AccountToken,
     wg_data: WireguardData,
-) -> Result<DeviceData, device::Error> {
-    let devices = timeout(TIMEOUT, service.list_devices_with_backoff(token.clone()))
-        .await
-        .map_err(|_error| {
-            log::error!("Failed to enumerate devices for account: timed out");
-            device::Error::Cancelled
-        })?
-        .map_err(|error| {
-            log::error!(
-                "{}",
-                error.display_chain_with_msg("Failed to enumerate devices for account")
-            );
-            error
-        })?;
+) -> Result<InnerDeviceConfig, device::Error> {
+    let devices = timeout(
+        TIMEOUT,
+        service.list_devices_with_backoff(account_token.clone()),
+    )
+    .await
+    .map_err(|_error| {
+        log::error!("Failed to enumerate devices for account: timed out");
+        device::Error::Cancelled
+    })?
+    .map_err(|error| {
+        log::error!(
+            "{}",
+            error.display_chain_with_msg("Failed to enumerate devices for account")
+        );
+        error
+    })?;
 
     for device in devices.into_iter() {
         if device.pubkey == wg_data.private_key.public_key() {
-            return Ok(DeviceData {
-                token,
-                device: InnerDevice::from(device),
-                wg_data,
+            return Ok(InnerDeviceConfig {
+                account_token,
+                device: InnerDevice::try_from_device(device, wg_data)?,
             });
         }
     }
     log::info!("The existing WireGuard key is not valid; generating a new device");
-    cache_from_account(service, token).await
+    cache_from_account(service, account_token).await
 }
 
 async fn cache_from_account(
     service: DeviceService,
-    token: AccountToken,
-) -> Result<DeviceData, device::Error> {
-    timeout(TIMEOUT, service.generate_for_account_with_backoff(token))
-        .await
-        .map_err(|_error| {
-            log::error!("Failed to generate new device for account: timed out");
-            device::Error::Cancelled
-        })?
-        .map_err(|error| {
-            log::error!(
-                "{}",
-                error.display_chain_with_msg("Failed to generate new device for account")
-            );
-            error
-        })
+    account_token: AccountToken,
+) -> Result<InnerDeviceConfig, device::Error> {
+    timeout(
+        TIMEOUT,
+        service.generate_for_account_with_backoff(account_token),
+    )
+    .await
+    .map_err(|_error| {
+        log::error!("Failed to generate new device for account: timed out");
+        device::Error::Cancelled
+    })?
+    .map_err(|error| {
+        log::error!(
+            "{}",
+            error.display_chain_with_msg("Failed to generate new device for account")
+        );
+        error
+    })
 }

chrispsyco/vpnvu-desktop

mullvad-daemon/src/device/api.rs
@@ -1,13 +1,11 @@
 use std::pin::Pin;
 
 use futures::{future::FusedFuture, Future};
-use mullvad_types::{
-    device::{Device, DeviceData},
-    wireguard::WireguardData,
-};
+use mullvad_types::{device::Device, wireguard::WireguardData};
 
-use super::{Error, ResponseTx};
-pub struct CurrentApiCall {
+use super::{Error, PrivateAccountAndDevice, ResponseTx};
+
+pub(crate) struct CurrentApiCall {
     current_call: Option<Call>,
 }
 
@@ -20,7 +18,7 @@ impl CurrentApiCall {
         self.current_call = None;
     }
 
-    pub fn set_login(&mut self, login: ApiCall<DeviceData>, tx: ResponseTx<()>) {
+    pub fn set_login(&mut self, login: ApiCall<PrivateAccountAndDevice>, tx: ResponseTx<()>) {
         self.current_call = Some(Call::Login(login, Some(tx)));
     }
 
@@ -89,7 +87,7 @@ impl FusedFuture for CurrentApiCall {
 type ApiCall<T> = Pin<Box<dyn Future<Output = Result<T, Error>> + Send>>;
 
 enum Call {
-    Login(ApiCall<DeviceData>, Option<ResponseTx<()>>),
+    Login(ApiCall<PrivateAccountAndDevice>, Option<ResponseTx<()>>),
     TimerKeyRotation(ApiCall<WireguardData>),
     OneshotKeyRotation(ApiCall<WireguardData>),
     Validation(ApiCall<Device>),
@@ -119,8 +117,8 @@ impl futures::Future for Call {
     }
 }
 
-pub enum ApiResult {
-    Login(Result<DeviceData, Error>, ResponseTx<()>),
+pub(crate) enum ApiResult {
+    Login(Result<PrivateAccountAndDevice, Error>, ResponseTx<()>),
     Rotation(Result<WireguardData, Error>),
     Validation(Result<Device, Error>),
 }

mullvad-daemon/src/device/service.rs
@@ -4,12 +4,12 @@ use chrono::{DateTime, Utc};
 use futures::future::{abortable, AbortHandle};
 use mullvad_types::{
     account::{AccountToken, VoucherSubmission},
-    device::{Device, DeviceData, DeviceId, InnerDevice},
+    device::{Device, DeviceId},
     wireguard::WireguardData,
 };
 use talpid_types::net::wireguard::PrivateKey;
 
-use super::Error;
+use super::{Error, PrivateAccountAndDevice, PrivateDevice};
 use mullvad_api::{
     availability::ApiAvailabilityHandle,
     rest::{self, Error as RestError, MullvadRestHandle},
@@ -42,14 +42,14 @@ impl DeviceService {
     /// Generate a new device for a given token
     pub fn generate_for_account(
         &self,
-        token: AccountToken,
-    ) -> impl Future<Output = Result<DeviceData, Error>> + Send {
+        account_token: AccountToken,
+    ) -> impl Future<Output = Result<PrivateAccountAndDevice, Error>> + Send {
         let private_key = PrivateKey::new_from_random();
         let pubkey = private_key.public_key();
 
         let proxy = self.proxy.clone();
         let api_handle = self.api_availability.clone();
-        let token_copy = token.clone();
+        let token_copy = account_token.clone();
         async move {
             let (device, addresses) = retry_future_n(
                 move || proxy.create(token_copy.clone(), pubkey.clone()),
@@ -60,28 +60,30 @@ impl DeviceService {
             .await
             .map_err(map_rest_error)?;
 
-            Ok(DeviceData {
-                token,
-                device: InnerDevice::from(device),
-                wg_data: WireguardData {
-                    private_key,
-                    addresses,
-                    created: Utc::now(),
-                },
+            Ok(PrivateAccountAndDevice {
+                account_token,
+                device: PrivateDevice::try_from_device(
+                    device,
+                    WireguardData {
+                        private_key,
+                        addresses,
+                        created: Utc::now(),
+                    },
+                )?,
             })
         }
     }
 
     pub async fn generate_for_account_with_backoff(
         &self,
-        token: AccountToken,
-    ) -> Result<DeviceData, Error> {
+        account_token: AccountToken,
+    ) -> Result<PrivateAccountAndDevice, Error> {
         let private_key = PrivateKey::new_from_random();
         let pubkey = private_key.public_key();
 
         let proxy = self.proxy.clone();
         let api_handle = self.api_availability.clone();
-        let token_copy = token.clone();
+        let token_copy = account_token.clone();
         let (device, addresses) = retry_future(
             move || api_handle.when_online(proxy.create(token_copy.clone(), pubkey.clone())),
             should_retry_backoff,
@@ -90,14 +92,16 @@ impl DeviceService {
         .await
         .map_err(map_rest_error)?;
 
-        Ok(DeviceData {
-            token,
-            device: InnerDevice::from(device),
-            wg_data: WireguardData {
-                private_key,
-                addresses,
-                created: Utc::now(),
-            },
+        Ok(PrivateAccountAndDevice {
+            account_token,
+            device: PrivateDevice::try_from_device(
+                device,
+                WireguardData {
+                    private_key,
+                    addresses,
+                    created: Utc::now(),
+                },
+            )?,
         })
     }
 

mullvad-daemon/src/migrations/device.rs
@@ -8,14 +8,10 @@
 
 use super::{v5::MigrationData, MigrationComplete};
 use crate::{
-    device::{self, DeviceService},
+    device::{self, DeviceService, PrivateAccountAndDevice, PrivateDevice},
     DaemonEventSender, InternalDaemonEvent,
 };
-use mullvad_types::{
-    account::AccountToken,
-    device::{DeviceData, InnerDevice},
-    wireguard::WireguardData,
-};
+use mullvad_types::{account::AccountToken, wireguard::WireguardData};
 use std::time::Duration;
 use talpid_core::mpsc::Sender;
 use talpid_types::ErrorExt;
@@ -61,51 +57,56 @@ pub(crate) fn generate_device(
 
 async fn cache_from_wireguard_key(
     service: DeviceService,
-    token: AccountToken,
+    account_token: AccountToken,
     wg_data: WireguardData,
-) -> Result<DeviceData, device::Error> {
-    let devices = timeout(TIMEOUT, service.list_devices_with_backoff(token.clone()))
-        .await
-        .map_err(|_error| {
-            log::error!("Failed to enumerate devices for account: timed out");
-            device::Error::Cancelled
-        })?
-        .map_err(|error| {
-            log::error!(
-                "{}",
-                error.display_chain_with_msg("Failed to enumerate devices for account")
-            );
-            error
-        })?;
+) -> Result<PrivateAccountAndDevice, device::Error> {
+    let devices = timeout(
+        TIMEOUT,
+        service.list_devices_with_backoff(account_token.clone()),
+    )
+    .await
+    .map_err(|_error| {
+        log::error!("Failed to enumerate devices for account: timed out");
+        device::Error::Cancelled
+    })?
+    .map_err(|error| {
+        log::error!(
+            "{}",
+            error.display_chain_with_msg("Failed to enumerate devices for account")
+        );
+        error
+    })?;
 
     for device in devices.into_iter() {
         if device.pubkey == wg_data.private_key.public_key() {
-            return Ok(DeviceData {
-                token,
-                device: InnerDevice::from(device),
-                wg_data,
+            return Ok(PrivateAccountAndDevice {
+                account_token,
+                device: PrivateDevice::try_from_device(device, wg_data)?,
             });
         }
     }
     log::info!("The existing WireGuard key is not valid; generating a new device");
-    cache_from_account(service, token).await
+    cache_from_account(service, account_token).await
 }
 
 async fn cache_from_account(
     service: DeviceService,
-    token: AccountToken,
-) -> Result<DeviceData, device::Error> {
-    timeout(TIMEOUT, service.generate_for_account_with_backoff(token))
-        .await
-        .map_err(|_error| {
-            log::error!("Failed to generate new device for account: timed out");
-            device::Error::Cancelled
-        })?
-        .map_err(|error| {
-            log::error!(
-                "{}",
-                error.display_chain_with_msg("Failed to generate new device for account")
-            );
-            error
-        })
+    account_token: AccountToken,
+) -> Result<PrivateAccountAndDevice, device::Error> {
+    timeout(
+        TIMEOUT,
+        service.generate_for_account_with_backoff(account_token),
+    )
+    .await
+    .map_err(|_error| {
+        log::error!("Failed to generate new device for account: timed out");
+        device::Error::Cancelled
+    })?
+    .map_err(|error| {
+        log::error!(
+            "{}",
+            error.display_chain_with_msg("Failed to generate new device for account")
+        );
+        error
+    })
 }