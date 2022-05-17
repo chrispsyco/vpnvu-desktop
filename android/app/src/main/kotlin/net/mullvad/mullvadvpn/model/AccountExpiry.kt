wilbernight/mullvadvpn-app

Refactor account expiry
TODO:
* Implement account expiry invalidation (if still required).
* Document simplification of expiry fetching.

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/AccountExpiry.kt
@@ -1,0 +1,16 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.joda.time.DateTime
+
+sealed class AccountExpiry : Parcelable {
+    @Parcelize
+    data class Available(val expiryDateTime: DateTime) : AccountExpiry()
+    @Parcelize
+    object NotAvailable : AccountExpiry()
+
+    fun date(): DateTime? {
+        return (this as? Available)?.expiryDateTime
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/service/notifications/AccountExpiryNotification.kt
@@ -10,7 +10,7 @@ import androidx.core.app.NotificationCompat
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.model.LoginStatus
+import net.mullvad.mullvadvpn.model.AccountExpiry
 import net.mullvad.mullvadvpn.service.MullvadDaemon
 import net.mullvad.mullvadvpn.service.endpoint.AccountCache
 import net.mullvad.mullvadvpn.util.Intermittent
@@ -44,33 +44,31 @@ class AccountExpiryNotification(
         true
     )
 
-    var loginStatus by observable<LoginStatus?>(null) { _, oldValue, newValue ->
+    var accountExpiry by observable<AccountExpiry>(
+        AccountExpiry.NotAvailable
+    ) { _, oldValue, newValue ->
         if (oldValue != newValue) {
             jobTracker.newUiJob("update") { update(newValue) }
         }
     }
 
     init {
-        accountCache.onLoginStatusChange.subscribe(this) { newStatus ->
-            loginStatus = newStatus
+        accountCache.onAccountExpiryChange.subscribe(this) { expiry ->
+            accountExpiry = expiry
         }
     }
 
     fun onDestroy() {
         accountCache.onAccountNumberChange.unsubscribe(this)
-        loginStatus = null
     }
 
-    private suspend fun update(loginStatus: LoginStatus?) {
-        val remainingTime = loginStatus?.expiry?.let { expiry -> Duration(DateTime.now(), expiry) }
-        val closeToExpire = remainingTime?.isShorterThan(REMAINING_TIME_FOR_REMINDERS) ?: false
-        val accountIsNew = loginStatus?.isNewAccount ?: false
+    private suspend fun update(expiry: AccountExpiry) {
+        val expiryDate = expiry.date()
+        val durationUntilExpiry = expiryDate?.remainingTime()
 
-        if (closeToExpire && !accountIsNew) {
-            val notification = build(loginStatus!!.expiry!!, remainingTime!!)
-
+        if (durationUntilExpiry?.isCloseToExpiry() == true) {
+            val notification = build(expiryDate, durationUntilExpiry)
             channel.notificationManager.notify(NOTIFICATION_ID, notification)
-
             jobTracker.newUiJob("scheduleUpdate") { scheduleUpdate() }
         } else {
             channel.notificationManager.cancel(NOTIFICATION_ID)
@@ -78,9 +76,17 @@ class AccountExpiryNotification(
         }
     }
 
+    private fun DateTime.remainingTime(): Duration {
+        return Duration(DateTime.now(), this)
+    }
+
+    private fun Duration.isCloseToExpiry(): Boolean {
+        return isShorterThan(REMAINING_TIME_FOR_REMINDERS)
+    }
+
     private suspend fun scheduleUpdate() {
         delay(TIME_BETWEEN_CHECKS)
-        update(loginStatus)
+        update(accountExpiry)
     }
 
     private suspend fun build(expiry: DateTime, remainingTime: Duration): Notification {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/AccountCache.kt
@@ -3,18 +3,20 @@ package net.mullvad.mullvadvpn.ui.serviceconnection
 import android.os.Messenger
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.EventDispatcher
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountExpiry
 import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.LoginStatus
 import net.mullvad.talpid.util.EventNotifier
 import org.joda.time.DateTime
 
 class AccountCache(private val connection: Messenger, eventDispatcher: EventDispatcher) {
-    val onAccountExpiryChange = EventNotifier<DateTime?>(null)
     val onLoginStatusChange = EventNotifier<LoginStatus?>(null)
 
     private var loginStatus by onLoginStatusChange.notifiable()
@@ -24,6 +26,9 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
         onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
     val accountCreationEvents = _accountCreationEvents.asSharedFlow()
+
+    private val _accountExpiryState = MutableStateFlow<AccountExpiry>(AccountExpiry.NotAvailable)
+    val accountExpiryState = _accountExpiryState.asStateFlow()
 
     private val _accountHistoryEvents = MutableSharedFlow<AccountHistory>(
         extraBufferCapacity = 1,
@@ -45,7 +50,6 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
 
             registerHandler(Event.LoginStatus::class) { event ->
                 loginStatus = event.status
-                onAccountExpiryChange.notifyIfChanged(loginStatus?.expiry)
             }
 
             registerHandler(Event.AccountCreationEvent::class) { event ->
@@ -54,6 +58,10 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
 
             registerHandler(Event.LoginEvent::class) { event ->
                 _loginEvents.tryEmit(event)
+            }
+
+            registerHandler(Event.AccountExpiryEvent::class) { event ->
+                _accountExpiryState.tryEmit(event.expiry)
             }
         }
     }
@@ -85,7 +93,6 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
     }
 
     fun onDestroy() {
-        onAccountExpiryChange.unsubscribeAll()
         onLoginStatusChange.unsubscribeAll()
     }
 }

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/AccountExpiry.kt
@@ -1,0 +1,17 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.joda.time.DateTime
+
+sealed class AccountExpiry : Parcelable {
+    @Parcelize
+    data class Available(val expiryDateTime: DateTime) : AccountExpiry()
+
+    @Parcelize
+    object Missing : AccountExpiry()
+
+    fun date(): DateTime? {
+        return (this as? Available)?.expiryDateTime
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/service/notifications/AccountExpiryNotification.kt
@@ -10,7 +10,7 @@ import androidx.core.app.NotificationCompat
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.model.LoginStatus
+import net.mullvad.mullvadvpn.model.AccountExpiry
 import net.mullvad.mullvadvpn.service.MullvadDaemon
 import net.mullvad.mullvadvpn.service.endpoint.AccountCache
 import net.mullvad.mullvadvpn.util.Intermittent
@@ -44,33 +44,31 @@ class AccountExpiryNotification(
         true
     )
 
-    var loginStatus by observable<LoginStatus?>(null) { _, oldValue, newValue ->
+    var accountExpiry by observable<AccountExpiry>(
+        AccountExpiry.Missing
+    ) { _, oldValue, newValue ->
         if (oldValue != newValue) {
             jobTracker.newUiJob("update") { update(newValue) }
         }
     }
 
     init {
-        accountCache.onLoginStatusChange.subscribe(this) { newStatus ->
-            loginStatus = newStatus
+        accountCache.onAccountExpiryChange.subscribe(this) { expiry ->
+            accountExpiry = expiry
         }
     }
 
     fun onDestroy() {
         accountCache.onAccountNumberChange.unsubscribe(this)
-        loginStatus = null
     }
 
-    private suspend fun update(loginStatus: LoginStatus?) {
-        val remainingTime = loginStatus?.expiry?.let { expiry -> Duration(DateTime.now(), expiry) }
-        val closeToExpire = remainingTime?.isShorterThan(REMAINING_TIME_FOR_REMINDERS) ?: false
-        val accountIsNew = loginStatus?.isNewAccount ?: false
+    private suspend fun update(expiry: AccountExpiry) {
+        val expiryDate = expiry.date()
+        val durationUntilExpiry = expiryDate?.remainingTime()
 
-        if (closeToExpire && !accountIsNew) {
-            val notification = build(loginStatus!!.expiry!!, remainingTime!!)
-
+        if (durationUntilExpiry?.isCloseToExpiry() == true) {
+            val notification = build(expiryDate, durationUntilExpiry)
             channel.notificationManager.notify(NOTIFICATION_ID, notification)
-
             jobTracker.newUiJob("scheduleUpdate") { scheduleUpdate() }
         } else {
             channel.notificationManager.cancel(NOTIFICATION_ID)
@@ -78,9 +76,17 @@ class AccountExpiryNotification(
         }
     }
 
+    private fun DateTime.remainingTime(): Duration {
+        return Duration(DateTime.now(), this)
+    }
+
+    private fun Duration.isCloseToExpiry(): Boolean {
+        return isShorterThan(REMAINING_TIME_FOR_REMINDERS)
+    }
+
     private suspend fun scheduleUpdate() {
         delay(TIME_BETWEEN_CHECKS)
-        update(loginStatus)
+        update(accountExpiry)
     }
 
     private suspend fun build(expiry: DateTime, remainingTime: Duration): Notification {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/serviceconnection/AccountCache.kt
@@ -3,18 +3,20 @@ package net.mullvad.mullvadvpn.ui.serviceconnection
 import android.os.Messenger
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.EventDispatcher
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountExpiry
 import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.LoginStatus
 import net.mullvad.talpid.util.EventNotifier
 import org.joda.time.DateTime
 
 class AccountCache(private val connection: Messenger, eventDispatcher: EventDispatcher) {
-    val onAccountExpiryChange = EventNotifier<DateTime?>(null)
     val onLoginStatusChange = EventNotifier<LoginStatus?>(null)
 
     private var loginStatus by onLoginStatusChange.notifiable()
@@ -24,6 +26,9 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
         onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
     val accountCreationEvents = _accountCreationEvents.asSharedFlow()
+
+    private val _accountExpiryState = MutableStateFlow<AccountExpiry>(AccountExpiry.Missing)
+    val accountExpiryState = _accountExpiryState.asStateFlow()
 
     private val _accountHistoryEvents = MutableSharedFlow<AccountHistory>(
         extraBufferCapacity = 1,
@@ -45,7 +50,6 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
 
             registerHandler(Event.LoginStatus::class) { event ->
                 loginStatus = event.status
-                onAccountExpiryChange.notifyIfChanged(loginStatus?.expiry)
             }
 
             registerHandler(Event.AccountCreationEvent::class) { event ->
@@ -54,6 +58,10 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
 
             registerHandler(Event.LoginEvent::class) { event ->
                 _loginEvents.tryEmit(event)
+            }
+
+            registerHandler(Event.AccountExpiryEvent::class) { event ->
+                _accountExpiryState.tryEmit(event.expiry)
             }
         }
     }
@@ -89,7 +97,6 @@ class AccountCache(private val connection: Messenger, eventDispatcher: EventDisp
     }
 
     fun onDestroy() {
-        onAccountExpiryChange.unsubscribeAll()
         onLoginStatusChange.unsubscribeAll()
     }
 }