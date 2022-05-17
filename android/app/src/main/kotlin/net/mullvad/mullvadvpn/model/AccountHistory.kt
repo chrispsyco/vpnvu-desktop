wilbernight/mullvadvpn-app

Refactor Android account history
TODO:
* Fetch account history on device changes.

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/AccountHistory.kt
@@ -1,0 +1,11 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+sealed class AccountHistory : Parcelable {
+    @Parcelize
+    data class WithHistory(val accountToken: String) : AccountHistory()
+    @Parcelize
+    object WithoutHistory : AccountHistory()
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/AccountCache.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.GetAccountDataResult
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.model.LoginStatus
@@ -44,7 +45,7 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
 
     val onAccountNumberChange = EventNotifier<String?>(null)
     val onAccountExpiryChange = EventNotifier<DateTime?>(null)
-    val onAccountHistoryChange = EventNotifier<String?>(null)
+    val onAccountHistoryChange = EventNotifier<AccountHistory>(AccountHistory.WithoutHistory)
     val onLoginStatusChange = EventNotifier<LoginStatus?>(null)
 
     var newlyCreatedAccount = false
@@ -68,7 +69,7 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
         }
 
         onAccountHistoryChange.subscribe(this) { history ->
-            endpoint.sendEvent(Event.AccountHistory(history))
+            endpoint.sendEvent(Event.AccountHistoryEvent(history))
         }
 
         onLoginStatusChange.subscribe(this) { status ->
@@ -224,7 +225,11 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
     private fun fetchAccountHistory() {
         jobTracker.newBackgroundJob("fetchHistory") {
             daemon.await().getAccountHistory().let { history ->
-                accountHistory = history
+                accountHistory = if (history != null) {
+                    AccountHistory.WithHistory(history)
+                } else {
+                    AccountHistory.WithoutHistory
+                }
             }
         }
     }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/LoginFragment.kt
@@ -16,6 +16,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 import net.mullvad.mullvadvpn.ui.widget.AccountLogin
 import net.mullvad.mullvadvpn.ui.widget.HeaderBar
@@ -120,6 +121,7 @@ class LoginFragment :
                 launch {
                     loginViewModel.accountHistory.collect { history ->
                         accountLogin.accountHistory = history
+                            .let { it as? AccountHistory.WithHistory }?.accountToken
                     }
                 }
                 launch {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/LoginViewModel.kt
@@ -8,9 +8,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.ui.serviceconnection.AccountCache
 
@@ -18,9 +20,10 @@ class LoginViewModel(
     application: Application
 ) : AndroidViewModel(application) {
     private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Default)
-    private val _accountHistory = MutableStateFlow<String?>(null)
     val uiState: StateFlow<LoginUiState> = _uiState
-    val accountHistory: StateFlow<String?> = _accountHistory
+
+    private val _accountHistory = MutableStateFlow<AccountHistory>(AccountHistory.WithoutHistory)
+    val accountHistory: StateFlow<AccountHistory> = _accountHistory
 
     private var accountCache: AccountCache? = null
 
@@ -42,8 +45,15 @@ class LoginViewModel(
     // Ensures the view model has an up-to-date instance of account cache. This is an intermediate
     // solution due to limitations in the current app architecture.
     fun updateAccountCacheInstance(newAccountCache: AccountCache?) {
-        accountCache?.unsubscribe()
-        accountCache = newAccountCache?.apply { subscribe() }
+        accountCache?.onLoginStatusChange?.unsubscribe(this)
+
+        accountCache = newAccountCache?.apply {
+            viewModelScope.launch {
+                accountHistoryEvents.collect {
+                    _accountHistory.value = it
+                }
+            }
+        }
     }
 
     fun clearAccountHistory() {
@@ -76,13 +86,7 @@ class LoginViewModel(
 
     @RestrictTo(RestrictTo.Scope.TESTS)
     public override fun onCleared() {
-        accountCache?.unsubscribe()
-    }
-
-    private fun AccountCache.subscribe() {
-        onAccountHistoryChange.subscribe(this) { history ->
-            _accountHistory.value = history
-        }
+        accountCache?.onLoginStatusChange?.unsubscribe(this)
     }
 
     private fun AccountCreationResult.mapToUiState(): LoginUiState {
@@ -99,11 +103,6 @@ class LoginViewModel(
             LoginResult.MaxDevicesReached -> LoginUiState.TooManyDevicesError
             else -> LoginUiState.OtherError(errorMessage = this.toString())
         }
-    }
-
-    private fun AccountCache.unsubscribe() {
-        onAccountHistoryChange.unsubscribe(this)
-        onLoginStatusChange.unsubscribe(this)
     }
 
     class Factory(val application: Application) :

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/AccountHistory.kt
@@ -1,0 +1,12 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+sealed class AccountHistory : Parcelable {
+    @Parcelize
+    data class Available(val accountToken: String) : AccountHistory()
+
+    @Parcelize
+    object Missing : AccountHistory()
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/service/endpoint/AccountCache.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.delay
 import net.mullvad.mullvadvpn.ipc.Event
 import net.mullvad.mullvadvpn.ipc.Request
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.GetAccountDataResult
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.model.LoginStatus
@@ -44,7 +45,7 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
 
     val onAccountNumberChange = EventNotifier<String?>(null)
     val onAccountExpiryChange = EventNotifier<DateTime?>(null)
-    val onAccountHistoryChange = EventNotifier<String?>(null)
+    val onAccountHistoryChange = EventNotifier<AccountHistory>(AccountHistory.Missing)
     val onLoginStatusChange = EventNotifier<LoginStatus?>(null)
 
     var newlyCreatedAccount = false
@@ -68,7 +69,7 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
         }
 
         onAccountHistoryChange.subscribe(this) { history ->
-            endpoint.sendEvent(Event.AccountHistory(history))
+            endpoint.sendEvent(Event.AccountHistoryEvent(history))
         }
 
         onLoginStatusChange.subscribe(this) { status ->
@@ -224,7 +225,11 @@ class AccountCache(private val endpoint: ServiceEndpoint) {
     private fun fetchAccountHistory() {
         jobTracker.newBackgroundJob("fetchHistory") {
             daemon.await().getAccountHistory().let { history ->
-                accountHistory = history
+                accountHistory = if (history != null) {
+                    AccountHistory.Available(history)
+                } else {
+                    AccountHistory.Missing
+                }
             }
         }
     }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/ui/LoginFragment.kt
@@ -16,6 +16,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnection
 import net.mullvad.mullvadvpn.ui.widget.AccountLogin
 import net.mullvad.mullvadvpn.ui.widget.HeaderBar
@@ -120,6 +121,7 @@ class LoginFragment :
                 launch {
                     loginViewModel.accountHistory.collect { history ->
                         accountLogin.accountHistory = history
+                            .let { it as? AccountHistory.Available }?.accountToken
                     }
                 }
                 launch {

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/LoginViewModel.kt
@@ -8,9 +8,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.model.AccountCreationResult
+import net.mullvad.mullvadvpn.model.AccountHistory
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.ui.serviceconnection.AccountCache
 
@@ -18,9 +20,10 @@ class LoginViewModel(
     application: Application
 ) : AndroidViewModel(application) {
     private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Default)
-    private val _accountHistory = MutableStateFlow<String?>(null)
     val uiState: StateFlow<LoginUiState> = _uiState
-    val accountHistory: StateFlow<String?> = _accountHistory
+
+    private val _accountHistory = MutableStateFlow<AccountHistory>(AccountHistory.Missing)
+    val accountHistory: StateFlow<AccountHistory> = _accountHistory
 
     private var accountCache: AccountCache? = null
 
@@ -42,8 +45,15 @@ class LoginViewModel(
     // Ensures the view model has an up-to-date instance of account cache. This is an intermediate
     // solution due to limitations in the current app architecture.
     fun updateAccountCacheInstance(newAccountCache: AccountCache?) {
-        accountCache?.unsubscribe()
-        accountCache = newAccountCache?.apply { subscribe() }
+        accountCache?.onLoginStatusChange?.unsubscribe(this)
+
+        accountCache = newAccountCache?.apply {
+            viewModelScope.launch {
+                accountHistoryEvents.collect {
+                    _accountHistory.value = it
+                }
+            }
+        }
     }
 
     fun clearAccountHistory() {
@@ -76,13 +86,7 @@ class LoginViewModel(
 
     @RestrictTo(RestrictTo.Scope.TESTS)
     public override fun onCleared() {
-        accountCache?.unsubscribe()
-    }
-
-    private fun AccountCache.subscribe() {
-        onAccountHistoryChange.subscribe(this) { history ->
-            _accountHistory.value = history
-        }
+        accountCache?.onLoginStatusChange?.unsubscribe(this)
     }
 
     private fun AccountCreationResult.mapToUiState(): LoginUiState {
@@ -99,11 +103,6 @@ class LoginViewModel(
             LoginResult.MaxDevicesReached -> LoginUiState.TooManyDevicesError
             else -> LoginUiState.OtherError(errorMessage = this.toString())
         }
-    }
-
-    private fun AccountCache.unsubscribe() {
-        onAccountHistoryChange.unsubscribe(this)
-        onLoginStatusChange.unsubscribe(this)
     }
 
     class Factory(val application: Application) :