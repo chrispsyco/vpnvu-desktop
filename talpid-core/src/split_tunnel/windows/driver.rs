mullvad/mullvadvpn-app

Replace winapi with windows-sys in talpid-core

talpid-core/src/split_tunnel/windows/driver.rs
@@ -23,26 +23,22 @@ use std::{
     time::Duration,
 };
 use talpid_types::ErrorExt;
-use winapi::{
-    shared::{
-        in6addr::IN6_ADDR,
-        inaddr::IN_ADDR,
-        minwindef::{FALSE, TRUE},
-        ntdef::NTSTATUS,
-        winerror::{
-            ERROR_ACCESS_DENIED, ERROR_FILE_NOT_FOUND, ERROR_INVALID_PARAMETER, ERROR_IO_PENDING,
-        },
+use windows_sys::Win32::{
+    Foundation::{
+        ERROR_ACCESS_DENIED, ERROR_FILE_NOT_FOUND, ERROR_INVALID_PARAMETER, ERROR_IO_PENDING,
+        HANDLE, NTSTATUS, WAIT_FAILED,
     },
-    um::{
-        ioapiset::{DeviceIoControl, GetOverlappedResult},
-        minwinbase::OVERLAPPED,
-        synchapi::{WaitForMultipleObjects, WaitForSingleObject},
-        tlhelp32::TH32CS_SNAPPROCESS,
-        winbase::{
-            FILE_FLAG_OVERLAPPED, INFINITE, WAIT_ABANDONED, WAIT_ABANDONED_0, WAIT_FAILED,
+    Networking::WinSock::{IN6_ADDR, IN_ADDR},
+    Storage::FileSystem::FILE_FLAG_OVERLAPPED,
+    System::{
+        Diagnostics::ToolHelp::TH32CS_SNAPPROCESS,
+        Ioctl::{FILE_ANY_ACCESS, METHOD_BUFFERED, METHOD_NEITHER},
+        Threading::{
+            WaitForMultipleObjects, WaitForSingleObject, WAIT_ABANDONED, WAIT_ABANDONED_0,
             WAIT_OBJECT_0,
         },
-        winioctl::{FILE_ANY_ACCESS, METHOD_BUFFERED, METHOD_NEITHER},
+        WindowsProgramming::INFINITE,
+        IO::{DeviceIoControl, GetOverlappedResult, OVERLAPPED},
     },
 };
 
@@ -842,7 +838,7 @@ pub unsafe fn device_io_control_buffer_async(
     let input_len = input.map(|input| input.len()).unwrap_or(0);
 
     let result = DeviceIoControl(
-        device.as_raw_handle(),
+        device.as_raw_handle() as isize,
         ioctl_code,
         input_ptr,
         u32::try_from(input_len).map_err(|_error| {
@@ -883,16 +879,16 @@ pub fn get_overlapped_result(
     let event = overlapped.get_event().unwrap();
 
     // SAFETY: This is a valid event object.
-    unsafe { wait_for_single_object(event.as_raw_handle(), None) }?;
+    unsafe { wait_for_single_object(event.as_handle(), None) }?;
 
     // SAFETY: The handle and overlapped object are valid.
     let mut returned_bytes = 0u32;
     let result = unsafe {
         GetOverlappedResult(
-            device.as_raw_handle(),
+            device.as_raw_handle() as isize,
             overlapped.as_mut_ptr(),
             &mut returned_bytes,
-            FALSE,
+            0,
         )
     };
     if result == 0 {
@@ -906,10 +902,7 @@ pub fn get_overlapped_result(
 /// # Safety
 ///
 /// * `object` must be a valid object that can be signaled, such as an event object.
-pub unsafe fn wait_for_single_object(
-    object: RawHandle,
-    timeout: Option<Duration>,
-) -> io::Result<()> {
+pub unsafe fn wait_for_single_object(object: HANDLE, timeout: Option<Duration>) -> io::Result<()> {
     let timeout = match timeout {
         Some(timeout) => u32::try_from(timeout.as_millis()).map_err(|_error| {
             io::Error::new(io::ErrorKind::InvalidInput, "the duration is too long")
@@ -931,16 +924,13 @@ pub unsafe fn wait_for_single_object(
 /// # Safety
 ///
 /// * `objects` must be a slice of valid objects that can be signaled, such as event objects.
-pub unsafe fn wait_for_multiple_objects(
-    objects: &[RawHandle],
-    wait_all: bool,
-) -> io::Result<RawHandle> {
+pub unsafe fn wait_for_multiple_objects(objects: &[HANDLE], wait_all: bool) -> io::Result<HANDLE> {
     let objects_len = u32::try_from(objects.len())
         .map_err(|_error| io::Error::new(io::ErrorKind::InvalidInput, "too many objects"))?;
     let result = WaitForMultipleObjects(
         objects_len,
         objects.as_ptr(),
-        if wait_all { TRUE } else { FALSE },
+        if wait_all { 1 } else { 0 },
         INFINITE,
     );
     let signaled_index = if result >= WAIT_OBJECT_0 && result < WAIT_OBJECT_0 + objects_len {

talpid-core/src/split_tunnel/windows/path_monitor.rs
@@ -13,34 +13,25 @@ use std::{
     sync::{mpsc as sync_mpsc, Arc},
     time::{Duration, Instant},
 };
-use winapi::{
-    self,
-    shared::{
-        minwindef::TRUE,
-        winerror::{
-            ERROR_NOT_FOUND, ERROR_OPERATION_ABORTED, ERROR_PATH_NOT_FOUND,
-            ERROR_UNRECOGNIZED_VOLUME,
-        },
+use windows_sys::Win32::{
+    Foundation::{
+        CloseHandle, ERROR_NOT_FOUND, ERROR_OPERATION_ABORTED, ERROR_PATH_NOT_FOUND,
+        ERROR_UNRECOGNIZED_VOLUME, HANDLE, INVALID_HANDLE_VALUE,
     },
-    um::{
-        fileapi::{GetFileAttributesW, GetFullPathNameW},
-        handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
-        ioapiset::{
+    Globalization::CompareStringOrdinal,
+    Storage::FileSystem::{
+        GetFileAttributesW, GetFullPathNameW, ReadDirectoryChangesW, FILE_ATTRIBUTE_REPARSE_POINT,
+        FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT, FILE_FLAG_OVERLAPPED,
+        FILE_NOTIFY_CHANGE_ATTRIBUTES, FILE_NOTIFY_CHANGE_DIR_NAME, FILE_NOTIFY_CHANGE_FILE_NAME,
+        FILE_NOTIFY_INFORMATION,
+    },
+    System::{
+        Ioctl::FSCTL_GET_REPARSE_POINT,
+        SystemServices::{IO_REPARSE_TAG_MOUNT_POINT, IO_REPARSE_TAG_SYMLINK},
+        WindowsProgramming::INFINITE,
+        IO::{
             CancelIoEx, CreateIoCompletionPort, DeviceIoControl, GetQueuedCompletionStatus,
-            PostQueuedCompletionStatus,
-        },
-        minwinbase::OVERLAPPED,
-        stringapiset::CompareStringOrdinal,
-        winbase::{
-            ReadDirectoryChangesW, FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT,
-            FILE_FLAG_OVERLAPPED, INFINITE,
-        },
-        winioctl::FSCTL_GET_REPARSE_POINT,
-        winnt::{
-            FILE_ATTRIBUTE_REPARSE_POINT, FILE_NOTIFY_CHANGE_ATTRIBUTES,
-            FILE_NOTIFY_CHANGE_DIR_NAME, FILE_NOTIFY_CHANGE_FILE_NAME, FILE_NOTIFY_INFORMATION,
-            HANDLE, IO_REPARSE_TAG_MOUNT_POINT, IO_REPARSE_TAG_SYMLINK,
-            MAXIMUM_REPARSE_DATA_BUFFER_SIZE,
+            PostQueuedCompletionStatus, OVERLAPPED,
         },
     },
 };
@@ -52,6 +43,7 @@ const PATH_MONITOR_COMPLETION_KEY_IGNORE: usize = usize::MAX;
 const CSTR_EQUAL: i32 = 2;
 
 const ANYSIZE_ARRAY: usize = 1;
+const MAXIMUM_REPARSE_DATA_BUFFER_SIZE: u32 = 16384;
 const SYMLINK_FLAG_RELATIVE: u32 = 0x00000001;
 
 // See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/c3a420cb-8a72-4adf-87e8-eee95379d78f.
@@ -155,7 +147,7 @@ fn resolve_link<T: AsRef<Path> + Copy>(path: T) -> io::Result<Option<PathBuf>> {
 
     if unsafe {
         DeviceIoControl(
-            file.as_raw_handle() as *mut _,
+            file.as_raw_handle() as isize,
             FSCTL_GET_REPARSE_POINT,
             ptr::null_mut(),
             0u32,
@@ -265,15 +257,15 @@ impl DirContext {
 
         let handle = unsafe {
             CreateIoCompletionPort(
-                dir_handle.as_raw_handle() as *mut _,
-                io_completion_port.as_raw_handle() as *mut _,
+                dir_handle.as_raw_handle() as isize,
+                io_completion_port.as_raw_handle() as isize,
                 completion_key,
                 // num of threads is ignored here
                 0,
             )
         };
 
-        if handle == ptr::null_mut() {
+        if handle == 0 {
             return Err(io::Error::last_os_error());
         }
 
@@ -290,10 +282,10 @@ impl DirContext {
         let mut _bytes_returned = 0;
         if unsafe {
             ReadDirectoryChangesW(
-                self.dir_handle.as_raw_handle() as *mut _,
+                self.dir_handle.as_raw_handle() as isize,
                 self.buffer.as_mut_ptr() as *mut _,
                 self.buffer.len() as u32,
-                TRUE,
+                1,
                 FILE_NOTIFY_CHANGE_FILE_NAME
                     | FILE_NOTIFY_CHANGE_DIR_NAME
                     | FILE_NOTIFY_CHANGE_ATTRIBUTES,
@@ -314,7 +306,7 @@ impl DirContext {
 
     /// Try to cancel a request. On success, return whether a request was cancelled.
     fn cancel_io(&mut self) -> io::Result<bool> {
-        if unsafe { CancelIoEx(self.dir_handle.as_raw_handle(), ptr::null_mut()) } == 0 {
+        if unsafe { CancelIoEx(self.dir_handle.as_raw_handle() as isize, ptr::null_mut()) } == 0 {
             match io::Error::last_os_error() {
                 _error if _error.raw_os_error() == Some(ERROR_NOT_FOUND as i32) => Ok(false),
                 error => Err(error),
@@ -349,10 +341,9 @@ struct CompletionPort {
 impl CompletionPort {
     // `concurrent_threads`: 0 ==> number of processors
     fn create(concurrent_threads: u32) -> io::Result<Self> {
-        let handle = unsafe {
-            CreateIoCompletionPort(INVALID_HANDLE_VALUE, ptr::null_mut(), 0, concurrent_threads)
-        };
-        if handle == ptr::null_mut() {
+        let handle =
+            unsafe { CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, concurrent_threads) };
+        if handle == 0 {
             return Err(io::Error::last_os_error());
         }
         Ok(CompletionPort { handle })
@@ -731,7 +722,7 @@ impl PathMonitor {
                         file_name.len() as i32,
                         file_name.as_ptr(),
                         file_name.len() as i32,
-                        TRUE,
+                        1,
                     )
                 };
                 match cmp_status {

talpid-core/src/windows/window.rs
@@ -2,26 +2,19 @@
 
 use std::{os::windows::io::AsRawHandle, ptr, sync::Arc, thread};
 use tokio::sync::broadcast;
-use winapi::{
-    shared::{
-        basetsd::LONG_PTR,
-        minwindef::{LPARAM, LRESULT, UINT, WPARAM},
-        windef::HWND,
-    },
-    um::{
-        libloaderapi::GetModuleHandleW,
-        processthreadsapi::GetThreadId,
-        winuser::{
-            CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, GetMessageW,
-            GetWindowLongPtrW, PostQuitMessage, PostThreadMessageW, SetWindowLongPtrW,
-            TranslateMessage, GWLP_USERDATA, GWLP_WNDPROC, PBT_APMRESUMEAUTOMATIC,
-            PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WM_DESTROY, WM_POWERBROADCAST, WM_USER,
-        },
+use windows_sys::Win32::{
+    Foundation::{HWND, LPARAM, LRESULT, WPARAM},
+    System::{LibraryLoader::GetModuleHandleW, Threading::GetThreadId},
+    UI::WindowsAndMessaging::{
+        CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, GetMessageW,
+        GetWindowLongPtrW, PostQuitMessage, PostThreadMessageW, SetWindowLongPtrW,
+        TranslateMessage, GWLP_USERDATA, GWLP_WNDPROC, PBT_APMRESUMEAUTOMATIC,
+        PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WM_DESTROY, WM_POWERBROADCAST, WM_USER,
     },
 };
 
 const CLASS_NAME: &[u8] = b"S\0T\0A\0T\0I\0C\0\0\0";
-const REQUEST_THREAD_SHUTDOWN: UINT = WM_USER + 1;
+const REQUEST_THREAD_SHUTDOWN: u32 = WM_USER + 1;
 
 /// Handle for closing an associated window.
 /// The window is not destroyed when this is dropped.
@@ -33,7 +26,7 @@ impl WindowCloseHandle {
     /// Close the window and wait for the thread.
     pub fn close(&mut self) {
         if let Some(thread) = self.thread.take() {
-            let thread_id = unsafe { GetThreadId(thread.as_raw_handle()) };
+            let thread_id = unsafe { GetThreadId(thread.as_raw_handle() as isize) };
             unsafe { PostThreadMessageW(thread_id, REQUEST_THREAD_SHUTDOWN, 0, 0) };
             let _ = thread.join();
         }
@@ -41,7 +34,7 @@ impl WindowCloseHandle {
 }
 
 /// Creates a dummy window whose messages are handled by `wnd_proc`.
-pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Send + 'static>(
+pub fn create_hidden_window<F: (Fn(HWND, u32, WPARAM, LPARAM) -> LRESULT) + Send + 'static>(
     wnd_proc: F,
 ) -> WindowCloseHandle {
     let join_handle = thread::spawn(move || {
@@ -55,8 +48,8 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
                 0,
                 0,
                 0,
-                ptr::null_mut(),
-                ptr::null_mut(),
+                0,
+                0,
                 GetModuleHandleW(ptr::null_mut()),
                 ptr::null_mut(),
             )
@@ -67,18 +60,14 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
         let raw_callback = Box::into_raw(Box::new(wnd_proc));
 
         unsafe {
-            SetWindowLongPtrW(dummy_window, GWLP_USERDATA, raw_callback as LONG_PTR);
-            SetWindowLongPtrW(
-                dummy_window,
-                GWLP_WNDPROC,
-                window_procedure::<F> as LONG_PTR,
-            );
+            SetWindowLongPtrW(dummy_window, GWLP_USERDATA, raw_callback as isize);
+            SetWindowLongPtrW(dummy_window, GWLP_WNDPROC, window_procedure::<F> as isize);
         }
 
         let mut msg = unsafe { std::mem::zeroed() };
 
         loop {
-            let status = unsafe { GetMessageW(&mut msg, ptr::null_mut(), 0, 0) };
+            let status = unsafe { GetMessageW(&mut msg, 0, 0, 0) };
 
             if status < 0 {
                 continue;
@@ -87,7 +76,7 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
                 break;
             }
 
-            if msg.hwnd.is_null() {
+            if msg.hwnd == 0 {
                 if msg.message == REQUEST_THREAD_SHUTDOWN {
                     unsafe { DestroyWindow(dummy_window) };
                 }
@@ -110,12 +99,12 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
 
 unsafe extern "system" fn window_procedure<F>(
     window: HWND,
-    message: UINT,
+    message: u32,
     wparam: WPARAM,
     lparam: LPARAM,
 ) -> LRESULT
 where
-    F: Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT,
+    F: Fn(HWND, u32, WPARAM, LPARAM) -> LRESULT,
 {
     if message == WM_DESTROY {
         PostQuitMessage(0);
@@ -146,7 +135,7 @@ pub enum PowerManagementEvent {
 impl PowerManagementEvent {
     fn try_from_winevent(wparam: usize) -> Option<Self> {
         use PowerManagementEvent::*;
-        match wparam {
+        match wparam as u32 {
             PBT_APMRESUMEAUTOMATIC => Some(ResumeAutomatic),
             PBT_APMRESUMESUSPEND => Some(ResumeSuspend),
             PBT_APMSUSPEND => Some(Suspend),

chrispsyco/vpnvu-desktop

talpid-core/src/split_tunnel/windows/driver.rs
@@ -23,26 +23,22 @@ use std::{
     time::Duration,
 };
 use talpid_types::ErrorExt;
-use winapi::{
-    shared::{
-        in6addr::IN6_ADDR,
-        inaddr::IN_ADDR,
-        minwindef::{FALSE, TRUE},
-        ntdef::NTSTATUS,
-        winerror::{
-            ERROR_ACCESS_DENIED, ERROR_FILE_NOT_FOUND, ERROR_INVALID_PARAMETER, ERROR_IO_PENDING,
-        },
+use windows_sys::Win32::{
+    Foundation::{
+        ERROR_ACCESS_DENIED, ERROR_FILE_NOT_FOUND, ERROR_INVALID_PARAMETER, ERROR_IO_PENDING,
+        HANDLE, NTSTATUS, WAIT_FAILED,
     },
-    um::{
-        ioapiset::{DeviceIoControl, GetOverlappedResult},
-        minwinbase::OVERLAPPED,
-        synchapi::{WaitForMultipleObjects, WaitForSingleObject},
-        tlhelp32::TH32CS_SNAPPROCESS,
-        winbase::{
-            FILE_FLAG_OVERLAPPED, INFINITE, WAIT_ABANDONED, WAIT_ABANDONED_0, WAIT_FAILED,
+    Networking::WinSock::{IN6_ADDR, IN_ADDR},
+    Storage::FileSystem::FILE_FLAG_OVERLAPPED,
+    System::{
+        Diagnostics::ToolHelp::TH32CS_SNAPPROCESS,
+        Ioctl::{FILE_ANY_ACCESS, METHOD_BUFFERED, METHOD_NEITHER},
+        Threading::{
+            WaitForMultipleObjects, WaitForSingleObject, WAIT_ABANDONED, WAIT_ABANDONED_0,
             WAIT_OBJECT_0,
         },
-        winioctl::{FILE_ANY_ACCESS, METHOD_BUFFERED, METHOD_NEITHER},
+        WindowsProgramming::INFINITE,
+        IO::{DeviceIoControl, GetOverlappedResult, OVERLAPPED},
     },
 };
 
@@ -842,7 +838,7 @@ pub unsafe fn device_io_control_buffer_async(
     let input_len = input.map(|input| input.len()).unwrap_or(0);
 
     let result = DeviceIoControl(
-        device.as_raw_handle(),
+        device.as_raw_handle() as HANDLE,
         ioctl_code,
         input_ptr,
         u32::try_from(input_len).map_err(|_error| {
@@ -883,16 +879,16 @@ pub fn get_overlapped_result(
     let event = overlapped.get_event().unwrap();
 
     // SAFETY: This is a valid event object.
-    unsafe { wait_for_single_object(event.as_raw_handle(), None) }?;
+    unsafe { wait_for_single_object(event.as_handle(), None) }?;
 
     // SAFETY: The handle and overlapped object are valid.
     let mut returned_bytes = 0u32;
     let result = unsafe {
         GetOverlappedResult(
-            device.as_raw_handle(),
+            device.as_raw_handle() as HANDLE,
             overlapped.as_mut_ptr(),
             &mut returned_bytes,
-            FALSE,
+            0,
         )
     };
     if result == 0 {
@@ -906,10 +902,7 @@ pub fn get_overlapped_result(
 /// # Safety
 ///
 /// * `object` must be a valid object that can be signaled, such as an event object.
-pub unsafe fn wait_for_single_object(
-    object: RawHandle,
-    timeout: Option<Duration>,
-) -> io::Result<()> {
+pub unsafe fn wait_for_single_object(object: HANDLE, timeout: Option<Duration>) -> io::Result<()> {
     let timeout = match timeout {
         Some(timeout) => u32::try_from(timeout.as_millis()).map_err(|_error| {
             io::Error::new(io::ErrorKind::InvalidInput, "the duration is too long")
@@ -931,16 +924,13 @@ pub unsafe fn wait_for_single_object(
 /// # Safety
 ///
 /// * `objects` must be a slice of valid objects that can be signaled, such as event objects.
-pub unsafe fn wait_for_multiple_objects(
-    objects: &[RawHandle],
-    wait_all: bool,
-) -> io::Result<RawHandle> {
+pub unsafe fn wait_for_multiple_objects(objects: &[HANDLE], wait_all: bool) -> io::Result<HANDLE> {
     let objects_len = u32::try_from(objects.len())
         .map_err(|_error| io::Error::new(io::ErrorKind::InvalidInput, "too many objects"))?;
     let result = WaitForMultipleObjects(
         objects_len,
         objects.as_ptr(),
-        if wait_all { TRUE } else { FALSE },
+        if wait_all { 1 } else { 0 },
         INFINITE,
     );
     let signaled_index = if result >= WAIT_OBJECT_0 && result < WAIT_OBJECT_0 + objects_len {

talpid-core/src/split_tunnel/windows/path_monitor.rs
@@ -13,34 +13,25 @@ use std::{
     sync::{mpsc as sync_mpsc, Arc},
     time::{Duration, Instant},
 };
-use winapi::{
-    self,
-    shared::{
-        minwindef::TRUE,
-        winerror::{
-            ERROR_NOT_FOUND, ERROR_OPERATION_ABORTED, ERROR_PATH_NOT_FOUND,
-            ERROR_UNRECOGNIZED_VOLUME,
-        },
+use windows_sys::Win32::{
+    Foundation::{
+        CloseHandle, ERROR_NOT_FOUND, ERROR_OPERATION_ABORTED, ERROR_PATH_NOT_FOUND,
+        ERROR_UNRECOGNIZED_VOLUME, HANDLE, INVALID_HANDLE_VALUE,
     },
-    um::{
-        fileapi::{GetFileAttributesW, GetFullPathNameW},
-        handleapi::{CloseHandle, INVALID_HANDLE_VALUE},
-        ioapiset::{
+    Globalization::CompareStringOrdinal,
+    Storage::FileSystem::{
+        GetFileAttributesW, GetFullPathNameW, ReadDirectoryChangesW, FILE_ATTRIBUTE_REPARSE_POINT,
+        FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT, FILE_FLAG_OVERLAPPED,
+        FILE_NOTIFY_CHANGE_ATTRIBUTES, FILE_NOTIFY_CHANGE_DIR_NAME, FILE_NOTIFY_CHANGE_FILE_NAME,
+        FILE_NOTIFY_INFORMATION,
+    },
+    System::{
+        Ioctl::FSCTL_GET_REPARSE_POINT,
+        SystemServices::{IO_REPARSE_TAG_MOUNT_POINT, IO_REPARSE_TAG_SYMLINK},
+        WindowsProgramming::INFINITE,
+        IO::{
             CancelIoEx, CreateIoCompletionPort, DeviceIoControl, GetQueuedCompletionStatus,
-            PostQueuedCompletionStatus,
-        },
-        minwinbase::OVERLAPPED,
-        stringapiset::CompareStringOrdinal,
-        winbase::{
-            ReadDirectoryChangesW, FILE_FLAG_BACKUP_SEMANTICS, FILE_FLAG_OPEN_REPARSE_POINT,
-            FILE_FLAG_OVERLAPPED, INFINITE,
-        },
-        winioctl::FSCTL_GET_REPARSE_POINT,
-        winnt::{
-            FILE_ATTRIBUTE_REPARSE_POINT, FILE_NOTIFY_CHANGE_ATTRIBUTES,
-            FILE_NOTIFY_CHANGE_DIR_NAME, FILE_NOTIFY_CHANGE_FILE_NAME, FILE_NOTIFY_INFORMATION,
-            HANDLE, IO_REPARSE_TAG_MOUNT_POINT, IO_REPARSE_TAG_SYMLINK,
-            MAXIMUM_REPARSE_DATA_BUFFER_SIZE,
+            PostQueuedCompletionStatus, OVERLAPPED,
         },
     },
 };
@@ -52,6 +43,7 @@ const PATH_MONITOR_COMPLETION_KEY_IGNORE: usize = usize::MAX;
 const CSTR_EQUAL: i32 = 2;
 
 const ANYSIZE_ARRAY: usize = 1;
+const MAXIMUM_REPARSE_DATA_BUFFER_SIZE: u32 = 16384;
 const SYMLINK_FLAG_RELATIVE: u32 = 0x00000001;
 
 // See https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-fscc/c3a420cb-8a72-4adf-87e8-eee95379d78f.
@@ -155,7 +147,7 @@ fn resolve_link<T: AsRef<Path> + Copy>(path: T) -> io::Result<Option<PathBuf>> {
 
     if unsafe {
         DeviceIoControl(
-            file.as_raw_handle() as *mut _,
+            file.as_raw_handle() as HANDLE,
             FSCTL_GET_REPARSE_POINT,
             ptr::null_mut(),
             0u32,
@@ -265,15 +257,15 @@ impl DirContext {
 
         let handle = unsafe {
             CreateIoCompletionPort(
-                dir_handle.as_raw_handle() as *mut _,
-                io_completion_port.as_raw_handle() as *mut _,
+                dir_handle.as_raw_handle() as HANDLE,
+                io_completion_port.as_raw_handle() as HANDLE,
                 completion_key,
                 // num of threads is ignored here
                 0,
             )
         };
 
-        if handle == ptr::null_mut() {
+        if handle == 0 {
             return Err(io::Error::last_os_error());
         }
 
@@ -290,10 +282,10 @@ impl DirContext {
         let mut _bytes_returned = 0;
         if unsafe {
             ReadDirectoryChangesW(
-                self.dir_handle.as_raw_handle() as *mut _,
+                self.dir_handle.as_raw_handle() as HANDLE,
                 self.buffer.as_mut_ptr() as *mut _,
                 self.buffer.len() as u32,
-                TRUE,
+                1,
                 FILE_NOTIFY_CHANGE_FILE_NAME
                     | FILE_NOTIFY_CHANGE_DIR_NAME
                     | FILE_NOTIFY_CHANGE_ATTRIBUTES,
@@ -314,7 +306,7 @@ impl DirContext {
 
     /// Try to cancel a request. On success, return whether a request was cancelled.
     fn cancel_io(&mut self) -> io::Result<bool> {
-        if unsafe { CancelIoEx(self.dir_handle.as_raw_handle(), ptr::null_mut()) } == 0 {
+        if unsafe { CancelIoEx(self.dir_handle.as_raw_handle() as HANDLE, ptr::null_mut()) } == 0 {
             match io::Error::last_os_error() {
                 _error if _error.raw_os_error() == Some(ERROR_NOT_FOUND as i32) => Ok(false),
                 error => Err(error),
@@ -349,10 +341,9 @@ struct CompletionPort {
 impl CompletionPort {
     // `concurrent_threads`: 0 ==> number of processors
     fn create(concurrent_threads: u32) -> io::Result<Self> {
-        let handle = unsafe {
-            CreateIoCompletionPort(INVALID_HANDLE_VALUE, ptr::null_mut(), 0, concurrent_threads)
-        };
-        if handle == ptr::null_mut() {
+        let handle =
+            unsafe { CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, concurrent_threads) };
+        if handle == 0 {
             return Err(io::Error::last_os_error());
         }
         Ok(CompletionPort { handle })
@@ -731,7 +722,7 @@ impl PathMonitor {
                         file_name.len() as i32,
                         file_name.as_ptr(),
                         file_name.len() as i32,
-                        TRUE,
+                        1,
                     )
                 };
                 match cmp_status {

talpid-core/src/windows/window.rs
@@ -2,26 +2,19 @@
 
 use std::{os::windows::io::AsRawHandle, ptr, sync::Arc, thread};
 use tokio::sync::broadcast;
-use winapi::{
-    shared::{
-        basetsd::LONG_PTR,
-        minwindef::{LPARAM, LRESULT, UINT, WPARAM},
-        windef::HWND,
-    },
-    um::{
-        libloaderapi::GetModuleHandleW,
-        processthreadsapi::GetThreadId,
-        winuser::{
-            CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, GetMessageW,
-            GetWindowLongPtrW, PostQuitMessage, PostThreadMessageW, SetWindowLongPtrW,
-            TranslateMessage, GWLP_USERDATA, GWLP_WNDPROC, PBT_APMRESUMEAUTOMATIC,
-            PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WM_DESTROY, WM_POWERBROADCAST, WM_USER,
-        },
+use windows_sys::Win32::{
+    Foundation::{HANDLE, HWND, LPARAM, LRESULT, WPARAM},
+    System::{LibraryLoader::GetModuleHandleW, Threading::GetThreadId},
+    UI::WindowsAndMessaging::{
+        CreateWindowExW, DefWindowProcW, DestroyWindow, DispatchMessageW, GetMessageW,
+        GetWindowLongPtrW, PostQuitMessage, PostThreadMessageW, SetWindowLongPtrW,
+        TranslateMessage, GWLP_USERDATA, GWLP_WNDPROC, PBT_APMRESUMEAUTOMATIC,
+        PBT_APMRESUMESUSPEND, PBT_APMSUSPEND, WM_DESTROY, WM_POWERBROADCAST, WM_USER,
     },
 };
 
 const CLASS_NAME: &[u8] = b"S\0T\0A\0T\0I\0C\0\0\0";
-const REQUEST_THREAD_SHUTDOWN: UINT = WM_USER + 1;
+const REQUEST_THREAD_SHUTDOWN: u32 = WM_USER + 1;
 
 /// Handle for closing an associated window.
 /// The window is not destroyed when this is dropped.
@@ -33,7 +26,7 @@ impl WindowCloseHandle {
     /// Close the window and wait for the thread.
     pub fn close(&mut self) {
         if let Some(thread) = self.thread.take() {
-            let thread_id = unsafe { GetThreadId(thread.as_raw_handle()) };
+            let thread_id = unsafe { GetThreadId(thread.as_raw_handle() as HANDLE) };
             unsafe { PostThreadMessageW(thread_id, REQUEST_THREAD_SHUTDOWN, 0, 0) };
             let _ = thread.join();
         }
@@ -41,7 +34,7 @@ impl WindowCloseHandle {
 }
 
 /// Creates a dummy window whose messages are handled by `wnd_proc`.
-pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Send + 'static>(
+pub fn create_hidden_window<F: (Fn(HWND, u32, WPARAM, LPARAM) -> LRESULT) + Send + 'static>(
     wnd_proc: F,
 ) -> WindowCloseHandle {
     let join_handle = thread::spawn(move || {
@@ -55,8 +48,8 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
                 0,
                 0,
                 0,
-                ptr::null_mut(),
-                ptr::null_mut(),
+                0,
+                0,
                 GetModuleHandleW(ptr::null_mut()),
                 ptr::null_mut(),
             )
@@ -67,18 +60,14 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
         let raw_callback = Box::into_raw(Box::new(wnd_proc));
 
         unsafe {
-            SetWindowLongPtrW(dummy_window, GWLP_USERDATA, raw_callback as LONG_PTR);
-            SetWindowLongPtrW(
-                dummy_window,
-                GWLP_WNDPROC,
-                window_procedure::<F> as LONG_PTR,
-            );
+            SetWindowLongPtrW(dummy_window, GWLP_USERDATA, raw_callback as isize);
+            SetWindowLongPtrW(dummy_window, GWLP_WNDPROC, window_procedure::<F> as isize);
         }
 
         let mut msg = unsafe { std::mem::zeroed() };
 
         loop {
-            let status = unsafe { GetMessageW(&mut msg, ptr::null_mut(), 0, 0) };
+            let status = unsafe { GetMessageW(&mut msg, 0, 0, 0) };
 
             if status < 0 {
                 continue;
@@ -87,7 +76,7 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
                 break;
             }
 
-            if msg.hwnd.is_null() {
+            if msg.hwnd == 0 {
                 if msg.message == REQUEST_THREAD_SHUTDOWN {
                     unsafe { DestroyWindow(dummy_window) };
                 }
@@ -110,12 +99,12 @@ pub fn create_hidden_window<F: (Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT) + Sen
 
 unsafe extern "system" fn window_procedure<F>(
     window: HWND,
-    message: UINT,
+    message: u32,
     wparam: WPARAM,
     lparam: LPARAM,
 ) -> LRESULT
 where
-    F: Fn(HWND, UINT, WPARAM, LPARAM) -> LRESULT,
+    F: Fn(HWND, u32, WPARAM, LPARAM) -> LRESULT,
 {
     if message == WM_DESTROY {
         PostQuitMessage(0);
@@ -146,7 +135,7 @@ pub enum PowerManagementEvent {
 impl PowerManagementEvent {
     fn try_from_winevent(wparam: usize) -> Option<Self> {
         use PowerManagementEvent::*;
-        match wparam {
+        match wparam as u32 {
             PBT_APMRESUMEAUTOMATIC => Some(ResumeAutomatic),
             PBT_APMRESUMESUSPEND => Some(ResumeSuspend),
             PBT_APMSUSPEND => Some(Suspend),