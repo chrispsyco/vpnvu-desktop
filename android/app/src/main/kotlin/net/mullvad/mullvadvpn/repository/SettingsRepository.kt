mullvad/mullvadvpn-app

Add setting change functions to repository and VM

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/SettingsRepository.kt
@@ -49,11 +49,15 @@ class SettingsRepository(
             )
     }
 
-    fun isLocalNetworkSharingEnabled(): Boolean {
-        return serviceConnectionManager.settingsListener()?.allowLan ?: false
-    }
-
     fun setWireguardMtu(value: Int?) {
         serviceConnectionManager.settingsListener()?.wireguardMtu = value
+    }
+
+    fun setAutoConnect(value: Boolean) {
+        serviceConnectionManager.settingsListener()?.autoConnect = value
+    }
+
+    fun setLocalNetworkSharing(value: Boolean) {
+        serviceConnectionManager.settingsListener()?.allowLan = value
     }
 }

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/SettingsRepository.kt
@@ -50,15 +50,19 @@ class SettingsRepository(
             )
     }
 
-    fun isLocalNetworkSharingEnabled(): Boolean {
-        return serviceConnectionManager.settingsListener()?.allowLan ?: false
-    }
-
     fun setWireguardMtu(value: Int?) {
         serviceConnectionManager.settingsListener()?.wireguardMtu = value
     }
 
     fun setObfuscationOptions(value: ObfuscationSettings) {
         serviceConnectionManager.settingsListener()?.obfuscationSettings = value
+    }
+
+    fun setAutoConnect(isEnabled: Boolean) {
+        serviceConnectionManager.settingsListener()?.autoConnect = isEnabled
+    }
+
+    fun setLocalNetworkSharing(isEnabled: Boolean) {
+        serviceConnectionManager.settingsListener()?.allowLan = isEnabled
     }
 }