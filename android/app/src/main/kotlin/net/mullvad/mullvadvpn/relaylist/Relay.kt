mullvad/mullvadvpn-app

Add search bar to select location screen
- Filter countries, cities and relays based on search string
- Remove collapsable toolbar and replace with search bar
- Improve expand behavior for relay location cells
- Remove cirular dependency for relays

android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/Relay.kt
@@ -2,16 +2,17 @@ package net.mullvad.mullvadvpn.relaylist
 
 import net.mullvad.mullvadvpn.model.LocationConstraint
 
-data class Relay(val city: RelayCity, override val name: String, override val active: Boolean) :
-    RelayItem {
+data class Relay(
+    override val name: String,
+    override val location: LocationConstraint,
+    override val locationName: String,
+    override val active: Boolean
+) : RelayItem {
     override val code = name
     override val type = RelayItemType.Relay
-    override val location = LocationConstraint.Hostname(city.country.code, city.code, name)
     override val hasChildren = false
 
     override val visibleChildCount = 0
-
-    override val locationName = "${city.name} ($name)"
 
     override var expanded
         get() = false

android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayCity.kt
@@ -2,18 +2,17 @@ package net.mullvad.mullvadvpn.relaylist
 
 import net.mullvad.mullvadvpn.model.LocationConstraint
 
-class RelayCity(
-    val country: RelayCountry,
+data class RelayCity(
     override val name: String,
     override val code: String,
+    override val location: LocationConstraint,
     override var expanded: Boolean,
     val relays: List<Relay>
 ) : RelayItem {
     override val type = RelayItemType.City
-    override val location = LocationConstraint.City(country.code, code)
 
     override val active
-        get() = relays.any { relay -> relay.active }
+        get() = relays.any { relay -> relay.active } || relays.isEmpty()
 
     override val hasChildren
         get() = relays.isNotEmpty()

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/Relay.kt
@@ -2,17 +2,15 @@ package net.mullvad.mullvadvpn.relaylist
 
 import net.mullvad.mullvadvpn.model.GeographicLocationConstraint
 
-data class Relay(val city: RelayCity, override val name: String, override val active: Boolean) :
-    RelayItem {
+data class Relay(
+    override val name: String,
+    override val location: GeographicLocationConstraint,
+    override val locationName: String,
+    override val active: Boolean
+) : RelayItem {
     override val code = name
     override val type = RelayItemType.Relay
-    override val location =
-        GeographicLocationConstraint.Hostname(city.country.code, city.code, name)
     override val hasChildren = false
-
-    override val visibleChildCount = 0
-
-    override val locationName = "${city.name} ($name)"
 
     override var expanded
         get() = false

android/app/src/main/kotlin/net/mullvad/mullvadvpn/relaylist/RelayCity.kt
@@ -2,30 +2,20 @@ package net.mullvad.mullvadvpn.relaylist
 
 import net.mullvad.mullvadvpn.model.GeographicLocationConstraint
 
-class RelayCity(
-    val country: RelayCountry,
+data class RelayCity(
     override val name: String,
     override val code: String,
+    override val location: GeographicLocationConstraint,
     override var expanded: Boolean,
     val relays: List<Relay>
 ) : RelayItem {
     override val type = RelayItemType.City
-    override val location = GeographicLocationConstraint.City(country.code, code)
 
     override val active
         get() = relays.any { relay -> relay.active }
 
     override val hasChildren
         get() = relays.isNotEmpty()
-
-    override val visibleChildCount: Int
-        get() {
-            return if (expanded) {
-                relays.size
-            } else {
-                0
-            }
-        }
 
     fun getItem(position: Int): GetItemResult {
         if (position == 0) {