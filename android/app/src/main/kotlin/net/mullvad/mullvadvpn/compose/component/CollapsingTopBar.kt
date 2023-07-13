mullvad/mullvadvpn-app

Add rotation of the back button as parameter

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/CollapsingTopBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -49,7 +50,8 @@ fun CollapsingTopBar(
     title: String,
     progress: Float,
     backTitle: String,
-    modifier: Modifier
+    modifier: Modifier,
+    shouldRotateBackButton: Boolean = false
 ) {
     val expandedToolbarHeight = dimensionResource(id = R.dimen.expanded_toolbar_height)
     val iconSize = dimensionResource(id = R.dimen.icon_size)
@@ -78,7 +80,10 @@ fun CollapsingTopBar(
         Image(
             painter = painterResource(id = R.drawable.icon_back),
             contentDescription = stringResource(id = R.string.back),
-            modifier = Modifier.width(iconSize).height(iconSize)
+            modifier =
+                Modifier.rotate(if (shouldRotateBackButton) 270f else 0f)
+                    .width(iconSize)
+                    .height(iconSize)
         )
         Spacer(modifier = Modifier.width(iconPadding).fillMaxHeight())
         Text(

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/CollapsingTopBar.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -50,8 +51,9 @@ fun CollapsingTopBar(
     title: String,
     progress: Float,
     backTitle: String,
+    modifier: Modifier,
     backIcon: Int? = null,
-    modifier: Modifier
+    shouldRotateBackButtonDown: Boolean = false
 ) {
     val expandedToolbarHeight = dimensionResource(id = R.dimen.expanded_toolbar_height)
     val iconSize = dimensionResource(id = R.dimen.icon_size)
@@ -81,7 +83,10 @@ fun CollapsingTopBar(
         Image(
             painter = painterResource(id = backIcon ?: R.drawable.icon_back),
             contentDescription = stringResource(id = R.string.back),
-            modifier = Modifier.width(iconSize).height(iconSize)
+            modifier =
+                Modifier.rotate(if (shouldRotateBackButtonDown) 270f else 0f)
+                    .width(iconSize)
+                    .height(iconSize)
         )
         Spacer(modifier = Modifier.width(iconPadding).fillMaxHeight())
         Text(