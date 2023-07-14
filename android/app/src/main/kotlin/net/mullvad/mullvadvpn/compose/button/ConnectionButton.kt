mullvad/mullvadvpn-app

Migrate connect fragment, expect notification bar, to compose

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/ConnectionButton.kt
@@ -1,0 +1,166 @@
+package net.mullvad.mullvadvpn.compose.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.theme.AlphaInactive
+import net.mullvad.mullvadvpn.compose.theme.AppTheme
+import net.mullvad.mullvadvpn.compose.theme.Dimens
+import net.mullvad.mullvadvpn.model.TunnelState
+import net.mullvad.talpid.tunnel.ActionAfterDisconnect
+
+@Composable
+fun ConnectionButton(
+    modifier: Modifier = Modifier,
+    state: TunnelState,
+    disconnectClick: () -> Unit,
+    reconnectClick: () -> Unit,
+    cancelClick: () -> Unit,
+    connectClick: () -> Unit
+) {
+    when (state) {
+        is TunnelState.Disconnected -> ConnectButton(modifier = modifier, onClick = connectClick)
+        is TunnelState.Disconnecting -> {
+            when (state.actionAfterDisconnect) {
+                ActionAfterDisconnect.Nothing ->
+                    ConnectButton(modifier = modifier, onClick = connectClick)
+                ActionAfterDisconnect.Block ->
+                    DisconnectButton(
+                        modifier = modifier,
+                        text = stringResource(id = R.string.disconnect),
+                        mainClick = connectClick,
+                        reconnectClick = reconnectClick
+                    )
+                ActionAfterDisconnect.Reconnect ->
+                    DisconnectButton(
+                        modifier = modifier,
+                        text = stringResource(id = R.string.disconnect),
+                        mainClick = connectClick,
+                        reconnectClick = reconnectClick
+                    )
+            }
+        }
+        is TunnelState.Connecting ->
+            DisconnectButton(
+                modifier = modifier,
+                text = stringResource(id = R.string.cancel),
+                mainClick = cancelClick,
+                reconnectClick = reconnectClick
+            )
+        is TunnelState.Connected ->
+            DisconnectButton(
+                modifier = modifier,
+                text = stringResource(id = R.string.disconnect),
+                mainClick = disconnectClick,
+                reconnectClick = reconnectClick
+            )
+        is TunnelState.Error -> {
+            if (state.errorState.isBlocking) {
+                DisconnectButton(
+                    modifier = modifier,
+                    text = stringResource(id = R.string.disconnect),
+                    mainClick = disconnectClick,
+                    reconnectClick = reconnectClick
+                )
+            } else {
+                DisconnectButton(
+                    modifier = modifier,
+                    text = stringResource(id = R.string.dismiss),
+                    mainClick = cancelClick,
+                    reconnectClick = reconnectClick
+                )
+            }
+        }
+    }
+}
+
+@Preview @Composable private fun PreviewConnectButton() {}
+
+@Composable
+private fun ConnectButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    ActionButton(
+        text = textResource(id = R.string.connect),
+        modifier = modifier,
+        onClick = onClick,
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            )
+    )
+}
+
+@Preview
+@Composable
+fun PreviewDisconnectButton() {
+    AppTheme { DisconnectButton(text = "Disconnect", mainClick = {}, reconnectClick = {}) }
+}
+
+@Composable
+private fun DisconnectButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    height: Dp = Dimens.connectButtonHeight,
+    mainClick: () -> Unit,
+    reconnectClick: () -> Unit
+) {
+    Row(modifier = modifier.height(height)) {
+        Button(
+            onClick = mainClick,
+            shape =
+                MaterialTheme.shapes.small.copy(
+                    topEnd = CornerSize(percent = 0),
+                    bottomEnd = CornerSize(percent = 0)
+                ),
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error.copy(alpha = AlphaInactive),
+                    contentColor = MaterialTheme.colorScheme.onError
+                ),
+            modifier = Modifier.weight(1f).height(height)
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.width(Dimens.listItemDivider))
+
+        FilledIconButton(
+            shape =
+                MaterialTheme.shapes.small.copy(
+                    topStart = CornerSize(percent = 0),
+                    bottomStart = CornerSize(percent = 0)
+                ),
+            colors =
+                IconButtonDefaults.filledIconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.error.copy(alpha = AlphaInactive),
+                    contentColor = MaterialTheme.colorScheme.onError
+                ),
+            onClick = reconnectClick,
+            modifier = Modifier.height(height).aspectRatio(1f, true)
+        ) {
+            Icon(painter = painterResource(id = R.drawable.icon_reload), contentDescription = null)
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/ConnectionStatusText.kt
@@ -1,0 +1,94 @@
+package net.mullvad.mullvadvpn.compose.component
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.theme.AppTheme
+import net.mullvad.mullvadvpn.compose.theme.typeface.connectionStatus
+import net.mullvad.mullvadvpn.model.TunnelState
+import net.mullvad.talpid.tunnel.ActionAfterDisconnect
+import net.mullvad.talpid.tunnel.ErrorState
+import net.mullvad.talpid.tunnel.ErrorStateCause
+
+@Preview
+@Composable
+fun PreviewConnectionStatusText() {
+    AppTheme {
+        SpacedColumn {
+            ConnectionStatusText(TunnelState.Disconnected)
+            ConnectionStatusText(TunnelState.Connecting(null, null))
+            ConnectionStatusText(
+                state = TunnelState.Error(ErrorState(ErrorStateCause.Ipv6Unavailable, true))
+            )
+        }
+    }
+}
+
+@Composable
+fun ConnectionStatusText(state: TunnelState) {
+    when (state) {
+        is TunnelState.Disconnecting -> {
+            when (state.actionAfterDisconnect) {
+                ActionAfterDisconnect.Nothing -> Disconnected()
+                ActionAfterDisconnect.Block -> Connected(false)
+                ActionAfterDisconnect.Reconnect -> Connecting(false)
+            }
+        }
+        is TunnelState.Disconnected -> Disconnected()
+        is TunnelState.Connecting -> Connecting(state.endpoint?.quantumResistant == true)
+        is TunnelState.Connected -> Connected(state.endpoint.quantumResistant)
+        is TunnelState.Error -> Error(state.errorState.isBlocking)
+    }
+}
+
+@Composable
+private fun Disconnected() {
+    Text(
+        text = textResource(id = R.string.unsecured_connection),
+        color = MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun Connecting(isQuantumResistant: Boolean) {
+    Text(
+        text =
+            textResource(
+                id =
+                    if (isQuantumResistant) R.string.quantum_creating_secure_connection
+                    else R.string.creating_secure_connection
+            ),
+        color = MaterialTheme.colorScheme.onPrimary,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun Connected(isQuantumResistant: Boolean) {
+    Text(
+        text =
+            textResource(
+                id =
+                    if (isQuantumResistant) R.string.quantum_secure_connection
+                    else R.string.secure_connection
+            ),
+        color = MaterialTheme.colorScheme.surface,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun Error(isBlocking: Boolean) {
+    Text(
+        text =
+            textResource(
+                id = if (isBlocking) R.string.blocked_connection else R.string.error_state
+            ),
+        color =
+            if (isBlocking) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/button/ConnectionButton.kt
@@ -1,0 +1,171 @@
+package net.mullvad.mullvadvpn.compose.button
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.component.textResource
+import net.mullvad.mullvadvpn.compose.theme.AlphaInactive
+import net.mullvad.mullvadvpn.compose.theme.AppTheme
+import net.mullvad.mullvadvpn.compose.theme.Dimens
+import net.mullvad.mullvadvpn.model.TunnelState
+import net.mullvad.talpid.tunnel.ActionAfterDisconnect
+
+@Composable
+fun ConnectionButton(
+    modifier: Modifier = Modifier,
+    state: TunnelState,
+    disconnectClick: () -> Unit,
+    reconnectClick: () -> Unit,
+    cancelClick: () -> Unit,
+    connectClick: () -> Unit
+) {
+    when (state) {
+        is TunnelState.Disconnected -> ConnectButton(modifier = modifier, onClick = connectClick)
+        is TunnelState.Disconnecting -> {
+            when (state.actionAfterDisconnect) {
+                ActionAfterDisconnect.Nothing ->
+                    ConnectButton(modifier = modifier, onClick = connectClick)
+                ActionAfterDisconnect.Block ->
+                    DisconnectButton(
+                        modifier = modifier,
+                        text = stringResource(id = R.string.disconnect),
+                        mainClick = connectClick,
+                        reconnectClick = reconnectClick
+                    )
+                ActionAfterDisconnect.Reconnect ->
+                    DisconnectButton(
+                        modifier = modifier,
+                        text = stringResource(id = R.string.disconnect),
+                        mainClick = connectClick,
+                        reconnectClick = reconnectClick
+                    )
+            }
+        }
+        is TunnelState.Connecting ->
+            DisconnectButton(
+                modifier = modifier,
+                text = stringResource(id = R.string.cancel),
+                mainClick = cancelClick,
+                reconnectClick = reconnectClick
+            )
+        is TunnelState.Connected ->
+            DisconnectButton(
+                modifier = modifier,
+                text = stringResource(id = R.string.disconnect),
+                mainClick = disconnectClick,
+                reconnectClick = reconnectClick
+            )
+        is TunnelState.Error -> {
+            if (state.errorState.isBlocking) {
+                DisconnectButton(
+                    modifier = modifier,
+                    text = stringResource(id = R.string.disconnect),
+                    mainClick = disconnectClick,
+                    reconnectClick = reconnectClick
+                )
+            } else {
+                DisconnectButton(
+                    modifier = modifier,
+                    text = stringResource(id = R.string.dismiss),
+                    mainClick = cancelClick,
+                    reconnectClick = reconnectClick
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewConnectButton() {
+    AppTheme { ConnectButton(onClick = {}) }
+}
+
+@Composable
+private fun ConnectButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    ActionButton(
+        text = textResource(id = R.string.connect),
+        modifier = modifier,
+        onClick = onClick,
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            )
+    )
+}
+
+@Preview
+@Composable
+fun PreviewDisconnectButton() {
+    AppTheme { DisconnectButton(text = "Disconnect", mainClick = {}, reconnectClick = {}) }
+}
+
+@Composable
+private fun DisconnectButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    height: Dp = Dimens.connectButtonHeight,
+    mainClick: () -> Unit,
+    reconnectClick: () -> Unit
+) {
+    Row(modifier = modifier.height(height)) {
+        Button(
+            onClick = mainClick,
+            shape =
+                MaterialTheme.shapes.small.copy(
+                    topEnd = CornerSize(percent = 0),
+                    bottomEnd = CornerSize(percent = 0)
+                ),
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.error.copy(alpha = AlphaInactive),
+                    contentColor = MaterialTheme.colorScheme.onError
+                ),
+            modifier = Modifier.weight(1f).height(height)
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.width(Dimens.listItemDivider))
+
+        FilledIconButton(
+            shape =
+                MaterialTheme.shapes.small.copy(
+                    topStart = CornerSize(percent = 0),
+                    bottomStart = CornerSize(percent = 0)
+                ),
+            colors =
+                IconButtonDefaults.filledIconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.error.copy(alpha = AlphaInactive),
+                    contentColor = MaterialTheme.colorScheme.onError
+                ),
+            onClick = reconnectClick,
+            modifier = Modifier.height(height).aspectRatio(1f, true)
+        ) {
+            Icon(painter = painterResource(id = R.drawable.icon_reload), contentDescription = null)
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/ConnectionStatusText.kt
@@ -1,0 +1,94 @@
+package net.mullvad.mullvadvpn.compose.component
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.R
+import net.mullvad.mullvadvpn.compose.theme.AppTheme
+import net.mullvad.mullvadvpn.compose.theme.typeface.connectionStatus
+import net.mullvad.mullvadvpn.model.TunnelState
+import net.mullvad.talpid.tunnel.ActionAfterDisconnect
+import net.mullvad.talpid.tunnel.ErrorState
+import net.mullvad.talpid.tunnel.ErrorStateCause
+
+@Preview
+@Composable
+fun PreviewConnectionStatusText() {
+    AppTheme {
+        SpacedColumn {
+            ConnectionStatusText(TunnelState.Disconnected)
+            ConnectionStatusText(TunnelState.Connecting(null, null))
+            ConnectionStatusText(
+                state = TunnelState.Error(ErrorState(ErrorStateCause.Ipv6Unavailable, true))
+            )
+        }
+    }
+}
+
+@Composable
+fun ConnectionStatusText(state: TunnelState) {
+    when (state) {
+        is TunnelState.Disconnecting -> {
+            when (state.actionAfterDisconnect) {
+                ActionAfterDisconnect.Nothing -> DisconnectedText()
+                ActionAfterDisconnect.Block -> ConnectedText(false)
+                ActionAfterDisconnect.Reconnect -> ConnectingText(false)
+            }
+        }
+        is TunnelState.Disconnected -> DisconnectedText()
+        is TunnelState.Connecting -> ConnectingText(state.endpoint?.quantumResistant == true)
+        is TunnelState.Connected -> ConnectedText(state.endpoint.quantumResistant)
+        is TunnelState.Error -> ErrorText(state.errorState.isBlocking)
+    }
+}
+
+@Composable
+private fun DisconnectedText() {
+    Text(
+        text = textResource(id = R.string.unsecured_connection),
+        color = MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun ConnectingText(isQuantumResistant: Boolean) {
+    Text(
+        text =
+            textResource(
+                id =
+                    if (isQuantumResistant) R.string.quantum_creating_secure_connection
+                    else R.string.creating_secure_connection
+            ),
+        color = MaterialTheme.colorScheme.onPrimary,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun ConnectedText(isQuantumResistant: Boolean) {
+    Text(
+        text =
+            textResource(
+                id =
+                    if (isQuantumResistant) R.string.quantum_secure_connection
+                    else R.string.secure_connection
+            ),
+        color = MaterialTheme.colorScheme.surface,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}
+
+@Composable
+private fun ErrorText(isBlocking: Boolean) {
+    Text(
+        text =
+            textResource(
+                id = if (isBlocking) R.string.blocked_connection else R.string.error_state
+            ),
+        color =
+            if (isBlocking) MaterialTheme.colorScheme.surface else MaterialTheme.colorScheme.error,
+        style = MaterialTheme.typography.connectionStatus
+    )
+}