mullvad/mullvadvpn-app

Coordinate alert presentation by integrating it into routing system

ios/MullvadVPN/Coordinators/SettingsCoordinator.swift
@@ -158,12 +158,14 @@ final class SettingsCoordinator: Coordinator, Presentable, Presenting, SettingsV
 
         case .preferences:
             return PreferencesViewController(
-                interactor: interactorFactory.makePreferencesInteractor()
+                interactor: interactorFactory.makePreferencesInteractor(),
+                alertPresenter: AlertPresenter(coordinator: self)
             )
 
         case .problemReport:
             return ProblemReportViewController(
-                interactor: interactorFactory.makeProblemReportInteractor()
+                interactor: interactorFactory.makeProblemReportInteractor(),
+                alertPresenter: AlertPresenter(coordinator: self)
             )
 
         case .faq:

chrispsyco/vpnvu-desktop

ios/MullvadVPN/Coordinators/SettingsCoordinator.swift
@@ -32,10 +32,6 @@ final class SettingsCoordinator: Coordinator, Presentable, Presenting, SettingsV
         navigationController
     }
 
-    var presentationContext: UIViewController {
-        navigationController
-    }
-
     var willNavigate: ((
         _ coordinator: SettingsCoordinator,
         _ from: SettingsNavigationRoute?,
@@ -158,12 +154,14 @@ final class SettingsCoordinator: Coordinator, Presentable, Presenting, SettingsV
 
         case .preferences:
             return PreferencesViewController(
-                interactor: interactorFactory.makePreferencesInteractor()
+                interactor: interactorFactory.makePreferencesInteractor(),
+                alertPresenter: AlertPresenter(context: self)
             )
 
         case .problemReport:
             return ProblemReportViewController(
-                interactor: interactorFactory.makeProblemReportInteractor()
+                interactor: interactorFactory.makeProblemReportInteractor(),
+                alertPresenter: AlertPresenter(context: self)
             )
 
         case .faq: