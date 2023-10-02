mullvad/mullvadvpn-app

Remove resource dimens and hardcoded colors in DeviceListScreen

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -18,11 +18,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import net.mullvad.mullvadvpn.R
@@ -34,13 +32,10 @@ import net.mullvad.mullvadvpn.compose.state.DeviceListItemUiState
 import net.mullvad.mullvadvpn.compose.state.DeviceListUiState
 import net.mullvad.mullvadvpn.lib.common.util.capitalizeFirstCharOfEachWord
 import net.mullvad.mullvadvpn.lib.common.util.parseAsDateTime
+import net.mullvad.mullvadvpn.lib.theme.AlphaInactive
+import net.mullvad.mullvadvpn.lib.theme.AlphaTopBar
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
-import net.mullvad.mullvadvpn.lib.theme.MullvadBlue
-import net.mullvad.mullvadvpn.lib.theme.MullvadGreen
-import net.mullvad.mullvadvpn.lib.theme.MullvadGreen40
-import net.mullvad.mullvadvpn.lib.theme.MullvadWhite
-import net.mullvad.mullvadvpn.lib.theme.MullvadWhite80
 import net.mullvad.mullvadvpn.model.Device
 import net.mullvad.mullvadvpn.util.formatDate
 
@@ -89,11 +84,11 @@ fun DeviceListScreen(
         )
     }
 
-    val topColor = colorResource(R.color.blue)
     ScaffoldWithTopBar(
-        topBarColor = topColor,
-        statusBarColor = topColor,
-        navigationBarColor = colorResource(id = R.color.darkBlue),
+        topBarColor = MaterialTheme.colorScheme.primary,
+        statusBarColor = MaterialTheme.colorScheme.primary,
+        navigationBarColor = MaterialTheme.colorScheme.secondary,
+        iconTintColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaTopBar),
         onSettingsClicked = onSettingsClicked,
         onAccountClicked = null,
     ) {
@@ -132,22 +127,28 @@ fun DeviceListScreen(
                         contentDescription = null, // No meaningful user info or action.
                         modifier =
                             Modifier.constrainAs(icon) {
-                                    top.linkTo(parent.top, margin = 30.dp)
+                                    top.linkTo(parent.top)
                                     start.linkTo(parent.start)
                                     end.linkTo(parent.end)
                                 }
-                                .width(64.dp)
-                                .height(64.dp)
+                                .padding(top = Dimens.iconFailSuccessTopMargin)
+                                .width(Dimens.iconFailSuccessSize)
+                                .height(Dimens.iconFailSuccessSize)
                     )
 
                     Column(
                         modifier =
                             Modifier.constrainAs(message) {
-                                top.linkTo(icon.bottom, margin = 16.dp)
-                                start.linkTo(parent.start, margin = 22.dp)
-                                end.linkTo(parent.end, margin = 22.dp)
-                                width = Dimension.fillToConstraints
-                            },
+                                    top.linkTo(icon.bottom)
+                                    start.linkTo(parent.start)
+                                    end.linkTo(parent.end)
+                                    width = Dimension.fillToConstraints
+                                }
+                                .padding(
+                                    start = Dimens.sideMargin,
+                                    end = Dimens.sideMargin,
+                                    top = Dimens.screenVerticalMargin
+                                ),
                     ) {
                         Text(
                             text =
@@ -176,17 +177,18 @@ fun DeviceListScreen(
                             modifier =
                                 Modifier.wrapContentHeight()
                                     .animateContentSize()
-                                    .padding(top = 8.dp)
+                                    .padding(top = Dimens.smallPadding)
                         )
                     }
 
                     Box(
                         modifier =
                             Modifier.constrainAs(list) {
-                                top.linkTo(message.bottom, margin = 20.dp)
-                                height = Dimension.wrapContent
-                                width = Dimension.matchParent
-                            }
+                                    top.linkTo(message.bottom)
+                                    height = Dimension.wrapContent
+                                    width = Dimension.matchParent
+                                }
+                                .padding(top = Dimens.spacingAboveButton)
                     ) {
                         Column {
                             state.deviceUiItems.forEach { deviceUiState ->
@@ -216,11 +218,16 @@ fun DeviceListScreen(
             Column(
                 modifier =
                     Modifier.constrainAs(buttons) {
-                        bottom.linkTo(parent.bottom, margin = 22.dp)
-                        start.linkTo(parent.start, margin = 22.dp)
-                        end.linkTo(parent.end, margin = 22.dp)
-                        width = Dimension.fillToConstraints
-                    }
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            width = Dimension.fillToConstraints
+                        }
+                        .padding(
+                            start = Dimens.sideMargin,
+                            end = Dimens.sideMargin,
+                            bottom = Dimens.screenVerticalMargin
+                        )
             ) {
                 ActionButton(
                     text = stringResource(id = R.string.continue_login),
@@ -228,10 +235,12 @@ fun DeviceListScreen(
                     isEnabled = state.hasTooManyDevices.not() && state.isLoading.not(),
                     colors =
                         ButtonDefaults.buttonColors(
-                            containerColor = MullvadGreen,
-                            disabledContainerColor = MullvadGreen40,
-                            disabledContentColor = MullvadWhite80,
-                            contentColor = MullvadWhite
+                            containerColor = MaterialTheme.colorScheme.inversePrimary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
+                            disabledContentColor =
+                                MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaInactive),
+                            disabledContainerColor =
+                                MaterialTheme.colorScheme.inversePrimary.copy(alpha = AlphaInactive)
                         )
                 )
 
@@ -240,10 +249,10 @@ fun DeviceListScreen(
                     onClick = onBackClick,
                     colors =
                         ButtonDefaults.buttonColors(
-                            containerColor = MullvadBlue,
-                            contentColor = MullvadWhite
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
                         ),
-                    modifier = Modifier.padding(top = 16.dp)
+                    modifier = Modifier.padding(top = Dimens.mediumPadding)
                 )
             }
         }

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import net.mullvad.mullvadvpn.R
@@ -33,13 +32,10 @@ import net.mullvad.mullvadvpn.compose.state.DeviceListItemUiState
 import net.mullvad.mullvadvpn.compose.state.DeviceListUiState
 import net.mullvad.mullvadvpn.lib.common.util.capitalizeFirstCharOfEachWord
 import net.mullvad.mullvadvpn.lib.common.util.parseAsDateTime
+import net.mullvad.mullvadvpn.lib.theme.AlphaInactive
+import net.mullvad.mullvadvpn.lib.theme.AlphaTopBar
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
-import net.mullvad.mullvadvpn.lib.theme.MullvadBlue
-import net.mullvad.mullvadvpn.lib.theme.MullvadGreen
-import net.mullvad.mullvadvpn.lib.theme.MullvadGreen40
-import net.mullvad.mullvadvpn.lib.theme.MullvadWhite
-import net.mullvad.mullvadvpn.lib.theme.MullvadWhite80
 import net.mullvad.mullvadvpn.model.Device
 import net.mullvad.mullvadvpn.util.formatDate
 
@@ -88,11 +84,11 @@ fun DeviceListScreen(
         )
     }
 
-    val topColor = MaterialTheme.colorScheme.primary
     ScaffoldWithTopBar(
-        topBarColor = topColor,
-        statusBarColor = topColor,
+        topBarColor = MaterialTheme.colorScheme.primary,
+        statusBarColor = MaterialTheme.colorScheme.primary,
         navigationBarColor = MaterialTheme.colorScheme.background,
+        iconTintColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaTopBar),
         onSettingsClicked = onSettingsClicked,
         onAccountClicked = null,
     ) {
@@ -101,7 +97,7 @@ fun DeviceListScreen(
                 Modifier.fillMaxHeight()
                     .fillMaxWidth()
                     .padding(it)
-                    .background(MaterialTheme.colorScheme.secondary)
+                    .background(MaterialTheme.colorScheme.background)
         ) {
             val (content, buttons) = createRefs()
 
@@ -131,22 +127,28 @@ fun DeviceListScreen(
                         contentDescription = null, // No meaningful user info or action.
                         modifier =
                             Modifier.constrainAs(icon) {
-                                    top.linkTo(parent.top, margin = 30.dp)
+                                    top.linkTo(parent.top)
                                     start.linkTo(parent.start)
                                     end.linkTo(parent.end)
                                 }
-                                .width(64.dp)
-                                .height(64.dp)
+                                .padding(top = Dimens.iconFailSuccessTopMargin)
+                                .width(Dimens.iconFailSuccessSize)
+                                .height(Dimens.iconFailSuccessSize)
                     )
 
                     Column(
                         modifier =
                             Modifier.constrainAs(message) {
-                                top.linkTo(icon.bottom, margin = 16.dp)
-                                start.linkTo(parent.start, margin = 22.dp)
-                                end.linkTo(parent.end, margin = 22.dp)
-                                width = Dimension.fillToConstraints
-                            },
+                                    top.linkTo(icon.bottom)
+                                    start.linkTo(parent.start)
+                                    end.linkTo(parent.end)
+                                    width = Dimension.fillToConstraints
+                                }
+                                .padding(
+                                    start = Dimens.sideMargin,
+                                    end = Dimens.sideMargin,
+                                    top = Dimens.screenVerticalMargin
+                                ),
                     ) {
                         Text(
                             text =
@@ -158,7 +160,8 @@ fun DeviceListScreen(
                                             R.string.max_devices_resolved_title
                                         }
                                 ),
-                            style = MaterialTheme.typography.headlineSmall
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.onBackground
                         )
 
                         Text(
@@ -172,20 +175,22 @@ fun DeviceListScreen(
                                         }
                                 ),
                             style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.onBackground,
                             modifier =
                                 Modifier.wrapContentHeight()
                                     .animateContentSize()
-                                    .padding(top = 8.dp)
+                                    .padding(top = Dimens.smallPadding)
                         )
                     }
 
                     Box(
                         modifier =
                             Modifier.constrainAs(list) {
-                                top.linkTo(message.bottom, margin = 20.dp)
-                                height = Dimension.wrapContent
-                                width = Dimension.matchParent
-                            }
+                                    top.linkTo(message.bottom)
+                                    height = Dimension.wrapContent
+                                    width = Dimension.matchParent
+                                }
+                                .padding(top = Dimens.spacingAboveButton)
                     ) {
                         Column {
                             state.deviceUiItems.forEach { deviceUiState ->
@@ -215,11 +220,16 @@ fun DeviceListScreen(
             Column(
                 modifier =
                     Modifier.constrainAs(buttons) {
-                        bottom.linkTo(parent.bottom, margin = 22.dp)
-                        start.linkTo(parent.start, margin = 22.dp)
-                        end.linkTo(parent.end, margin = 22.dp)
-                        width = Dimension.fillToConstraints
-                    }
+                            bottom.linkTo(parent.bottom)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            width = Dimension.fillToConstraints
+                        }
+                        .padding(
+                            start = Dimens.sideMargin,
+                            end = Dimens.sideMargin,
+                            bottom = Dimens.screenVerticalMargin
+                        )
             ) {
                 ActionButton(
                     text = stringResource(id = R.string.continue_login),
@@ -227,10 +237,12 @@ fun DeviceListScreen(
                     isEnabled = state.hasTooManyDevices.not() && state.isLoading.not(),
                     colors =
                         ButtonDefaults.buttonColors(
-                            containerColor = MullvadGreen,
-                            disabledContainerColor = MullvadGreen40,
-                            disabledContentColor = MullvadWhite80,
-                            contentColor = MullvadWhite
+                            containerColor = MaterialTheme.colorScheme.inversePrimary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
+                            disabledContentColor =
+                                MaterialTheme.colorScheme.onPrimary.copy(alpha = AlphaInactive),
+                            disabledContainerColor =
+                                MaterialTheme.colorScheme.inversePrimary.copy(alpha = AlphaInactive)
                         )
                 )
 
@@ -239,10 +251,10 @@ fun DeviceListScreen(
                     onClick = onBackClick,
                     colors =
                         ButtonDefaults.buttonColors(
-                            containerColor = MullvadBlue,
-                            contentColor = MullvadWhite
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary,
                         ),
-                    modifier = Modifier.padding(top = 16.dp)
+                    modifier = Modifier.padding(top = Dimens.mediumPadding)
                 )
             }
         }