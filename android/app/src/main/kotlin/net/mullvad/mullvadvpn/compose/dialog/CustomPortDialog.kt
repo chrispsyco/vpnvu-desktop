mullvad/mullvadvpn-app

Fix button spacing

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/CustomPortDialog.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -61,7 +62,7 @@ fun CustomPortDialog(
             )
         },
         confirmButton = {
-            Column {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSeparation)) {
                 PrimaryButton(
                     text = stringResource(id = R.string.custom_port_dialog_submit),
                     onClick = { onSave(port.value) },
@@ -72,13 +73,11 @@ fun CustomPortDialog(
                 if (showReset) {
                     NegativeButton(
                         text = stringResource(R.string.custom_port_dialog_remove),
-                        onClick = onReset,
-                        modifier = Modifier.padding(top = Dimens.mediumPadding),
+                        onClick = onReset
                     )
                 }
                 PrimaryButton(
                     text = stringResource(id = R.string.cancel),
-                    modifier = Modifier.padding(top = Dimens.mediumPadding),
                     onClick = onDismissRequest
                 )
             }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DnsDialog.kt
@@ -137,7 +137,7 @@ fun DnsDialog(
             }
         },
         confirmButton = {
-            Column(verticalArrangement = Arrangement.spacedBy(Dimens.mediumPadding)) {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSeparation)) {
                 PrimaryButton(
                     modifier = Modifier.fillMaxWidth(),
                     onClick = onAttemptToSave,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/MtuDialog.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -82,7 +83,7 @@ fun MtuDialog(
             }
         },
         confirmButton = {
-            Column {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSeparation)) {
                 PrimaryButton(
                     modifier = Modifier.fillMaxWidth(),
                     isEnabled = isValidMtu,
@@ -96,13 +97,13 @@ fun MtuDialog(
                 )
 
                 PrimaryButton(
-                    modifier = Modifier.padding(top = Dimens.mediumPadding).fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     text = stringResource(R.string.reset_to_default_button),
                     onClick = onRestoreDefaultValue
                 )
 
                 PrimaryButton(
-                    modifier = Modifier.padding(top = Dimens.mediumPadding).fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     text = stringResource(R.string.cancel),
                     onClick = onDismiss
                 )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/RedeemVoucherDialog.kt
@@ -112,7 +112,7 @@ fun RedeemVoucherDialog(
                     VariantButton(
                         text = stringResource(id = R.string.redeem),
                         onClick = { onRedeem(uiState.voucherInput) },
-                        modifier = Modifier.padding(bottom = Dimens.mediumPadding),
+                        modifier = Modifier.padding(bottom = Dimens.buttonSeparation),
                         isEnabled = uiState.voucherInput.length == VOUCHER_LENGTH
                     )
                 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -238,7 +238,7 @@ fun DeviceListScreen(
                 PrimaryButton(
                     text = stringResource(id = R.string.back),
                     onClick = onBackClick,
-                    modifier = Modifier.padding(top = Dimens.mediumPadding)
+                    modifier = Modifier.padding(top = Dimens.buttonSeparation)
                 )
             }
         }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ReportProblemScreen.kt
@@ -163,16 +163,18 @@ fun ReportProblemScreen(
                 colors = mullvadWhiteTextFieldColors()
             )
 
-            PrimaryButton(
-                onClick = onNavigateToViewLogs,
-                text = stringResource(id = R.string.view_logs)
-            )
-
-            VariantButton(
-                onClick = { onSendReport(email, description) },
-                isEnabled = description.isNotEmpty(),
-                text = stringResource(id = R.string.send)
-            )
+            Column {
+                PrimaryButton(
+                    onClick = onNavigateToViewLogs,
+                    text = stringResource(id = R.string.view_logs)
+                )
+                Spacer(modifier = Modifier.height(Dimens.buttonSeparation))
+                VariantButton(
+                    onClick = { onSendReport(email, description) },
+                    isEnabled = description.isNotEmpty(),
+                    text = stringResource(id = R.string.send)
+                )
+            }
         }
     }
 }
@@ -268,7 +270,9 @@ private fun ColumnScope.ErrorContent(retry: () -> Unit, onDismiss: () -> Unit) {
     )
     Spacer(modifier = Modifier.weight(1f))
     PrimaryButton(
-        modifier = Modifier.fillMaxWidth().padding(vertical = Dimens.mediumPadding),
+        modifier =
+            Modifier.fillMaxWidth()
+                .padding(top = Dimens.mediumPadding, bottom = Dimens.buttonSeparation),
         onClick = onDismiss,
         text = stringResource(id = R.string.edit_message)
     )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/WelcomeScreen.kt
@@ -282,7 +282,7 @@ private fun PaymentPanel(
                     Modifier.padding(
                         start = Dimens.sideMargin,
                         end = Dimens.sideMargin,
-                        bottom = Dimens.screenVerticalMargin
+                        bottom = Dimens.buttonSeparation
                     )
             )
         }

android/lib/theme/src/main/kotlin/net/mullvad/mullvadvpn/lib/theme/dimensions/Dimensions.kt
@@ -9,7 +9,7 @@ data class Dimensions(
     val backButtonSideMargin: Dp = 30.dp,
     val bigIconSize: Dp = 44.dp,
     val buttonHeight: Dp = 44.dp,
-    val buttonSeparation: Dp = 18.dp,
+    val buttonSeparation: Dp = 12.dp,
     val buttonVerticalPadding: Dp = 8.dp,
     val cellEndPadding: Dp = 16.dp,
     val cellFooterTopPadding: Dp = 6.dp,

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/CustomPortDialog.kt
@@ -1,10 +1,10 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -61,7 +61,7 @@ fun CustomPortDialog(
             )
         },
         confirmButton = {
-            Column {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSpacing)) {
                 PrimaryButton(
                     text = stringResource(id = R.string.custom_port_dialog_submit),
                     onClick = { onSave(port.value) },
@@ -72,13 +72,11 @@ fun CustomPortDialog(
                 if (showReset) {
                     NegativeButton(
                         text = stringResource(R.string.custom_port_dialog_remove),
-                        onClick = onReset,
-                        modifier = Modifier.padding(top = Dimens.mediumPadding),
+                        onClick = onReset
                     )
                 }
                 PrimaryButton(
                     text = stringResource(id = R.string.cancel),
-                    modifier = Modifier.padding(top = Dimens.mediumPadding),
                     onClick = onDismissRequest
                 )
             }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DnsDialog.kt
@@ -137,7 +137,7 @@ fun DnsDialog(
             }
         },
         confirmButton = {
-            Column(verticalArrangement = Arrangement.spacedBy(Dimens.mediumPadding)) {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSpacing)) {
                 PrimaryButton(
                     modifier = Modifier.fillMaxWidth(),
                     onClick = onAttemptToSave,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/MtuDialog.kt
@@ -1,5 +1,6 @@
 package net.mullvad.mullvadvpn.compose.dialog
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -82,7 +83,7 @@ fun MtuDialog(
             }
         },
         confirmButton = {
-            Column {
+            Column(verticalArrangement = Arrangement.spacedBy(Dimens.buttonSpacing)) {
                 PrimaryButton(
                     modifier = Modifier.fillMaxWidth(),
                     isEnabled = isValidMtu,
@@ -96,13 +97,13 @@ fun MtuDialog(
                 )
 
                 PrimaryButton(
-                    modifier = Modifier.padding(top = Dimens.mediumPadding).fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     text = stringResource(R.string.reset_to_default_button),
                     onClick = onRestoreDefaultValue
                 )
 
                 PrimaryButton(
-                    modifier = Modifier.padding(top = Dimens.mediumPadding).fillMaxWidth(),
+                    modifier = Modifier.fillMaxWidth(),
                     text = stringResource(R.string.cancel),
                     onClick = onDismiss
                 )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/RedeemVoucherDialog.kt
@@ -111,7 +111,7 @@ fun RedeemVoucherDialog(
                     VariantButton(
                         text = stringResource(id = R.string.redeem),
                         onClick = { onRedeem(uiState.voucherInput) },
-                        modifier = Modifier.padding(bottom = Dimens.mediumPadding),
+                        modifier = Modifier.padding(bottom = Dimens.buttonSpacing),
                         isEnabled = uiState.voucherInput.length == VOUCHER_LENGTH
                     )
                 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -238,7 +236,7 @@ fun DeviceListScreen(
                 PrimaryButton(
                     text = stringResource(id = R.string.back),
                     onClick = onBackClick,
-                    modifier = Modifier.padding(top = Dimens.mediumPadding)
+                    modifier = Modifier.padding(top = Dimens.buttonSpacing)
                 )
             }
         }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/ReportProblemScreen.kt
@@ -162,16 +162,18 @@ fun ReportProblemScreen(
                 colors = mullvadWhiteTextFieldColors()
             )
 
-            PrimaryButton(
-                onClick = onNavigateToViewLogs,
-                text = stringResource(id = R.string.view_logs)
-            )
-
-            VariantButton(
-                onClick = { onSendReport(email, description) },
-                isEnabled = description.isNotEmpty(),
-                text = stringResource(id = R.string.send)
-            )
+            Column {
+                PrimaryButton(
+                    onClick = onNavigateToViewLogs,
+                    text = stringResource(id = R.string.view_logs)
+                )
+                Spacer(modifier = Modifier.height(Dimens.buttonSpacing))
+                VariantButton(
+                    onClick = { onSendReport(email, description) },
+                    isEnabled = description.isNotEmpty(),
+                    text = stringResource(id = R.string.send)
+                )
+            }
         }
     }
 }
@@ -265,7 +267,9 @@ private fun ColumnScope.ErrorContent(retry: () -> Unit, onDismiss: () -> Unit) {
     )
     Spacer(modifier = Modifier.weight(1f))
     PrimaryButton(
-        modifier = Modifier.fillMaxWidth().padding(vertical = Dimens.mediumPadding),
+        modifier =
+            Modifier.fillMaxWidth()
+                .padding(top = Dimens.mediumPadding, bottom = Dimens.buttonSpacing),
         onClick = onDismiss,
         text = stringResource(id = R.string.edit_message)
     )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/WelcomeScreen.kt
@@ -282,7 +282,7 @@ private fun PaymentPanel(
                     Modifier.padding(
                         start = Dimens.sideMargin,
                         end = Dimens.sideMargin,
-                        bottom = Dimens.screenVerticalMargin
+                        bottom = Dimens.buttonSpacing
                     )
             )
         }

android/lib/theme/src/main/kotlin/net/mullvad/mullvadvpn/lib/theme/dimensions/Dimensions.kt
@@ -9,7 +9,7 @@ data class Dimensions(
     val backButtonSideMargin: Dp = 30.dp,
     val bigIconSize: Dp = 44.dp,
     val buttonHeight: Dp = 44.dp,
-    val buttonSeparation: Dp = 18.dp,
+    val buttonSpacing: Dp = 12.dp,
     val buttonVerticalPadding: Dp = 8.dp,
     val cellEndPadding: Dp = 16.dp,
     val cellFooterTopPadding: Dp = 6.dp,