mullvad/mullvadvpn-app

Add timeout to login screen

android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/LoginConstant.kt
@@ -1,0 +1,3 @@
+package net.mullvad.mullvadvpn.constant
+
+const val LOGIN_TIMEOUT = 10000L // 10 seconds

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/LoginViewModel.kt
@@ -19,12 +19,14 @@ import net.mullvad.mullvadvpn.compose.state.LoginError
 import net.mullvad.mullvadvpn.compose.state.LoginState
 import net.mullvad.mullvadvpn.compose.state.LoginState.*
 import net.mullvad.mullvadvpn.compose.state.LoginUiState
+import net.mullvad.mullvadvpn.constant.LOGIN_TIMEOUT
 import net.mullvad.mullvadvpn.model.AccountCreationResult
 import net.mullvad.mullvadvpn.model.AccountToken
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.repository.AccountRepository
 import net.mullvad.mullvadvpn.repository.DeviceRepository
 import net.mullvad.mullvadvpn.usecase.NewDeviceNotificationUseCase
+import net.mullvad.mullvadvpn.util.awaitWithTimeoutOrNull
 
 private const val MINIMUM_LOADING_SPINNER_TIME_MILLIS = 500L
 
@@ -81,7 +83,8 @@ class LoginViewModel(
             delay(MINIMUM_LOADING_SPINNER_TIME_MILLIS)
 
             val uiState =
-                when (val result = loginDeferred.await()) {
+                // If timed out will go to the else branch
+                when (val result = loginDeferred.awaitWithTimeoutOrNull(LOGIN_TIMEOUT)) {
                     LoginResult.Ok -> {
                         launch {
                             delay(1000)

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/constant/LoginConstant.kt
@@ -1,0 +1,3 @@
+package net.mullvad.mullvadvpn.constant
+
+const val LOGIN_TIMEOUT_MILLIS = 10000L // 10 seconds

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/LoginViewModel.kt
@@ -19,12 +19,14 @@ import net.mullvad.mullvadvpn.compose.state.LoginError
 import net.mullvad.mullvadvpn.compose.state.LoginState
 import net.mullvad.mullvadvpn.compose.state.LoginState.*
 import net.mullvad.mullvadvpn.compose.state.LoginUiState
+import net.mullvad.mullvadvpn.constant.LOGIN_TIMEOUT_MILLIS
 import net.mullvad.mullvadvpn.model.AccountCreationResult
 import net.mullvad.mullvadvpn.model.AccountToken
 import net.mullvad.mullvadvpn.model.LoginResult
 import net.mullvad.mullvadvpn.repository.AccountRepository
 import net.mullvad.mullvadvpn.repository.DeviceRepository
 import net.mullvad.mullvadvpn.usecase.NewDeviceNotificationUseCase
+import net.mullvad.mullvadvpn.util.awaitWithTimeoutOrNull
 
 private const val MINIMUM_LOADING_SPINNER_TIME_MILLIS = 500L
 
@@ -81,7 +83,8 @@ class LoginViewModel(
             delay(MINIMUM_LOADING_SPINNER_TIME_MILLIS)
 
             val uiState =
-                when (val result = loginDeferred.await()) {
+                // If timed out will go to the else branch
+                when (val result = loginDeferred.awaitWithTimeoutOrNull(LOGIN_TIMEOUT_MILLIS)) {
                     LoginResult.Ok -> {
                         launch {
                             delay(1000)