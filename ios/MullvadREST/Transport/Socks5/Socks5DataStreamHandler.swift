mullvad/mullvadvpn-app

Add socks5 forwarding proxy

ios/MullvadTransport/Socks5/Socks5DataStreamHandler.swift
@@ -1,0 +1,80 @@
+//
+//  Socks5DataStreamHandler.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 20/10/2023.
+//
+
+import Foundation
+import Network
+
+/// The object handling bidirectional streaming of data between local and remote connection.
+struct Socks5DataStreamHandler {
+    /// How many bytes the handler can receive at one time, when streaming data between local and remote connection.
+    static let maxBytesToRead = Int(UInt16.max)
+
+    /// Local TCP connection.
+    let localConnection: NWConnection
+
+    /// Remote TCP connection to the socks proxy.
+    let remoteConnection: NWConnection
+
+    /// Error handler.
+    let errorHandler: (Error) -> Void
+
+    /// Start streaming data between local and remote connection.
+    func start() {
+        streamOutboundTraffic()
+        streamInboundTraffic()
+    }
+
+    /// Pass outbound traffic from local to remote connection.
+    private func streamOutboundTraffic() {
+        localConnection.receive(
+            minimumIncompleteLength: 1,
+            maximumLength: Self.maxBytesToRead
+        ) { [self] content, contentContext, isComplete, error in
+            if let error {
+                errorHandler(Socks5Error.localConnectionFailure(error))
+                return
+            }
+
+            remoteConnection.send(
+                content: content,
+                isComplete: isComplete,
+                completion: .contentProcessed { [self] error in
+                    if let error {
+                        errorHandler(Socks5Error.remoteConnectionFailure(error))
+                    } else if !isComplete {
+                        streamOutboundTraffic()
+                    }
+                }
+            )
+        }
+    }
+
+    /// Pass inbound traffic from remote to local connection.
+    private func streamInboundTraffic() {
+        remoteConnection.receive(
+            minimumIncompleteLength: 1,
+            maximumLength: Self.maxBytesToRead
+        ) { [self] content, contentContext, isComplete, error in
+            if let error {
+                errorHandler(Socks5Error.remoteConnectionFailure(error))
+                return
+            }
+
+            localConnection.send(
+                content: content,
+                isComplete: isComplete,
+                completion: .contentProcessed { [self] error in
+                    if let error {
+                        errorHandler(Socks5Error.localConnectionFailure(error))
+                    } else if !isComplete {
+                        streamInboundTraffic()
+                    }
+                }
+            )
+        }
+    }
+}

ios/MullvadTransport/Socks5/Socks5EndpointReader.swift
@@ -1,0 +1,124 @@
+//
+//  Socks5EndpointReader.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 21/10/2023.
+//
+
+import Foundation
+import MullvadTypes
+import Network
+
+/// The object reading the endpoint data from connection.
+struct Socks5EndpointReader {
+    /// Connection to the socks proxy.
+    let connection: NWConnection
+
+    /// The expected address type.
+    let addressType: Socks5AddressType
+
+    /// Completion handler called upon success.
+    let onComplete: (Socks5Endpoint) -> Void
+
+    /// Failure handler.
+    let onFailure: (Error) -> Void
+
+    /// Start reading endpoint from connection.
+    func perform() {
+        // The length of IPv4 address in bytes.
+        let ipv4AddressLength = 4
+
+        // The length of IPv6 address in bytes.
+        let ipv6AddressLength = 16
+
+        switch addressType {
+        case .ipv4:
+            readBoundAddressAndPortInner(addressLength: ipv4AddressLength)
+
+        case .ipv6:
+            readBoundAddressAndPortInner(addressLength: ipv6AddressLength)
+
+        case .domainName:
+            readBoundDomainNameLength { [self] domainLength in
+                readBoundAddressAndPortInner(addressLength: domainLength)
+            }
+        }
+    }
+
+    private func readBoundAddressAndPortInner(addressLength: Int) {
+        // The length of port in bytes.
+        let portLength = MemoryLayout<UInt16>.size
+
+        // The entire length of address + port
+        let byteSize = addressLength + portLength
+
+        connection.receive(exactLength: byteSize) { [self] addressData, contentContext, isComplete, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let addressData {
+                do {
+                    let endpoint = try parseEndpoint(addressData: addressData, addressLength: addressLength)
+
+                    onComplete(endpoint)
+                } catch {
+                    onFailure(error)
+                }
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func readBoundDomainNameLength(completion: @escaping (Int) -> Void) {
+        // The length of domain length parameter in bytes.
+        let domainLengthLength = MemoryLayout<UInt8>.size
+
+        connection.receive(exactLength: domainLengthLength) { [self] data, contentContext, isComplete, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let domainNameLength = data?.first {
+                completion(Int(domainNameLength))
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func parseEndpoint(addressData: Data, addressLength: Int) throws -> Socks5Endpoint {
+        // The length of port in bytes.
+        let portLength = MemoryLayout<UInt16>.size
+
+        guard addressData.count == addressLength + portLength else { throw Socks5Error.unexpectedEndOfStream }
+
+        // Read address bytes.
+        let addressBytes = addressData[0 ..< addressLength]
+
+        // Read port bytes.
+        let port = addressData[addressLength...].withUnsafeBytes { buffer in
+            let value = buffer.load(as: UInt16.self)
+
+            // Port is passed in network byte order. Convert it to host order.
+            return UInt16(bigEndian: value)
+        }
+
+        // Parse address into endpoint.
+        switch addressType {
+        case .ipv4:
+            guard let ipAddress = IPv4Address(addressBytes) else { throw Socks5Error.parseIPv4Address }
+
+            return .ipv4(IPv4Endpoint(ip: ipAddress, port: port))
+
+        case .ipv6:
+            guard let ipAddress = IPv6Address(addressBytes) else { throw Socks5Error.parseIPv6Address }
+
+            return .ipv6(IPv6Endpoint(ip: ipAddress, port: port))
+
+        case .domainName:
+            guard let hostname = String(bytes: addressBytes, encoding: .utf8),
+                  let endpoint = Socks5HostEndpoint(hostname: hostname, port: port) else {
+                throw Socks5Error.decodeDomainName
+            }
+            return .domain(endpoint)
+        }
+    }
+}

ios/MullvadTransport/Socks5/Socks5HandshakeNegotiation.swift
@@ -1,0 +1,68 @@
+//
+//  Socks5HandshakeNegotiation.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 20/10/2023.
+//
+
+import Foundation
+import Network
+
+/// The object handling a handshake negotiation with socks proxy.
+struct Socks5HandshakeNegotiation {
+    let connection: NWConnection
+    let handshake: Socks5Handshake
+    let onComplete: (Socks5HandshakeReply) -> Void
+    let onFailure: (Error) -> Void
+
+    func perform() {
+        connection.send(content: handshake.rawData, completion: .contentProcessed { [self] error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else {
+                readReply()
+            }
+        })
+    }
+
+    private func readReply() {
+        // The length of a handshake reply in bytes.
+        let replyLength = 2
+
+        connection.receive(exactLength: replyLength) { [self] data, context, isComplete, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let data {
+                do {
+                    onComplete(try parseReply(data: data))
+                } catch {
+                    onFailure(error)
+                }
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func parseReply(data: Data) throws -> Socks5HandshakeReply {
+        var iterator = data.makeIterator()
+
+        guard let version = iterator.next() else { throw Socks5Error.unexpectedEndOfStream }
+        guard version == Socks5Constants.socksVersion else { throw Socks5Error.invalidSocksVersion }
+
+        guard let rawMethod = iterator.next() else { throw Socks5Error.unexpectedEndOfStream }
+
+        // The response code returned by the server when none of the auth methods listed by the client are acceptable.
+        let authMethodsUnacceptableReplyCode: UInt8 = 0xff
+
+        guard rawMethod != authMethodsUnacceptableReplyCode else {
+            throw Socks5Error.unacceptableAuthMethods
+        }
+
+        guard let method = Socks5AuthenticationMethod(rawValue: rawMethod) else {
+            throw Socks5Error.unsupportedAuthMethod
+        }
+
+        return Socks5HandshakeReply(method: method)
+    }
+}

chrispsyco/vpnvu-desktop

ios/MullvadREST/Transport/Socks5/Socks5DataStreamHandler.swift
@@ -1,0 +1,80 @@
+//
+//  Socks5DataStreamHandler.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 20/10/2023.
+//
+
+import Foundation
+import Network
+
+/// The object handling bidirectional streaming of data between local and remote connection.
+struct Socks5DataStreamHandler {
+    /// How many bytes the handler can receive at one time, when streaming data between local and remote connection.
+    static let maxBytesToRead = Int(UInt16.max)
+
+    /// Local TCP connection.
+    let localConnection: NWConnection
+
+    /// Remote TCP connection to the socks proxy.
+    let remoteConnection: NWConnection
+
+    /// Error handler.
+    let errorHandler: (Error) -> Void
+
+    /// Start streaming data between local and remote connection.
+    func start() {
+        streamOutboundTraffic()
+        streamInboundTraffic()
+    }
+
+    /// Pass outbound traffic from local to remote connection.
+    private func streamOutboundTraffic() {
+        localConnection.receive(
+            minimumIncompleteLength: 1,
+            maximumLength: Self.maxBytesToRead
+        ) { [self] content, _, isComplete, error in
+            if let error {
+                errorHandler(Socks5Error.localConnectionFailure(error))
+                return
+            }
+
+            remoteConnection.send(
+                content: content,
+                isComplete: isComplete,
+                completion: .contentProcessed { [self] error in
+                    if let error {
+                        errorHandler(Socks5Error.remoteConnectionFailure(error))
+                    } else if !isComplete {
+                        streamOutboundTraffic()
+                    }
+                }
+            )
+        }
+    }
+
+    /// Pass inbound traffic from remote to local connection.
+    private func streamInboundTraffic() {
+        remoteConnection.receive(
+            minimumIncompleteLength: 1,
+            maximumLength: Self.maxBytesToRead
+        ) { [self] content, _, isComplete, error in
+            if let error {
+                errorHandler(Socks5Error.remoteConnectionFailure(error))
+                return
+            }
+
+            localConnection.send(
+                content: content,
+                isComplete: isComplete,
+                completion: .contentProcessed { [self] error in
+                    if let error {
+                        errorHandler(Socks5Error.localConnectionFailure(error))
+                    } else if !isComplete {
+                        streamInboundTraffic()
+                    }
+                }
+            )
+        }
+    }
+}

ios/MullvadREST/Transport/Socks5/Socks5EndpointReader.swift
@@ -1,0 +1,124 @@
+//
+//  Socks5EndpointReader.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 21/10/2023.
+//
+
+import Foundation
+import MullvadTypes
+import Network
+
+/// The object reading the endpoint data from connection.
+struct Socks5EndpointReader {
+    /// Connection to the socks proxy.
+    let connection: NWConnection
+
+    /// The expected address type.
+    let addressType: Socks5AddressType
+
+    /// Completion handler called upon success.
+    let onComplete: (Socks5Endpoint) -> Void
+
+    /// Failure handler.
+    let onFailure: (Error) -> Void
+
+    /// Start reading endpoint from connection.
+    func perform() {
+        // The length of IPv4 address in bytes.
+        let ipv4AddressLength = 4
+
+        // The length of IPv6 address in bytes.
+        let ipv6AddressLength = 16
+
+        switch addressType {
+        case .ipv4:
+            readBoundAddressAndPortInner(addressLength: ipv4AddressLength)
+
+        case .ipv6:
+            readBoundAddressAndPortInner(addressLength: ipv6AddressLength)
+
+        case .domainName:
+            readBoundDomainNameLength { [self] domainLength in
+                readBoundAddressAndPortInner(addressLength: domainLength)
+            }
+        }
+    }
+
+    private func readBoundAddressAndPortInner(addressLength: Int) {
+        // The length of port in bytes.
+        let portLength = MemoryLayout<UInt16>.size
+
+        // The entire length of address + port
+        let byteSize = addressLength + portLength
+
+        connection.receive(exactLength: byteSize) { [self] addressData, _, _, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let addressData {
+                do {
+                    let endpoint = try parseEndpoint(addressData: addressData, addressLength: addressLength)
+
+                    onComplete(endpoint)
+                } catch {
+                    onFailure(error)
+                }
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func readBoundDomainNameLength(completion: @escaping (Int) -> Void) {
+        // The length of domain length parameter in bytes.
+        let domainLengthLength = MemoryLayout<UInt8>.size
+
+        connection.receive(exactLength: domainLengthLength) { [self] data, _, _, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let domainNameLength = data?.first {
+                completion(Int(domainNameLength))
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func parseEndpoint(addressData: Data, addressLength: Int) throws -> Socks5Endpoint {
+        // The length of port in bytes.
+        let portLength = MemoryLayout<UInt16>.size
+
+        guard addressData.count == addressLength + portLength else { throw Socks5Error.unexpectedEndOfStream }
+
+        // Read address bytes.
+        let addressBytes = addressData[0 ..< addressLength]
+
+        // Read port bytes.
+        let port = addressData[addressLength...].withUnsafeBytes { buffer in
+            let value = buffer.load(as: UInt16.self)
+
+            // Port is passed in network byte order. Convert it to host order.
+            return UInt16(bigEndian: value)
+        }
+
+        // Parse address into endpoint.
+        switch addressType {
+        case .ipv4:
+            guard let ipAddress = IPv4Address(addressBytes) else { throw Socks5Error.parseIPv4Address }
+
+            return .ipv4(IPv4Endpoint(ip: ipAddress, port: port))
+
+        case .ipv6:
+            guard let ipAddress = IPv6Address(addressBytes) else { throw Socks5Error.parseIPv6Address }
+
+            return .ipv6(IPv6Endpoint(ip: ipAddress, port: port))
+
+        case .domainName:
+            guard let hostname = String(bytes: addressBytes, encoding: .utf8),
+                  let endpoint = Socks5HostEndpoint(hostname: hostname, port: port) else {
+                throw Socks5Error.decodeDomainName
+            }
+            return .domain(endpoint)
+        }
+    }
+}

ios/MullvadREST/Transport/Socks5/Socks5HandshakeNegotiation.swift
@@ -1,0 +1,68 @@
+//
+//  Socks5HandshakeNegotiation.swift
+//  MullvadTransport
+//
+//  Created by pronebird on 20/10/2023.
+//
+
+import Foundation
+import Network
+
+/// The object handling a handshake negotiation with socks proxy.
+struct Socks5HandshakeNegotiation {
+    let connection: NWConnection
+    let handshake: Socks5Handshake
+    let onComplete: (Socks5HandshakeReply) -> Void
+    let onFailure: (Error) -> Void
+
+    func perform() {
+        connection.send(content: handshake.rawData, completion: .contentProcessed { [self] error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else {
+                readReply()
+            }
+        })
+    }
+
+    private func readReply() {
+        // The length of a handshake reply in bytes.
+        let replyLength = 2
+
+        connection.receive(exactLength: replyLength) { [self] data, _, _, error in
+            if let error {
+                onFailure(Socks5Error.remoteConnectionFailure(error))
+            } else if let data {
+                do {
+                    onComplete(try parseReply(data: data))
+                } catch {
+                    onFailure(error)
+                }
+            } else {
+                onFailure(Socks5Error.unexpectedEndOfStream)
+            }
+        }
+    }
+
+    private func parseReply(data: Data) throws -> Socks5HandshakeReply {
+        var iterator = data.makeIterator()
+
+        guard let version = iterator.next() else { throw Socks5Error.unexpectedEndOfStream }
+        guard version == Socks5Constants.socksVersion else { throw Socks5Error.invalidSocksVersion }
+
+        guard let rawMethod = iterator.next() else { throw Socks5Error.unexpectedEndOfStream }
+
+        // The response code returned by the server when none of the auth methods listed by the client are acceptable.
+        let authMethodsUnacceptableReplyCode: UInt8 = 0xff
+
+        guard rawMethod != authMethodsUnacceptableReplyCode else {
+            throw Socks5Error.unacceptableAuthMethods
+        }
+
+        guard let method = Socks5AuthenticationMethod(rawValue: rawMethod) else {
+            throw Socks5Error.unsupportedAuthMethod
+        }
+
+        return Socks5HandshakeReply(method: method)
+    }
+}