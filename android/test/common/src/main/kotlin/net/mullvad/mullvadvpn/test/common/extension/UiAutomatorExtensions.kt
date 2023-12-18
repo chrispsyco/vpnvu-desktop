mullvad/mullvadvpn-app

Fix changes dialog dismiss in mockapi tests

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/extension/UiAutomatorExtensions.kt
@@ -37,6 +37,15 @@ fun UiDevice.clickAgreeOnPrivacyDisclaimer() {
     findObjectWithTimeout(By.text("Agree and continue")).click()
 }
 
+// The dialog will only be shown when there's a new version code and bundled release notes.
+fun UiDevice.dismissChangelogDialogIfShow() {
+    try {
+        findObjectWithTimeout(By.text("Got it!")).click()
+    } catch (e: IllegalArgumentException) {
+        // This is OK since it means the changes dialog wasn't show.
+    }
+}
+
 fun UiDevice.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove(
     timeout: Long = DEFAULT_INTERACTION_TIMEOUT
 ) {

android/test/mockapi/src/main/kotlin/net/mullvad/mullvadvpn/test/mockapi/LoginMockApiTest.kt
@@ -7,6 +7,7 @@ import net.mullvad.mullvadvpn.compose.test.LOGIN_TITLE_TEST_TAG
 import net.mullvad.mullvadvpn.test.common.constant.DEFAULT_INTERACTION_TIMEOUT
 import net.mullvad.mullvadvpn.test.common.extension.clickAgreeOnPrivacyDisclaimer
 import net.mullvad.mullvadvpn.test.common.extension.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove
+import net.mullvad.mullvadvpn.test.common.extension.dismissChangelogDialogIfShow
 import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
 import net.mullvad.mullvadvpn.test.mockapi.util.currentUtcTimeWithOffsetZero
 import org.junit.Assert.assertTrue
@@ -28,6 +29,7 @@ class LoginMockApiTest : MockApiTest() {
         // Act
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShow()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 
@@ -53,6 +55,7 @@ class LoginMockApiTest : MockApiTest() {
         app.launch(endpoint)
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShow()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 
@@ -73,6 +76,7 @@ class LoginMockApiTest : MockApiTest() {
         app.launch(endpoint)
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShow()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 

chrispsyco/vpnvu-desktop

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/extension/UiAutomatorExtensions.kt
@@ -37,6 +37,15 @@ fun UiDevice.clickAgreeOnPrivacyDisclaimer() {
     findObjectWithTimeout(By.text("Agree and continue")).click()
 }
 
+// The dialog will only be shown when there's a new version code and bundled release notes.
+fun UiDevice.dismissChangelogDialogIfShown() {
+    try {
+        findObjectWithTimeout(By.text("Got it!")).click()
+    } catch (e: IllegalArgumentException) {
+        // This is OK since it means the changes dialog wasn't shown.
+    }
+}
+
 fun UiDevice.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove(
     timeout: Long = DEFAULT_INTERACTION_TIMEOUT
 ) {

android/test/mockapi/src/main/kotlin/net/mullvad/mullvadvpn/test/mockapi/LoginMockApiTest.kt
@@ -7,6 +7,7 @@ import net.mullvad.mullvadvpn.compose.test.LOGIN_TITLE_TEST_TAG
 import net.mullvad.mullvadvpn.test.common.constant.DEFAULT_INTERACTION_TIMEOUT
 import net.mullvad.mullvadvpn.test.common.extension.clickAgreeOnPrivacyDisclaimer
 import net.mullvad.mullvadvpn.test.common.extension.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove
+import net.mullvad.mullvadvpn.test.common.extension.dismissChangelogDialogIfShown
 import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
 import net.mullvad.mullvadvpn.test.mockapi.util.currentUtcTimeWithOffsetZero
 import org.junit.Assert.assertTrue
@@ -28,6 +29,7 @@ class LoginMockApiTest : MockApiTest() {
         // Act
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShown()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 
@@ -53,6 +55,7 @@ class LoginMockApiTest : MockApiTest() {
         app.launch(endpoint)
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShown()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 
@@ -73,6 +76,7 @@ class LoginMockApiTest : MockApiTest() {
         app.launch(endpoint)
         device.clickAgreeOnPrivacyDisclaimer()
         device.clickAllowOnNotificationPermissionPromptIfApiLevel33AndAbove()
+        device.dismissChangelogDialogIfShown()
         app.waitForLoginPrompt()
         app.attemptLogin(validAccountToken)
 