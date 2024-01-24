mullvad/mullvadvpn-app

Enable testing API access with a given configuration

ios/MullvadVPN/AccessMethodRepository/ProxyConfigurationTester.swift
@@ -8,31 +8,38 @@
 
 import Combine
 import Foundation
+import MullvadREST
 import MullvadSettings
+import MullvadTypes
 
 /// A concrete implementation of an access method proxy configuration.
 class ProxyConfigurationTester: ProxyConfigurationTesterProtocol {
-    private var cancellable: Cancellable?
+    private var cancellable: MullvadTypes.Cancellable?
+    private let transportProvider: ConfiguredTransportProvider
+    private var headRequest: REST.HeadRequest?
 
-    static let shared = ProxyConfigurationTester()
-
-    init() {}
+    init(transportProvider: ConfiguredTransportProvider) {
+        self.transportProvider = transportProvider
+    }
 
     func start(configuration: PersistentProxyConfiguration, completion: @escaping (Error?) -> Void) {
-        let workItem = DispatchWorkItem {
-            let randomResult = (0 ... 255).randomElement()?.isMultiple(of: 2) ?? true
-
-            completion(randomResult ? nil : URLError(.timedOut))
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: workItem)
-
-        cancellable = AnyCancellable {
-            workItem.cancel()
+        do {
+            let transport = try transportProvider.makeTransport(with: configuration)
+            let request = REST.HeadRequest(transport: transport)
+            headRequest = request
+            cancellable = request.makeRequest { error in
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+            }
+        } catch {
+            completion(error)
         }
     }
 
     func cancel() {
+        cancellable?.cancel()
         cancellable = nil
+        headRequest = nil
     }
 }

ios/MullvadVPN/AppDelegate.swift
@@ -43,6 +43,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private let migrationManager = MigrationManager()
 
     private(set) var accessMethodRepository = AccessMethodRepository()
+    private(set) var shadowsocksLoader: ShadowsocksLoaderProtocol!
+    private(set) var configuredTransportProvider: ConfiguredTransportProvider!
 
     // MARK: - Application lifecycle
 
@@ -94,14 +96,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         // This init cannot fail as long as the security group identifier is valid
         let sharedUserDefaults = UserDefaults(suiteName: ApplicationConfiguration.securityGroupIdentifier)!
+        shadowsocksLoader = ShadowsocksLoader(
+            shadowsocksCache: shadowsocksCache,
+            relayCache: relayCache,
+            constraintsUpdater: constraintsUpdater
+        )
+
+        configuredTransportProvider = ConfiguredTransportProvider(
+            shadowsocksLoader: shadowsocksLoader,
+            addressCache: addressCache
+        )
+
         let transportStrategy = TransportStrategy(
             sharedUserDefaults,
             datasource: accessMethodRepository,
-            shadowsocksLoader: ShadowsocksLoader(
-                shadowsocksCache: shadowsocksCache,
-                relayCache: relayCache,
-                constraintsUpdater: constraintsUpdater
-            )
+            shadowsocksLoader: shadowsocksLoader
         )
 
         let transportProvider = TransportProvider(

ios/MullvadVPN/Coordinators/ApplicationCoordinator.swift
@@ -78,6 +78,7 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
     private var appPreferences: AppPreferencesDataSource
     private var outgoingConnectionService: OutgoingConnectionServiceHandling
     private var accessMethodRepository: AccessMethodRepositoryProtocol
+    private let configuredTransportProvider: ConfiguredTransportProvider
 
     private var outOfTimeTimer: Timer?
 
@@ -94,7 +95,8 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         accountsProxy: RESTAccountHandling,
         outgoingConnectionService: OutgoingConnectionServiceHandling,
         appPreferences: AppPreferencesDataSource,
-        accessMethodRepository: AccessMethodRepositoryProtocol
+        accessMethodRepository: AccessMethodRepositoryProtocol,
+        transportProvider: ConfiguredTransportProvider
     ) {
         self.tunnelManager = tunnelManager
         self.storePaymentManager = storePaymentManager
@@ -105,6 +107,7 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         self.appPreferences = appPreferences
         self.outgoingConnectionService = outgoingConnectionService
         self.accessMethodRepository = accessMethodRepository
+        self.configuredTransportProvider = transportProvider
 
         super.init()
 
@@ -758,10 +761,14 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         )
 
         let navigationController = CustomNavigationController()
+
+        let configurationTester = ProxyConfigurationTester(transportProvider: configuredTransportProvider)
+
         let coordinator = SettingsCoordinator(
             navigationController: navigationController,
             interactorFactory: interactorFactory,
-            accessMethodRepository: accessMethodRepository
+            accessMethodRepository: accessMethodRepository,
+            proxyConfigurationTester: configurationTester
         )
 
         coordinator.didFinish = { [weak self] _ in

chrispsyco/vpnvu-desktop

ios/MullvadVPN/AccessMethodRepository/ProxyConfigurationTester.swift
@@ -8,31 +8,38 @@
 
 import Combine
 import Foundation
+import MullvadREST
 import MullvadSettings
+import MullvadTypes
 
 /// A concrete implementation of an access method proxy configuration.
 class ProxyConfigurationTester: ProxyConfigurationTesterProtocol {
-    private var cancellable: Cancellable?
+    private var cancellable: MullvadTypes.Cancellable?
+    private let transportProvider: ProxyConfigurationTransportProvider
+    private var headRequest: REST.APIAvailabilityTestRequest?
 
-    static let shared = ProxyConfigurationTester()
-
-    init() {}
+    init(transportProvider: ProxyConfigurationTransportProvider) {
+        self.transportProvider = transportProvider
+    }
 
     func start(configuration: PersistentProxyConfiguration, completion: @escaping (Error?) -> Void) {
-        let workItem = DispatchWorkItem {
-            let randomResult = (0 ... 255).randomElement()?.isMultiple(of: 2) ?? true
-
-            completion(randomResult ? nil : URLError(.timedOut))
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: workItem)
-
-        cancellable = AnyCancellable {
-            workItem.cancel()
+        do {
+            let transport = try transportProvider.makeTransport(with: configuration)
+            let request = REST.APIAvailabilityTestRequest(transport: transport)
+            headRequest = request
+            cancellable = request.makeRequest { error in
+                DispatchQueue.main.async {
+                    completion(error)
+                }
+            }
+        } catch {
+            completion(error)
         }
     }
 
     func cancel() {
+        cancellable?.cancel()
         cancellable = nil
+        headRequest = nil
     }
 }

ios/MullvadVPN/AppDelegate.swift
@@ -43,6 +43,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private let migrationManager = MigrationManager()
 
     private(set) var accessMethodRepository = AccessMethodRepository()
+    private(set) var shadowsocksLoader: ShadowsocksLoaderProtocol!
+    private(set) var configuredTransportProvider: ProxyConfigurationTransportProvider!
 
     // MARK: - Application lifecycle
 
@@ -94,14 +96,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
         // This init cannot fail as long as the security group identifier is valid
         let sharedUserDefaults = UserDefaults(suiteName: ApplicationConfiguration.securityGroupIdentifier)!
+        shadowsocksLoader = ShadowsocksLoader(
+            shadowsocksCache: shadowsocksCache,
+            relayCache: relayCache,
+            constraintsUpdater: constraintsUpdater
+        )
+
+        configuredTransportProvider = ProxyConfigurationTransportProvider(
+            shadowsocksLoader: shadowsocksLoader,
+            addressCache: addressCache
+        )
+
         let transportStrategy = TransportStrategy(
             sharedUserDefaults,
             datasource: accessMethodRepository,
-            shadowsocksLoader: ShadowsocksLoader(
-                shadowsocksCache: shadowsocksCache,
-                relayCache: relayCache,
-                constraintsUpdater: constraintsUpdater
-            )
+            shadowsocksLoader: shadowsocksLoader
         )
 
         let transportProvider = TransportProvider(

ios/MullvadVPN/Coordinators/ApplicationCoordinator.swift
@@ -78,6 +78,7 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
     private var appPreferences: AppPreferencesDataSource
     private var outgoingConnectionService: OutgoingConnectionServiceHandling
     private var accessMethodRepository: AccessMethodRepositoryProtocol
+    private let configuredTransportProvider: ProxyConfigurationTransportProvider
 
     private var outOfTimeTimer: Timer?
 
@@ -94,7 +95,8 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         accountsProxy: RESTAccountHandling,
         outgoingConnectionService: OutgoingConnectionServiceHandling,
         appPreferences: AppPreferencesDataSource,
-        accessMethodRepository: AccessMethodRepositoryProtocol
+        accessMethodRepository: AccessMethodRepositoryProtocol,
+        transportProvider: ProxyConfigurationTransportProvider
     ) {
         self.tunnelManager = tunnelManager
         self.storePaymentManager = storePaymentManager
@@ -105,6 +107,7 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         self.appPreferences = appPreferences
         self.outgoingConnectionService = outgoingConnectionService
         self.accessMethodRepository = accessMethodRepository
+        self.configuredTransportProvider = transportProvider
 
         super.init()
 
@@ -758,10 +761,14 @@ final class ApplicationCoordinator: Coordinator, Presenting, RootContainerViewCo
         )
 
         let navigationController = CustomNavigationController()
+
+        let configurationTester = ProxyConfigurationTester(transportProvider: configuredTransportProvider)
+
         let coordinator = SettingsCoordinator(
             navigationController: navigationController,
             interactorFactory: interactorFactory,
-            accessMethodRepository: accessMethodRepository
+            accessMethodRepository: accessMethodRepository,
+            proxyConfigurationTester: configurationTester
         )
 
         coordinator.didFinish = { [weak self] _ in