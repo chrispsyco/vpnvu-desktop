sdht0/mullvadvpn-app

Add setting to toggle maps

gui/src/main/settings.ts
@@ -93,6 +93,10 @@ export default class Settings implements Readonly<ISettings> {
       this.delegate.handleUnpinnedWindowChange();
     });
 
+    IpcMainEventChannel.guiSettings.handleSetDisplayMap((displayMap: boolean) => {
+      this.guiSettings.displayMap = displayMap;
+    });
+
     IpcMainEventChannel.currentVersion.handleDisplayedChangelog(() => {
       this.guiSettings.changelogDisplayedForVersion = this.currentVersion.gui;
     });

gui/src/renderer/app.tsx
@@ -335,6 +335,8 @@ export default class AppRenderer {
   public openUrl = (url: string) => IpcRendererEventChannel.app.openUrl(url);
   public showOpenDialog = (options: Electron.OpenDialogOptions) =>
     IpcRendererEventChannel.app.showOpenDialog(options);
+  public setDisplayMap = (displayMap: boolean): void =>
+    IpcRendererEventChannel.guiSettings.setDisplayMap(displayMap);
 
   public login = async (accountToken: AccountToken) => {
     const actions = this.reduxActions;

gui/src/shared/gui-settings-state.ts
@@ -31,4 +31,7 @@ export interface IGuiSettingsState {
   // The last version that the changelog dialog was shown for. This is used to only show the
   // changelog after upgrade.
   changelogDisplayedForVersion: string;
+
+  // Tells the app whether or not to show the map in the main view.
+  displayMap: boolean;
 }

gui/src/shared/ipc-schema.ts
@@ -178,6 +178,7 @@ export const ipcSchema = {
     setMonochromaticIcon: send<boolean>(),
     setPreferredLocale: invoke<string, ITranslations>(),
     setUnpinnedWindow: send<boolean>(),
+    setDisplayMap: send<boolean>(),
   },
   account: {
     '': notifyRenderer<IAccountData | undefined>(),

chrispsyco/vpnvu-desktop

gui/src/main/settings.ts
@@ -111,6 +111,10 @@ export default class Settings implements Readonly<ISettings> {
       this.delegate.handleUnpinnedWindowChange();
     });
 
+    IpcMainEventChannel.guiSettings.handleSetAnimateMap((animateMap: boolean) => {
+      this.guiSettings.animateMap = animateMap;
+    });
+
     IpcMainEventChannel.currentVersion.handleDisplayedChangelog(() => {
       this.guiSettings.changelogDisplayedForVersion = this.currentVersion.gui;
     });

gui/src/renderer/app.tsx
@@ -364,6 +364,8 @@ export default class AppRenderer {
   public testCustomApiAccessMethod = (method: CustomProxy) =>
     IpcRendererEventChannel.settings.testCustomApiAccessMethod(method);
   public getMapData = () => IpcRendererEventChannel.map.getData();
+  public setAnimateMap = (displayMap: boolean): void =>
+    IpcRendererEventChannel.guiSettings.setAnimateMap(displayMap);
 
   public login = async (accountToken: AccountToken) => {
     const actions = this.reduxActions;

gui/src/shared/gui-settings-state.ts
@@ -31,4 +31,7 @@ export interface IGuiSettingsState {
   // The last version that the changelog dialog was shown for. This is used to only show the
   // changelog after upgrade.
   changelogDisplayedForVersion: string;
+
+  // Tells the app whether or not to show the map in the main view.
+  animateMap: boolean;
 }

gui/src/shared/ipc-schema.ts
@@ -196,6 +196,7 @@ export const ipcSchema = {
     setMonochromaticIcon: send<boolean>(),
     setPreferredLocale: invoke<string, ITranslations>(),
     setUnpinnedWindow: send<boolean>(),
+    setAnimateMap: send<boolean>(),
   },
   account: {
     '': notifyRenderer<IAccountData | undefined>(),