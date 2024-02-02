chrispsyco/vpnvu-desktop

commit 8b4d5aa7ba4cdb7a3f45ce271b4e7ee174c75882

Fix `shellcheck` lints of our various bash scripts
The following lints have been fixed:
SC2046,SC2086,SC2068,SC2148,SC2007,SC2004,SC2006,
SC2164,SC2145,SC1091,SC2034,SC2155.

build-apk.sh
@@ -21,7 +21,7 @@ BUILD_BUNDLE="no"
 CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"target"}
 SKIP_STRIPPING=${SKIP_STRIPPING:-"no"}
 
-while [ ! -z "${1:-""}" ]; do
+while [ -n "${1:-""}" ]; do
     if [[ "${1:-""}" == "--dev-build" ]]; then
         BUILD_TYPE="debug"
         GRADLE_BUILD_TYPE="debug"
@@ -103,7 +103,7 @@ for ARCHITECTURE in ${ARCHITECTURES:-aarch64 armv7 x86_64 i686}; do
     esac
 
     echo "Building mullvad-daemon for $TARGET"
-    cargo build $CARGO_ARGS --target "$TARGET" --package mullvad-jni
+    cargo build "$CARGO_ARGS" --target "$TARGET" --package mullvad-jni
 
     STRIP_TOOL="${NDK_TOOLCHAIN_DIR}/llvm-strip"
     TARGET_LIB_PATH="$SCRIPT_DIR/android/app/build/extraJni/$ABI/libmullvad_jni.so"
@@ -117,7 +117,7 @@ for ARCHITECTURE in ${ARCHITECTURES:-aarch64 armv7 x86_64 i686}; do
 done
 
 echo "Updating relays.json..."
-cargo run --bin relay_list $CARGO_ARGS > build/relays.json
+cargo run --bin relay_list "$CARGO_ARGS" > build/relays.json
 
 cd "$SCRIPT_DIR/android"
 $GRADLE_CMD --console plain "${GRADLE_TASKS[@]}"

android/docs/diagrams/update_graphs.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-plantuml $SCRIPT_DIR/*.puml
+plantuml "$SCRIPT_DIR"/*.puml

android/fdroid-build/env.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 # Sourcing this file prepares the environment for building inside the F-Droid build server
 
 # Ensure Cargo tools are accessible
+# shellcheck source=/dev/null
 source "$HOME/.cargo/env"
 
 # Ensure Go compiler is accessible

android/fdroid-build/init.sh
@@ -5,12 +5,12 @@ set -eux
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 REPO_DIR="$SCRIPT_DIR/../../"
-TOOLCHAINS_DIR="$HOME/android-ndk-toolchains"
 
 # Install Rust
 curl -sf -L https://sh.rustup.rs > /tmp/rustup.sh
 chmod +x /tmp/rustup.sh
 /tmp/rustup.sh -y
+# shellcheck source=/dev/null
 source "$HOME/.cargo/env"
 rustup set profile minimal
 rustup target add \

android/scripts/generate-pngs.sh
@@ -46,11 +46,12 @@ function convert_image() {
 
     local source_image="$1"
     local dpi_config="$2"
+    local destination_image
 
     if (( $# >= 3 )); then
-        local destination_image="$3"
+        destination_image="$3"
     else
-        local destination_image="$(basename "$source_image" .svg | sed -e 's/-/_/g')"
+        destination_image="$(basename "$source_image" .svg | sed -e 's/-/_/g')"
     fi
 
     if (( $# >= 4 )); then
@@ -59,8 +60,10 @@ function convert_image() {
         local destination_dir="drawable"
     fi
 
-    local dpi="$(echo "$dpi_config" | cut -f1 -d'-')"
-    local size="$(echo "$dpi_config" | cut -f2 -d'-')"
+    local dpi
+    dpi="$(echo "$dpi_config" | cut -f1 -d'-')"
+    local size
+    size="$(echo "$dpi_config" | cut -f2 -d'-')"
 
     local dpi_dir="../lib/resource/src/main/res/${destination_dir}-${dpi}"
 

android/scripts/run-instrumented-tests-locally.sh
@@ -4,7 +4,7 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/..
 ./gradlew assembleOssProdAndroidTest
 ./gradlew app:assembleOssProdDebug
-$SCRIPT_DIR/run-instrumented-tests.sh app
+"$SCRIPT_DIR"/run-instrumented-tests.sh app

android/scripts/run-instrumented-tests.sh
@@ -23,7 +23,7 @@ INVALID_TEST_ACCOUNT_TOKEN="${INVALID_TEST_ACCOUNT_TOKEN:-}"
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --test-type)
-            if [[ ! -z ${2-} && "$2" =~ ^(app|mockapi|e2e)$ ]]; then
+            if [[ -n "${2-}" && "$2" =~ ^(app|mockapi|e2e)$ ]]; then
                 TEST_TYPE="$2"
             else
                 echo "Error: Bad or missing test type. Must be one of: app, mockapi, e2e"
@@ -32,7 +32,7 @@ while [[ "$#" -gt 0 ]]; do
             shift 2
             ;;
         --infra-flavor)
-            if [[ ! -z ${2-} && "$2" =~ ^(prod|stagemole)$ ]]; then
+            if [[ -n "${2-}" && "$2" =~ ^(prod|stagemole)$ ]]; then
                 INFRA_FLAVOR="$2"
             else
                 echo "Error: Bad or missing infra flavor. Must be one of: prod, stagemole"
@@ -41,7 +41,7 @@ while [[ "$#" -gt 0 ]]; do
             shift 2
             ;;
         --billing-flavor)
-            if [[ ! -z ${2-} && "$2" =~ ^(oss|play)$ ]]; then
+            if [[ -n "${2-}" && "$2" =~ ^(oss|play)$ ]]; then
                 BILLING_FLAVOR="$2"
             else
                 echo "Error: Bad or missing billing flavor. Must be one of: oss, play"

building/build-and-publish-container-image.sh
@@ -53,7 +53,7 @@ trap 'delete_tmp_signature_dir' EXIT
 
 log_header "Pushing $full_container_name:latest"
 podman push "$full_container_name:latest" \
-    --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT \
+    --sign-by "$CONTAINER_SIGNING_KEY_FINGERPRINT" \
     --digestfile "$tmp_signature_dir/digest_latest"
 
 digest=$(cat "$tmp_signature_dir/digest_latest")
@@ -68,7 +68,7 @@ cp "$signature_dir/signature-1" "$tmp_signature_dir/signature-2"
 
 log_header "Pushing $full_container_name:$tag"
 podman push "$full_container_name:$tag" \
-    --sign-by $CONTAINER_SIGNING_KEY_FINGERPRINT \
+    --sign-by "$CONTAINER_SIGNING_KEY_FINGERPRINT" \
     --digestfile "$tmp_signature_dir/digest_$tag"
 
 if ! cmp -s "$tmp_signature_dir/digest_latest" "$tmp_signature_dir/digest_$tag"; then

commit 47cccb2e385d75bad5a2c5ce32f95db1cfbb7e37

Properly pass arguments to Cargo in `build-apk.sh`
The `build-apk.sh` script broke in 8b4d5aa7ba4cdb7a3f45ce271b4e7ee174c75882
due to quoting a variable containing a string with space seperated
arguments. While it is best practice to quote variables to prevent word
splitting, word splitting was the behavior that we wanted in this
particular case.

This commit fixes this issue by building an array of arguments instead,
which when expanded will pass along the arguments to `cargo build` properly
as multiple strings. The script now works, and `shellcheck` is happy!

build-apk.sh
@@ -15,7 +15,7 @@ BUILD_TYPE="release"
 GRADLE_BUILD_TYPE="release"
 GRADLE_TASKS=(createOssProdReleaseDistApk createPlayProdReleaseDistApk)
 BUNDLE_TASKS=(createPlayProdReleaseDistBundle)
-CARGO_ARGS="--release"
+CARGO_ARGS=( "--release" )
 EXTRA_WGGO_ARGS=""
 BUILD_BUNDLE="no"
 CARGO_TARGET_DIR=${CARGO_TARGET_DIR:-"target"}
@@ -25,7 +25,7 @@ while [ -n "${1:-""}" ]; do
     if [[ "${1:-""}" == "--dev-build" ]]; then
         BUILD_TYPE="debug"
         GRADLE_BUILD_TYPE="debug"
-        CARGO_ARGS=""
+        CARGO_ARGS=()
         GRADLE_TASKS=(createOssProdDebugDistApk)
         BUNDLE_TASKS=(createOssProdDebugDistBundle)
     elif [[ "${1:-""}" == "--fdroid" ]]; then
@@ -55,9 +55,9 @@ fi
 if [[ "$BUILD_TYPE" == "release" && "$PRODUCT_VERSION" != *"-dev-"* ]]; then
     echo "Removing old Rust build artifacts"
     cargo clean
-    CARGO_ARGS+=" --locked"
+    CARGO_ARGS+=( "--locked" )
 else
-    CARGO_ARGS+=" --features api-override"
+    CARGO_ARGS+=( "--features" "api-override" )
     GRADLE_TASKS+=(createPlayDevmoleReleaseDistApk createPlayStagemoleReleaseDistApk)
     BUNDLE_TASKS+=(createPlayDevmoleReleaseDistBundle createPlayStagemoleReleaseDistBundle)
 fi
@@ -103,7 +103,7 @@ for ARCHITECTURE in ${ARCHITECTURES:-aarch64 armv7 x86_64 i686}; do
     esac
 
     echo "Building mullvad-daemon for $TARGET"
-    cargo build "$CARGO_ARGS" --target "$TARGET" --package mullvad-jni
+    cargo build "${CARGO_ARGS[@]}" --target "$TARGET" --package mullvad-jni
 
     STRIP_TOOL="${NDK_TOOLCHAIN_DIR}/llvm-strip"
     TARGET_LIB_PATH="$SCRIPT_DIR/android/app/build/extraJni/$ABI/libmullvad_jni.so"
@@ -117,7 +117,7 @@ for ARCHITECTURE in ${ARCHITECTURES:-aarch64 armv7 x86_64 i686}; do
 done
 
 echo "Updating relays.json..."
-cargo run --bin relay_list "$CARGO_ARGS" > build/relays.json
+cargo run --bin relay_list "${CARGO_ARGS[@]}" > build/relays.json
 
 cd "$SCRIPT_DIR/android"
 $GRADLE_CMD --console plain "${GRADLE_TASKS[@]}"