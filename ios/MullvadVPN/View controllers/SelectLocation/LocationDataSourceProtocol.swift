mullvad/mullvadvpn-app

Split select location view into two sections

ios/MullvadVPN/View controllers/SelectLocation/LocationDataSourceProtocol.swift
@@ -12,57 +12,46 @@ import MullvadTypes
 import UIKit
 
 protocol LocationDataSourceProtocol {
-    var nodeByLocation: [RelayLocation: SelectLocationNode] { get }
-
-    func search(by text: String) -> [RelayLocation]
-
-    func reload(
-        _ response: REST.ServerRelaysResponse,
-        relays: [REST.ServerRelay]
-    ) -> [RelayLocation]
+    var nodes: [LocationNode] { get }
+    var searchableNodes: [LocationNode] { get }
 }
 
 extension LocationDataSourceProtocol {
-    func makeRootNode(name: String) -> SelectLocationNode {
-        SelectLocationNode(nodeType: .root, location: .country("#root"), displayName: name)
-    }
-
-    func createNode(
-        root: SelectLocationNode,
-        ancestorOrSelf: RelayLocation,
-        serverLocation: REST.ServerLocation,
-        relay: REST.ServerRelay,
-        wasShowingChildren: Bool
-    ) -> SelectLocationNode {
-        let node: SelectLocationNode
-
-        switch ancestorOrSelf {
-        case .country:
-            node = SelectLocationNode(
-                nodeType: .country,
-                location: ancestorOrSelf,
-                displayName: serverLocation.country,
-                showsChildren: wasShowingChildren
-            )
-            root.addChild(node)
-        case let .city(countryCode, _):
-            node = SelectLocationNode(
-                nodeType: .city,
-                location: ancestorOrSelf,
-                displayName: serverLocation.city,
-                showsChildren: wasShowingChildren
-            )
-            nodeByLocation[.country(countryCode)]!.addChild(node)
-
-        case let .hostname(countryCode, cityCode, _):
-            node = SelectLocationNode(
-                nodeType: .relay,
-                location: ancestorOrSelf,
-                displayName: relay.hostname,
-                isActive: relay.active
-            )
-            nodeByLocation[.city(countryCode, cityCode)]!.addChild(node)
+    func search(by text: String) -> [LocationNode] {
+        guard !text.isEmpty else {
+            return nodes
         }
-        return node
+
+        var filteredNodes: [LocationNode] = []
+        searchableNodes.forEach { countryNode in
+            countryNode.showsChildren = false
+
+            if countryNode.nodeName.fuzzyMatch(text) {
+                filteredNodes.append(countryNode)
+            }
+
+            countryNode.children.forEach { cityNode in
+                cityNode.showsChildren = false
+
+                let relaysContainSearchString = cityNode.children
+                    .contains(where: { $0.nodeName.fuzzyMatch(text) })
+
+                if cityNode.nodeName.fuzzyMatch(text) || relaysContainSearchString {
+                    if !filteredNodes.contains(countryNode) {
+                        filteredNodes.append(countryNode)
+                    }
+
+                    filteredNodes.append(cityNode)
+                    countryNode.showsChildren = true
+
+                    if relaysContainSearchString {
+                        filteredNodes.append(contentsOf: cityNode.children.map { $0 })
+                        cityNode.showsChildren = true
+                    }
+                }
+            }
+        }
+
+        return filteredNodes
     }
 }

chrispsyco/vpnvu-desktop

ios/MullvadVPN/View controllers/SelectLocation/LocationDataSourceProtocol.swift
@@ -12,57 +12,67 @@ import MullvadTypes
 import UIKit
 
 protocol LocationDataSourceProtocol {
-    var nodeByLocation: [RelayLocation: SelectLocationNode] { get }
-
-    func search(by text: String) -> [RelayLocation]
-
-    func reload(
-        _ response: REST.ServerRelaysResponse,
-        relays: [REST.ServerRelay]
-    ) -> [RelayLocation]
+    var nodes: [LocationNode] { get }
+    var searchableNodes: [LocationNode] { get }
 }
 
 extension LocationDataSourceProtocol {
-    func makeRootNode(name: String) -> SelectLocationNode {
-        SelectLocationNode(nodeType: .root, location: .country("#root"), displayName: name)
+    func search(by text: String) -> [LocationNode] {
+        guard !text.isEmpty else {
+            resetNodes()
+            return nodes
+        }
+
+        var filteredNodes: [LocationNode] = []
+
+        searchableNodes.forEach { countryNode in
+            countryNode.showsChildren = false
+
+            if countryNode.name.fuzzyMatch(text) {
+                filteredNodes.append(countryNode)
+            }
+
+            countryNode.children.forEach { cityNode in
+                cityNode.showsChildren = false
+                cityNode.isHiddenFromSearch = true
+
+                var relaysContainSearchString = false
+                cityNode.children.forEach { hostNode in
+                    hostNode.isHiddenFromSearch = true
+
+                    if hostNode.name.fuzzyMatch(text) {
+                        relaysContainSearchString = true
+                        hostNode.isHiddenFromSearch = false
+                    }
+                }
+
+                if cityNode.name.fuzzyMatch(text) || relaysContainSearchString {
+                    if !filteredNodes.contains(countryNode) {
+                        filteredNodes.append(countryNode)
+                    }
+
+                    countryNode.showsChildren = true
+                    cityNode.isHiddenFromSearch = false
+
+                    if relaysContainSearchString {
+                        cityNode.showsChildren = true
+                    }
+                }
+            }
+        }
+
+        return filteredNodes
     }
 
-    func createNode(
-        root: SelectLocationNode,
-        ancestorOrSelf: RelayLocation,
-        serverLocation: REST.ServerLocation,
-        relay: REST.ServerRelay,
-        wasShowingChildren: Bool
-    ) -> SelectLocationNode {
-        let node: SelectLocationNode
+    private func resetNodes() {
+        nodes.forEach { node in
+            node.showsChildren = false
+            node.isHiddenFromSearch = false
 
-        switch ancestorOrSelf {
-        case .country:
-            node = SelectLocationNode(
-                nodeType: .country,
-                location: ancestorOrSelf,
-                displayName: serverLocation.country,
-                showsChildren: wasShowingChildren
-            )
-            root.addChild(node)
-        case let .city(countryCode, _):
-            node = SelectLocationNode(
-                nodeType: .city,
-                location: ancestorOrSelf,
-                displayName: serverLocation.city,
-                showsChildren: wasShowingChildren
-            )
-            nodeByLocation[.country(countryCode)]!.addChild(node)
-
-        case let .hostname(countryCode, cityCode, _):
-            node = SelectLocationNode(
-                nodeType: .relay,
-                location: ancestorOrSelf,
-                displayName: relay.hostname,
-                isActive: relay.active
-            )
-            nodeByLocation[.city(countryCode, cityCode)]!.addChild(node)
+            node.forEachDescendant { descendantNode in
+                descendantNode.showsChildren = false
+                descendantNode.isHiddenFromSearch = false
+            }
         }
-        return node
     }
 }