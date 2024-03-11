mullvad/mullvadvpn-app

Improve divider useage and replace deprecated divider

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/FilterScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +35,8 @@ import net.mullvad.mullvadvpn.compose.button.ApplyButton
 import net.mullvad.mullvadvpn.compose.cell.CheckboxCell
 import net.mullvad.mullvadvpn.compose.cell.ExpandableComposeCell
 import net.mullvad.mullvadvpn.compose.cell.SelectableCell
+import net.mullvad.mullvadvpn.compose.extensions.itemWithDivider
+import net.mullvad.mullvadvpn.compose.extensions.itemsWithDivider
 import net.mullvad.mullvadvpn.compose.state.RelayFilterState
 import net.mullvad.mullvadvpn.compose.transitions.SlideInFromRightTransition
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
@@ -146,8 +146,7 @@ fun FilterScreen(
         },
     ) { contentPadding ->
         LazyColumn(modifier = Modifier.padding(contentPadding).fillMaxSize()) {
-            item {
-                Divider()
+            itemWithDivider {
                 ExpandableComposeCell(
                     title = stringResource(R.string.ownership),
                     isExpanded = ownershipExpanded,
@@ -164,8 +163,7 @@ fun FilterScreen(
                         onCellClicked = { onSelectedOwnership(null) }
                     )
                 }
-                items(uiState.filteredOwnershipByProviders) { ownership ->
-                    Divider()
+                itemsWithDivider(uiState.filteredOwnershipByProviders) { ownership ->
                     SelectableCell(
                         title = stringResource(id = ownership.stringResource()),
                         isSelected = ownership == uiState.selectedOwnership,
@@ -173,8 +171,7 @@ fun FilterScreen(
                     )
                 }
             }
-            item {
-                Divider()
+            itemWithDivider {
                 ExpandableComposeCell(
                     title = stringResource(R.string.providers),
                     isExpanded = providerExpanded,
@@ -184,16 +181,14 @@ fun FilterScreen(
                 )
             }
             if (providerExpanded) {
-                item {
-                    Divider()
+                itemWithDivider {
                     CheckboxCell(
                         providerName = stringResource(R.string.all_providers),
                         checked = uiState.isAllProvidersChecked,
                         onCheckedChange = { isChecked -> onAllProviderCheckChange(isChecked) }
                     )
                 }
-                items(uiState.filteredProvidersByOwnership) { provider ->
-                    Divider()
+                itemsWithDivider(uiState.filteredProvidersByOwnership) { provider ->
                     CheckboxCell(
                         providerName = provider.name,
                         checked = provider in uiState.selectedProviders,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -67,6 +65,7 @@ import net.mullvad.mullvadvpn.compose.destinations.WireguardPortInfoDialogDestin
 import net.mullvad.mullvadvpn.compose.dialog.WireguardCustomPortNavArgs
 import net.mullvad.mullvadvpn.compose.dialog.WireguardPortInfoDialogArgument
 import net.mullvad.mullvadvpn.compose.extensions.itemWithDivider
+import net.mullvad.mullvadvpn.compose.extensions.itemsIndexedWithDivider
 import net.mullvad.mullvadvpn.compose.state.VpnSettingsUiState
 import net.mullvad.mullvadvpn.compose.test.LAZY_LIST_LAST_ITEM_TEST_TAG
 import net.mullvad.mullvadvpn.compose.test.LAZY_LIST_QUANTUM_ITEM_OFF_TEST_TAG
@@ -436,7 +435,7 @@ fun VpnSettingsScreen(
             }
 
             if (uiState.isCustomDnsEnabled) {
-                itemsIndexed(uiState.customDnsItems) { index, item ->
+                itemsIndexedWithDivider(uiState.customDnsItems) { index, item ->
                     DnsCell(
                         address = item.address,
                         isUnreachableLocalDnsWarningVisible =
@@ -444,7 +443,6 @@ fun VpnSettingsScreen(
                         onClick = { navigateToDns(index, item.address) },
                         modifier = Modifier.animateItemPlacement()
                     )
-                    Divider()
                 }
 
                 itemWithDivider {

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/FilterScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +35,8 @@ import net.mullvad.mullvadvpn.compose.button.ApplyButton
 import net.mullvad.mullvadvpn.compose.cell.CheckboxCell
 import net.mullvad.mullvadvpn.compose.cell.ExpandableComposeCell
 import net.mullvad.mullvadvpn.compose.cell.SelectableCell
+import net.mullvad.mullvadvpn.compose.extensions.itemWithDivider
+import net.mullvad.mullvadvpn.compose.extensions.itemsWithDivider
 import net.mullvad.mullvadvpn.compose.state.RelayFilterState
 import net.mullvad.mullvadvpn.compose.transitions.SlideInFromRightTransition
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
@@ -146,28 +146,17 @@ fun FilterScreen(
         },
     ) { contentPadding ->
         LazyColumn(modifier = Modifier.padding(contentPadding).fillMaxSize()) {
-            item {
-                Divider()
-                OwnershipHeader(ownershipExpanded, { ownershipExpanded = it })
-            }
+            itemWithDivider { OwnershipHeader(ownershipExpanded) { ownershipExpanded = it } }
             if (ownershipExpanded) {
                 item { AnyOwnership(state, onSelectedOwnership) }
-                items(state.filteredOwnershipByProviders) { ownership ->
-                    Divider()
+                itemsWithDivider(state.filteredOwnershipByProviders) { ownership ->
                     Ownership(ownership, state, onSelectedOwnership)
                 }
             }
-            item {
-                Divider()
-                ProvidersHeader(providerExpanded, { providerExpanded = it })
-            }
+            itemWithDivider() { ProvidersHeader(providerExpanded) { providerExpanded = it } }
             if (providerExpanded) {
-                item {
-                    Divider()
-                    AllProviders(state, onAllProviderCheckChange)
-                }
-                items(state.filteredProvidersByOwnership) { provider ->
-                    Divider()
+                itemWithDivider { AllProviders(state, onAllProviderCheckChange) }
+                itemsWithDivider(state.filteredProvidersByOwnership) { provider ->
                     Provider(provider, state, onSelectedProvider)
                 }
             }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreen.kt
@@ -7,8 +7,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -67,6 +65,7 @@ import net.mullvad.mullvadvpn.compose.destinations.WireguardPortInfoDialogDestin
 import net.mullvad.mullvadvpn.compose.dialog.WireguardCustomPortNavArgs
 import net.mullvad.mullvadvpn.compose.dialog.WireguardPortInfoDialogArgument
 import net.mullvad.mullvadvpn.compose.extensions.itemWithDivider
+import net.mullvad.mullvadvpn.compose.extensions.itemsIndexedWithDivider
 import net.mullvad.mullvadvpn.compose.state.VpnSettingsUiState
 import net.mullvad.mullvadvpn.compose.test.LAZY_LIST_LAST_ITEM_TEST_TAG
 import net.mullvad.mullvadvpn.compose.test.LAZY_LIST_QUANTUM_ITEM_OFF_TEST_TAG
@@ -436,7 +435,7 @@ fun VpnSettingsScreen(
             }
 
             if (state.isCustomDnsEnabled) {
-                itemsIndexed(state.customDnsItems) { index, item ->
+                itemsIndexedWithDivider(state.customDnsItems) { index, item ->
                     DnsCell(
                         address = item.address,
                         isUnreachableLocalDnsWarningVisible =
@@ -444,7 +443,6 @@ fun VpnSettingsScreen(
                         onClick = { navigateToDns(index, item.address) },
                         modifier = Modifier.animateItemPlacement()
                     )
-                    Divider()
                 }
 
                 itemWithDivider {