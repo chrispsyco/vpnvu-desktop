mullvad/mullvadvpn-app

Add WireGuard tests for iOS app

ios/MullvadVPNUITests/Networking/Networking.swift
@@ -63,6 +63,7 @@ class Networking {
         throw NetworkingError.internalError(reason: "Failed to determine device's IP address")
     }
 
+    /// Get configured ad serving domain as URL object
     private static func getAdServingDomainURL() -> URL? {
         guard let adServingDomain = Bundle(for: BaseUITestCase.self)
             .infoDictionary?["AdServingDomain"] as? String,
@@ -74,13 +75,24 @@ class Networking {
         return adServingDomainURL
     }
 
+    /// Get configured ad serving domain
     private static func getAdServingDomain() throws -> String {
-        guard let adServingDomain = Bundle(for: BaseUITestCase.self)
+        guard let adServingDomain = Bundle(for: Networking.self)
             .infoDictionary?["AdServingDomain"] as? String else {
             throw NetworkingError.notConfiguredError
         }
 
         return adServingDomain
+    }
+
+    /// Get configured domain to use for Internet connectivity checks
+    private static func getShouldBeReachableDomain() throws -> String {
+        guard let shouldBeReachableDomain = Bundle(for: Networking.self)
+            .infoDictionary?["ShouldBeReachableDomain"] as? String else {
+            throw NetworkingError.notConfiguredError
+        }
+
+        return shouldBeReachableDomain
     }
 
     /// Check whether host and port is reachable by attempting to connect a socket
@@ -132,6 +144,16 @@ class Networking {
         let apiIPAddress = try MullvadAPIWrapper.getAPIIPAddress()
         let apiPort = try MullvadAPIWrapper.getAPIPort()
         XCTAssertFalse(try canConnectSocket(host: apiIPAddress, port: apiPort))
+    }
+
+    /// Verify that the device has Internet connectivity
+    public static func verifyCanAccessInternet() throws {
+        XCTAssertTrue(try canConnectSocket(host: getShouldBeReachableDomain(), port: "80"))
+    }
+
+    /// Verify that the device do not have Internet connectivity
+    public static func verifyCannotAccessInternet() throws {
+        XCTAssertFalse(try canConnectSocket(host: getShouldBeReachableDomain(), port: "80"))
     }
 
     /// Verify that an ad serving domain is reachable by making sure a connection can be established on port 80

ios/MullvadVPNUITests/Pages/VPNSettingsPage.swift
@@ -1,0 +1,79 @@
+//
+//  VPNSettingsPage.swift
+//  MullvadVPNUITests
+//
+//  Created by Niklas Berglund on 2024-03-04.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class VPNSettingsPage: Page {
+    @discardableResult override init(_ app: XCUIApplication) {
+        super.init(app)
+    }
+
+    private func cellExpandButton(_ cellAccessiblityIdentifier: AccessibilityIdentifier) -> XCUIElement {
+        let table = app.tables[AccessibilityIdentifier.vpnSettingsTableView]
+        let matchingCells = table.otherElements.containing(.any, identifier: cellAccessiblityIdentifier.rawValue)
+        let expandButton = matchingCells.buttons[AccessibilityIdentifier.collapseButton]
+
+        return expandButton
+    }
+
+    @discardableResult func tapBackButton() -> Self {
+        // Workaround for setting accessibility identifier on navigation bar button being non-trivial
+        app.buttons.matching(identifier: "Settings").allElementsBoundByIndex.last?.tap()
+        return self
+    }
+
+    @discardableResult func tapWireGuardPortsExpandButton() -> Self {
+        cellExpandButton(AccessibilityIdentifier.wireGuardPortsCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationExpandButton() -> Self {
+        cellExpandButton(AccessibilityIdentifier.wireGuardObfuscationCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapUDPOverTCPPortExpandButton() -> Self {
+        cellExpandButton(AccessibilityIdentifier.udpOverTCPPortCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapQuantumResistantTunnelExpandButton() -> Self {
+        cellExpandButton(AccessibilityIdentifier.quantumResistantTunnelCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationValueAutomaticCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationAutomatic]
+            .tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationValueOnCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationOn].tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationValueOffCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationOff].tap()
+
+        return self
+    }
+
+    @discardableResult func tapCustomWireGuardPortTextField() -> Self {
+        app.textFields[AccessibilityIdentifier.customWireGuardPortTextField].tap()
+
+        return self
+    }
+}

chrispsyco/vpnvu-desktop

ios/MullvadVPNUITests/Networking/Networking.swift
@@ -63,24 +63,24 @@ class Networking {
         throw NetworkingError.internalError(reason: "Failed to determine device's IP address")
     }
 
-    private static func getAdServingDomainURL() -> URL? {
-        guard let adServingDomain = Bundle(for: BaseUITestCase.self)
-            .infoDictionary?["AdServingDomain"] as? String,
-            let adServingDomainURL = URL(string: adServingDomain) else {
-            XCTFail("Ad serving domain not configured")
-            return nil
-        }
-
-        return adServingDomainURL
-    }
-
+    /// Get configured ad serving domain
     private static func getAdServingDomain() throws -> String {
-        guard let adServingDomain = Bundle(for: BaseUITestCase.self)
+        guard let adServingDomain = Bundle(for: Networking.self)
             .infoDictionary?["AdServingDomain"] as? String else {
             throw NetworkingError.notConfiguredError
         }
 
         return adServingDomain
+    }
+
+    /// Get configured domain to use for Internet connectivity checks
+    private static func getAlwaysReachableDomain() throws -> String {
+        guard let shouldBeReachableDomain = Bundle(for: Networking.self)
+            .infoDictionary?["ShouldBeReachableDomain"] as? String else {
+            throw NetworkingError.notConfiguredError
+        }
+
+        return shouldBeReachableDomain
     }
 
     /// Check whether host and port is reachable by attempting to connect a socket
@@ -132,6 +132,16 @@ class Networking {
         let apiIPAddress = try MullvadAPIWrapper.getAPIIPAddress()
         let apiPort = try MullvadAPIWrapper.getAPIPort()
         XCTAssertFalse(try canConnectSocket(host: apiIPAddress, port: apiPort))
+    }
+
+    /// Verify that the device has Internet connectivity
+    public static func verifyCanAccessInternet() throws {
+        XCTAssertTrue(try canConnectSocket(host: getAlwaysReachableDomain(), port: "80"))
+    }
+
+    /// Verify that the device does not have Internet connectivity
+    public static func verifyCannotAccessInternet() throws {
+        XCTAssertFalse(try canConnectSocket(host: getAlwaysReachableDomain(), port: "80"))
     }
 
     /// Verify that an ad serving domain is reachable by making sure a connection can be established on port 80

ios/MullvadVPNUITests/Pages/VPNSettingsPage.swift
@@ -1,0 +1,79 @@
+//
+//  VPNSettingsPage.swift
+//  MullvadVPNUITests
+//
+//  Created by Niklas Berglund on 2024-03-04.
+//  Copyright © 2024 Mullvad VPN AB. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class VPNSettingsPage: Page {
+    @discardableResult override init(_ app: XCUIApplication) {
+        super.init(app)
+    }
+
+    private func cellExpandCollapseButton(_ cellAccessiblityIdentifier: AccessibilityIdentifier) -> XCUIElement {
+        let table = app.tables[AccessibilityIdentifier.vpnSettingsTableView]
+        let matchingCells = table.otherElements.containing(.any, identifier: cellAccessiblityIdentifier.rawValue)
+        let expandButton = matchingCells.buttons[AccessibilityIdentifier.collapseButton]
+
+        return expandButton
+    }
+
+    @discardableResult func tapBackButton() -> Self {
+        // Workaround for setting accessibility identifier on navigation bar button being non-trivial
+        app.buttons.matching(identifier: "Settings").allElementsBoundByIndex.last?.tap()
+        return self
+    }
+
+    @discardableResult func tapWireGuardPortsExpandButton() -> Self {
+        cellExpandCollapseButton(AccessibilityIdentifier.wireGuardPortsCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationExpandButton() -> Self {
+        cellExpandCollapseButton(AccessibilityIdentifier.wireGuardObfuscationCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapUDPOverTCPPortExpandButton() -> Self {
+        cellExpandCollapseButton(AccessibilityIdentifier.udpOverTCPPortCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapQuantumResistantTunnelExpandButton() -> Self {
+        cellExpandCollapseButton(AccessibilityIdentifier.quantumResistantTunnelCell).tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationAutomaticCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationAutomatic]
+            .tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationOnCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationOn].tap()
+
+        return self
+    }
+
+    @discardableResult func tapWireGuardObfuscationOffCell() -> Self {
+        app.cells[AccessibilityIdentifier.wireGuardObfuscationOff].tap()
+
+        return self
+    }
+
+    @discardableResult func tapCustomWireGuardPortTextField() -> Self {
+        app.textFields[AccessibilityIdentifier.customWireGuardPortTextField].tap()
+
+        return self
+    }
+}