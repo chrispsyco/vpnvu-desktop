mullvad/mullvadvpn-app

Move tracking unsaved changes into coordinator

ios/MullvadVPN/Coordinators/CustomLists/EditCustomListCoordinator.swift
@@ -21,12 +21,16 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
     let customList: CustomList
     let nodes: [LocationNode]
     let subject: CurrentValueSubject<CustomListViewModel, Never>
+    private lazy var alertPresenter: AlertPresenter = {
+        AlertPresenter(context: self)
+    }()
 
     var presentedViewController: UIViewController {
         navigationController
     }
 
     var didFinish: ((EditCustomListCoordinator, FinishAction, CustomList) -> Void)?
+    var didCancel: ((EditCustomListCoordinator) -> Void)?
 
     init(
         navigationController: UINavigationController,
@@ -50,7 +54,7 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
         let controller = CustomListViewController(
             interactor: customListInteractor,
             subject: subject,
-            alertPresenter: AlertPresenter(context: self)
+            alertPresenter: alertPresenter
         )
         controller.delegate = self
 
@@ -61,7 +65,76 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
             comment: ""
         )
 
+        navigationController.interactivePopGestureRecognizer?.delegate = self
         navigationController.pushViewController(controller, animated: true)
+
+        guard let interceptableNavigationController = navigationController as? InterceptableNavigationController else {
+            return
+        }
+
+        interceptableNavigationController.shouldPopViewController = { [weak self] viewController in
+            guard
+                let self,
+                let customListViewController = viewController as? CustomListViewController,
+                customListViewController.hasUnsavedChanges
+            else { return true }
+            presentUnsavedChangesDialog()
+            return false
+        }
+
+        interceptableNavigationController.shouldPopToViewController = { [weak self] viewController in
+            guard
+                let self,
+                let customListViewController = viewController as? CustomListViewController,
+                customListViewController.hasUnsavedChanges
+            else { return true }
+
+            presentUnsavedChangesDialog()
+            return false
+        }
+    }
+
+    private func presentUnsavedChangesDialog() {
+        let message = NSMutableAttributedString(
+            markdownString: NSLocalizedString(
+                "CUSTOM_LISTS_UNSAVED_CHANGES_PROMPT",
+                tableName: "CustomLists",
+                value: "You have unsaved changes.",
+                comment: ""
+            ),
+            options: MarkdownStylingOptions(font: .preferredFont(forTextStyle: .body))
+        )
+
+        let presentation = AlertPresentation(
+            id: "api-custom-lists-unsaved-changes-alert",
+            icon: .alert,
+            attributedMessage: message,
+            buttons: [
+                AlertAction(
+                    title: NSLocalizedString(
+                        "CUSTOM_LISTS_DISCARD_CHANGES_BUTTON",
+                        tableName: "CustomLists",
+                        value: "Discard changes",
+                        comment: ""
+                    ),
+                    style: .destructive,
+                    handler: {
+                        self.didCancel?(self)
+                    }
+                ),
+                AlertAction(
+                    title: NSLocalizedString(
+                        "CUSTOM_LISTS_BACK_TO_EDITING_BUTTON",
+                        tableName: "CustomLists",
+                        value: "Back to editing",
+                        comment: ""
+                    ),
+                    style: .default
+                ),
+            ]
+        )
+
+        alertPresenter.showAlert(presentation: presentation, animated: true)
     }
 }
 
@@ -88,5 +161,21 @@ extension EditCustomListCoordinator: CustomListViewControllerDelegate {
         coordinator.start()
 
         addChild(coordinator)
+    }
+}
+
+extension EditCustomListCoordinator: UIGestureRecognizerDelegate {
+    // For some reason, intercepting `popViewController(animated: Bool)` in `InterceptibleNavigationController`
+    // by SWIPING back leads to weird behaviour where subsequent navigation seem to happen systemwise but not
+    // UI-wise. This leads to the UI freezing up, and the only remedy is to restart the app.
+    //
+    // To get around this issue we can intercept the back swipe gesture and manually perform the transition
+    // instead, thereby bypassing the inner mechanisms that seem to go out of sync.
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard gestureRecognizer == navigationController.interactivePopGestureRecognizer else {
+            return true
+        }
+        navigationController.popViewController(animated: true)
+        return false
     }
 }

chrispsyco/vpnvu-desktop

ios/MullvadVPN/Coordinators/CustomLists/EditCustomListCoordinator.swift
@@ -21,12 +21,16 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
     let customList: CustomList
     let nodes: [LocationNode]
     let subject: CurrentValueSubject<CustomListViewModel, Never>
+    private lazy var alertPresenter: AlertPresenter = {
+        AlertPresenter(context: self)
+    }()
 
     var presentedViewController: UIViewController {
         navigationController
     }
 
     var didFinish: ((EditCustomListCoordinator, FinishAction, CustomList) -> Void)?
+    var didCancel: ((EditCustomListCoordinator) -> Void)?
 
     init(
         navigationController: UINavigationController,
@@ -50,7 +54,7 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
         let controller = CustomListViewController(
             interactor: customListInteractor,
             subject: subject,
-            alertPresenter: AlertPresenter(context: self)
+            alertPresenter: alertPresenter
         )
         controller.delegate = self
 
@@ -61,7 +65,77 @@ class EditCustomListCoordinator: Coordinator, Presentable, Presenting {
             comment: ""
         )
 
+        navigationController.interactivePopGestureRecognizer?.delegate = self
         navigationController.pushViewController(controller, animated: true)
+
+        guard let interceptibleNavigationController = navigationController as? InterceptibleNavigationController else {
+            return
+        }
+
+        interceptibleNavigationController.shouldPopViewController = { [weak self] viewController in
+            guard
+                let self,
+                let customListViewController = viewController as? CustomListViewController,
+                customListViewController.hasUnsavedChanges
+            else { return true }
+
+            presentUnsavedChangesDialog()
+            return false
+        }
+
+        interceptibleNavigationController.shouldPopToViewController = { [weak self] viewController in
+            guard
+                let self,
+                let customListViewController = viewController as? CustomListViewController,
+                customListViewController.hasUnsavedChanges
+            else { return true }
+
+            presentUnsavedChangesDialog()
+            return false
+        }
+    }
+
+    private func presentUnsavedChangesDialog() {
+        let message = NSMutableAttributedString(
+            markdownString: NSLocalizedString(
+                "CUSTOM_LISTS_UNSAVED_CHANGES_PROMPT",
+                tableName: "CustomLists",
+                value: "You have unsaved changes.",
+                comment: ""
+            ),
+            options: MarkdownStylingOptions(font: .preferredFont(forTextStyle: .body))
+        )
+
+        let presentation = AlertPresentation(
+            id: "api-custom-lists-unsaved-changes-alert",
+            icon: .alert,
+            attributedMessage: message,
+            buttons: [
+                AlertAction(
+                    title: NSLocalizedString(
+                        "CUSTOM_LISTS_DISCARD_CHANGES_BUTTON",
+                        tableName: "CustomLists",
+                        value: "Discard changes",
+                        comment: ""
+                    ),
+                    style: .destructive,
+                    handler: {
+                        self.didCancel?(self)
+                    }
+                ),
+                AlertAction(
+                    title: NSLocalizedString(
+                        "CUSTOM_LISTS_BACK_TO_EDITING_BUTTON",
+                        tableName: "CustomLists",
+                        value: "Back to editing",
+                        comment: ""
+                    ),
+                    style: .default
+                ),
+            ]
+        )
+
+        alertPresenter.showAlert(presentation: presentation, animated: true)
     }
 }
 
@@ -88,5 +162,21 @@ extension EditCustomListCoordinator: CustomListViewControllerDelegate {
         coordinator.start()
 
         addChild(coordinator)
+    }
+}
+
+extension EditCustomListCoordinator: UIGestureRecognizerDelegate {
+    // For some reason, intercepting `popViewController(animated: Bool)` in `InterceptibleNavigationController`
+    // by SWIPING back leads to weird behaviour where subsequent navigation seem to happen systemwise but not
+    // UI-wise. This leads to the UI freezing up, and the only remedy is to restart the app.
+    //
+    // To get around this issue we can intercept the back swipe gesture and manually perform the transition
+    // instead, thereby bypassing the inner mechanisms that seem to go out of sync.
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        guard gestureRecognizer == navigationController.interactivePopGestureRecognizer else {
+            return true
+        }
+        navigationController.popViewController(animated: true)
+        return false
     }
 }