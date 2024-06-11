mullvad/mullvadvpn-app

Prevent loading when logging out

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/AccountScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -55,6 +56,7 @@ import net.mullvad.mullvadvpn.compose.transitions.SlideInFromBottomTransition
 import net.mullvad.mullvadvpn.compose.util.LaunchedEffectCollect
 import net.mullvad.mullvadvpn.compose.util.SecureScreenWhileInView
 import net.mullvad.mullvadvpn.compose.util.createCopyToClipboardHandle
+import net.mullvad.mullvadvpn.lib.model.AccountToken
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentProduct
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentStatus
 import net.mullvad.mullvadvpn.lib.payment.model.ProductId
@@ -76,7 +78,7 @@ private fun PreviewAccountScreen() {
             state =
                 AccountUiState(
                     deviceName = "Test Name",
-                    accountNumber = "1234123412341234",
+                    accountToken = AccountToken("1234123412341234"),
                     accountExpiry = null,
                     showSitePayment = true,
                     billingPaymentState =
@@ -201,7 +203,10 @@ fun AccountScreen(
                     onInfoClick = navigateToDeviceInfo
                 )
 
-                AccountNumberRow(accountNumber = state.accountNumber ?: "", onCopyAccountNumber)
+                AccountNumberRow(
+                    accountNumber = state.accountToken?.value ?: "",
+                    onCopyAccountNumber
+                )
 
                 PaidUntilRow(accountExpiry = state.accountExpiry)
             }

android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModelTest.kt
@@ -39,17 +39,20 @@ class AccountViewModelTest {
     private val mockDeviceRepository: DeviceRepository = mockk(relaxUnitFun = true)
     private val mockPaymentUseCase: PaymentUseCase = mockk(relaxed = true)
 
-    private val deviceState: MutableStateFlow<DeviceState?> = MutableStateFlow(null)
-    private val paymentAvailability = MutableStateFlow<PaymentAvailability?>(null)
-    private val purchaseResult = MutableStateFlow<PurchaseResult?>(null)
-    private val accountExpiryState = MutableStateFlow(null)
-
     private val dummyDevice =
         Device(id = DeviceId.fromString(UUID), name = "fake_name", creationDate = DateTime.now())
     private val dummyAccountToken: AccountToken =
         AccountToken(
             DUMMY_DEVICE_NAME,
         )
+
+    private val deviceState: MutableStateFlow<DeviceState?> =
+        MutableStateFlow(
+            DeviceState.LoggedIn(accountToken = dummyAccountToken, device = dummyDevice)
+        )
+    private val paymentAvailability = MutableStateFlow<PaymentAvailability?>(null)
+    private val purchaseResult = MutableStateFlow<PurchaseResult?>(null)
+    private val accountExpiryState = MutableStateFlow(null)
 
     private lateinit var viewModel: AccountViewModel
 
@@ -80,11 +83,10 @@ class AccountViewModelTest {
     fun `given device state LoggedIn uiState should contain accountNumber`() = runTest {
         // Act, Assert
         viewModel.uiState.test {
-            awaitItem() // Default state
             deviceState.value =
                 DeviceState.LoggedIn(accountToken = dummyAccountToken, device = dummyDevice)
             val result = awaitItem()
-            assertEquals(DUMMY_DEVICE_NAME, result.accountNumber)
+            assertEquals(dummyAccountToken, result.accountToken)
         }
     }
 
@@ -100,8 +102,6 @@ class AccountViewModelTest {
     @Test
     fun `when paymentAvailability emits ProductsUnavailable uiState should be NoPayment`() =
         runTest {
-            // Arrange in setup
-
             // Act, Assert
             viewModel.uiState.test {
                 awaitItem() // Default state

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/AccountScreen.kt
@@ -55,6 +55,7 @@ import net.mullvad.mullvadvpn.compose.transitions.SlideInFromBottomTransition
 import net.mullvad.mullvadvpn.compose.util.LaunchedEffectCollect
 import net.mullvad.mullvadvpn.compose.util.SecureScreenWhileInView
 import net.mullvad.mullvadvpn.compose.util.createCopyToClipboardHandle
+import net.mullvad.mullvadvpn.lib.model.AccountNumber
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentProduct
 import net.mullvad.mullvadvpn.lib.payment.model.PaymentStatus
 import net.mullvad.mullvadvpn.lib.payment.model.ProductId
@@ -76,7 +77,7 @@ private fun PreviewAccountScreen() {
             state =
                 AccountUiState(
                     deviceName = "Test Name",
-                    accountNumber = "1234123412341234",
+                    accountNumber = AccountNumber("1234123412341234"),
                     accountExpiry = null,
                     showSitePayment = true,
                     billingPaymentState =
@@ -201,7 +202,10 @@ fun AccountScreen(
                     onInfoClick = navigateToDeviceInfo
                 )
 
-                AccountNumberRow(accountNumber = state.accountNumber ?: "", onCopyAccountNumber)
+                AccountNumberRow(
+                    accountNumber = state.accountNumber?.value ?: "",
+                    onCopyAccountNumber
+                )
 
                 PaidUntilRow(accountExpiry = state.accountExpiry)
             }

android/app/src/test/kotlin/net/mullvad/mullvadvpn/viewmodel/AccountViewModelTest.kt
@@ -39,17 +39,20 @@ class AccountViewModelTest {
     private val mockDeviceRepository: DeviceRepository = mockk(relaxUnitFun = true)
     private val mockPaymentUseCase: PaymentUseCase = mockk(relaxed = true)
 
-    private val deviceState: MutableStateFlow<DeviceState?> = MutableStateFlow(null)
-    private val paymentAvailability = MutableStateFlow<PaymentAvailability?>(null)
-    private val purchaseResult = MutableStateFlow<PurchaseResult?>(null)
-    private val accountExpiryState = MutableStateFlow(null)
-
     private val dummyDevice =
         Device(id = DeviceId.fromString(UUID), name = "fake_name", creationDate = DateTime.now())
     private val dummyAccountNumber: AccountNumber =
         AccountNumber(
             DUMMY_DEVICE_NAME,
         )
+
+    private val deviceState: MutableStateFlow<DeviceState?> =
+        MutableStateFlow(
+            DeviceState.LoggedIn(accountNumber = dummyAccountNumber, device = dummyDevice)
+        )
+    private val paymentAvailability = MutableStateFlow<PaymentAvailability?>(null)
+    private val purchaseResult = MutableStateFlow<PurchaseResult?>(null)
+    private val accountExpiryState = MutableStateFlow(null)
 
     private lateinit var viewModel: AccountViewModel
 
@@ -80,11 +83,10 @@ class AccountViewModelTest {
     fun `given device state LoggedIn uiState should contain accountNumber`() = runTest {
         // Act, Assert
         viewModel.uiState.test {
-            awaitItem() // Default state
             deviceState.value =
                 DeviceState.LoggedIn(accountNumber = dummyAccountNumber, device = dummyDevice)
             val result = awaitItem()
-            assertEquals(DUMMY_DEVICE_NAME, result.accountNumber)
+            assertEquals(dummyAccountNumber, result.accountNumber)
         }
     }
 
@@ -100,8 +102,6 @@ class AccountViewModelTest {
     @Test
     fun `when paymentAvailability emits ProductsUnavailable uiState should be NoPayment`() =
         runTest {
-            // Arrange in setup
-
             // Act, Assert
             viewModel.uiState.test {
                 awaitItem() // Default state