mullvad/mullvadvpn-app

Parse changelog with bullet point list instead of just newlines

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
@@ -1,10 +1,14 @@
 package net.mullvad.mullvadvpn.repository
 
 import android.content.SharedPreferences
+import androidx.compose.ui.util.fastJoinToString
 import net.mullvad.mullvadvpn.util.IChangelogDataProvider
+import net.mullvad.mullvadvpn.util.trim
 
 private const val MISSING_VERSION_CODE = -1
 private const val NEWLINE_CHAR = '\n'
+private const val SPACE_STRING = " "
+private const val BULLET_POINT_CHAR = '-'
 private const val LAST_SHOWED_CHANGELOG_VERSION_CODE = "last_showed_changelog_version_code"
 
 class ChangelogRepository(
@@ -18,7 +22,10 @@ class ChangelogRepository(
     fun setVersionCodeOfMostRecentChangelogShowed(versionCode: Int) =
         preferences.edit().putInt(LAST_SHOWED_CHANGELOG_VERSION_CODE, versionCode).apply()
 
-    fun getLastVersionChanges(): List<String> {
-        return dataProvider.getChangelog().split(NEWLINE_CHAR).filter { it.isNotEmpty() }
-    }
+    fun getLastVersionChanges(): List<String> =
+        dataProvider
+            .getChangelog()
+            .split(BULLET_POINT_CHAR)
+            .map { it.split(NEWLINE_CHAR).trim().fastJoinToString(SPACE_STRING).trim() }
+            .filter { it.isNotEmpty() }
 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/StringExtensions.kt
@@ -12,3 +12,5 @@ fun String.appendHideNavOnPlayBuild(isPlayBuild: Boolean): String =
 
 fun String.removeHtmlTags(): String =
     Html.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+
+fun List<String>.trim() = map { it.trim() }

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/ChangelogRepository.kt
@@ -2,9 +2,12 @@ package net.mullvad.mullvadvpn.repository
 
 import android.content.SharedPreferences
 import net.mullvad.mullvadvpn.util.IChangelogDataProvider
+import net.mullvad.mullvadvpn.util.trimAll
 
 private const val MISSING_VERSION_CODE = -1
 private const val NEWLINE_CHAR = '\n'
+private const val SPACE_STRING = " "
+private const val BULLET_POINT_CHAR = '-'
 private const val LAST_SHOWED_CHANGELOG_VERSION_CODE = "last_showed_changelog_version_code"
 
 class ChangelogRepository(
@@ -18,7 +21,10 @@ class ChangelogRepository(
     fun setVersionCodeOfMostRecentChangelogShowed(versionCode: Int) =
         preferences.edit().putInt(LAST_SHOWED_CHANGELOG_VERSION_CODE, versionCode).apply()
 
-    fun getLastVersionChanges(): List<String> {
-        return dataProvider.getChangelog().split(NEWLINE_CHAR).filter { it.isNotEmpty() }
-    }
+    fun getLastVersionChanges(): List<String> =
+        dataProvider
+            .getChangelog()
+            .split(BULLET_POINT_CHAR)
+            .map { it.split(NEWLINE_CHAR).trimAll().joinToString(SPACE_STRING).trim() }
+            .filter { it.isNotEmpty() }
 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/util/StringExtensions.kt
@@ -12,3 +12,5 @@ fun String.appendHideNavOnPlayBuild(isPlayBuild: Boolean): String =
 
 fun String.removeHtmlTags(): String =
     Html.fromHtml(this, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+
+fun List<String>.trimAll() = map { it.trim() }