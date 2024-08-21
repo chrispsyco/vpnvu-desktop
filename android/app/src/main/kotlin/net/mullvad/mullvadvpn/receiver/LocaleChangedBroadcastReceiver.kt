mullvad/mullvadvpn-app

Add support for translated location names

android/app/src/main/kotlin/net/mullvad/mullvadvpn/broadcastreceiver/LocaleChangedBroadcastReceiver.kt
@@ -1,0 +1,18 @@
+package net.mullvad.mullvadvpn.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import net.mullvad.mullvadvpn.lib.shared.LocaleRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class LocaleChangedBroadcastReceiver : BroadcastReceiver(), KoinComponent {
+    private val localeRepository by inject<LocaleRepository>()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_LOCALE_CHANGED) {
+            localeRepository.refreshLocale()
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/RelayListRepository.kt
@@ -1,11 +1,15 @@
 package net.mullvad.mullvadvpn.repository
 
+import arrow.optics.Every
+import arrow.optics.copy
+import arrow.optics.dsl.every
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -17,18 +21,48 @@ import net.mullvad.mullvadvpn.lib.model.RelayItem
 import net.mullvad.mullvadvpn.lib.model.RelayItemId
 import net.mullvad.mullvadvpn.lib.model.WireguardConstraints
 import net.mullvad.mullvadvpn.lib.model.WireguardEndpointData
+import net.mullvad.mullvadvpn.lib.model.cities
+import net.mullvad.mullvadvpn.lib.model.name
+import net.mullvad.mullvadvpn.lib.shared.RelayLocationTranslationRepository
 import net.mullvad.mullvadvpn.relaylist.findByGeoLocationId
+import net.mullvad.mullvadvpn.util.sortedByName
 
 class RelayListRepository(
     private val managementService: ManagementService,
+    private val translationRepository: RelayLocationTranslationRepository,
     dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
     val relayList: StateFlow<List<RelayItem.Location.Country>> =
-        managementService.relayCountries.stateIn(
-            CoroutineScope(dispatcher),
-            SharingStarted.WhileSubscribed(),
-            emptyList()
-        )
+        combine(managementService.relayCountries, translationRepository.translations) {
+                countries,
+                translations ->
+                countries.translateRelay(translations)
+            }
+            .stateIn(CoroutineScope(dispatcher), SharingStarted.WhileSubscribed(), emptyList())
+
+    private fun List<RelayItem.Location.Country>.translateRelay(
+        translations: Map<String, String>
+    ): List<RelayItem.Location.Country> {
+        if (translations.isEmpty()) {
+            return this
+        }
+
+        return Every.list<RelayItem.Location.Country>()
+            .modify(this) {
+                it.copy<RelayItem.Location.Country> {
+                    RelayItem.Location.Country.name set translations.getOrDefault(it.name, it.name)
+                    RelayItem.Location.Country.cities.every(Every.list()).name transform
+                        { cityName ->
+                            translations.getOrDefault(cityName, cityName)
+                        }
+                    RelayItem.Location.Country.cities transform
+                        { cities ->
+                            cities.sortedByName { it.name }
+                        }
+                }
+            }
+            .sortedByName { it.name }
+    }
 
     val wireguardEndpointData: StateFlow<WireguardEndpointData> =
         managementService.wireguardEndpointData.stateIn(

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/receiver/LocaleChangedBroadcastReceiver.kt
@@ -1,0 +1,18 @@
+package net.mullvad.mullvadvpn.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import net.mullvad.mullvadvpn.lib.shared.LocaleRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class LocaleChangedBroadcastReceiver : BroadcastReceiver(), KoinComponent {
+    private val localeRepository by inject<LocaleRepository>()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_LOCALE_CHANGED) {
+            localeRepository.refreshLocale()
+        }
+    }
+}

android/app/src/main/kotlin/net/mullvad/mullvadvpn/repository/RelayListRepository.kt
@@ -1,11 +1,15 @@
 package net.mullvad.mullvadvpn.repository
 
+import arrow.optics.Every
+import arrow.optics.copy
+import arrow.optics.dsl.every
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -17,18 +21,45 @@ import net.mullvad.mullvadvpn.lib.model.RelayItem
 import net.mullvad.mullvadvpn.lib.model.RelayItemId
 import net.mullvad.mullvadvpn.lib.model.WireguardConstraints
 import net.mullvad.mullvadvpn.lib.model.WireguardEndpointData
+import net.mullvad.mullvadvpn.lib.model.cities
+import net.mullvad.mullvadvpn.lib.model.name
+import net.mullvad.mullvadvpn.lib.shared.RelayLocationTranslationRepository
 import net.mullvad.mullvadvpn.relaylist.findByGeoLocationId
+import net.mullvad.mullvadvpn.relaylist.sortedByName
 
 class RelayListRepository(
     private val managementService: ManagementService,
+    private val translationRepository: RelayLocationTranslationRepository,
     dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) {
     val relayList: StateFlow<List<RelayItem.Location.Country>> =
-        managementService.relayCountries.stateIn(
-            CoroutineScope(dispatcher),
-            SharingStarted.WhileSubscribed(),
-            emptyList()
-        )
+        combine(managementService.relayCountries, translationRepository.translations) {
+                countries,
+                translations ->
+                countries.translateRelays(translations)
+            }
+            .stateIn(CoroutineScope(dispatcher), SharingStarted.WhileSubscribed(), emptyList())
+
+    private fun List<RelayItem.Location.Country>.translateRelays(
+        translations: Map<String, String>
+    ): List<RelayItem.Location.Country> {
+        if (translations.isEmpty()) {
+            return this
+        }
+
+        return Every.list<RelayItem.Location.Country>()
+            .modify(this) {
+                it.copy {
+                    RelayItem.Location.Country.name set translations.getOrDefault(it.name, it.name)
+                    RelayItem.Location.Country.cities.every(Every.list()).name transform
+                        { cityName ->
+                            translations.getOrDefault(cityName, cityName)
+                        }
+                    RelayItem.Location.Country.cities transform { cities -> cities.sortedByName() }
+                }
+            }
+            .sortedByName()
+    }
 
     val wireguardEndpointData: StateFlow<WireguardEndpointData> =
         managementService.wireguardEndpointData.stateIn(