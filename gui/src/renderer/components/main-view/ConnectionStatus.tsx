mullvad/mullvadvpn-app

Implement main view connection island

gui/src/renderer/components/main-view/ConnectionStatus.tsx
@@ -1,0 +1,54 @@
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { TunnelState } from '../../../shared/daemon-rpc-types';
+import { messages } from '../../../shared/gettext';
+import { useSelector } from '../../redux/store';
+import { largeText } from '../common-styles';
+
+const StyledConnectionStatus = styled.span<{ $color: string }>(largeText, (props) => ({
+  minHeight: '24px',
+  color: props.$color,
+  marginBottom: '4px',
+}));
+
+export default function ConnectionStatus() {
+  const tunnelState = useSelector((state) => state.connection.status);
+  const lockdownMode = useSelector((state) => state.settings.blockWhenDisconnected);
+
+  const color = getConnectionSTatusLabelColor(tunnelState, lockdownMode);
+  const text = getConnectionStatusLabelText(tunnelState);
+
+  return <StyledConnectionStatus $color={color}>{text}</StyledConnectionStatus>;
+}
+
+function getConnectionSTatusLabelColor(tunnelState: TunnelState, lockdownMode: boolean) {
+  switch (tunnelState.state) {
+    case 'connected':
+      return colors.green;
+    case 'connecting':
+    case 'disconnecting':
+      return colors.white;
+    case 'disconnected':
+      return lockdownMode ? colors.green : colors.red;
+    case 'error':
+      return tunnelState.details.blockingError ? colors.red : colors.white;
+  }
+}
+
+function getConnectionStatusLabelText(tunnelState: TunnelState) {
+  switch (tunnelState.state) {
+    case 'connected':
+      return messages.gettext('CONNECTED');
+    case 'connecting':
+      return messages.gettext('CONNECTING...');
+    case 'disconnecting':
+      return messages.gettext('DISCONNECTING...');
+    case 'disconnected':
+      return messages.gettext('DISCONNECTED');
+    case 'error':
+      return tunnelState.details.blockingError
+        ? messages.gettext('FAILED TO SECURE CONNECTION')
+        : messages.gettext('BLOCKED CONNECTION');
+  }
+}

gui/src/renderer/components/main-view/Hostname.tsx
@@ -1,0 +1,69 @@
+import { sprintf } from 'sprintf-js';
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { messages } from '../../../shared/gettext';
+import { IConnectionReduxState } from '../../redux/connection/reducers';
+import { useSelector } from '../../redux/store';
+import Accordion from '../Accordion';
+import { smallText } from '../common-styles';
+import Marquee from '../Marquee';
+
+const StyledAccordion = styled(Accordion)({
+  flexShrink: 0,
+});
+
+const StyledHostname = styled.span(smallText, {
+  color: colors.white60,
+  fontWeight: '400',
+  flexShrink: 0,
+  minHeight: '1em',
+});
+
+export default function Hostname() {
+  const tunnelState = useSelector((state) => state.connection.status.state);
+  const connection = useSelector((state) => state.connection);
+  const text = getHostnameText(connection);
+
+  return (
+    <StyledAccordion expanded={tunnelState === 'connecting' || tunnelState === 'connected'}>
+      <StyledHostname>
+        <Marquee>{text}</Marquee>
+      </StyledHostname>
+    </StyledAccordion>
+  );
+}
+
+function getHostnameText(connection: IConnectionReduxState) {
+  let hostname = '';
+
+  if (connection.hostname && connection.bridgeHostname) {
+    hostname = sprintf(messages.pgettext('connection-info', '%(relay)s via %(entry)s'), {
+      relay: connection.hostname,
+      entry: connection.bridgeHostname,
+    });
+  } else if (connection.hostname && connection.entryHostname) {
+    hostname = sprintf(
+      // TRANSLATORS: The hostname line displayed below the country on the main screen
+      // TRANSLATORS: Available placeholders:
+      // TRANSLATORS: %(relay)s - the relay hostname
+      // TRANSLATORS: %(entry)s - the entry relay hostname
+      messages.pgettext('connection-info', '%(relay)s via %(entry)s'),
+      {
+        relay: connection.hostname,
+        entry: connection.entryHostname,
+      },
+    );
+  } else if (
+    (connection.status.state === 'connecting' || connection.status.state === 'connected') &&
+    connection.status.details?.endpoint.proxy !== undefined
+  ) {
+    hostname = sprintf(messages.pgettext('connection-info', '%(relay)s via Custom bridge'), {
+      relay: connection.hostname,
+    });
+  } else if (connection.hostname) {
+    hostname = connection.hostname;
+  }
+
+  return hostname;
+}

gui/src/renderer/components/main-view/Location.tsx
@@ -1,0 +1,41 @@
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { TunnelState } from '../../../shared/daemon-rpc-types';
+import { useSelector } from '../../redux/store';
+import Accordion from '../Accordion';
+import { largeText } from '../common-styles';
+import Marquee from '../Marquee';
+
+const StyledLocation = styled.span(largeText, {
+  color: colors.white,
+  flexShrink: 0,
+});
+
+export default function Location() {
+  const connection = useSelector((state) => state.connection);
+  const text = getLocationText(connection.status, connection.country, connection.city);
+
+  return (
+    <Accordion expanded={connection.status.state !== 'error'}>
+      <StyledLocation>
+        <Marquee>{text}</Marquee>
+      </StyledLocation>
+    </Accordion>
+  );
+}
+
+function getLocationText(tunnelState: TunnelState, country?: string, city?: string): string {
+  country = country ?? '';
+
+  switch (tunnelState.state) {
+    case 'connected':
+    case 'connecting':
+      return city ? `${country}, ${city}` : country;
+    case 'disconnecting':
+    case 'disconnected':
+      return country;
+    case 'error':
+      return '';
+  }
+}

chrispsyco/vpnvu-desktop

gui/src/renderer/components/main-view/ConnectionStatus.tsx
@@ -1,0 +1,58 @@
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { TunnelState } from '../../../shared/daemon-rpc-types';
+import { messages } from '../../../shared/gettext';
+import { useSelector } from '../../redux/store';
+import { largeText } from '../common-styles';
+
+const StyledConnectionStatus = styled.span<{ $color: string }>(largeText, (props) => ({
+  minHeight: '24px',
+  color: props.$color,
+  marginBottom: '4px',
+}));
+
+export default function ConnectionStatus() {
+  const tunnelState = useSelector((state) => state.connection.status);
+  const lockdownMode = useSelector((state) => state.settings.blockWhenDisconnected);
+
+  const color = getConnectionSTatusLabelColor(tunnelState, lockdownMode);
+  const text = getConnectionStatusLabelText(tunnelState);
+
+  return (
+    <StyledConnectionStatus role="status" $color={color}>
+      {text}
+    </StyledConnectionStatus>
+  );
+}
+
+function getConnectionSTatusLabelColor(tunnelState: TunnelState, lockdownMode: boolean) {
+  switch (tunnelState.state) {
+    case 'connected':
+      return colors.green;
+    case 'connecting':
+    case 'disconnecting':
+      return colors.white;
+    case 'disconnected':
+      return lockdownMode ? colors.white : colors.red;
+    case 'error':
+      return tunnelState.details.blockingError ? colors.red : colors.white;
+  }
+}
+
+function getConnectionStatusLabelText(tunnelState: TunnelState) {
+  switch (tunnelState.state) {
+    case 'connected':
+      return messages.gettext('CONNECTED');
+    case 'connecting':
+      return messages.gettext('CONNECTING...');
+    case 'disconnecting':
+      return messages.gettext('DISCONNECTING...');
+    case 'disconnected':
+      return messages.gettext('DISCONNECTED');
+    case 'error':
+      return tunnelState.details.blockingError
+        ? messages.gettext('FAILED TO SECURE CONNECTION')
+        : messages.gettext('BLOCKED CONNECTION');
+  }
+}

gui/src/renderer/components/main-view/Hostname.tsx
@@ -1,0 +1,69 @@
+import { sprintf } from 'sprintf-js';
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { messages } from '../../../shared/gettext';
+import { IConnectionReduxState } from '../../redux/connection/reducers';
+import { useSelector } from '../../redux/store';
+import { smallText } from '../common-styles';
+import Marquee from '../Marquee';
+import { ConnectionPanelAccordion } from './styles';
+
+const StyledAccordion = styled(ConnectionPanelAccordion)({
+  flexShrink: 0,
+});
+
+const StyledHostname = styled.span(smallText, {
+  color: colors.white60,
+  fontWeight: '400',
+  flexShrink: 0,
+  minHeight: '1em',
+});
+
+export default function Hostname() {
+  const tunnelState = useSelector((state) => state.connection.status.state);
+  const connection = useSelector((state) => state.connection);
+  const text = getHostnameText(connection);
+
+  return (
+    <StyledAccordion expanded={tunnelState === 'connecting' || tunnelState === 'connected'}>
+      <StyledHostname data-testid="hostname-line">
+        <Marquee>{text}</Marquee>
+      </StyledHostname>
+    </StyledAccordion>
+  );
+}
+
+function getHostnameText(connection: IConnectionReduxState) {
+  let hostname = '';
+
+  if (connection.hostname && connection.bridgeHostname) {
+    hostname = sprintf(messages.pgettext('connection-info', '%(relay)s via %(entry)s'), {
+      relay: connection.hostname,
+      entry: connection.bridgeHostname,
+    });
+  } else if (connection.hostname && connection.entryHostname) {
+    hostname = sprintf(
+      // TRANSLATORS: The hostname line displayed below the country on the main screen
+      // TRANSLATORS: Available placeholders:
+      // TRANSLATORS: %(relay)s - the relay hostname
+      // TRANSLATORS: %(entry)s - the entry relay hostname
+      messages.pgettext('connection-info', '%(relay)s via %(entry)s'),
+      {
+        relay: connection.hostname,
+        entry: connection.entryHostname,
+      },
+    );
+  } else if (
+    (connection.status.state === 'connecting' || connection.status.state === 'connected') &&
+    connection.status.details?.endpoint.proxy !== undefined
+  ) {
+    hostname = sprintf(messages.pgettext('connection-info', '%(relay)s via Custom bridge'), {
+      relay: connection.hostname,
+    });
+  } else if (connection.hostname) {
+    hostname = connection.hostname;
+  }
+
+  return hostname;
+}

gui/src/renderer/components/main-view/Location.tsx
@@ -1,0 +1,41 @@
+import styled from 'styled-components';
+
+import { colors } from '../../../config.json';
+import { TunnelState } from '../../../shared/daemon-rpc-types';
+import { useSelector } from '../../redux/store';
+import { largeText } from '../common-styles';
+import Marquee from '../Marquee';
+import { ConnectionPanelAccordion } from './styles';
+
+const StyledLocation = styled.span(largeText, {
+  color: colors.white,
+  flexShrink: 0,
+});
+
+export default function Location() {
+  const connection = useSelector((state) => state.connection);
+  const text = getLocationText(connection.status, connection.country, connection.city);
+
+  return (
+    <ConnectionPanelAccordion expanded={connection.status.state !== 'error'}>
+      <StyledLocation>
+        <Marquee>{text}</Marquee>
+      </StyledLocation>
+    </ConnectionPanelAccordion>
+  );
+}
+
+function getLocationText(tunnelState: TunnelState, country?: string, city?: string): string {
+  country = country ?? '';
+
+  switch (tunnelState.state) {
+    case 'connected':
+    case 'connecting':
+      return city ? `${country}, ${city}` : country;
+    case 'disconnecting':
+    case 'disconnected':
+      return country;
+    case 'error':
+      return '';
+  }
+}