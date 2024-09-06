mullvad/mullvadvpn-app

Refactor to use preview parameter providers

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -49,8 +49,7 @@ import net.mullvad.mullvadvpn.compose.component.MullvadCircularProgressIndicator
 import net.mullvad.mullvadvpn.compose.component.ScaffoldWithTopBar
 import net.mullvad.mullvadvpn.compose.component.drawVerticalScrollbar
 import net.mullvad.mullvadvpn.compose.extensions.dropUnlessResumed
-import net.mullvad.mullvadvpn.compose.preview.DeviceListPreviewParameterProvider
-import net.mullvad.mullvadvpn.compose.state.DeviceItemUiState
+import net.mullvad.mullvadvpn.compose.preview.DeviceListUiStateParameterProvider
 import net.mullvad.mullvadvpn.compose.state.DeviceListUiState
 import net.mullvad.mullvadvpn.compose.transitions.DefaultTransition
 import net.mullvad.mullvadvpn.compose.util.CollectSideEffectWithLifecycle
@@ -58,7 +57,6 @@ import net.mullvad.mullvadvpn.compose.util.showSnackbarImmediately
 import net.mullvad.mullvadvpn.lib.model.AccountNumber
 import net.mullvad.mullvadvpn.lib.model.Device
 import net.mullvad.mullvadvpn.lib.model.DeviceId
-import net.mullvad.mullvadvpn.lib.model.GetDeviceListError
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.lib.theme.color.selected
@@ -72,32 +70,9 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 @Preview
 private fun PreviewDeviceListScreenContent(
-    @PreviewParameter(DeviceListPreviewParameterProvider::class) devices: List<DeviceItemUiState>
+    @PreviewParameter(DeviceListUiStateParameterProvider::class) state: DeviceListUiState
 ) {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Content(devices = devices)) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListScreenEmpty() {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Content(devices = emptyList())) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListLoading() {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Loading) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListError() {
-    AppTheme {
-        DeviceListScreen(
-            state =
-                DeviceListUiState.Error(GetDeviceListError.Unknown(IllegalStateException("Error")))
-        )
-    }
+    AppTheme { DeviceListScreen(state = state) }
 }
 
 data class DeviceListNavArgs(val accountNumber: AccountNumber)

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoginScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.ramcosta.composedestinations.annotation.Destination
@@ -63,6 +64,7 @@ import net.mullvad.mullvadvpn.compose.button.PrimaryButton
 import net.mullvad.mullvadvpn.compose.button.VariantButton
 import net.mullvad.mullvadvpn.compose.component.MullvadCircularProgressIndicatorLarge
 import net.mullvad.mullvadvpn.compose.component.ScaffoldWithTopBar
+import net.mullvad.mullvadvpn.compose.preview.LoginUiStateParameterProvider
 import net.mullvad.mullvadvpn.compose.state.LoginError
 import net.mullvad.mullvadvpn.compose.state.LoginState
 import net.mullvad.mullvadvpn.compose.state.LoginState.Idle
@@ -84,32 +86,10 @@ import org.koin.androidx.compose.koinViewModel
 
 @Preview
 @Composable
-private fun PreviewIdle() {
-    AppTheme { LoginScreen(state = LoginUiState()) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoggingIn() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Loading.LoggingIn)) }
-}
-
-@Preview
-@Composable
-private fun PreviewCreatingAccount() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Loading.CreatingAccount)) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoginError() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Idle(LoginError.InvalidCredentials))) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoginSuccess() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Success)) }
+private fun PreviewBase(
+    @PreviewParameter(LoginUiStateParameterProvider::class) state: LoginUiState
+) {
+    AppTheme { LoginScreen(state = state) }
 }
 
 @Destination<RootGraph>(style = LoginTransition::class)

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/DeviceListScreen.kt
@@ -49,8 +49,7 @@ import net.mullvad.mullvadvpn.compose.component.MullvadCircularProgressIndicator
 import net.mullvad.mullvadvpn.compose.component.ScaffoldWithTopBar
 import net.mullvad.mullvadvpn.compose.component.drawVerticalScrollbar
 import net.mullvad.mullvadvpn.compose.extensions.dropUnlessResumed
-import net.mullvad.mullvadvpn.compose.preview.DeviceListPreviewParameterProvider
-import net.mullvad.mullvadvpn.compose.state.DeviceItemUiState
+import net.mullvad.mullvadvpn.compose.preview.DeviceListUiStatePreviewParameterProvider
 import net.mullvad.mullvadvpn.compose.state.DeviceListUiState
 import net.mullvad.mullvadvpn.compose.transitions.DefaultTransition
 import net.mullvad.mullvadvpn.compose.util.CollectSideEffectWithLifecycle
@@ -58,7 +57,6 @@ import net.mullvad.mullvadvpn.compose.util.showSnackbarImmediately
 import net.mullvad.mullvadvpn.lib.model.AccountNumber
 import net.mullvad.mullvadvpn.lib.model.Device
 import net.mullvad.mullvadvpn.lib.model.DeviceId
-import net.mullvad.mullvadvpn.lib.model.GetDeviceListError
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 import net.mullvad.mullvadvpn.lib.theme.color.selected
@@ -72,32 +70,9 @@ import org.koin.androidx.compose.koinViewModel
 @Composable
 @Preview
 private fun PreviewDeviceListScreenContent(
-    @PreviewParameter(DeviceListPreviewParameterProvider::class) devices: List<DeviceItemUiState>
+    @PreviewParameter(DeviceListUiStatePreviewParameterProvider::class) state: DeviceListUiState
 ) {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Content(devices = devices)) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListScreenEmpty() {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Content(devices = emptyList())) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListLoading() {
-    AppTheme { DeviceListScreen(state = DeviceListUiState.Loading) }
-}
-
-@Composable
-@Preview
-private fun PreviewDeviceListError() {
-    AppTheme {
-        DeviceListScreen(
-            state =
-                DeviceListUiState.Error(GetDeviceListError.Unknown(IllegalStateException("Error")))
-        )
-    }
+    AppTheme { DeviceListScreen(state = state) }
 }
 
 data class DeviceListNavArgs(val accountNumber: AccountNumber)

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoginScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.compose.dropUnlessResumed
 import com.ramcosta.composedestinations.annotation.Destination
@@ -63,6 +64,7 @@ import net.mullvad.mullvadvpn.compose.button.PrimaryButton
 import net.mullvad.mullvadvpn.compose.button.VariantButton
 import net.mullvad.mullvadvpn.compose.component.MullvadCircularProgressIndicatorLarge
 import net.mullvad.mullvadvpn.compose.component.ScaffoldWithTopBar
+import net.mullvad.mullvadvpn.compose.preview.LoginUiStatePreviewParameterProvider
 import net.mullvad.mullvadvpn.compose.state.LoginError
 import net.mullvad.mullvadvpn.compose.state.LoginState
 import net.mullvad.mullvadvpn.compose.state.LoginState.Idle
@@ -84,32 +86,10 @@ import org.koin.androidx.compose.koinViewModel
 
 @Preview
 @Composable
-private fun PreviewIdle() {
-    AppTheme { LoginScreen(state = LoginUiState()) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoggingIn() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Loading.LoggingIn)) }
-}
-
-@Preview
-@Composable
-private fun PreviewCreatingAccount() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Loading.CreatingAccount)) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoginError() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Idle(LoginError.InvalidCredentials))) }
-}
-
-@Preview
-@Composable
-private fun PreviewLoginSuccess() {
-    AppTheme { LoginScreen(state = LoginUiState(loginState = Success)) }
+private fun PreviewLoginScreen(
+    @PreviewParameter(LoginUiStatePreviewParameterProvider::class) state: LoginUiState
+) {
+    AppTheme { LoginScreen(state = state) }
 }
 
 @Destination<RootGraph>(style = LoginTransition::class)