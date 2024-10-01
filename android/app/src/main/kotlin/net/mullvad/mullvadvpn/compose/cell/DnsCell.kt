mullvad/mullvadvpn-app

Use material icons

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/DnsCell.kt
@@ -1,12 +1,13 @@
 package net.mullvad.mullvadvpn.compose.cell
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +39,7 @@ fun DnsCell(
         bodyView = {
             if (isUnreachableLocalDnsWarningVisible) {
                 Icon(
-                    painter = painterResource(id = R.drawable.icon_alert),
+                    imageVector = Icons.Default.Warning,
                     contentDescription = stringResource(id = R.string.confirm_local_dns),
                     tint = MaterialTheme.colorScheme.warning,
                 )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/NavigationComposeCell.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.OpenInNew
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,11 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.compose.component.ChevronRight
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 
@@ -67,7 +68,7 @@ fun NavigationComposeCell(
     showWarning: Boolean = false,
     textColor: Color = MaterialTheme.colorScheme.onPrimary,
     bodyView: @Composable () -> Unit = {
-        ChevronRight(contentDescription = title, tint = textColor)
+        Icon(Icons.Default.ChevronRight, contentDescription = title, tint = textColor)
     },
     isRowEnabled: Boolean = true,
     onClick: () -> Unit,
@@ -96,7 +97,7 @@ internal fun NavigationTitleView(
 ) {
     if (showWarning) {
         Icon(
-            painter = painterResource(id = R.drawable.icon_alert),
+            imageVector = Icons.Default.Warning,
             modifier = Modifier.padding(end = Dimens.smallPadding),
             contentDescription = null,
             tint = MaterialTheme.colorScheme.error,
@@ -115,7 +116,7 @@ internal fun NavigationTitleView(
 @Composable
 internal fun DefaultExternalLinkView(chevronContentDescription: String, tint: Color) {
     Icon(
-        painter = painterResource(id = R.drawable.icon_extlink),
+        imageVector = Icons.Default.OpenInNew,
         contentDescription = chevronContentDescription,
         tint = tint,
     )
@@ -139,7 +140,11 @@ internal fun NavigationCellBody(
         if (isExternalLink) {
             DefaultExternalLinkView(content, tint = contentColor)
         } else {
-            ChevronRight(tint = contentColor, contentDescription = contentBodyDescription)
+            Icon(
+                Icons.Default.ChevronRight,
+                tint = contentColor,
+                contentDescription = contentBodyDescription,
+            )
         }
     }
 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/CopyableObfuscationView.kt
@@ -2,6 +2,11 @@ package net.mullvad.mullvadvpn.compose.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,7 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.R
@@ -38,8 +42,8 @@ fun CopyableObfuscationView(
             modifier = Modifier.weight(1f),
         )
         AnimatedIconButton(
-            defaultIcon = painterResource(id = R.drawable.icon_hide),
-            secondaryIcon = painterResource(id = R.drawable.icon_show),
+            defaultIcon = Icons.Default.VisibilityOff,
+            secondaryIcon = Icons.Default.Visibility,
             defaultIconTint = MaterialTheme.colorScheme.onSurface,
             secondaryIconTint = MaterialTheme.colorScheme.onSurface,
             isToggleButton = true,
@@ -54,8 +58,8 @@ fun CopyableObfuscationView(
 @Composable
 fun CopyAnimatedIconButton(onClick: () -> Unit) {
     AnimatedIconButton(
-        defaultIcon = painterResource(id = R.drawable.icon_copy),
-        secondaryIcon = painterResource(id = R.drawable.icon_tick),
+        defaultIcon = Icons.Default.ContentCopy,
+        secondaryIcon = Icons.Default.Check,
         defaultIconTint = MaterialTheme.colorScheme.onSurface,
         secondaryIconTint = MaterialTheme.colorScheme.tertiary,
         isToggleButton = false,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DaitaConfirmationDialog.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WarningAmber
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
@@ -44,7 +45,7 @@ fun DaitaConfirmation(navigator: ResultBackNavigator<Boolean>) {
         icon = {
             Icon(
                 modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
-                painter = painterResource(id = R.drawable.icon_alert),
+                imageVector = Icons.Default.WarningAmber,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onSurface,
             )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/NegativeConfirmationDialog.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WarningAmber
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
@@ -14,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -80,7 +81,7 @@ fun NegativeConfirmationDialog(
         icon = {
             Icon(
                 modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
-                painter = painterResource(id = R.drawable.icon_alert),
+                imageVector = Icons.Default.WarningAmber,
                 contentDescription = stringResource(id = R.string.remove_button),
                 tint = MaterialTheme.colorScheme.error,
             )

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/DnsCell.kt
@@ -1,12 +1,14 @@
 package net.mullvad.mullvadvpn.compose.cell
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.rounded.Error
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +40,7 @@ fun DnsCell(
         bodyView = {
             if (isUnreachableLocalDnsWarningVisible) {
                 Icon(
-                    painter = painterResource(id = R.drawable.icon_alert),
+                    imageVector = Icons.Rounded.Error,
                     contentDescription = stringResource(id = R.string.confirm_local_dns),
                     tint = MaterialTheme.colorScheme.warning,
                 )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/cell/NavigationComposeCell.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.OpenInNew
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -13,11 +17,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import net.mullvad.mullvadvpn.R
-import net.mullvad.mullvadvpn.compose.component.ChevronRight
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
 
@@ -67,7 +68,7 @@ fun NavigationComposeCell(
     showWarning: Boolean = false,
     textColor: Color = MaterialTheme.colorScheme.onPrimary,
     bodyView: @Composable () -> Unit = {
-        ChevronRight(contentDescription = title, tint = textColor)
+        Icon(Icons.Default.ChevronRight, contentDescription = title, tint = textColor)
     },
     isRowEnabled: Boolean = true,
     onClick: () -> Unit,
@@ -96,7 +97,7 @@ internal fun NavigationTitleView(
 ) {
     if (showWarning) {
         Icon(
-            painter = painterResource(id = R.drawable.icon_alert),
+            imageVector = Icons.Default.Error,
             modifier = Modifier.padding(end = Dimens.smallPadding),
             contentDescription = null,
             tint = MaterialTheme.colorScheme.error,
@@ -115,7 +116,7 @@ internal fun NavigationTitleView(
 @Composable
 internal fun DefaultExternalLinkView(chevronContentDescription: String, tint: Color) {
     Icon(
-        painter = painterResource(id = R.drawable.icon_extlink),
+        imageVector = Icons.Default.OpenInNew,
         contentDescription = chevronContentDescription,
         tint = tint,
     )
@@ -139,7 +140,11 @@ internal fun NavigationCellBody(
         if (isExternalLink) {
             DefaultExternalLinkView(content, tint = contentColor)
         } else {
-            ChevronRight(tint = contentColor, contentDescription = contentBodyDescription)
+            Icon(
+                Icons.Default.ChevronRight,
+                tint = contentColor,
+                contentDescription = contentBodyDescription,
+            )
         }
     }
 }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/component/CopyableObfuscationView.kt
@@ -2,6 +2,11 @@ package net.mullvad.mullvadvpn.compose.component
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,7 +15,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import net.mullvad.mullvadvpn.R
@@ -38,8 +42,8 @@ fun CopyableObfuscationView(
             modifier = Modifier.weight(1f),
         )
         AnimatedIconButton(
-            defaultIcon = painterResource(id = R.drawable.icon_hide),
-            secondaryIcon = painterResource(id = R.drawable.icon_show),
+            defaultIcon = Icons.Default.Visibility,
+            secondaryIcon = Icons.Default.VisibilityOff,
             defaultIconTint = MaterialTheme.colorScheme.onSurface,
             secondaryIconTint = MaterialTheme.colorScheme.onSurface,
             isToggleButton = true,
@@ -54,8 +58,8 @@ fun CopyableObfuscationView(
 @Composable
 fun CopyAnimatedIconButton(onClick: () -> Unit) {
     AnimatedIconButton(
-        defaultIcon = painterResource(id = R.drawable.icon_copy),
-        secondaryIcon = painterResource(id = R.drawable.icon_tick),
+        defaultIcon = Icons.Default.ContentCopy,
+        secondaryIcon = Icons.Default.Check,
         defaultIconTint = MaterialTheme.colorScheme.onSurface,
         secondaryIconTint = MaterialTheme.colorScheme.tertiary,
         isToggleButton = false,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DaitaConfirmationDialog.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.dropUnlessResumed
@@ -44,7 +45,7 @@ fun DaitaConfirmation(navigator: ResultBackNavigator<Boolean>) {
         icon = {
             Icon(
                 modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
-                painter = painterResource(id = R.drawable.icon_alert),
+                imageVector = Icons.Default.Error,
                 contentDescription = null,
                 tint = MaterialTheme.colorScheme.onSurface,
             )

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/NegativeConfirmationDialog.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
@@ -14,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
@@ -80,7 +81,7 @@ fun NegativeConfirmationDialog(
         icon = {
             Icon(
                 modifier = Modifier.fillMaxWidth().height(Dimens.dialogIconHeight),
-                painter = painterResource(id = R.drawable.icon_alert),
+                imageVector = Icons.Default.Error,
                 contentDescription = stringResource(id = R.string.remove_button),
                 tint = MaterialTheme.colorScheme.error,
             )