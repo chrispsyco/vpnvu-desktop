mullvad/mullvadvpn-app

Fix exhaustive-deps rule violations

gui/src/renderer/components/NavigationBar.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useContext, useEffect, useLayoutEffect, useMemo, useRef } from 'react';
+import React, { useCallback, useContext, useEffect, useLayoutEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import { colors } from '../../config.json';
 import { messages } from '../../shared/gettext';
 import { useAppContext } from '../context';
 import { transitions, useHistory } from '../lib/history';
-import { useCombinedRefs } from '../lib/utilityHooks';
+import { useCombinedRefs, useEffectEvent, useInitialValue } from '../lib/utilityHooks';
 import CustomScrollbars, { CustomScrollbarsRef, IScrollEvent } from './CustomScrollbars';
 import InfoButton from './InfoButton';
 import { BackActionContext } from './KeyboardNavigation';
@@ -97,36 +97,44 @@ export const NavigationScrollbars = React.forwardRef(function NavigationScrollba
   const ref = useRef<CustomScrollbarsRef>();
   const combinedRefs = useCombinedRefs(forwardedRef, ref);
 
+  const beforeunload = useEffectEvent(() => {
+    if (ref.current) {
+      history.location.state.scrollPosition = ref.current.getScrollPosition();
+      setNavigationHistory(history.asObject);
+    }
+  });
+
   useEffect(() => {
-    const beforeunload = () => {
-      if (ref.current) {
-        history.location.state.scrollPosition = ref.current.getScrollPosition();
-        setNavigationHistory(history.asObject);
-      }
-    };
-
     window.addEventListener('beforeunload', beforeunload);
-
     return () => window.removeEventListener('beforeunload', beforeunload);
   }, []);
 
-  useLayoutEffect(() => {
+  const onMount = useEffectEvent(() => {
     const location = history.location;
     if (history.action === 'POP') {
       ref.current?.scrollTo(...location.state.scrollPosition);
     }
+  });
 
-    return () => {
-      if (history.action === 'PUSH' && ref.current) {
-        location.state.scrollPosition = ref.current.getScrollPosition();
-        setNavigationHistory(history.asObject);
-      }
-    };
+  const onUnmount = useEffectEvent(() => {
+    const location = history.location;
+    if (history.action === 'PUSH' && ref.current) {
+      location.state.scrollPosition = ref.current.getScrollPosition();
+      setNavigationHistory(history.asObject);
+    }
+  });
+
+  useLayoutEffect(() => {
+    onMount();
+    return () => onUnmount();
   }, []);
 
-  const handleScroll = useCallback((event: IScrollEvent) => {
-    onScroll(event);
-  }, []);
+  const handleScroll = useCallback(
+    (event: IScrollEvent) => {
+      onScroll(event);
+    },
+    [onScroll],
+  );
 
   return (
     <CustomScrollbars
@@ -189,7 +197,9 @@ export function BackBarItem() {
   const history = useHistory();
   // Compare the transition name with dismiss to infer wheter or not the view will slide
   // horizontally or vertically and then use matching button.
-  const backIcon = useMemo(() => history.getPopTransition().name !== transitions.dismiss.name, []);
+  const backIcon = useInitialValue(
+    () => history.getPopTransition().name !== transitions.dismiss.name,
+  );
   const { parentBackAction } = useContext(BackActionContext);
   const iconSource = backIcon ? 'icon-back' : 'icon-close-down';
   const ariaLabel = backIcon ? messages.gettext('Back') : messages.gettext('Close');

gui/src/renderer/components/SettingsTextImport.tsx
@@ -5,7 +5,7 @@ import { colors } from '../../config.json';
 import { messages } from '../../shared/gettext';
 import useActions from '../lib/actionsHook';
 import { useHistory } from '../lib/history';
-import { useCombinedRefs, useStyledRef } from '../lib/utilityHooks';
+import { useCombinedRefs, useEffectEvent, useStyledRef } from '../lib/utilityHooks';
 import settingsImportActions from '../redux/settings-import/actions';
 import { useSelector } from '../redux/store';
 import ImageView from './ImageView';
@@ -21,18 +21,18 @@ const StyledTextArea = styled.textarea({
 });
 
 export default function SettingsTextImport() {
-  const history = useHistory();
+  const { pop } = useHistory();
 
   const { saveSettingsImportForm } = useActions(settingsImportActions);
   // The textarea value is saved in redux to make it persistent when leaving the view.
   const initialValue = useSelector((state) => state.settingsImport.value);
 
   const textareaRef = useStyledRef<HTMLTextAreaElement>();
-  const onTextareaLoad = useCallback((element?: HTMLTextAreaElement) => {
+  const onTextareaLoad = useEffectEvent((element?: HTMLTextAreaElement) => {
     if (element) {
       element.value = initialValue;
     }
-  }, []);
+  });
 
   const combinedTextAreaRef = useCombinedRefs(textareaRef, onTextareaLoad);
 
@@ -40,15 +40,15 @@ export default function SettingsTextImport() {
     if (textareaRef.current?.value) {
       saveSettingsImportForm(textareaRef.current.value, true);
     }
-    history.pop();
-  }, [history]);
+    pop();
+  }, [pop, saveSettingsImportForm, textareaRef]);
 
   const back = useCallback(() => {
     if (textareaRef.current) {
       saveSettingsImportForm(textareaRef.current.value, false);
     }
-    history.pop();
-  }, [history]);
+    pop();
+  }, [pop, saveSettingsImportForm, textareaRef]);
 
   return (
     <BackAction action={back}>

chrispsyco/vpnvu-desktop

gui/src/renderer/components/NavigationBar.tsx
@@ -5,7 +5,7 @@ import { colors } from '../../config.json';
 import { messages } from '../../shared/gettext';
 import { useAppContext } from '../context';
 import { transitions, useHistory } from '../lib/history';
-import { useCombinedRefs } from '../lib/utilityHooks';
+import { useCombinedRefs, useEffectEvent } from '../lib/utilityHooks';
 import CustomScrollbars, { CustomScrollbarsRef, IScrollEvent } from './CustomScrollbars';
 import InfoButton from './InfoButton';
 import { BackActionContext } from './KeyboardNavigation';
@@ -97,36 +97,44 @@ export const NavigationScrollbars = React.forwardRef(function NavigationScrollba
   const ref = useRef<CustomScrollbarsRef>();
   const combinedRefs = useCombinedRefs(forwardedRef, ref);
 
+  const beforeunload = useEffectEvent(() => {
+    if (ref.current) {
+      history.location.state.scrollPosition = ref.current.getScrollPosition();
+      setNavigationHistory(history.asObject);
+    }
+  });
+
   useEffect(() => {
-    const beforeunload = () => {
-      if (ref.current) {
-        history.location.state.scrollPosition = ref.current.getScrollPosition();
-        setNavigationHistory(history.asObject);
-      }
-    };
-
     window.addEventListener('beforeunload', beforeunload);
-
     return () => window.removeEventListener('beforeunload', beforeunload);
   }, []);
 
-  useLayoutEffect(() => {
+  const onMount = useEffectEvent(() => {
     const location = history.location;
     if (history.action === 'POP') {
       ref.current?.scrollTo(...location.state.scrollPosition);
     }
+  });
 
-    return () => {
-      if (history.action === 'PUSH' && ref.current) {
-        location.state.scrollPosition = ref.current.getScrollPosition();
-        setNavigationHistory(history.asObject);
-      }
-    };
+  const onUnmount = useEffectEvent(() => {
+    const location = history.location;
+    if (history.action === 'PUSH' && ref.current) {
+      location.state.scrollPosition = ref.current.getScrollPosition();
+      setNavigationHistory(history.asObject);
+    }
+  });
+
+  useLayoutEffect(() => {
+    onMount();
+    return () => onUnmount();
   }, []);
 
-  const handleScroll = useCallback((event: IScrollEvent) => {
-    onScroll(event);
-  }, []);
+  const handleScroll = useCallback(
+    (event: IScrollEvent) => {
+      onScroll(event);
+    },
+    [onScroll],
+  );
 
   return (
     <CustomScrollbars
@@ -189,7 +197,10 @@ export function BackBarItem() {
   const history = useHistory();
   // Compare the transition name with dismiss to infer wheter or not the view will slide
   // horizontally or vertically and then use matching button.
-  const backIcon = useMemo(() => history.getPopTransition().name !== transitions.dismiss.name, []);
+  const backIcon = useMemo(
+    () => history.getPopTransition().name !== transitions.dismiss.name,
+    [history],
+  );
   const { parentBackAction } = useContext(BackActionContext);
   const iconSource = backIcon ? 'icon-back' : 'icon-close-down';
   const ariaLabel = backIcon ? messages.gettext('Back') : messages.gettext('Close');

gui/src/renderer/components/SettingsTextImport.tsx
@@ -5,7 +5,7 @@ import { colors } from '../../config.json';
 import { messages } from '../../shared/gettext';
 import useActions from '../lib/actionsHook';
 import { useHistory } from '../lib/history';
-import { useCombinedRefs, useStyledRef } from '../lib/utilityHooks';
+import { useCombinedRefs, useRefCallback, useStyledRef } from '../lib/utilityHooks';
 import settingsImportActions from '../redux/settings-import/actions';
 import { useSelector } from '../redux/store';
 import ImageView from './ImageView';
@@ -21,18 +21,18 @@ const StyledTextArea = styled.textarea({
 });
 
 export default function SettingsTextImport() {
-  const history = useHistory();
+  const { pop } = useHistory();
 
   const { saveSettingsImportForm } = useActions(settingsImportActions);
   // The textarea value is saved in redux to make it persistent when leaving the view.
   const initialValue = useSelector((state) => state.settingsImport.value);
 
   const textareaRef = useStyledRef<HTMLTextAreaElement>();
-  const onTextareaLoad = useCallback((element?: HTMLTextAreaElement) => {
+  const onTextareaLoad = useRefCallback((element?: HTMLTextAreaElement) => {
     if (element) {
       element.value = initialValue;
     }
-  }, []);
+  });
 
   const combinedTextAreaRef = useCombinedRefs(textareaRef, onTextareaLoad);
 
@@ -40,15 +40,15 @@ export default function SettingsTextImport() {
     if (textareaRef.current?.value) {
       saveSettingsImportForm(textareaRef.current.value, true);
     }
-    history.pop();
-  }, [history]);
+    pop();
+  }, [pop, saveSettingsImportForm, textareaRef]);
 
   const back = useCallback(() => {
     if (textareaRef.current) {
       saveSettingsImportForm(textareaRef.current.value, false);
     }
-    history.pop();
-  }, [history]);
+    pop();
+  }, [pop, saveSettingsImportForm, textareaRef]);
 
   return (
     <BackAction action={back}>