mullvad/mullvadvpn-app

Use KeyboardType.Number on Amazon FireStick

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoginScreen.kt
@@ -82,6 +82,7 @@ import net.mullvad.mullvadvpn.compose.transitions.LoginTransition
 import net.mullvad.mullvadvpn.compose.util.CollectSideEffectWithLifecycle
 import net.mullvad.mullvadvpn.compose.util.OnNavResultValue
 import net.mullvad.mullvadvpn.compose.util.accountNumberVisualTransformation
+import net.mullvad.mullvadvpn.compose.util.numberPasswordInputType
 import net.mullvad.mullvadvpn.compose.util.showSnackbarImmediately
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
 import net.mullvad.mullvadvpn.lib.theme.Dimens
@@ -285,7 +286,7 @@ private fun ColumnScope.LoginInput(
         keyboardOptions =
             KeyboardOptions(
                 imeAction = if (state.loginButtonEnabled) ImeAction.Done else ImeAction.None,
-                keyboardType = KeyboardType.NumberPassword,
+                keyboardType = KeyboardType.numberPasswordInputType(LocalContext.current),
             ),
         onValueChange = onAccountNumberChange,
         singleLine = true,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/util/Keyboard.kt
@@ -1,0 +1,15 @@
+package net.mullvad.mullvadvpn.compose.util
+
+import android.content.Context
+import androidx.compose.ui.text.input.KeyboardType
+
+fun KeyboardType.Companion.numberPasswordInputType(context: Context): KeyboardType =
+    if (isFireStick(context)) {
+        Number
+    } else {
+        NumberPassword
+    }
+
+// see: https://developer.amazon.com/docs/fire-tv/identify-amazon-fire-tv-devices.html
+private fun isFireStick(context: Context): Boolean =
+    context.packageManager.hasSystemFeature("amazon.hardware.fire_tv")

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/LoginScreen.kt
@@ -81,6 +81,7 @@ import net.mullvad.mullvadvpn.compose.textfield.mullvadWhiteTextFieldColors
 import net.mullvad.mullvadvpn.compose.transitions.LoginTransition
 import net.mullvad.mullvadvpn.compose.util.CollectSideEffectWithLifecycle
 import net.mullvad.mullvadvpn.compose.util.OnNavResultValue
+import net.mullvad.mullvadvpn.compose.util.accountNumberKeyboardType
 import net.mullvad.mullvadvpn.compose.util.accountNumberVisualTransformation
 import net.mullvad.mullvadvpn.compose.util.showSnackbarImmediately
 import net.mullvad.mullvadvpn.lib.theme.AppTheme
@@ -285,7 +286,7 @@ private fun ColumnScope.LoginInput(
         keyboardOptions =
             KeyboardOptions(
                 imeAction = if (state.loginButtonEnabled) ImeAction.Done else ImeAction.None,
-                keyboardType = KeyboardType.NumberPassword,
+                keyboardType = KeyboardType.accountNumberKeyboardType(LocalContext.current),
             ),
         onValueChange = onAccountNumberChange,
         singleLine = true,

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/util/Keyboard.kt
@@ -1,0 +1,15 @@
+package net.mullvad.mullvadvpn.compose.util
+
+import android.content.Context
+import androidx.compose.ui.text.input.KeyboardType
+
+fun KeyboardType.Companion.accountNumberKeyboardType(context: Context): KeyboardType =
+    if (isFireStick(context)) {
+        Number
+    } else {
+        NumberPassword
+    }
+
+// See: https://developer.amazon.com/docs/fire-tv/identify-amazon-fire-tv-devices.html
+private fun isFireStick(context: Context): Boolean =
+    context.packageManager.hasSystemFeature("amazon.hardware.fire_tv")