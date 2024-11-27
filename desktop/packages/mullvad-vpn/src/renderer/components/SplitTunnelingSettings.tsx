mullvad/mullvadvpn-app

Add daemon restart button to split tunneling settings on MacOS

desktop/packages/mullvad-vpn/src/renderer/components/SplitTunnelingSettings.tsx
@@ -26,6 +26,7 @@ import { ModalAlert, ModalAlertType } from './Modal';
 import { NavigationBar, NavigationContainer, NavigationItems, TitleBarItem } from './NavigationBar';
 import SettingsHeader, { HeaderSubTitle, HeaderTitle } from './SettingsHeader';
 import {
+  Spacing,
   StyledActionIcon,
   StyledBrowseButton,
   StyledCellButton,
@@ -37,6 +38,7 @@ import {
   StyledIcon,
   StyledIconPlaceholder,
   StyledListContainer,
+  StyledMiniTitle,
   StyledNavigationScrollbars,
   StyledNoResult,
   StyledNoResultText,
@@ -534,7 +536,8 @@ interface MacOsSplitTunnelingAvailabilityProps {
 function MacOsSplitTunnelingAvailability({
   needFullDiskPermissions,
 }: MacOsSplitTunnelingAvailabilityProps) {
-  const { showFullDiskAccessSettings } = useAppContext();
+  const { showFullDiskAccessSettings, daemonPrepareRestart } = useAppContext();
+  const restartDaemon = () => daemonPrepareRestart(true);
 
   return (
     <>
@@ -546,8 +549,20 @@ function MacOsSplitTunnelingAvailability({
               'To use split tunneling please enable “Full disk access” for “Mullvad VPN” in the macOS system settings.',
             )}
           </HeaderSubTitle>
+          <Spacing height="24px" />
           <StyledSystemSettingsButton onClick={showFullDiskAccessSettings}>
             Open System Settings
+          </StyledSystemSettingsButton>
+          <Spacing height="32px" />
+          <StyledMiniTitle>
+            {messages.pgettext(
+              'split-tunneling-view',
+              'Enabled "Fill disk access" and still having issues?',
+            )}
+          </StyledMiniTitle>
+          <Spacing height="8px" />
+          <StyledSystemSettingsButton onClick={restartDaemon}>
+            Restart Mullvad Service
           </StyledSystemSettingsButton>
         </>
       ) : null}

desktop/packages/mullvad-vpn/src/renderer/components/SplitTunnelingSettingsStyles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { colors } from '../../config.json';
 import * as AppButton from './AppButton';
 import * as Cell from './cell';
-import { measurements, normalText } from './common-styles';
+import { measurements, normalText, miniText } from './common-styles';
 import ImageView from './ImageView';
 import { NavigationScrollbars } from './NavigationBar';
 import SearchBar from './SearchBar';
@@ -118,6 +118,11 @@ export const StyledHeaderTitle = styled(HeaderTitle)({
   flex: 1,
 });
 
+export const StyledMiniTitle = styled.span({
+  ...miniText,
+  color: colors.white60,
+});
+
 export const StyledSearchBar = styled(SearchBar)({
   marginLeft: measurements.viewMargin,
   marginRight: measurements.viewMargin,
@@ -126,5 +131,8 @@ export const StyledSearchBar = styled(SearchBar)({
 
 export const StyledSystemSettingsButton = styled(SmallButton)({
   width: '100%',
-  marginTop: '24px',
 });
+
+export const Spacing = styled.div<{ height: string }>((props) => ({
+  height: props.height,
+}));

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/SplitTunnelingSettings.tsx
@@ -20,7 +20,7 @@ import * as Cell from './cell';
 import { CustomScrollbarsRef } from './CustomScrollbars';
 import ImageView from './ImageView';
 import { BackAction } from './KeyboardNavigation';
-import { Layout, SettingsContainer } from './Layout';
+import { Layout, SettingsContainer, Spacing } from './Layout';
 import List from './List';
 import { ModalAlert, ModalAlertType } from './Modal';
 import { NavigationBar, NavigationContainer, NavigationItems, TitleBarItem } from './NavigationBar';
@@ -37,13 +37,14 @@ import {
   StyledIcon,
   StyledIconPlaceholder,
   StyledListContainer,
+  StyledMiniTitle,
   StyledNavigationScrollbars,
   StyledNoResult,
   StyledNoResultText,
   StyledPageCover,
   StyledSearchBar,
   StyledSpinnerRow,
-  StyledSystemSettingsButton,
+  WideSmallButton,
 } from './SplitTunnelingSettingsStyles';
 import Switch from './Switch';
 
@@ -534,7 +535,8 @@ interface MacOsSplitTunnelingAvailabilityProps {
 function MacOsSplitTunnelingAvailability({
   needFullDiskPermissions,
 }: MacOsSplitTunnelingAvailabilityProps) {
-  const { showFullDiskAccessSettings } = useAppContext();
+  const { showFullDiskAccessSettings, daemonPrepareRestart } = useAppContext();
+  const restartDaemon = useCallback(() => daemonPrepareRestart(true), [daemonPrepareRestart]);
 
   return (
     <>
@@ -546,9 +548,21 @@ function MacOsSplitTunnelingAvailability({
               'To use split tunneling please enable “Full disk access” for “Mullvad VPN” in the macOS system settings.',
             )}
           </HeaderSubTitle>
-          <StyledSystemSettingsButton onClick={showFullDiskAccessSettings}>
-            Open System Settings
-          </StyledSystemSettingsButton>
+          <Spacing height="24px" />
+          <WideSmallButton onClick={showFullDiskAccessSettings}>
+            {messages.pgettext('split-tunneling-view', 'Open System Settings')}
+          </WideSmallButton>
+          <Spacing height="32px" />
+          <StyledMiniTitle>
+            {messages.pgettext(
+              'split-tunneling-view',
+              'Enabled "Full disk access" and still having issues?',
+            )}
+          </StyledMiniTitle>
+          <Spacing height="8px" />
+          <WideSmallButton onClick={restartDaemon}>
+            {messages.pgettext('split-tunneling-view', 'Restart Mullvad Service')}
+          </WideSmallButton>
         </>
       ) : null}
     </>

desktop/packages/mullvad-vpn/src/renderer/components/SplitTunnelingSettingsStyles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { colors } from '../../config.json';
 import * as AppButton from './AppButton';
 import * as Cell from './cell';
-import { measurements, normalText } from './common-styles';
+import { measurements, miniText, normalText } from './common-styles';
 import ImageView from './ImageView';
 import { NavigationScrollbars } from './NavigationBar';
 import SearchBar from './SearchBar';
@@ -118,13 +118,21 @@ export const StyledHeaderTitle = styled(HeaderTitle)({
   flex: 1,
 });
 
+export const StyledMiniTitle = styled.span({
+  ...miniText,
+  color: colors.white60,
+});
+
 export const StyledSearchBar = styled(SearchBar)({
   marginLeft: measurements.viewMargin,
   marginRight: measurements.viewMargin,
   marginBottom: measurements.buttonVerticalMargin,
 });
 
-export const StyledSystemSettingsButton = styled(SmallButton)({
+export const WideSmallButton = styled(SmallButton)({
   width: '100%',
-  marginTop: '24px',
 });
+
+export const Spacing = styled.div<{ height: string }>((props) => ({
+  height: props.height,
+}));