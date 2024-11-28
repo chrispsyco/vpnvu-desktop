mullvad/mullvadvpn-app

Split view margin spacing into vertical and horizontal variant

desktop/packages/mullvad-vpn/src/renderer/components/common-styles.ts
@@ -81,7 +81,8 @@ export const spacings = {
 
 export const measurements = {
   rowMinHeight: spacings.spacing9,
-  viewMargin: spacings.spacing6,
+  horizontalViewMargin: spacings.spacing6,
+  verticalViewMargin: spacings.spacing5,
   rowVerticalMargin: spacings.spacing6,
   buttonVerticalMargin: spacings.spacing5,
 };

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/common-styles.ts
@@ -88,7 +88,8 @@ export const spacings = {
 
 export const measurements = {
   rowMinHeight: spacings.spacing9,
-  viewMargin: spacings.spacing6,
+  horizontalViewMargin: spacings.spacing5,
+  verticalViewMargin: spacings.spacing6,
   rowVerticalMargin: spacings.spacing6,
   buttonVerticalMargin: spacings.spacing5,
 };