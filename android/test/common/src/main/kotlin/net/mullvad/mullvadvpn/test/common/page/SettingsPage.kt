mullvad/mullvadvpn-app

Migrate ConnectionTest e2e tests to use POP

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SettingsPage.kt
@@ -1,0 +1,20 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import androidx.test.uiautomator.By
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+
+class SettingsPage internal constructor() : Page() {
+    private val bySettingsText = By.text("Settings")
+
+    override fun assertIsDisplayed() {
+        uiDevice.findObjectWithTimeout(bySettingsText)
+    }
+
+    fun clickVpnSettings() {
+        uiDevice.findObjectWithTimeout(By.res(VPN_SETTINGS_CELL_TEST_TAG)).click()
+    }
+
+    companion object {
+        const val VPN_SETTINGS_CELL_TEST_TAG = "vpn_settings_cell_test_tag"
+    }
+}

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SystemVpnConfigurationAlert.kt
@@ -1,0 +1,16 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import androidx.test.uiautomator.By
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+
+class SystemVpnConfigurationAlert internal constructor() : Page() {
+    private val byOkText = By.text("OK")
+
+    override fun assertIsDisplayed() {
+        uiDevice.findObjectWithTimeout(byOkText)
+    }
+
+    fun clickOk() {
+        uiDevice.findObjectWithTimeout(byOkText).click()
+    }
+}

chrispsyco/vpnvu-desktop

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SettingsPage.kt
@@ -1,0 +1,20 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import androidx.test.uiautomator.By
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+
+class SettingsPage internal constructor() : Page() {
+    private val settingsSelector = By.text("Settings")
+
+    override fun assertIsDisplayed() {
+        uiDevice.findObjectWithTimeout(settingsSelector)
+    }
+
+    fun clickVpnSettings() {
+        uiDevice.findObjectWithTimeout(By.res(VPN_SETTINGS_CELL_TEST_TAG)).click()
+    }
+
+    companion object {
+        const val VPN_SETTINGS_CELL_TEST_TAG = "vpn_settings_cell_test_tag"
+    }
+}

android/test/common/src/main/kotlin/net/mullvad/mullvadvpn/test/common/page/SystemVpnConfigurationAlert.kt
@@ -1,0 +1,16 @@
+package net.mullvad.mullvadvpn.test.common.page
+
+import androidx.test.uiautomator.By
+import net.mullvad.mullvadvpn.test.common.extension.findObjectWithTimeout
+
+class SystemVpnConfigurationAlert internal constructor() : Page() {
+    private val okSelector = By.text("OK")
+
+    override fun assertIsDisplayed() {
+        uiDevice.findObjectWithTimeout(okSelector)
+    }
+
+    fun clickOk() {
+        uiDevice.findObjectWithTimeout(okSelector).click()
+    }
+}