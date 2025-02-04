mullvad/mullvadvpn-app

Do not show add server when custom dns server list is empty

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/communication/DnsDialogResult.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 interface DnsDialogResult : Parcelable {
-    @Parcelize data object Success : DnsDialogResult
+    @Parcelize data class Success(val allDeleted: Boolean) : DnsDialogResult
 
     @Parcelize data object Error : DnsDialogResult
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DnsDialog.kt
@@ -51,8 +51,8 @@ fun Dns(resultNavigator: ResultBackNavigator<DnsDialogResult>) {
 
     CollectSideEffectWithLifecycle(viewModel.uiSideEffect) {
         when (it) {
-            DnsDialogSideEffect.Complete ->
-                resultNavigator.navigateBack(result = DnsDialogResult.Success)
+            is DnsDialogSideEffect.Complete ->
+                resultNavigator.navigateBack(result = DnsDialogResult.Success(it.isAllDeleted))
             DnsDialogSideEffect.Error ->
                 resultNavigator.navigateBack(result = DnsDialogResult.Error)
         }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreen.kt
@@ -161,7 +161,12 @@ fun VpnSettings(
 
     dnsDialogResult.OnNavResultValue { result ->
         when (result) {
-            DnsDialogResult.Success -> vm.showApplySettingChangesWarningToast()
+            is DnsDialogResult.Success -> {
+                vm.showApplySettingChangesWarningToast()
+                if (result.allDeleted) {
+                    vm.onToggleCustomDns(false)
+                }
+            }
             DnsDialogResult.Cancel -> vm.onDnsDialogDismissed()
             DnsDialogResult.Error -> {
                 vm.showGenericErrorToast()
@@ -473,19 +478,21 @@ fun VpnSettingsScreen(
                     )
                 }
 
-                itemWithDivider {
-                    BaseCell(
-                        onCellClicked = { navigateToDns(null, null) },
-                        headlineContent = {
-                            Text(
-                                text = stringResource(id = R.string.add_a_server),
-                                color = MaterialTheme.colorScheme.onSurface,
-                            )
-                        },
-                        bodyView = {},
-                        background = MaterialTheme.colorScheme.surfaceContainerLow,
-                        startPadding = biggerPadding,
-                    )
+                if (state.customDnsItems.isNotEmpty()) {
+                    itemWithDivider {
+                        BaseCell(
+                            onCellClicked = { navigateToDns(null, null) },
+                            headlineContent = {
+                                Text(
+                                    text = stringResource(id = R.string.add_a_server),
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                )
+                            },
+                            bodyView = {},
+                            background = MaterialTheme.colorScheme.surfaceContainerLow,
+                            startPadding = biggerPadding,
+                        )
+                    }
                 }
             }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/DnsDialogViewModel.kt
@@ -22,10 +22,11 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.lib.model.Settings
 import net.mullvad.mullvadvpn.repository.SettingsRepository
+import net.mullvad.mullvadvpn.usecase.DeleteCustomDnsUseCase
 import org.apache.commons.validator.routines.InetAddressValidator
 
 sealed interface DnsDialogSideEffect {
-    data object Complete : DnsDialogSideEffect
+    data class Complete(val isAllDeleted: Boolean) : DnsDialogSideEffect
 
     data object Error : DnsDialogSideEffect
 }
@@ -52,6 +53,7 @@ class DnsDialogViewModel(
     private val repository: SettingsRepository,
     private val inetAddressValidator: InetAddressValidator,
     savedStateHandle: SavedStateHandle,
+    private val deleteCustomDnsUseCase: DeleteCustomDnsUseCase,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModel() {
     private val navArgs = DnsDestination.argsFrom(savedStateHandle)
@@ -127,17 +129,17 @@ class DnsDialogViewModel(
 
             result.fold(
                 { _uiSideEffect.send(DnsDialogSideEffect.Error) },
-                { _uiSideEffect.send(DnsDialogSideEffect.Complete) },
+                { _uiSideEffect.send(DnsDialogSideEffect.Complete(false)) },
             )
         }
 
     fun onRemoveDnsClick(index: Int) =
         viewModelScope.launch(dispatcher) {
-            repository
-                .deleteCustomDns(index)
+            deleteCustomDnsUseCase
+                .invoke(index)
                 .fold(
                     { _uiSideEffect.send(DnsDialogSideEffect.Error) },
-                    { _uiSideEffect.send(DnsDialogSideEffect.Complete) },
+                    { _uiSideEffect.send(DnsDialogSideEffect.Complete(it == 0)) },
                 )
         }
 

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/communication/DnsDialogResult.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 interface DnsDialogResult : Parcelable {
-    @Parcelize data object Success : DnsDialogResult
+    @Parcelize data class Success(val isDnsListEmpty: Boolean) : DnsDialogResult
 
     @Parcelize data object Error : DnsDialogResult
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/dialog/DnsDialog.kt
@@ -51,8 +51,8 @@ fun Dns(resultNavigator: ResultBackNavigator<DnsDialogResult>) {
 
     CollectSideEffectWithLifecycle(viewModel.uiSideEffect) {
         when (it) {
-            DnsDialogSideEffect.Complete ->
-                resultNavigator.navigateBack(result = DnsDialogResult.Success)
+            is DnsDialogSideEffect.Complete ->
+                resultNavigator.navigateBack(result = DnsDialogResult.Success(it.isDnsListEmpty))
             DnsDialogSideEffect.Error ->
                 resultNavigator.navigateBack(result = DnsDialogResult.Error)
         }

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreen.kt
@@ -158,7 +158,12 @@ fun VpnSettings(
 
     dnsDialogResult.OnNavResultValue { result ->
         when (result) {
-            DnsDialogResult.Success -> vm.showApplySettingChangesWarningToast()
+            is DnsDialogResult.Success -> {
+                vm.showApplySettingChangesWarningToast()
+                if (result.isDnsListEmpty) {
+                    vm.onToggleCustomDns(false)
+                }
+            }
             DnsDialogResult.Cancel -> vm.onDnsDialogDismissed()
             DnsDialogResult.Error -> {
                 vm.showGenericErrorToast()
@@ -466,19 +471,21 @@ fun VpnSettingsScreen(
                     )
                 }
 
-                itemWithDivider {
-                    BaseCell(
-                        onCellClicked = { navigateToDns(null, null) },
-                        headlineContent = {
-                            Text(
-                                text = stringResource(id = R.string.add_a_server),
-                                color = MaterialTheme.colorScheme.onSurface,
-                            )
-                        },
-                        bodyView = {},
-                        background = MaterialTheme.colorScheme.surfaceContainerLow,
-                        startPadding = biggerPadding,
-                    )
+                if (state.customDnsItems.isNotEmpty()) {
+                    itemWithDivider {
+                        BaseCell(
+                            onCellClicked = { navigateToDns(null, null) },
+                            headlineContent = {
+                                Text(
+                                    text = stringResource(id = R.string.add_a_server),
+                                    color = MaterialTheme.colorScheme.onSurface,
+                                )
+                            },
+                            bodyView = {},
+                            background = MaterialTheme.colorScheme.surfaceContainerLow,
+                            startPadding = biggerPadding,
+                        )
+                    }
                 }
             }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/viewmodel/DnsDialogViewModel.kt
@@ -22,10 +22,11 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import net.mullvad.mullvadvpn.lib.model.Settings
 import net.mullvad.mullvadvpn.repository.SettingsRepository
+import net.mullvad.mullvadvpn.usecase.DeleteCustomDnsUseCase
 import org.apache.commons.validator.routines.InetAddressValidator
 
 sealed interface DnsDialogSideEffect {
-    data object Complete : DnsDialogSideEffect
+    data class Complete(val isDnsListEmpty: Boolean) : DnsDialogSideEffect
 
     data object Error : DnsDialogSideEffect
 }
@@ -52,6 +53,7 @@ class DnsDialogViewModel(
     private val repository: SettingsRepository,
     private val inetAddressValidator: InetAddressValidator,
     savedStateHandle: SavedStateHandle,
+    private val deleteCustomDnsUseCase: DeleteCustomDnsUseCase,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModel() {
     private val navArgs = DnsDestination.argsFrom(savedStateHandle)
@@ -127,17 +129,17 @@ class DnsDialogViewModel(
 
             result.fold(
                 { _uiSideEffect.send(DnsDialogSideEffect.Error) },
-                { _uiSideEffect.send(DnsDialogSideEffect.Complete) },
+                { _uiSideEffect.send(DnsDialogSideEffect.Complete(false)) },
             )
         }
 
     fun onRemoveDnsClick(index: Int) =
         viewModelScope.launch(dispatcher) {
-            repository
-                .deleteCustomDns(index)
+            deleteCustomDnsUseCase
+                .invoke(index)
                 .fold(
                     { _uiSideEffect.send(DnsDialogSideEffect.Error) },
-                    { _uiSideEffect.send(DnsDialogSideEffect.Complete) },
+                    { _uiSideEffect.send(DnsDialogSideEffect.Complete(it == 0)) },
                 )
         }
 