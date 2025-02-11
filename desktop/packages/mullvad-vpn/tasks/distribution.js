mullvad/mullvadvpn-app

Implement shortcut parsing
Co-authored-by: David Lönnhager <david.l@mullvad.net>
Co-authored-by: Markus Pettersson <markus.pettersson@mullvad.net>
Co-authored-by: Joakim Hulthe <joakim.hulthe@mullvad.net>

desktop/packages/mullvad-vpn/tasks/distribution.js
@@ -62,6 +62,7 @@ function newConfig() {
       '!node_modules/grpc-tools',
       '!node_modules/@types',
       '!node_modules/nseventforwarder/debug',
+      '!node_modules/rust-utils/debug',
     ],
 
     // Make sure that all files declared in "extraResources" exists and abort if they don't.
@@ -299,12 +300,16 @@ async function packWin() {
             process.env.SETUP_SUBDIR = '.';
             process.env.TARGET_SUBDIR = 'x86_64-pc-windows-msvc';
             process.env.DIST_SUBDIR = '';
+
+            execFileSync('npm', ['-w', 'rust-utils', 'run', 'build-x86']);
             break;
           case 'arm64':
             process.env.TARGET_TRIPLE = 'aarch64-pc-windows-msvc';
             process.env.SETUP_SUBDIR = 'aarch64-pc-windows-msvc';
             process.env.TARGET_SUBDIR = 'aarch64-pc-windows-msvc';
             process.env.DIST_SUBDIR = 'aarch64-pc-windows-msvc';
+
+            execFileSync('npm', ['-w', 'rust-utils', 'run', 'build-arm']);
             break;
           default:
             throw new Error('Invalid or unknown target (only one may be specified)');

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/tasks/distribution.js
@@ -62,6 +62,7 @@ function newConfig() {
       '!node_modules/grpc-tools',
       '!node_modules/@types',
       '!node_modules/nseventforwarder/debug',
+      '!node_modules/win-shortcuts/debug',
     ],
 
     // Make sure that all files declared in "extraResources" exists and abort if they don't.
@@ -300,12 +301,16 @@ async function packWin() {
             process.env.SETUP_SUBDIR = '.';
             process.env.TARGET_SUBDIR = 'x86_64-pc-windows-msvc';
             process.env.DIST_SUBDIR = '';
+
+            execFileSync('npm', ['-w', 'win-shortcuts', 'run', 'build-x86']);
             break;
           case 'arm64':
             process.env.TARGET_TRIPLE = 'aarch64-pc-windows-msvc';
             process.env.SETUP_SUBDIR = 'aarch64-pc-windows-msvc';
             process.env.TARGET_SUBDIR = 'aarch64-pc-windows-msvc';
             process.env.DIST_SUBDIR = 'aarch64-pc-windows-msvc';
+
+            execFileSync('npm', ['-w', 'win-shortcuts', 'run', 'build-arm']);
             break;
           default:
             throw new Error('Invalid or unknown target (only one may be specified)');