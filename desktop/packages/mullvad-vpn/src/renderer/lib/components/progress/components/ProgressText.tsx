mullvad/mullvadvpn-app

Add Progress component

desktop/packages/mullvad-vpn/src/renderer/lib/components/progress/components/ProgressText.tsx
@@ -1,0 +1,14 @@
+import { Colors } from '../../../foundations';
+import { LabelTiny, LabelTinyProps } from '../../typography';
+import { useProgress } from '../ProgressContext';
+
+export type ProgressTextProps = LabelTinyProps;
+
+export const ProgressText = ({ children, ...props }: ProgressTextProps) => {
+  const { disabled } = useProgress();
+  return (
+    <LabelTiny color={disabled ? Colors.white40 : Colors.white} {...props}>
+      {children}
+    </LabelTiny>
+  );
+};

desktop/packages/mullvad-vpn/src/renderer/lib/components/progress/components/index.ts
@@ -1,0 +1,4 @@
+export * from './ProgressContentGroup';
+export * from './ProgressPercent';
+export * from './ProgressRange';
+export * from './ProgressText';

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/lib/components/progress/components/ProgressText.tsx
@@ -1,0 +1,14 @@
+import { Colors } from '../../../foundations';
+import { LabelTiny, LabelTinyProps } from '../../typography';
+import { useProgress } from '../ProgressContext';
+
+export type ProgressTextProps = LabelTinyProps;
+
+export const ProgressText = ({ children, ...props }: ProgressTextProps) => {
+  const { disabled } = useProgress();
+  return (
+    <LabelTiny color={disabled ? Colors.white40 : Colors.white60} {...props}>
+      {children}
+    </LabelTiny>
+  );
+};

desktop/packages/mullvad-vpn/src/renderer/lib/components/progress/components/index.ts
@@ -1,0 +1,5 @@
+export * from './ProgressFooter';
+export * from './ProgressPercent';
+export * from './ProgressRange';
+export * from './ProgressText';
+export * from './ProgressTrack';