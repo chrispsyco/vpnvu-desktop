oIoTShipTalk/mullvadvpn-app

Add settings for excludeLocalNetworks

ios/MullvadSettings/TunnelSettingsV7.swift
@@ -1,15 +1,16 @@
 //
-//  TunnelSettingsV6.swift
-//  MullvadSettings
+//  TunnelSettingsV6 2.swift
+//  MullvadVPN
 //
-//  Created by Mojgan on 2024-08-08.
+//  Created by Steffen Ernst on 2025-02-04.
 //  Copyright © 2025 Mullvad VPN AB. All rights reserved.
 //
+
 
 import Foundation
 import MullvadTypes
 
-public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
+public struct TunnelSettingsV7: Codable, Equatable, TunnelSettings, Sendable {
     /// Relay constraints.
     public var relayConstraints: RelayConstraints
 
@@ -27,6 +28,9 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
 
     /// DAITA settings.
     public var daita: DAITASettings
+    
+    /// Exclude local network.
+    public var excludeLocalNetworks: Bool
 
     public init(
         relayConstraints: RelayConstraints = RelayConstraints(),
@@ -34,7 +38,8 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
         wireGuardObfuscation: WireGuardObfuscationSettings = WireGuardObfuscationSettings(),
         tunnelQuantumResistance: TunnelQuantumResistance = .automatic,
         tunnelMultihopState: MultihopState = .off,
-        daita: DAITASettings = DAITASettings()
+        daita: DAITASettings = DAITASettings(),
+        excludeLocalNetworks: Bool = false
     ) {
         self.relayConstraints = relayConstraints
         self.dnsSettings = dnsSettings
@@ -42,15 +47,10 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
         self.tunnelQuantumResistance = tunnelQuantumResistance
         self.tunnelMultihopState = tunnelMultihopState
         self.daita = daita
+        self.excludeLocalNetworks = excludeLocalNetworks
     }
 
     public func upgradeToNextVersion() -> any TunnelSettings {
-        TunnelSettingsV7(relayConstraints: relayConstraints,
-                         dnsSettings: dnsSettings,
-                         wireGuardObfuscation: wireGuardObfuscation,
-                         tunnelQuantumResistance: tunnelQuantumResistance,
-                         tunnelMultihopState: tunnelMultihopState,
-                         daita: daita,
-                         excludeLocalNetworks: false)
+        self
     }
 }

chrispsyco/vpnvu-desktop

ios/MullvadSettings/TunnelSettingsV7.swift
@@ -1,15 +1,16 @@
 //
-//  TunnelSettingsV6.swift
-//  MullvadSettings
+//  TunnelSettingsV6 2.swift
+//  MullvadVPN
 //
-//  Created by Mojgan on 2024-08-08.
+//  Created by Steffen Ernst on 2025-02-04.
 //  Copyright © 2025 Mullvad VPN AB. All rights reserved.
 //
+
 
 import Foundation
 import MullvadTypes
 
-public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
+public struct TunnelSettingsV7: Codable, Equatable, TunnelSettings, Sendable {
     /// Relay constraints.
     public var relayConstraints: RelayConstraints
 
@@ -27,6 +28,9 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
 
     /// DAITA settings.
     public var daita: DAITASettings
+    
+    /// Local networks sharing.
+    public var localNetworkSharing: Bool
 
     public init(
         relayConstraints: RelayConstraints = RelayConstraints(),
@@ -34,7 +38,8 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
         wireGuardObfuscation: WireGuardObfuscationSettings = WireGuardObfuscationSettings(),
         tunnelQuantumResistance: TunnelQuantumResistance = .automatic,
         tunnelMultihopState: MultihopState = .off,
-        daita: DAITASettings = DAITASettings()
+        daita: DAITASettings = DAITASettings(),
+        localNetworkSharing: Bool = false
     ) {
         self.relayConstraints = relayConstraints
         self.dnsSettings = dnsSettings
@@ -42,15 +47,10 @@ public struct TunnelSettingsV6: Codable, Equatable, TunnelSettings, Sendable {
         self.tunnelQuantumResistance = tunnelQuantumResistance
         self.tunnelMultihopState = tunnelMultihopState
         self.daita = daita
+        self.localNetworkSharing = localNetworkSharing
     }
 
     public func upgradeToNextVersion() -> any TunnelSettings {
-        TunnelSettingsV7(relayConstraints: relayConstraints,
-                         dnsSettings: dnsSettings,
-                         wireGuardObfuscation: wireGuardObfuscation,
-                         tunnelQuantumResistance: tunnelQuantumResistance,
-                         tunnelMultihopState: tunnelMultihopState,
-                         daita: daita,
-                         localNetworkSharing: false)
+        self
     }
 }