mullvad/mullvadvpn-app

Add initial web installer and upgrades scaffold
Currently, hardcoded URLs are used to download the app and signature,
and pgp is used for verification

Ignore download button if download is in progress

TEMP: use max size rather than exact size for app

installer-downloader/src/winapi_impl/mod.rs
@@ -1,0 +1,22 @@
+use native_windows_gui as nwg;
+
+use crate::controller::{AppDelegate, AppDelegateQueue};
+
+mod delegate;
+mod ui;
+
+pub fn main() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+
+    let window = ui::AppWindow::default();
+    let window = window.layout().unwrap();
+
+    let queue = window.borrow().queue();
+
+    queue.queue_main(|window| {
+        crate::controller::initialize_controller(window);
+    });
+
+    nwg::dispatch_thread_events();
+}

chrispsyco/vpnvu-desktop

installer-downloader/src/winapi_impl/mod.rs
@@ -1,0 +1,22 @@
+use native_windows_gui as nwg;
+
+use crate::delegate::{AppDelegate, AppDelegateQueue};
+
+mod delegate;
+mod ui;
+
+pub fn main() {
+    nwg::init().expect("Failed to init Native Windows GUI");
+    nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+
+    let window = ui::AppWindow::default();
+    let window = window.layout().unwrap();
+
+    let queue = window.borrow().queue();
+
+    queue.queue_main(|window| {
+        crate::controller::initialize_controller(window);
+    });
+
+    nwg::dispatch_thread_events();
+}