mullvad/mullvadvpn-app

Convert spacing enum to const object

desktop/packages/mullvad-vpn/src/renderer/components/ErrorView.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Flex, Logo } from '../lib/components';
-import { Colors, Spacings } from '../lib/foundations';
+import { Colors } from '../lib/foundations';
 import { AppMainHeader } from './app-main-header';
 import { measurements } from './common-styles';
 import { Container, Layout } from './Layout';
@@ -43,12 +43,8 @@ export default function ErrorView(props: ErrorViewProps) {
         {!props.settingsUnavailable && <AppMainHeader.SettingsButton />}
       </AppMainHeader>
       <StyledContainer>
-        <Flex $flexDirection="column" $gap={Spacings.small}>
-          <Flex
-            $flexDirection="column"
-            $alignItems="center"
-            $justifyContent="end"
-            $gap={Spacings.medium}>
+        <Flex $flexDirection="column" $gap="small">
+          <Flex $flexDirection="column" $alignItems="center" $justifyContent="end" $gap="medium">
             <Logo variant="icon" size="2" />
             <Logo variant="text" size="2" />
           </Flex>

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/margin.ts
@@ -1,33 +1,50 @@
-import { Spacings } from '../../foundations';
+import { Spacings, spacings } from '../../foundations';
 import { LayoutSpacings } from './types';
 
-export const all = (margin: Spacings) => ({ margin });
+export const all = (margin: Spacings) => {
+  const marginAll = spacings[margin];
+  return { margin: marginAll };
+};
 
 const vertical = (margin: Spacings) => ({
   ...top(margin),
   ...bottom(margin),
 });
 
-const horizontal = (margin: Spacings) => ({
-  ...left(margin),
-  ...right(margin),
-});
+const horizontal = (margin: Spacings) => {
+  return {
+    ...left(margin),
+    ...right(margin),
+  };
+};
 
-const top = (marginTop: Spacings) => ({
-  marginTop,
-});
+const top = (margin: Spacings) => {
+  const marginTop = spacings[margin];
+  return {
+    marginTop,
+  };
+};
 
-const right = (marginRight: Spacings) => ({
-  marginRight,
-});
+const right = (margin: Spacings) => {
+  const marginRight = spacings[margin];
+  return {
+    marginRight,
+  };
+};
 
-const bottom = (marginBottom: Spacings) => ({
-  marginBottom,
-});
+const bottom = (margin: Spacings) => {
+  const marginBottom = spacings[margin];
+  return {
+    marginBottom,
+  };
+};
 
-const left = (marginLeft: Spacings) => ({
-  marginLeft,
-});
+const left = (margin: Spacings) => {
+  const marginLeft = spacings[margin];
+  return {
+    marginLeft,
+  };
+};
 
 export const margin: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties> = {
   all,

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/padding.ts
@@ -1,7 +1,10 @@
-import { Spacings } from '../../foundations';
+import { Spacings, spacings } from '../../foundations';
 import { LayoutSpacings } from './types';
 
-export const all = (padding: Spacings) => ({ padding });
+export const all = (padding: Spacings) => {
+  const paddingAll = spacings[padding];
+  return { padding: paddingAll };
+};
 
 const vertical = (padding: Spacings) => ({
   ...top(padding),
@@ -13,21 +16,33 @@ const horizontal = (padding: Spacings) => ({
   ...right(padding),
 });
 
-const top = (paddingTop: Spacings) => ({
-  paddingTop,
-});
+const top = (padding: Spacings) => {
+  const paddingTop = spacings[padding];
+  return {
+    paddingTop,
+  };
+};
 
-const right = (paddingRight: Spacings) => ({
-  paddingRight,
-});
+const right = (padding: Spacings) => {
+  const paddingRight = spacings[padding];
+  return {
+    paddingRight,
+  };
+};
 
-const bottom = (paddingBottom: Spacings) => ({
-  paddingBottom,
-});
+const bottom = (padding: Spacings) => {
+  const paddingBottom = spacings[padding];
+  return {
+    paddingBottom,
+  };
+};
 
-const left = (paddingLeft: Spacings) => ({
-  paddingLeft,
-});
+const left = (padding: Spacings) => {
+  const paddingLeft = spacings[padding];
+  return {
+    paddingLeft,
+  };
+};
 
 export const padding: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties> = {
   all,

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/ErrorView.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Flex, Logo } from '../lib/components';
-import { Colors, Spacings } from '../lib/foundations';
+import { Colors } from '../lib/foundations';
 import { AppMainHeader } from './app-main-header';
 import { measurements } from './common-styles';
 import { Container, Layout } from './Layout';
@@ -43,12 +43,8 @@ export default function ErrorView(props: ErrorViewProps) {
         {!props.settingsUnavailable && <AppMainHeader.SettingsButton />}
       </AppMainHeader>
       <StyledContainer>
-        <Flex $flexDirection="column" $gap={Spacings.small}>
-          <Flex
-            $flexDirection="column"
-            $alignItems="center"
-            $justifyContent="end"
-            $gap={Spacings.medium}>
+        <Flex $flexDirection="column" $gap="medium">
+          <Flex $flexDirection="column" $alignItems="center" $justifyContent="end" $gap="medium">
             <Logo variant="icon" size="2" />
             <Logo variant="text" size="2" />
           </Flex>

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/margin.ts
@@ -1,33 +1,50 @@
-import { Spacings } from '../../foundations';
+import { Spacings, spacings } from '../../foundations';
 import { LayoutSpacings } from './types';
 
-export const all = (margin: Spacings) => ({ margin });
+export const all = (value: Spacings) => {
+  const marginAll = spacings[value];
+  return { margin: marginAll };
+};
 
-const vertical = (margin: Spacings) => ({
-  ...top(margin),
-  ...bottom(margin),
+const vertical = (value: Spacings) => ({
+  ...top(value),
+  ...bottom(value),
 });
 
-const horizontal = (margin: Spacings) => ({
-  ...left(margin),
-  ...right(margin),
-});
+const horizontal = (value: Spacings) => {
+  return {
+    ...left(value),
+    ...right(value),
+  };
+};
 
-const top = (marginTop: Spacings) => ({
-  marginTop,
-});
+const top = (value: Spacings) => {
+  const marginTop = spacings[value];
+  return {
+    marginTop,
+  };
+};
 
-const right = (marginRight: Spacings) => ({
-  marginRight,
-});
+const right = (value: Spacings) => {
+  const marginRight = spacings[value];
+  return {
+    marginRight,
+  };
+};
 
-const bottom = (marginBottom: Spacings) => ({
-  marginBottom,
-});
+const bottom = (value: Spacings) => {
+  const marginBottom = spacings[value];
+  return {
+    marginBottom,
+  };
+};
 
-const left = (marginLeft: Spacings) => ({
-  marginLeft,
-});
+const left = (value: Spacings) => {
+  const marginLeft = spacings[value];
+  return {
+    marginLeft,
+  };
+};
 
 export const margin: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties> = {
   all,

desktop/packages/mullvad-vpn/src/renderer/lib/components/layout/padding.ts
@@ -1,33 +1,48 @@
-import { Spacings } from '../../foundations';
+import { Spacings, spacings } from '../../foundations';
 import { LayoutSpacings } from './types';
 
-export const all = (padding: Spacings) => ({ padding });
+export const all = (value: Spacings) => {
+  const paddingAll = spacings[value];
+  return { padding: paddingAll };
+};
 
-const vertical = (padding: Spacings) => ({
-  ...top(padding),
-  ...bottom(padding),
+const vertical = (value: Spacings) => ({
+  ...top(value),
+  ...bottom(value),
 });
 
-const horizontal = (padding: Spacings) => ({
-  ...left(padding),
-  ...right(padding),
+const horizontal = (value: Spacings) => ({
+  ...left(value),
+  ...right(value),
 });
 
-const top = (paddingTop: Spacings) => ({
-  paddingTop,
-});
+const top = (value: Spacings) => {
+  const paddingTop = spacings[value];
+  return {
+    paddingTop,
+  };
+};
 
-const right = (paddingRight: Spacings) => ({
-  paddingRight,
-});
+const right = (value: Spacings) => {
+  const paddingRight = spacings[value];
+  return {
+    paddingRight,
+  };
+};
 
-const bottom = (paddingBottom: Spacings) => ({
-  paddingBottom,
-});
+const bottom = (value: Spacings) => {
+  const paddingBottom = spacings[value];
+  return {
+    paddingBottom,
+  };
+};
 
-const left = (paddingLeft: Spacings) => ({
-  paddingLeft,
-});
+const left = (value: Spacings) => {
+  const paddingLeft = spacings[value];
+  return {
+    paddingLeft,
+  };
+};
 
 export const padding: Record<keyof LayoutSpacings, (value: Spacings) => React.CSSProperties> = {
   all,