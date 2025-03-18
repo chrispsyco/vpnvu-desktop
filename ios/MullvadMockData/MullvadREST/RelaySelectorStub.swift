mullvad/mullvadvpn-app

Show number of available relays in filter view

ios/MullvadMockData/MullvadREST/RelaySelectorStub.swift
@@ -14,9 +14,14 @@ import WireGuardKitTypes
 /// Relay selector stub that accepts a block that can be used to provide custom implementation.
 public final class RelaySelectorStub: RelaySelectorProtocol {
     var selectedRelaysResult: (UInt) throws -> SelectedRelays
+    var candidatesResult: (() throws -> RelaysCandidates)?
 
-    init(selectedRelaysResult: @escaping (UInt) throws -> SelectedRelays) {
+    init(
+        selectedRelaysResult: @escaping (UInt) throws -> SelectedRelays,
+        candidatesResult: (() throws -> RelaysCandidates)? = nil
+    ) {
         self.selectedRelaysResult = selectedRelaysResult
+        self.candidatesResult = candidatesResult
     }
 
     public func selectRelays(
@@ -25,6 +30,12 @@ public final class RelaySelectorStub: RelaySelectorProtocol {
     ) throws -> SelectedRelays {
         return try selectedRelaysResult(connectionAttemptCount)
     }
+
+    public func findCandidates(
+        tunnelSettings: LatestTunnelSettings
+    ) throws -> RelaysCandidates {
+        return try candidatesResult?() ?? RelaysCandidates(entryRelays: [], exitRelays: [])
+    }
 }
 
 extension RelaySelectorStub {
@@ -32,7 +43,7 @@ extension RelaySelectorStub {
     public static func nonFallible() -> RelaySelectorStub {
         let publicKey = PrivateKey().publicKey.rawValue
 
-        return RelaySelectorStub { _ in
+        return RelaySelectorStub(selectedRelaysResult: { _ in
             let cityRelay = SelectedRelay(
                 endpoint: MullvadEndpoint(
                     ipv4Relay: IPv4Endpoint(ip: .loopback, port: 1300),
@@ -56,13 +67,15 @@ extension RelaySelectorStub {
                 exit: cityRelay,
                 retryAttempt: 0
             )
-        }
+        }, candidatesResult: nil)
     }
 
     /// Returns a relay selector that cannot satisfy constraints .
     public static func unsatisfied() -> RelaySelectorStub {
-        return RelaySelectorStub { _ in
+        return RelaySelectorStub(selectedRelaysResult: { _ in
             throw NoRelaysSatisfyingConstraintsError(.relayConstraintNotMatching)
-        }
+        }, candidatesResult: {
+            throw NoRelaysSatisfyingConstraintsError(.relayConstraintNotMatching)
+        })
     }
 }

ios/MullvadREST/Relay/RelaySelectorProtocol.swift
@@ -16,6 +16,10 @@ public protocol RelaySelectorProtocol {
         tunnelSettings: LatestTunnelSettings,
         connectionAttemptCount: UInt
     ) throws -> SelectedRelays
+
+    func findCandidates(
+        tunnelSettings: LatestTunnelSettings
+    ) throws -> RelaysCandidates
 }
 
 /// Struct describing the selected relay.

ios/MullvadREST/Relay/RelaySelectorWrapper.swift
@@ -10,7 +10,7 @@ import MullvadSettings
 import MullvadTypes
 
 public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
-    let relayCache: RelayCacheProtocol
+    public let relayCache: RelayCacheProtocol
 
     public init(relayCache: RelayCacheProtocol) {
         self.relayCache = relayCache
@@ -20,12 +20,7 @@ public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
         tunnelSettings: LatestTunnelSettings,
         connectionAttemptCount: UInt
     ) throws -> SelectedRelays {
-        let obfuscation = try ObfuscatorPortSelector(
-            relays: try relayCache.read().relays
-        ).obfuscate(
-            tunnelSettings: tunnelSettings,
-            connectionAttemptCount: connectionAttemptCount
-        )
+        let obfuscation = try prepareObfuscation(for: tunnelSettings, connectionAttemptCount: connectionAttemptCount)
 
         return switch tunnelSettings.tunnelMultihopState {
         case .off:
@@ -43,5 +38,42 @@ public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
                 daitaSettings: tunnelSettings.daita
             ).pick()
         }
+    }
+
+    public func findCandidates(tunnelSettings: LatestTunnelSettings) throws -> RelaysCandidates {
+        let obfuscation = try prepareObfuscation(for: tunnelSettings, connectionAttemptCount: 0)
+
+        let findCandidates: (REST.ServerRelaysResponse, Bool) throws
+            -> [RelayWithLocation<REST.ServerRelay>] = { relays, daitaEnabled in
+                try RelaySelector.WireGuard.findCandidates(
+                    by: .any,
+                    in: relays,
+                    filterConstraint: tunnelSettings.relayConstraints.filter,
+                    daitaEnabled: daitaEnabled
+                )
+            }
+
+        if tunnelSettings.daita.isAutomaticRouting {
+            let entryCandidates = try findCandidates(
+                tunnelSettings.tunnelMultihopState.isEnabled ? obfuscation.entryRelays : obfuscation.exitRelays,
+                tunnelSettings.daita.daitaState.isEnabled
+            )
+            let exitCandidates = try findCandidates(obfuscation.exitRelays, false)
+            return RelaysCandidates(entryRelays: entryCandidates, exitRelays: exitCandidates)
+        } else {
+            let exitCandidates = try findCandidates(obfuscation.exitRelays, tunnelSettings.daita.daitaState.isEnabled)
+            return RelaysCandidates(entryRelays: nil, exitRelays: exitCandidates)
+        }
+    }
+
+    private func prepareObfuscation(
+        for tunnelSettings: LatestTunnelSettings,
+        connectionAttemptCount: UInt
+    ) throws -> ObfuscatorPortSelection {
+        let relays = try relayCache.read().relays
+        return try ObfuscatorPortSelector(relays: relays).obfuscate(
+            tunnelSettings: tunnelSettings,
+            connectionAttemptCount: connectionAttemptCount
+        )
     }
 }

chrispsyco/vpnvu-desktop

ios/MullvadMockData/MullvadREST/RelaySelectorStub.swift
@@ -14,9 +14,14 @@ import WireGuardKitTypes
 /// Relay selector stub that accepts a block that can be used to provide custom implementation.
 public final class RelaySelectorStub: RelaySelectorProtocol {
     var selectedRelaysResult: (UInt) throws -> SelectedRelays
+    var candidatesResult: (() throws -> RelayCandidates)?
 
-    init(selectedRelaysResult: @escaping (UInt) throws -> SelectedRelays) {
+    init(
+        selectedRelaysResult: @escaping (UInt) throws -> SelectedRelays,
+        candidatesResult: (() throws -> RelayCandidates)? = nil
+    ) {
         self.selectedRelaysResult = selectedRelaysResult
+        self.candidatesResult = candidatesResult
     }
 
     public func selectRelays(
@@ -25,6 +30,12 @@ public final class RelaySelectorStub: RelaySelectorProtocol {
     ) throws -> SelectedRelays {
         return try selectedRelaysResult(connectionAttemptCount)
     }
+
+    public func findCandidates(
+        tunnelSettings: LatestTunnelSettings
+    ) throws -> RelayCandidates {
+        return try candidatesResult?() ?? RelayCandidates(entryRelays: [], exitRelays: [])
+    }
 }
 
 extension RelaySelectorStub {
@@ -32,7 +43,7 @@ extension RelaySelectorStub {
     public static func nonFallible() -> RelaySelectorStub {
         let publicKey = PrivateKey().publicKey.rawValue
 
-        return RelaySelectorStub { _ in
+        return RelaySelectorStub(selectedRelaysResult: { _ in
             let cityRelay = SelectedRelay(
                 endpoint: MullvadEndpoint(
                     ipv4Relay: IPv4Endpoint(ip: .loopback, port: 1300),
@@ -56,13 +67,15 @@ extension RelaySelectorStub {
                 exit: cityRelay,
                 retryAttempt: 0
             )
-        }
+        }, candidatesResult: nil)
     }
 
     /// Returns a relay selector that cannot satisfy constraints .
     public static func unsatisfied() -> RelaySelectorStub {
-        return RelaySelectorStub { _ in
+        return RelaySelectorStub(selectedRelaysResult: { _ in
             throw NoRelaysSatisfyingConstraintsError(.relayConstraintNotMatching)
-        }
+        }, candidatesResult: {
+            throw NoRelaysSatisfyingConstraintsError(.relayConstraintNotMatching)
+        })
     }
 }

ios/MullvadREST/Relay/RelaySelectorProtocol.swift
@@ -16,6 +16,10 @@ public protocol RelaySelectorProtocol {
         tunnelSettings: LatestTunnelSettings,
         connectionAttemptCount: UInt
     ) throws -> SelectedRelays
+
+    func findCandidates(
+        tunnelSettings: LatestTunnelSettings
+    ) throws -> RelayCandidates
 }
 
 /// Struct describing the selected relay.

ios/MullvadREST/Relay/RelaySelectorWrapper.swift
@@ -10,7 +10,7 @@ import MullvadSettings
 import MullvadTypes
 
 public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
-    let relayCache: RelayCacheProtocol
+    public let relayCache: RelayCacheProtocol
 
     public init(relayCache: RelayCacheProtocol) {
         self.relayCache = relayCache
@@ -20,12 +20,7 @@ public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
         tunnelSettings: LatestTunnelSettings,
         connectionAttemptCount: UInt
     ) throws -> SelectedRelays {
-        let obfuscation = try ObfuscatorPortSelector(
-            relays: try relayCache.read().relays
-        ).obfuscate(
-            tunnelSettings: tunnelSettings,
-            connectionAttemptCount: connectionAttemptCount
-        )
+        let obfuscation = try prepareObfuscation(for: tunnelSettings, connectionAttemptCount: connectionAttemptCount)
 
         return switch tunnelSettings.tunnelMultihopState {
         case .off:
@@ -43,5 +38,42 @@ public final class RelaySelectorWrapper: RelaySelectorProtocol, Sendable {
                 daitaSettings: tunnelSettings.daita
             ).pick()
         }
+    }
+
+    public func findCandidates(tunnelSettings: LatestTunnelSettings) throws -> RelayCandidates {
+        let obfuscation = try prepareObfuscation(for: tunnelSettings, connectionAttemptCount: 0)
+
+        let findCandidates: (REST.ServerRelaysResponse, Bool) throws
+            -> [RelayWithLocation<REST.ServerRelay>] = { relays, daitaEnabled in
+                try RelaySelector.WireGuard.findCandidates(
+                    by: .any,
+                    in: relays,
+                    filterConstraint: tunnelSettings.relayConstraints.filter,
+                    daitaEnabled: daitaEnabled
+                )
+            }
+
+        if tunnelSettings.daita.isAutomaticRouting || tunnelSettings.tunnelMultihopState.isEnabled {
+            let entryCandidates = try findCandidates(
+                tunnelSettings.tunnelMultihopState.isEnabled ? obfuscation.entryRelays : obfuscation.exitRelays,
+                tunnelSettings.daita.daitaState.isEnabled
+            )
+            let exitCandidates = try findCandidates(obfuscation.exitRelays, false)
+            return RelayCandidates(entryRelays: entryCandidates, exitRelays: exitCandidates)
+        } else {
+            let exitCandidates = try findCandidates(obfuscation.exitRelays, tunnelSettings.daita.daitaState.isEnabled)
+            return RelayCandidates(entryRelays: nil, exitRelays: exitCandidates)
+        }
+    }
+
+    private func prepareObfuscation(
+        for tunnelSettings: LatestTunnelSettings,
+        connectionAttemptCount: UInt
+    ) throws -> ObfuscatorPortSelection {
+        let relays = try relayCache.read().relays
+        return try ObfuscatorPortSelector(relays: relays).obfuscate(
+            tunnelSettings: tunnelSettings,
+            connectionAttemptCount: connectionAttemptCount
+        )
     }
 }