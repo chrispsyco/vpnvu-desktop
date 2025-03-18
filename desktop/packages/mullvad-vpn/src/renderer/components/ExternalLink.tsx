mullvad/mullvadvpn-app

Refactor app specific logic in Link into ExternalLink and InternalLink

desktop/packages/mullvad-vpn/src/renderer/components/ExternalLink.tsx
@@ -1,0 +1,24 @@
+import { useCallback } from 'react';
+
+import { Url } from '../../shared/constants';
+import { useAppContext } from '../context';
+import { Link, LinkProps } from '../lib/components';
+
+export type ExternalLinkPops = Omit<LinkProps<'a'>, 'href' | 'as'> & {
+  to: Url;
+};
+
+export const ExternalLink = ({ to, onClick, ...props }: ExternalLinkPops) => {
+  const { openUrl } = useAppContext();
+  const navigate = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault();
+      if (onClick) {
+        onClick(e);
+      }
+      return openUrl(to);
+    },
+    [onClick, openUrl, to],
+  );
+  return <Link href="" onClick={navigate} {...props} />;
+};

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/ExternalLink.tsx
@@ -1,0 +1,24 @@
+import { useCallback } from 'react';
+
+import { Url } from '../../shared/constants';
+import { useAppContext } from '../context';
+import { Link, LinkProps } from '../lib/components';
+
+export type ExternalLinkProps = Omit<LinkProps<'a'>, 'href' | 'as'> & {
+  to: Url;
+};
+
+export const ExternalLink = ({ to, onClick, ...props }: ExternalLinkProps) => {
+  const { openUrl } = useAppContext();
+  const navigate = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault();
+      if (onClick) {
+        onClick(e);
+      }
+      return openUrl(to);
+    },
+    [onClick, openUrl, to],
+  );
+  return <Link href="" onClick={navigate} {...props} />;
+};