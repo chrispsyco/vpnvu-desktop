mullvad/mullvadvpn-app

Add storekit 2 functions to mullvad api

ios/MullvadVPN/View controllers/Account/AccountInteractor.swift
@@ -49,10 +49,55 @@ final class AccountInteractor: Sendable {
         await tunnelManager.unsetAccount()
     }
 
-    func sendStoreKitReceipt(_ transaction: VerificationResult<Transaction>, for accountNumber: String) async throws {
-        _ = try await apiProxy.createApplePayment(
-            accountNumber: accountNumber,
-            receiptString: transaction.jwsRepresentation.data(using: .utf8)!
-        ).execute()
+    // This function is for testing only
+    func getPaymentToken(for accountNumber: String) async -> Result<String, Error> {
+        await withCheckedContinuation { continuation in
+            let proxy = REST.MullvadAPIProxy(
+                transportProvider: APITransportProvider(
+                    requestFactory: .init(
+                        apiContext: REST.apiContext,
+                        encoder: REST.Coding.makeJSONEncoder()
+                    )
+                ),
+                dispatchQueue: .main,
+                responseDecoder: REST.Coding.makeJSONDecoder()
+            )
+            _ = proxy
+                .initStorekitPayment(
+                    accountNumber: accountNumber,
+                    retryStrategy: .noRetry,
+                    completionHandler: { result in
+                        continuation.resume(returning: result)
+                    }
+                )
+        }
+    }
+
+    // This function is for testing only
+    func sendStoreKitReceipt(
+        _ transaction: VerificationResult<Transaction>,
+        for accountNumber: String
+    ) async -> Result<Void, Error> {
+        await withCheckedContinuation { c in
+            let proxy = REST.MullvadAPIProxy(
+                transportProvider: APITransportProvider(
+                    requestFactory: .init(
+                        apiContext: REST.apiContext,
+                        encoder: REST.Coding.makeJSONEncoder()
+                    )
+                ),
+                dispatchQueue: .main,
+                responseDecoder: REST.Coding.makeJSONDecoder()
+            )
+            _ = proxy
+                .checkStorekitPayment(
+                    accountNumber: accountNumber,
+                    transaction: StorekitTransaction(transaction: transaction.jwsRepresentation),
+                    retryStrategy: .noRetry,
+                    completionHandler: { result in
+                        c.resume(returning: result)
+                    }
+                )
+        }
     }
 }

chrispsyco/vpnvu-desktop

ios/MullvadVPN/View controllers/Account/AccountInteractor.swift
@@ -49,10 +49,35 @@ final class AccountInteractor: Sendable {
         await tunnelManager.unsetAccount()
     }
 
-    func sendStoreKitReceipt(_ transaction: VerificationResult<Transaction>, for accountNumber: String) async throws {
-        _ = try await apiProxy.createApplePayment(
-            accountNumber: accountNumber,
-            receiptString: transaction.jwsRepresentation.data(using: .utf8)!
-        ).execute()
+    // This function is for testing only
+    func getPaymentToken(for accountNumber: String) async -> Result<String, Error> {
+        await withCheckedContinuation { continuation in
+            _ = apiProxy
+                .initStorekitPayment(
+                    accountNumber: accountNumber,
+                    retryStrategy: .noRetry,
+                    completionHandler: { result in
+                        continuation.resume(returning: result)
+                    }
+                )
+        }
+    }
+
+    // This function is for testing only
+    func sendStoreKitReceipt(
+        _ transaction: VerificationResult<Transaction>,
+        for accountNumber: String
+    ) async -> Result<Void, Error> {
+        await withCheckedContinuation { c in
+            _ = apiProxy
+                .checkStorekitPayment(
+                    accountNumber: accountNumber,
+                    transaction: StorekitTransaction(transaction: transaction.jwsRepresentation),
+                    retryStrategy: .noRetry,
+                    completionHandler: { result in
+                        c.resume(returning: result)
+                    }
+                )
+        }
     }
 }