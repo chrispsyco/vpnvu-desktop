mullvad/mullvadvpn-app

Remove the 2 character limit in search locations
Also fixes back behavior in custom list locations

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CustomListLocationsUiState.kt
@@ -15,7 +15,11 @@ sealed interface CustomListLocationsUiState {
     sealed interface Content : CustomListLocationsUiState {
         val searchTerm: String
 
-        data class Empty(override val newList: Boolean, override val searchTerm: String) : Content {
+        data class Empty(
+            override val newList: Boolean,
+            override val searchTerm: String,
+            val isSearching: Boolean,
+        ) : Content {
             override val saveEnabled: Boolean = false
             override val hasUnsavedChanges: Boolean = false
         }

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/state/CustomListLocationsUiState.kt
@@ -1,34 +1,19 @@
 package net.mullvad.mullvadvpn.compose.state
 
 import net.mullvad.mullvadvpn.lib.model.RelayItem
+import net.mullvad.mullvadvpn.util.Lce
 
-sealed interface CustomListLocationsUiState {
-    val newList: Boolean
-    val saveEnabled: Boolean
-    val hasUnsavedChanges: Boolean
+data class CustomListLocationsUiState(
+    val newList: Boolean,
+    val content: Lce<Unit, CustomListLocationsData, Unit>,
+)
 
-    data class Loading(override val newList: Boolean = false) : CustomListLocationsUiState {
-        override val saveEnabled: Boolean = false
-        override val hasUnsavedChanges: Boolean = false
-    }
-
-    sealed interface Content : CustomListLocationsUiState {
-        val searchTerm: String
-
-        data class Empty(override val newList: Boolean, override val searchTerm: String) : Content {
-            override val saveEnabled: Boolean = false
-            override val hasUnsavedChanges: Boolean = false
-        }
-
-        data class Data(
-            override val newList: Boolean = false,
-            val locations: List<RelayLocationListItem>,
-            override val searchTerm: String = "",
-            override val saveEnabled: Boolean = false,
-            override val hasUnsavedChanges: Boolean = false,
-        ) : Content
-    }
-}
+data class CustomListLocationsData(
+    val saveEnabled: Boolean,
+    val hasUnsavedChanges: Boolean,
+    val searchTerm: String,
+    val locations: List<RelayLocationListItem>,
+)
 
 data class RelayLocationListItem(
     val item: RelayItem.Location,