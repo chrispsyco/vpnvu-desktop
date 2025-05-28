mullvad/mullvadvpn-app

Use hooks for version and connection state management in app info view

desktop/packages/mullvad-vpn/src/renderer/components/views/app-info/hooks/useSetttingsShowBetaReleases.tsx
@@ -1,0 +1,10 @@
+import { useAppContext } from '../../../../context';
+import { useSelector } from '../../../../redux/store';
+
+export const useSetttingsShowBetaReleases = () => {
+  const { setShowBetaReleases } = useAppContext();
+  return {
+    showBetaReleases: useSelector((state) => state.settings.showBetaReleases),
+    setShowBetaReleases,
+  };
+};

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/app-info/hooks/useSetttingsShowBetaReleases.tsx
@@ -1,0 +1,10 @@
+import { useAppContext } from '../../../../context';
+import { useSelector } from '../../../../redux/store';
+
+export const useSettingsShowBetaReleases = () => {
+  const { setShowBetaReleases } = useAppContext();
+  return {
+    showBetaReleases: useSelector((state) => state.settings.showBetaReleases),
+    setShowBetaReleases,
+  };
+};