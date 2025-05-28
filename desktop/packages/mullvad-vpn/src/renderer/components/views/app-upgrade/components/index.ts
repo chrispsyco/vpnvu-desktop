mullvad/mullvadvpn-app

Add app-upgrade view

desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/index.ts
@@ -1,0 +1,7 @@
+export * from './cancel-button';
+export * from './download-progress';
+export * from './install-button';
+export * from './report-problem-button';
+export * from './update-button';
+export * from './upgrade-details';
+export * from './upgrade-label';

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/app-upgrade/components/index.ts
@@ -1,0 +1,8 @@
+export * from './cancel-button';
+export * from './download-progress';
+export * from './install-button';
+export * from './report-problem-button';
+export * from './retry-upgrade-button';
+export * from './upgrade-button';
+export * from './upgrade-details';
+export * from './upgrade-label';