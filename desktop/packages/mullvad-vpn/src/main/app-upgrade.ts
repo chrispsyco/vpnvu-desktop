mullvad/mullvadvpn-app

Add a class to handle Main IPC and Daemon's AppUpgrade integration

desktop/packages/mullvad-vpn/src/main/app-upgrade.ts
@@ -1,0 +1,37 @@
+import { DaemonAppUpgradeEvent } from '../shared/daemon-rpc-types';
+import log from '../shared/logging';
+import { DaemonRpc, SubscriptionListener } from './daemon-rpc';
+import { IpcMainEventChannel } from './ipc-event-channel';
+
+export default class AppUpgrade {
+  public constructor(private daemonRpc: DaemonRpc) {}
+
+  public registerIpcListeners() {
+    IpcMainEventChannel.app.handleUpgrade(() => {
+      this.daemonRpc.appUpgrade();
+    });
+
+    IpcMainEventChannel.app.handleUpgradeAbort(() => {
+      this.daemonRpc.appUpgradeAbort();
+    });
+  }
+
+  public subscribeEvents() {
+    const appUpgradeEventListener = new SubscriptionListener(
+      (appUpgradeEvent: DaemonAppUpgradeEvent) => {
+        if (appUpgradeEvent.type === 'APP_UPGRADE_ERROR') {
+          IpcMainEventChannel.app.notifyUpgradeError?.(appUpgradeEvent.error);
+        } else {
+          IpcMainEventChannel.app.notifyUpgradeEvent?.(appUpgradeEvent);
+        }
+      },
+      (error: Error) => {
+        log.error(`Cannot deserialize the app upgrade event: ${error.message}`);
+      },
+    );
+
+    this.daemonRpc.subscribeAppUpgradeEventListener(appUpgradeEventListener);
+
+    return appUpgradeEventListener;
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/main/app-upgrade.ts
@@ -1,0 +1,37 @@
+import { DaemonAppUpgradeEvent } from '../shared/daemon-rpc-types';
+import log from '../shared/logging';
+import { DaemonRpc, SubscriptionListener } from './daemon-rpc';
+import { IpcMainEventChannel } from './ipc-event-channel';
+
+export default class AppUpgrade {
+  public constructor(private daemonRpc: DaemonRpc) {}
+
+  public registerIpcListeners() {
+    IpcMainEventChannel.app.handleUpgrade(() => {
+      this.daemonRpc.appUpgrade();
+    });
+
+    IpcMainEventChannel.app.handleUpgradeAbort(() => {
+      this.daemonRpc.appUpgradeAbort();
+    });
+  }
+
+  public subscribeEvents() {
+    const daemonAppUpgradeEventListener = new SubscriptionListener(
+      (appUpgradeEvent: DaemonAppUpgradeEvent) => {
+        if (appUpgradeEvent.type === 'APP_UPGRADE_ERROR') {
+          IpcMainEventChannel.app.notifyUpgradeError?.(appUpgradeEvent.error);
+        } else {
+          IpcMainEventChannel.app.notifyUpgradeEvent?.(appUpgradeEvent);
+        }
+      },
+      (error: Error) => {
+        log.error(`Cannot deserialize the app upgrade event: ${error.message}`);
+      },
+    );
+
+    this.daemonRpc.subscribeAppUpgradeEventListener(daemonAppUpgradeEventListener);
+
+    return daemonAppUpgradeEventListener;
+  }
+}