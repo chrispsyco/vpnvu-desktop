mullvad/mullvadvpn-app

Use AlarmManager for notifications
Instead of scheduling system notifications from a flow we now
schedule them independently from the app lifecycle via AlarmManager.

This is done so that for example an expiry notification that the user
dismissed won't get redisplayed if the app process gets killed and
then restarted.

When the account exiry time is fetched we schedule an alarm that will
show a notification 3 days before the account time expires. This alarm
then also schedules a new alarm to show the following notification and
so on.

To make this work this PR also introduces two new broadcast receivers;
one on boot received listener and one on time time/timezone changed
listener.

Beause Android clears alarms when the devices is rebooted/the time is
changed we need these listeners to re-trigger the alarm.

To enable the broadcast receivers to re-trigger the alarm we also have
to persist the expiry time in the DataStore preferences.

android/app/src/main/kotlin/net/mullvad/mullvadvpn/receiver/NotificationAlarmReceiver.kt
@@ -1,0 +1,43 @@
+package net.mullvad.mullvadvpn.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import co.touchlab.kermit.Logger
+import java.time.Duration
+import java.time.ZonedDateTime
+import kotlinx.coroutines.runBlocking
+import net.mullvad.mullvadvpn.service.notifications.accountexpiry.AccountExpiryScheduledNotificationProvider
+import net.mullvad.mullvadvpn.usecase.ScheduleNotificationAlarmUseCase
+import net.mullvad.mullvadvpn.util.serializable
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class NotificationAlarmReceiver : BroadcastReceiver(), KoinComponent {
+    private val notificationProvider by inject<AccountExpiryScheduledNotificationProvider>()
+    private val scheduleNotificationAlarmUseCase by inject<ScheduleNotificationAlarmUseCase>()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val expiry: ZonedDateTime? = intent?.serializable(ACCOUNT_EXPIRY_KEY)
+        if (expiry == null) {
+            Logger.e("NotificationAlarmReceiver: Intent missing account expiry")
+            return
+        }
+
+        Logger.d("Account expiry alarm triggered")
+        val untilExpiry = Duration.between(ZonedDateTime.now(), expiry)
+
+        runBlocking {
+            notificationProvider.showNotification(untilExpiry)
+            // Only schedule the next alarm if we still have time left on the account.
+            if (context != null && expiry > ZonedDateTime.now()) {
+                scheduleNotificationAlarmUseCase(context = context, accountExpiry = expiry)
+            }
+        }
+    }
+
+    companion object {
+        const val ACCOUNT_EXPIRY_KEY: String = "account_expiry_key"
+    }
+}

chrispsyco/vpnvu-desktop

android/app/src/main/kotlin/net/mullvad/mullvadvpn/receiver/NotificationAlarmReceiver.kt
@@ -1,0 +1,43 @@
+package net.mullvad.mullvadvpn.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import co.touchlab.kermit.Logger
+import java.time.Duration
+import java.time.ZonedDateTime
+import kotlinx.coroutines.runBlocking
+import net.mullvad.mullvadvpn.service.notifications.accountexpiry.AccountExpiryNotificationProvider
+import net.mullvad.mullvadvpn.usecase.ScheduleNotificationAlarmUseCase
+import net.mullvad.mullvadvpn.util.serializable
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class NotificationAlarmReceiver : BroadcastReceiver(), KoinComponent {
+    private val notificationProvider by inject<AccountExpiryNotificationProvider>()
+    private val scheduleNotificationAlarmUseCase by inject<ScheduleNotificationAlarmUseCase>()
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val expiry: ZonedDateTime? = intent?.serializable(ACCOUNT_EXPIRY_KEY)
+        if (expiry == null) {
+            Logger.e("NotificationAlarmReceiver: Intent missing account expiry")
+            return
+        }
+
+        Logger.d("Account expiry alarm triggered")
+        val untilExpiry = Duration.between(ZonedDateTime.now(), expiry)
+
+        runBlocking {
+            notificationProvider.showNotification(untilExpiry)
+            // Only schedule the next alarm if we still have time left on the account.
+            if (context != null && expiry > ZonedDateTime.now()) {
+                scheduleNotificationAlarmUseCase(context = context, accountExpiry = expiry)
+            }
+        }
+    }
+
+    companion object {
+        const val ACCOUNT_EXPIRY_KEY: String = "account_expiry_key"
+    }
+}