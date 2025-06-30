mullvad/mullvadvpn-app

Implement daemon status management and related IPC communication

desktop/packages/mullvad-vpn/src/shared/ipc-types.ts
@@ -35,3 +35,5 @@ export interface IHistoryObject {
 }
 
 export type ScrollPositions = Record<string, [number, number]>;
+
+export type DaemonConnectionStatus = 'start-requested' | 'running' | 'stopped';

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/shared/ipc-types.ts
@@ -35,3 +35,5 @@ export interface IHistoryObject {
 }
 
 export type ScrollPositions = Record<string, [number, number]>;
+
+export type DaemonStatus = 'start-requested' | 'running' | 'stopped';