mullvad/mullvadvpn-app

Remove listbox prefix from SettingsListbox option components

desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/input-option/components/input-option-context/InputOptionContext.tsx
@@ -1,0 +1,28 @@
+import React, { createContext, ReactNode, useContext } from 'react';
+
+type InputOptionContextType = {
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  labelId: string | undefined;
+};
+
+const InputOptionContextContext = createContext<InputOptionContextType | undefined>(undefined);
+
+type InputOptionProviderProps = {
+  children: ReactNode;
+} & InputOptionContextType;
+
+export const InputOptionProvider = ({ children, ...props }: InputOptionProviderProps) => {
+  return (
+    <InputOptionContextContext.Provider value={props}>
+      {children}
+    </InputOptionContextContext.Provider>
+  );
+};
+
+export const useInputOption = (): InputOptionContextType => {
+  const context = useContext(InputOptionContextContext);
+  if (!context) {
+    throw new Error('useInputOption must be used within an InputOptionProvider');
+  }
+  return context;
+};

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/settings-listbox/components/input-option/InputOptionContext.tsx
@@ -1,0 +1,28 @@
+import React, { createContext, ReactNode, useContext } from 'react';
+
+type InputOptionContextType = {
+  inputRef: React.RefObject<HTMLInputElement | null>;
+  labelId: string | undefined;
+};
+
+const InputOptionContextContext = createContext<InputOptionContextType | undefined>(undefined);
+
+type InputOptionProviderProps = {
+  children: ReactNode;
+} & InputOptionContextType;
+
+export const InputOptionProvider = ({ children, ...props }: InputOptionProviderProps) => {
+  return (
+    <InputOptionContextContext.Provider value={props}>
+      {children}
+    </InputOptionContextContext.Provider>
+  );
+};
+
+export const useInputOptionContext = (): InputOptionContextType => {
+  const context = useContext(InputOptionContextContext);
+  if (!context) {
+    throw new Error('useInputOptionContext must be used within an InputOptionProvider');
+  }
+  return context;
+};