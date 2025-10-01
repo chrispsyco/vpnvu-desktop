mullvad/mullvadvpn-app

Add expired route object model

desktop/packages/mullvad-vpn/test/e2e/route-object-models/expired/expired-route-object-model.ts
@@ -1,0 +1,25 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class ExpiredRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.expired);
+  }
+
+  async gotoRedeemVoucher() {
+    await this.selectors.redeemVoucherButton().click();
+    await this.utils.waitForRoute(RoutePath.redeemVoucher);
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/expired/expired-route-object-model.ts
@@ -1,0 +1,25 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class ExpiredRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.expectRoute(RoutePath.expired);
+  }
+
+  async gotoRedeemVoucher() {
+    await this.selectors.redeemVoucherButton().click();
+    await this.utils.expectRoute(RoutePath.redeemVoucher);
+  }
+}