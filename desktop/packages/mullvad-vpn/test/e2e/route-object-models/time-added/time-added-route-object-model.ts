mullvad/mullvadvpn-app

Add time added route object model

desktop/packages/mullvad-vpn/test/e2e/route-object-models/time-added/time-added-route-object-model.ts
@@ -1,0 +1,24 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class TimeAddedRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.timeAdded);
+  }
+
+  async gotoNext() {
+    await this.selectors.nextButton().click();
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/time-added/time-added-route-object-model.ts
@@ -1,0 +1,24 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class TimeAddedRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.expectRoute(RoutePath.timeAdded);
+  }
+
+  async gotoNext() {
+    await this.selectors.nextButton().click();
+  }
+}