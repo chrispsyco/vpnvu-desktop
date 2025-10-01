mullvad/mullvadvpn-app

Handle too many devices view in getNavigationBase

desktop/packages/mullvad-vpn/test/e2e/route-object-models/too-many-devices/too-many-devices-route-object-model.ts
@@ -1,0 +1,24 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class TooManyDevicesRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.tooManyDevices);
+  }
+
+  async continue() {
+    await this.selectors.continueButton().click();
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/too-many-devices/too-many-devices-route-object-model.ts
@@ -1,0 +1,24 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class TooManyDevicesRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.expectRoute(RoutePath.tooManyDevices);
+  }
+
+  async continue() {
+    await this.selectors.continueButton().click();
+  }
+}