mullvad/mullvadvpn-app

Move resetNavigation into components

desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
@@ -29,6 +29,7 @@ import SettingsImport from './SettingsImport';
 import SettingsTextImport from './SettingsTextImport';
 import Shadowsocks from './Shadowsocks';
 import SplitTunnelingSettings from './SplitTunnelingSettings';
+import StateTriggeredNavigation from './StateTriggeredNavigation';
 import Support from './Support';
 import TooManyDevices from './TooManyDevices';
 import UdpOverTcp from './UdpOverTcp';
@@ -53,44 +54,47 @@ export default function AppRouter() {
   const currentLocation = useViewTransitions(onNavigation);
 
   return (
-    <Focus ref={focusRef}>
-      <Switch key={currentLocation.key} location={currentLocation}>
-        <Route exact path={RoutePath.launch} component={LaunchView} />
-        <Route exact path={RoutePath.login} component={LoginView} />
-        <Route exact path={RoutePath.tooManyDevices} component={TooManyDevices} />
-        <Route exact path={RoutePath.deviceRevoked} component={DeviceRevokedView} />
-        <Route exact path={RoutePath.main} component={MainView} />
-        <Route exact path={RoutePath.expired} component={ExpiredAccountErrorView} />
-        <Route exact path={RoutePath.redeemVoucher} component={VoucherInput} />
-        <Route exact path={RoutePath.voucherSuccess} component={VoucherVerificationSuccess} />
-        <Route exact path={RoutePath.timeAdded} component={TimeAdded} />
-        <Route exact path={RoutePath.setupFinished} component={SetupFinished} />
-        <Route exact path={RoutePath.account} component={Account} />
-        <Route exact path={RoutePath.settings} component={SettingsView} />
-        <Route exact path={RoutePath.selectLanguage} component={SelectLanguage} />
-        <Route exact path={RoutePath.userInterfaceSettings} component={UserInterfaceSettings} />
-        <Route exact path={RoutePath.multihopSettings} component={MultihopSettings} />
-        <Route exact path={RoutePath.vpnSettings} component={VpnSettings} />
-        <Route exact path={RoutePath.wireguardSettings} component={WireguardSettings} />
-        <Route exact path={RoutePath.daitaSettings} component={DaitaSettings} />
-        <Route exact path={RoutePath.udpOverTcp} component={UdpOverTcp} />
-        <Route exact path={RoutePath.shadowsocks} component={Shadowsocks} />
-        <Route exact path={RoutePath.openVpnSettings} component={OpenVpnSettings} />
-        <Route exact path={RoutePath.splitTunneling} component={SplitTunnelingSettings} />
-        <Route exact path={RoutePath.apiAccessMethods} component={ApiAccessMethods} />
-        <Route exact path={RoutePath.settingsImport} component={SettingsImport} />
-        <Route exact path={RoutePath.settingsTextImport} component={SettingsTextImport} />
-        <Route exact path={RoutePath.editApiAccessMethods} component={EditApiAccessMethod} />
-        <Route exact path={RoutePath.support} component={Support} />
-        <Route exact path={RoutePath.problemReport} component={ProblemReport} />
-        <Route exact path={RoutePath.debug} component={Debug} />
-        <Route exact path={RoutePath.selectLocation} component={SelectLocation} />
-        <Route exact path={RoutePath.editCustomBridge} component={EditCustomBridge} />
-        <Route exact path={RoutePath.filter} component={Filter} />
-        <Route exact path={RoutePath.appInfo} component={AppInfoView} />
-        <Route exact path={RoutePath.changelog} component={ChangelogView} />
-        <Route exact path={RoutePath.appUpgrade} component={AppUpgradeView} />
-      </Switch>
-    </Focus>
+    <>
+      <StateTriggeredNavigation />
+      <Focus ref={focusRef}>
+        <Switch key={currentLocation.key} location={currentLocation}>
+          <Route exact path={RoutePath.launch} component={LaunchView} />
+          <Route exact path={RoutePath.login} component={LoginView} />
+          <Route exact path={RoutePath.tooManyDevices} component={TooManyDevices} />
+          <Route exact path={RoutePath.deviceRevoked} component={DeviceRevokedView} />
+          <Route exact path={RoutePath.main} component={MainView} />
+          <Route exact path={RoutePath.expired} component={ExpiredAccountErrorView} />
+          <Route exact path={RoutePath.redeemVoucher} component={VoucherInput} />
+          <Route exact path={RoutePath.voucherSuccess} component={VoucherVerificationSuccess} />
+          <Route exact path={RoutePath.timeAdded} component={TimeAdded} />
+          <Route exact path={RoutePath.setupFinished} component={SetupFinished} />
+          <Route exact path={RoutePath.account} component={Account} />
+          <Route exact path={RoutePath.settings} component={SettingsView} />
+          <Route exact path={RoutePath.selectLanguage} component={SelectLanguage} />
+          <Route exact path={RoutePath.userInterfaceSettings} component={UserInterfaceSettings} />
+          <Route exact path={RoutePath.multihopSettings} component={MultihopSettings} />
+          <Route exact path={RoutePath.vpnSettings} component={VpnSettings} />
+          <Route exact path={RoutePath.wireguardSettings} component={WireguardSettings} />
+          <Route exact path={RoutePath.daitaSettings} component={DaitaSettings} />
+          <Route exact path={RoutePath.udpOverTcp} component={UdpOverTcp} />
+          <Route exact path={RoutePath.shadowsocks} component={Shadowsocks} />
+          <Route exact path={RoutePath.openVpnSettings} component={OpenVpnSettings} />
+          <Route exact path={RoutePath.splitTunneling} component={SplitTunnelingSettings} />
+          <Route exact path={RoutePath.apiAccessMethods} component={ApiAccessMethods} />
+          <Route exact path={RoutePath.settingsImport} component={SettingsImport} />
+          <Route exact path={RoutePath.settingsTextImport} component={SettingsTextImport} />
+          <Route exact path={RoutePath.editApiAccessMethods} component={EditApiAccessMethod} />
+          <Route exact path={RoutePath.support} component={Support} />
+          <Route exact path={RoutePath.problemReport} component={ProblemReport} />
+          <Route exact path={RoutePath.debug} component={Debug} />
+          <Route exact path={RoutePath.selectLocation} component={SelectLocation} />
+          <Route exact path={RoutePath.editCustomBridge} component={EditCustomBridge} />
+          <Route exact path={RoutePath.filter} component={Filter} />
+          <Route exact path={RoutePath.appInfo} component={AppInfoView} />
+          <Route exact path={RoutePath.changelog} component={ChangelogView} />
+          <Route exact path={RoutePath.appUpgrade} component={AppUpgradeView} />
+        </Switch>
+      </Focus>
+    </>
   );
 }

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/AppRouter.tsx
@@ -23,6 +23,7 @@ import ProblemReport from './ProblemReport';
 import SelectLanguage from './SelectLanguage';
 import SettingsImport from './SettingsImport';
 import SettingsTextImport from './SettingsTextImport';
+import StateTriggeredNavigation from './StateTriggeredNavigation';
 import Support from './Support';
 import TooManyDevices from './TooManyDevices';
 import UserInterfaceSettings from './UserInterfaceSettings';
@@ -53,44 +54,47 @@ export default function AppRouter() {
   const currentLocation = useViewTransitions(onNavigation);
 
   return (
-    <Focus ref={focusRef}>
-      <Switch key={currentLocation.key} location={currentLocation}>
-        <Route exact path={RoutePath.launch} component={LaunchView} />
-        <Route exact path={RoutePath.login} component={LoginView} />
-        <Route exact path={RoutePath.tooManyDevices} component={TooManyDevices} />
-        <Route exact path={RoutePath.deviceRevoked} component={DeviceRevokedView} />
-        <Route exact path={RoutePath.main} component={MainView} />
-        <Route exact path={RoutePath.expired} component={ExpiredAccountErrorView} />
-        <Route exact path={RoutePath.redeemVoucher} component={VoucherInput} />
-        <Route exact path={RoutePath.voucherSuccess} component={VoucherVerificationSuccess} />
-        <Route exact path={RoutePath.timeAdded} component={TimeAdded} />
-        <Route exact path={RoutePath.setupFinished} component={SetupFinished} />
-        <Route exact path={RoutePath.account} component={Account} />
-        <Route exact path={RoutePath.settings} component={SettingsView} />
-        <Route exact path={RoutePath.selectLanguage} component={SelectLanguage} />
-        <Route exact path={RoutePath.userInterfaceSettings} component={UserInterfaceSettings} />
-        <Route exact path={RoutePath.multihopSettings} component={MultihopSettingsView} />
-        <Route exact path={RoutePath.vpnSettings} component={VpnSettingsView} />
-        <Route exact path={RoutePath.wireguardSettings} component={WireguardSettingsView} />
-        <Route exact path={RoutePath.daitaSettings} component={DaitaSettingsView} />
-        <Route exact path={RoutePath.udpOverTcp} component={UdpOverTcpSettingsView} />
-        <Route exact path={RoutePath.shadowsocks} component={ShadowsocksSettingsView} />
-        <Route exact path={RoutePath.openVpnSettings} component={OpenVpnSettingsView} />
-        <Route exact path={RoutePath.splitTunneling} component={SplitTunnelingView} />
-        <Route exact path={RoutePath.apiAccessMethods} component={ApiAccessMethods} />
-        <Route exact path={RoutePath.settingsImport} component={SettingsImport} />
-        <Route exact path={RoutePath.settingsTextImport} component={SettingsTextImport} />
-        <Route exact path={RoutePath.editApiAccessMethods} component={EditApiAccessMethod} />
-        <Route exact path={RoutePath.support} component={Support} />
-        <Route exact path={RoutePath.problemReport} component={ProblemReport} />
-        <Route exact path={RoutePath.debug} component={Debug} />
-        <Route exact path={RoutePath.selectLocation} component={SelectLocation} />
-        <Route exact path={RoutePath.editCustomBridge} component={EditCustomBridge} />
-        <Route exact path={RoutePath.filter} component={Filter} />
-        <Route exact path={RoutePath.appInfo} component={AppInfoView} />
-        <Route exact path={RoutePath.changelog} component={ChangelogView} />
-        <Route exact path={RoutePath.appUpgrade} component={AppUpgradeView} />
-      </Switch>
-    </Focus>
+    <>
+      <StateTriggeredNavigation />
+      <Focus ref={focusRef}>
+        <Switch key={currentLocation.key} location={currentLocation}>
+          <Route exact path={RoutePath.launch} component={LaunchView} />
+          <Route exact path={RoutePath.login} component={LoginView} />
+          <Route exact path={RoutePath.tooManyDevices} component={TooManyDevices} />
+          <Route exact path={RoutePath.deviceRevoked} component={DeviceRevokedView} />
+          <Route exact path={RoutePath.main} component={MainView} />
+          <Route exact path={RoutePath.expired} component={ExpiredAccountErrorView} />
+          <Route exact path={RoutePath.redeemVoucher} component={VoucherInput} />
+          <Route exact path={RoutePath.voucherSuccess} component={VoucherVerificationSuccess} />
+          <Route exact path={RoutePath.timeAdded} component={TimeAdded} />
+          <Route exact path={RoutePath.setupFinished} component={SetupFinished} />
+          <Route exact path={RoutePath.account} component={Account} />
+          <Route exact path={RoutePath.settings} component={SettingsView} />
+          <Route exact path={RoutePath.selectLanguage} component={SelectLanguage} />
+          <Route exact path={RoutePath.userInterfaceSettings} component={UserInterfaceSettings} />
+          <Route exact path={RoutePath.multihopSettings} component={MultihopSettingsView} />
+          <Route exact path={RoutePath.vpnSettings} component={VpnSettingsView} />
+          <Route exact path={RoutePath.wireguardSettings} component={WireguardSettingsView} />
+          <Route exact path={RoutePath.daitaSettings} component={DaitaSettingsView} />
+          <Route exact path={RoutePath.udpOverTcp} component={UdpOverTcpSettingsView} />
+          <Route exact path={RoutePath.shadowsocks} component={ShadowsocksSettingsView} />
+          <Route exact path={RoutePath.openVpnSettings} component={OpenVpnSettingsView} />
+          <Route exact path={RoutePath.splitTunneling} component={SplitTunnelingView} />
+          <Route exact path={RoutePath.apiAccessMethods} component={ApiAccessMethods} />
+          <Route exact path={RoutePath.settingsImport} component={SettingsImport} />
+          <Route exact path={RoutePath.settingsTextImport} component={SettingsTextImport} />
+          <Route exact path={RoutePath.editApiAccessMethods} component={EditApiAccessMethod} />
+          <Route exact path={RoutePath.support} component={Support} />
+          <Route exact path={RoutePath.problemReport} component={ProblemReport} />
+          <Route exact path={RoutePath.debug} component={Debug} />
+          <Route exact path={RoutePath.selectLocation} component={SelectLocation} />
+          <Route exact path={RoutePath.editCustomBridge} component={EditCustomBridge} />
+          <Route exact path={RoutePath.filter} component={Filter} />
+          <Route exact path={RoutePath.appInfo} component={AppInfoView} />
+          <Route exact path={RoutePath.changelog} component={ChangelogView} />
+          <Route exact path={RoutePath.appUpgrade} component={AppUpgradeView} />
+        </Switch>
+      </Focus>
+    </>
   );
 }