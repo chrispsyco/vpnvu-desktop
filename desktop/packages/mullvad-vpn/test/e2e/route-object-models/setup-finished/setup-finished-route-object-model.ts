mullvad/mullvadvpn-app

Add setup finished route object model

desktop/packages/mullvad-vpn/test/e2e/route-object-models/setup-finished/setup-finished-route-object-model.ts
@@ -1,0 +1,25 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class SetupFinishedRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.setupFinished);
+  }
+
+  async startUsingTheApp() {
+    await this.selectors.startUsingTheAppButton().click();
+    await this.utils.waitForRoute(RoutePath.main);
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/setup-finished/setup-finished-route-object-model.ts
@@ -1,0 +1,25 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class SetupFinishedRouteObjectModel {
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(
+    private readonly page: Page,
+    private readonly utils: TestUtils,
+  ) {
+    this.selectors = createSelectors(this.page);
+  }
+
+  async waitForRoute() {
+    await this.utils.expectRoute(RoutePath.setupFinished);
+  }
+
+  async startUsingTheApp() {
+    await this.selectors.startUsingTheAppButton().click();
+    await this.utils.expectRoute(RoutePath.main);
+  }
+}