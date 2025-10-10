mullvad/mullvadvpn-app

Add tests for manage devices view

desktop/packages/mullvad-vpn/test/e2e/route-object-models/main/main-route-object-model.ts
@@ -29,6 +29,11 @@ export class MainRouteObjectModel {
     await this.utils.waitForRoute(RoutePath.selectLocation);
   }
 
+  async gotoAccount() {
+    await this.selectors.accountButton().click();
+    await this.utils.waitForRoute(RoutePath.account);
+  }
+
   async expandConnectionPanel() {
     await this.selectors.connectionPanelChevronButton().click();
   }

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/main/main-route-object-model.ts
@@ -29,6 +29,11 @@ export class MainRouteObjectModel {
     await this.utils.expectRoute(RoutePath.selectLocation);
   }
 
+  async gotoAccount() {
+    await this.selectors.accountButton().click();
+    await this.utils.expectRoute(RoutePath.account);
+  }
+
   async expandConnectionPanel() {
     await this.selectors.connectionPanelChevronButton().click();
   }