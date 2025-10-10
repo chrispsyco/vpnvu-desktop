mullvad/mullvadvpn-app

Add account route object model

desktop/packages/mullvad-vpn/test/e2e/route-object-models/account/account-route-object-model.ts
@@ -1,0 +1,26 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class AccountRouteObjectModel {
+  readonly page: Page;
+  readonly utils: TestUtils;
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(page: Page, util: TestUtils) {
+    this.page = page;
+    this.utils = util;
+    this.selectors = createSelectors(page);
+  }
+
+  async waitForRoute() {
+    await this.utils.waitForRoute(RoutePath.account);
+  }
+
+  async gotoManageDevices() {
+    await this.selectors.manageDevicesButton().click();
+    await this.utils.waitForRoute(RoutePath.manageDevices);
+  }
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/account/account-route-object-model.ts
@@ -1,0 +1,22 @@
+import { Page } from 'playwright';
+
+import { RoutePath } from '../../../../src/shared/routes';
+import { TestUtils } from '../../utils';
+import { createSelectors } from './selectors';
+
+export class AccountRouteObjectModel {
+  readonly page: Page;
+  readonly utils: TestUtils;
+  readonly selectors: ReturnType<typeof createSelectors>;
+
+  constructor(page: Page, util: TestUtils) {
+    this.page = page;
+    this.utils = util;
+    this.selectors = createSelectors(page);
+  }
+
+  async gotoManageDevices() {
+    await this.selectors.manageDevicesButton().click();
+    await this.utils.expectRoute(RoutePath.manageDevices);
+  }
+}