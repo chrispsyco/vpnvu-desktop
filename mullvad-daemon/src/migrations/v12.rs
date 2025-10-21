mullvad/mullvadvpn-app

Add support for multihop entry filters in daemon
In the upcoming re-design of select location, separate sets of filters
can now be picked for the entry and the exit relays. This commit adds
support for that in the relay selector.

In order to not affect the current behavior of the desktop and Android
apps before the new UI is implemented, the entry filters are set to the
same as the exit filters when the relay settings are updated via gRPC.

mullvad-daemon/src/migrations/v12.rs
@@ -1,0 +1,160 @@
+use super::Result;
+use mullvad_types::settings::SettingsVersion;
+
+/// This version introduces 2 new fields to the [mullvad_constraints::WireguardConstraints] struct:
+/// pub entry_providers: Constraint<Providers>,
+/// pub entry_ownership: Constraint<Ownership>,
+/// When set, these filters apply to the exit relay when multihop is used.
+/// A migration is needed to transfer the current providers and ownership to these new fields
+/// so that the user's current filters don't change.
+pub fn migrate(settings: &mut serde_json::Value) -> Result<()> {
+    if !version_matches(settings) {
+        return Ok(());
+    }
+
+    log::info!("Migrating settings format to V13");
+
+    migrate_filters_to_new_exit_only_filters(settings);
+
+    settings["settings_version"] = serde_json::json!(SettingsVersion::V13);
+
+    Ok(())
+}
+
+fn version_matches(settings: &serde_json::Value) -> bool {
+    settings
+        .get("settings_version")
+        .map(|version| version == SettingsVersion::V12 as u64)
+        .unwrap_or(false)
+}
+
+fn migrate_filters_to_new_exit_only_filters(settings: &mut serde_json::Value) -> Option<()> {
+    let normal = settings.get_mut("relay_settings")?.get_mut("normal")?;
+    let providers = normal.get("providers")?.clone();
+    let ownership = normal.get("ownership")?.clone();
+
+    let wireguard_constraints = normal.get_mut("wireguard_constraints")?.as_object_mut()?;
+
+    wireguard_constraints.insert("exit_providers".to_string(), providers);
+    wireguard_constraints.insert("exit_ownership".to_string(), ownership);
+
+    Some(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::{migrate, version_matches};
+
+    const V12_SETTINGS: &str = r#"
+{
+  "relay_settings": {
+    "normal": {
+      "location": {
+        "only": {
+          "location": {
+            "country": "fr"
+          }
+        }
+      },
+      "providers": {
+        "only": {
+          "providers": [
+            "Blix",
+            "Creanova"
+          ]
+        }
+      },
+      "ownership": {
+        "only": "MullvadOwned"
+      },
+      "tunnel_protocol": "wireguard",
+      "wireguard_constraints": {
+        "port": "any",
+        "ip_version": "any",
+        "allowed_ips": "any",
+        "use_multihop": true,
+        "entry_location": {
+          "only": {
+            "location": {
+              "country": "se"
+            }
+          }
+        }
+      },
+      "openvpn_constraints": {
+        "port": "any"
+      }
+    }
+  },
+  "settings_version": 12
+}
+"#;
+
+    const V13_SETTINGS: &str = r#"
+{
+  "relay_settings": {
+    "normal": {
+      "location": {
+        "only": {
+          "location": {
+            "country": "fr"
+          }
+        }
+      },
+      "providers": {
+        "only": {
+          "providers": [
+            "Blix",
+            "Creanova"
+          ]
+        }
+      },
+      "ownership": {
+        "only": "MullvadOwned"
+      },
+      "tunnel_protocol": "wireguard",
+      "wireguard_constraints": {
+        "port": "any",
+        "ip_version": "any",
+        "allowed_ips": "any",
+        "use_multihop": true,
+        "entry_location": {
+          "only": {
+            "location": {
+              "country": "se"
+            }
+          }
+        },
+        "exit_providers": {
+          "only": {
+            "providers": [
+              "Blix",
+              "Creanova"
+            ]
+          }
+        },
+        "exit_ownership": {
+          "only": "MullvadOwned"
+        }
+      },
+      "openvpn_constraints": {
+        "port": "any"
+      }
+    }
+  },
+  "settings_version": 13
+}
+"#;
+
+    #[test]
+    fn test_v12_to_v13_migration() {
+        let mut old_settings = serde_json::from_str(V12_SETTINGS).unwrap();
+
+        assert!(version_matches(&old_settings));
+
+        migrate(&mut old_settings).unwrap();
+        let new_settings: serde_json::Value = serde_json::from_str(V13_SETTINGS).unwrap();
+
+        assert_eq!(&old_settings, &new_settings);
+    }
+}

mullvad-relay-selector/tests/relay_selector.rs
@@ -284,6 +284,18 @@ fn unwrap_entry_relay(get_result: GetRelay) -> Relay {
     }
 }
 
+fn unwrap_multihop_entry_exit_relays(get_result: GetRelay) -> (Relay, Relay) {
+    match get_result {
+        GetRelay::Wireguard {
+            inner: crate::WireguardConfig::Multihop { entry, exit },
+            ..
+        } => (entry, exit),
+        relay => {
+            panic!("Relay is not a Wireguard multihop relay: {relay:?}")
+        }
+    }
+}
+
 fn unwrap_endpoint(get_result: GetRelay) -> MullvadEndpoint {
     match get_result {
         GetRelay::Wireguard { endpoint, .. } => MullvadEndpoint::Wireguard(endpoint),
@@ -1192,6 +1204,39 @@ fn test_ownership() {
     }
 }
 
+#[test]
+fn test_multihop_exit_ownership() {
+    let relay_selector = default_relay_selector();
+
+    for _ in 0..100 {
+        // Construct an arbitrary query for owned relays.
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .ownership(Ownership::MullvadOwned)
+            .entry_ownership(Ownership::Rented)
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+        // Check that the _exit_ relay is owned by Mullvad.
+        assert!(unwrap_relay(relay.clone()).owned);
+        // Check that the _entry_ relay is rented.
+        assert!(!unwrap_entry_relay(relay).owned);
+    }
+
+    for _ in 0..100 {
+        // Construct an arbitrary query for rented relays.
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .ownership(Ownership::Rented)
+            .entry_ownership(Ownership::MullvadOwned)
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+        // Check that the _exit_ relay is rented.
+        assert!(!unwrap_relay(relay.clone()).owned);
+        // Check that the _entry_ relay is owned by Mullvad.
+        assert!(unwrap_entry_relay(relay).owned);
+    }
+}
+
 /// Verify that server and port selection varies between retry attempts.
 #[test]
 fn test_load_balancing() {
@@ -1255,6 +1300,37 @@ fn test_providers() {
                 "Relay selector should have picked a Wireguard relay, instead chose {wrong_relay:?}"
             ),
         };
+    }
+}
+
+#[test]
+fn test_multihop_exit_providers() {
+    const EXPECTED_PROVIDERS: [&str; 2] = ["provider0", "provider2"];
+    const EXPECTED_ENTRY_PROVIDERS: [&str; 2] = ["provider1", "provider3"];
+    let providers = Providers::new(EXPECTED_PROVIDERS).unwrap();
+    let entry_providers = Providers::new(EXPECTED_ENTRY_PROVIDERS).unwrap();
+    let relay_selector = default_relay_selector();
+
+    for _attempt in 0..100 {
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .providers(providers.clone())
+            .entry_providers(entry_providers.clone())
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+
+        let (entry, exit) = unwrap_multihop_entry_exit_relays(relay);
+
+        assert!(
+            EXPECTED_PROVIDERS.contains(&exit.provider.as_str()),
+            "cannot find exit provider {provider} in {EXPECTED_PROVIDERS:?}",
+            provider = exit.provider
+        );
+        assert!(
+            EXPECTED_ENTRY_PROVIDERS.contains(&entry.provider.as_str()),
+            "cannot find entry provider {provider} in {EXPECTED_ENTRY_PROVIDERS:?}",
+            provider = entry.provider
+        );
     }
 }
 

chrispsyco/vpnvu-desktop

mullvad-daemon/src/migrations/v12.rs
@@ -1,0 +1,102 @@
+use super::Result;
+use mullvad_types::settings::SettingsVersion;
+
+/// This version introduces 2 new fields to the [mullvad_constraints::WireguardConstraints] struct:
+/// pub entry_providers: Constraint<Providers>,
+/// pub entry_ownership: Constraint<Ownership>,
+/// When set, these filters apply to the entry relay when multihop is used.
+/// A migration is needed to transfer the current providers and ownership to these new fields
+/// so that the user's current filters don't change.
+pub fn migrate(settings: &mut serde_json::Value) -> Result<()> {
+    if !version_matches(settings) {
+        return Ok(());
+    }
+
+    log::info!("Migrating settings format to V13");
+
+    migrate_filters_to_new_entry_only_filters(settings);
+
+    settings["settings_version"] = serde_json::json!(SettingsVersion::V13);
+
+    Ok(())
+}
+
+fn version_matches(settings: &serde_json::Value) -> bool {
+    settings
+        .get("settings_version")
+        .map(|version| version == SettingsVersion::V12 as u64)
+        .unwrap_or(false)
+}
+
+fn migrate_filters_to_new_entry_only_filters(settings: &mut serde_json::Value) -> Option<()> {
+    let normal = settings.get_mut("relay_settings")?.get_mut("normal")?;
+    let providers = normal.get("providers")?.clone();
+    let ownership = normal.get("ownership")?.clone();
+
+    let wireguard_constraints = normal.get_mut("wireguard_constraints")?.as_object_mut()?;
+
+    wireguard_constraints.insert("entry_providers".to_string(), providers);
+    wireguard_constraints.insert("entry_ownership".to_string(), ownership);
+
+    Some(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::{migrate, version_matches};
+
+    const V12_SETTINGS: &str = r#"
+{
+  "relay_settings": {
+    "normal": {
+      "location": {
+        "only": {
+          "location": {
+            "country": "fr"
+          }
+        }
+      },
+      "providers": {
+        "only": {
+          "providers": [
+            "Blix",
+            "Creanova"
+          ]
+        }
+      },
+      "ownership": {
+        "only": "MullvadOwned"
+      },
+      "tunnel_protocol": "wireguard",
+      "wireguard_constraints": {
+        "port": "any",
+        "ip_version": "any",
+        "allowed_ips": "any",
+        "use_multihop": true,
+        "entry_location": {
+          "only": {
+            "location": {
+              "country": "se"
+            }
+          }
+        }
+      },
+      "openvpn_constraints": {
+        "port": "any"
+      }
+    }
+  },
+  "settings_version": 12
+}
+"#;
+
+    #[test]
+    fn test_v12_to_v13_migration() {
+        let mut old_settings = serde_json::from_str(V12_SETTINGS).unwrap();
+
+        assert!(version_matches(&old_settings));
+
+        migrate(&mut old_settings).unwrap();
+        insta::assert_snapshot!(serde_json::to_string_pretty(&old_settings).unwrap());
+    }
+}

mullvad-relay-selector/tests/relay_selector.rs
@@ -284,6 +284,18 @@ fn unwrap_entry_relay(get_result: GetRelay) -> Relay {
     }
 }
 
+fn unwrap_multihop_entry_exit_relays(get_result: GetRelay) -> (Relay, Relay) {
+    match get_result {
+        GetRelay::Wireguard {
+            inner: crate::WireguardConfig::Multihop { entry, exit },
+            ..
+        } => (entry, exit),
+        relay => {
+            panic!("Relay is not a Wireguard multihop relay: {relay:?}")
+        }
+    }
+}
+
 fn unwrap_endpoint(get_result: GetRelay) -> MullvadEndpoint {
     match get_result {
         GetRelay::Wireguard { endpoint, .. } => MullvadEndpoint::Wireguard(endpoint),
@@ -1192,6 +1204,41 @@ fn test_ownership() {
     }
 }
 
+/// Verify that any query which sets an explicit [`Ownership`] is respected by the relay selector
+/// and that it works to set separate entry and exit ownerships for a multihop.
+#[test]
+fn test_multihop_ownership() {
+    let relay_selector = default_relay_selector();
+
+    for _ in 0..100 {
+        // Construct an arbitrary query for owned relays.
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .ownership(Ownership::MullvadOwned)
+            .entry_ownership(Ownership::Rented)
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+        // Check that the _exit_ relay is owned by Mullvad.
+        assert!(unwrap_relay(relay.clone()).owned);
+        // Check that the _entry_ relay is rented.
+        assert!(!unwrap_entry_relay(relay).owned);
+    }
+
+    for _ in 0..100 {
+        // Construct an arbitrary query for rented relays.
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .ownership(Ownership::Rented)
+            .entry_ownership(Ownership::MullvadOwned)
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+        // Check that the _exit_ relay is rented.
+        assert!(!unwrap_relay(relay.clone()).owned);
+        // Check that the _entry_ relay is owned by Mullvad.
+        assert!(unwrap_entry_relay(relay).owned);
+    }
+}
+
 /// Verify that server and port selection varies between retry attempts.
 #[test]
 fn test_load_balancing() {
@@ -1255,6 +1302,40 @@ fn test_providers() {
                 "Relay selector should have picked a Wireguard relay, instead chose {wrong_relay:?}"
             ),
         };
+    }
+}
+
+/// Construct a query for a relay with specific providers and verify that every chosen relay has
+/// the correct associated provider and that it works to select a separate set of providers for
+/// entry and exit relays when doing a multihop.
+#[test]
+fn test_multihop_providers() {
+    const EXPECTED_PROVIDERS: [&str; 2] = ["provider0", "provider2"];
+    const EXPECTED_ENTRY_PROVIDERS: [&str; 2] = ["provider1", "provider3"];
+    let providers = Providers::new(EXPECTED_PROVIDERS).unwrap();
+    let entry_providers = Providers::new(EXPECTED_ENTRY_PROVIDERS).unwrap();
+    let relay_selector = default_relay_selector();
+
+    for _attempt in 0..100 {
+        let query = RelayQueryBuilder::wireguard()
+            .multihop()
+            .providers(providers.clone())
+            .entry_providers(entry_providers.clone())
+            .build();
+        let relay = relay_selector.get_relay_by_query(query).unwrap();
+
+        let (entry, exit) = unwrap_multihop_entry_exit_relays(relay);
+
+        assert!(
+            EXPECTED_PROVIDERS.contains(&exit.provider.as_str()),
+            "cannot find exit provider {provider} in {EXPECTED_PROVIDERS:?}",
+            provider = exit.provider
+        );
+        assert!(
+            EXPECTED_ENTRY_PROVIDERS.contains(&entry.provider.as_str()),
+            "cannot find entry provider {provider} in {EXPECTED_ENTRY_PROVIDERS:?}",
+            provider = entry.provider
+        );
     }
 }
 