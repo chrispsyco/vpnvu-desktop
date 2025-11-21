mullvad/mullvadvpn-app

Create new ListItem component/VPN settings design
This is the first part of the work to switch to the new list item
design. It adds the ListItem design system component and various
components that use the ListItem.

As of this PR only the main VPN Settings view is converted to
the new design.

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/CustomPortListItem.kt
@@ -1,0 +1,100 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.model.Port
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.ui.component.DividerButton
+import net.mullvad.mullvadvpn.lib.ui.component.SpacedColumn
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.ListItemDefaults
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+
+@Preview
+@Composable
+private fun PreviewCustomPortListItem() {
+    AppTheme {
+        SpacedColumn(Modifier.background(MaterialTheme.colorScheme.surface)) {
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = true,
+                port = Port(4444),
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = true,
+                isEnabled = false,
+                port = Port(44449),
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = false,
+                port = null,
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+        }
+    }
+}
+
+@Composable
+fun CustomPortListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    isEnabled: Boolean = true,
+    isSelected: Boolean,
+    port: Port?,
+    mainTestTag: String = "",
+    numberTestTag: String = "",
+    onMainCellClicked: (() -> Unit)? = null,
+    onPortCellClicked: () -> Unit,
+) {
+    SelectableListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        isEnabled = isEnabled,
+        isSelected = isSelected,
+        testTag = mainTestTag,
+        onClick = onMainCellClicked,
+        content = {
+            Column {
+                Text(title)
+                if (port != null) {
+                    Text(
+                        "Port: ${port.value}",
+                        style = MaterialTheme.typography.labelLarge,
+                        color =
+                            if (isEnabled) MaterialTheme.colorScheme.onSurfaceVariant
+                            else ListItemDefaults.colors().disabledHeadlineColor,
+                    )
+                }
+            }
+        },
+        trailingContent = {
+            DividerButton(
+                modifier = Modifier.testTag(numberTestTag),
+                onClick = onPortCellClicked,
+                isEnabled = isEnabled,
+                icon = Icons.Default.Edit,
+            )
+        },
+    )
+}

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/InfoListItem.kt
@@ -1,0 +1,72 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
+import net.mullvad.mullvadvpn.lib.ui.component.ComponentTokens
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.MullvadListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+
+@Preview
+@Composable
+private fun PreviewInfoListItem() {
+    AppTheme {
+        InfoListItem(
+            title = "Information row title",
+            isEnabled = true,
+            onCellClicked = {},
+            onInfoClicked = {},
+        )
+    }
+}
+
+@Composable
+fun InfoListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    isEnabled: Boolean = true,
+    backgroundAlpha: Float = 1f,
+    onCellClicked: (() -> Unit)? = null,
+    onInfoClicked: (() -> Unit),
+    testTag: String = "",
+) {
+
+    MullvadListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        isEnabled = isEnabled,
+        onClick = onCellClicked,
+        testTag = testTag,
+        backgroundAlpha = backgroundAlpha,
+        content = { Text(title) },
+        trailingContent = {
+            Box(
+                modifier =
+                    Modifier.width(ComponentTokens.infoIconContainerWidth)
+                        .padding(end = Dimens.smallPadding)
+                        .fillMaxHeight(),
+                contentAlignment = Alignment.Center,
+            ) {
+                IconButton(onClick = onInfoClicked) {
+                    Icon(imageVector = Icons.Default.Info, contentDescription = null)
+                }
+            }
+        },
+    )
+}

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/NavigationListItem.kt
@@ -1,0 +1,73 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
+import net.mullvad.mullvadvpn.lib.ui.component.SpacedColumn
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.MullvadListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+
+@Preview
+@Composable
+private fun PreviewNavigationListItem() {
+    AppTheme {
+        SpacedColumn(Modifier.background(MaterialTheme.colorScheme.surface)) {
+            NavigationListItem(title = "Navigation sample", showWarning = false, onClick = {})
+            NavigationListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Navigation sample",
+                showWarning = true,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Suppress("ComposableLambdaParameterNaming")
+@Composable
+fun NavigationListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    showWarning: Boolean = false,
+    isRowEnabled: Boolean = true,
+    onClick: () -> Unit,
+    testTag: String? = null,
+    icon: @Composable ((BoxScope) -> Unit) = {
+        Icon(Icons.AutoMirrored.Default.KeyboardArrowRight, contentDescription = title)
+    },
+) {
+    MullvadListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        onClick = onClick,
+        isEnabled = isRowEnabled,
+        testTag = testTag,
+        leadingContent = {
+            if (showWarning) {
+                Icon(
+                    imageVector = Icons.Default.Error,
+                    modifier = Modifier.padding(end = Dimens.smallPadding),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.error,
+                )
+            }
+        },
+        content = { Text(title) },
+        trailingContent = icon,
+    )
+}

chrispsyco/vpnvu-desktop

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/CustomPortListItem.kt
@@ -1,0 +1,103 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.model.Port
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.ui.component.DividerButton
+import net.mullvad.mullvadvpn.lib.ui.component.R
+import net.mullvad.mullvadvpn.lib.ui.component.preview.PreviewSpacedColumn
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.ListItemDefaults
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+import net.mullvad.mullvadvpn.lib.ui.util.applyIfNotNull
+
+@Preview
+@Composable
+private fun PreviewCustomPortListItem() {
+    AppTheme {
+        PreviewSpacedColumn(Modifier.background(MaterialTheme.colorScheme.surface)) {
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = true,
+                port = Port(4444),
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = true,
+                isEnabled = false,
+                port = Port(44449),
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+            CustomPortListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Custom",
+                isSelected = false,
+                port = null,
+                onPortCellClicked = {},
+                onMainCellClicked = {},
+            )
+        }
+    }
+}
+
+@Composable
+fun CustomPortListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    isEnabled: Boolean = true,
+    isSelected: Boolean,
+    port: Port?,
+    mainTestTag: String? = null,
+    numberTestTag: String? = null,
+    onMainCellClicked: (() -> Unit)? = null,
+    onPortCellClicked: () -> Unit,
+) {
+    SelectableListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        isEnabled = isEnabled,
+        isSelected = isSelected,
+        testTag = mainTestTag,
+        onClick = onMainCellClicked,
+        content = {
+            Column {
+                Text(title)
+                if (port != null) {
+                    Text(
+                        stringResource(id = R.string.port_x, port.value),
+                        style = MaterialTheme.typography.labelLarge,
+                        color =
+                            if (isEnabled) MaterialTheme.colorScheme.onSurfaceVariant
+                            else ListItemDefaults.colors().disabledHeadlineColor,
+                    )
+                }
+            }
+        },
+        trailingContent = {
+            DividerButton(
+                modifier = Modifier.applyIfNotNull(numberTestTag) { testTag(it) },
+                onClick = onPortCellClicked,
+                isEnabled = isEnabled,
+                icon = Icons.Default.Edit,
+            )
+        },
+    )
+}

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/InfoListItem.kt
@@ -1,0 +1,75 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.MullvadListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+
+@Preview
+@Composable
+private fun PreviewInfoListItem() {
+    AppTheme {
+        InfoListItem(
+            title = "Information row title",
+            isEnabled = true,
+            onCellClicked = {},
+            onInfoClicked = {},
+        )
+    }
+}
+
+@Composable
+fun InfoListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    isEnabled: Boolean = true,
+    backgroundAlpha: Float = 1f,
+    iconContentDescription: String? = null,
+    onCellClicked: (() -> Unit)? = null,
+    onInfoClicked: (() -> Unit),
+    testTag: String? = null,
+) {
+
+    MullvadListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        isEnabled = isEnabled,
+        onClick = onCellClicked,
+        testTag = testTag,
+        backgroundAlpha = backgroundAlpha,
+        content = { Text(title) },
+        trailingContent = {
+            Box(
+                modifier =
+                    Modifier.width(ListItemComponentTokens.infoIconContainerWidth)
+                        .padding(end = Dimens.smallPadding)
+                        .fillMaxHeight(),
+                contentAlignment = Alignment.Center,
+            ) {
+                IconButton(onClick = onInfoClicked) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = iconContentDescription,
+                    )
+                }
+            }
+        },
+    )
+}

android/lib/ui/component/src/main/kotlin/net/mullvad/mullvadvpn/lib/ui/component/listitem/NavigationListItem.kt
@@ -1,0 +1,75 @@
+package net.mullvad.mullvadvpn.lib.ui.component.listitem
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import net.mullvad.mullvadvpn.lib.theme.AppTheme
+import net.mullvad.mullvadvpn.lib.theme.Dimens
+import net.mullvad.mullvadvpn.lib.ui.component.R
+import net.mullvad.mullvadvpn.lib.ui.component.preview.PreviewSpacedColumn
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Hierarchy
+import net.mullvad.mullvadvpn.lib.ui.designsystem.MullvadListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+
+@Preview
+@Composable
+private fun PreviewNavigationListItem() {
+    AppTheme {
+        PreviewSpacedColumn(Modifier.background(MaterialTheme.colorScheme.surface)) {
+            NavigationListItem(title = "Navigation sample", showWarning = false, onClick = {})
+            NavigationListItem(
+                hierarchy = Hierarchy.Child1,
+                title = "Navigation sample",
+                showWarning = true,
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Suppress("ComposableLambdaParameterNaming")
+@Composable
+fun NavigationListItem(
+    modifier: Modifier = Modifier,
+    hierarchy: Hierarchy = Hierarchy.Parent,
+    position: Position = Position.Single,
+    title: String,
+    showWarning: Boolean = false,
+    isRowEnabled: Boolean = true,
+    onClick: () -> Unit,
+    testTag: String? = null,
+    icon: @Composable ((BoxScope) -> Unit) = {
+        Icon(Icons.AutoMirrored.Default.KeyboardArrowRight, contentDescription = title)
+    },
+) {
+    MullvadListItem(
+        modifier = modifier,
+        hierarchy = hierarchy,
+        position = position,
+        onClick = onClick,
+        isEnabled = isRowEnabled,
+        testTag = testTag,
+        leadingContent = {
+            if (showWarning) {
+                Icon(
+                    imageVector = Icons.Default.Error,
+                    modifier = Modifier.padding(end = Dimens.smallPadding),
+                    contentDescription = stringResource(R.string.warning),
+                    tint = MaterialTheme.colorScheme.error,
+                )
+            }
+        },
+        content = { Text(title) },
+        trailingContent = icon,
+    )
+}