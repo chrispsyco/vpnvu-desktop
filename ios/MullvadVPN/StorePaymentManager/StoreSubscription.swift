mullvad/mullvadvpn-app

Migrate StoreKit to version 2

ios/MullvadVPN/StorePaymentManager/StoreSubscription.swift
@@ -6,11 +6,36 @@
 //  Copyright © 2025 Mullvad VPN AB. All rights reserved.
 //
 
-import Foundation
 import StoreKit
 
+// MARK: StoreKit 2 flow
+
 enum StoreSubscription: String, CaseIterable {
-    /// Thirty days non-renewable subscription
+    case thirtyDays = "net.mullvad.MullvadVPN.subscription.storekit2.30days"
+    case ninetyDays = "net.mullvad.MullvadVPN.subscription.storekit2.90days"
+
+    var localizedTitle: String {
+        switch self {
+        case .thirtyDays:
+            return NSLocalizedString("Add 30 days time (%@)", comment: "")
+        case .ninetyDays:
+            return NSLocalizedString("Add 90 days time (%@)", comment: "")
+        }
+    }
+}
+
+extension Product {
+    var customLocalizedTitle: String? {
+        guard let localizedTitle = LegacyStoreSubscription(rawValue: id)?.localizedTitle else {
+            return nil
+        }
+        return String(format: localizedTitle, price.description)
+    }
+}
+
+// MARK: Legacy StoreKit flow
+
+enum LegacyStoreSubscription: String, CaseIterable {
     case thirtyDays = "net.mullvad.MullvadVPN.subscription.30days"
     case ninetyDays = "net.mullvad.MullvadVPN.subscription.90days"
 
@@ -26,7 +51,7 @@ enum StoreSubscription: String, CaseIterable {
 
 extension SKProduct {
     var customLocalizedTitle: String? {
-        guard let localizedTitle = StoreSubscription(rawValue: productIdentifier)?.localizedTitle,
+        guard let localizedTitle = LegacyStoreSubscription(rawValue: productIdentifier)?.localizedTitle,
             let localizedPrice
         else {
             return nil
@@ -35,7 +60,7 @@ extension SKProduct {
     }
 }
 
-extension Set<StoreSubscription> {
+extension Set<LegacyStoreSubscription> {
     var productIdentifiersSet: Set<String> {
         Set<String>(map { $0.rawValue })
     }

chrispsyco/vpnvu-desktop

ios/MullvadVPN/StorePaymentManager/StoreSubscription.swift
@@ -6,11 +6,36 @@
 //  Copyright © 2025 Mullvad VPN AB. All rights reserved.
 //
 
-import Foundation
 import StoreKit
 
+// MARK: StoreKit 2 flow
+
 enum StoreSubscription: String, CaseIterable {
-    /// Thirty days non-renewable subscription
+    case thirtyDays = "net.mullvad.MullvadVPN.subscription.storekit2.30days"
+    case ninetyDays = "net.mullvad.MullvadVPN.subscription.storekit2.90days"
+
+    var localizedTitle: String {
+        switch self {
+        case .thirtyDays:
+            return NSLocalizedString("Add 30 days time (%@)", comment: "")
+        case .ninetyDays:
+            return NSLocalizedString("Add 90 days time (%@)", comment: "")
+        }
+    }
+}
+
+extension Product {
+    var customLocalizedTitle: String? {
+        guard let localizedTitle = StoreSubscription(rawValue: id)?.localizedTitle else {
+            return nil
+        }
+        return String(format: localizedTitle, displayPrice)
+    }
+}
+
+// MARK: Legacy StoreKit flow
+
+enum LegacyStoreSubscription: String, CaseIterable {
     case thirtyDays = "net.mullvad.MullvadVPN.subscription.30days"
     case ninetyDays = "net.mullvad.MullvadVPN.subscription.90days"
 
@@ -26,7 +51,7 @@ enum StoreSubscription: String, CaseIterable {
 
 extension SKProduct {
     var customLocalizedTitle: String? {
-        guard let localizedTitle = StoreSubscription(rawValue: productIdentifier)?.localizedTitle,
+        guard let localizedTitle = LegacyStoreSubscription(rawValue: productIdentifier)?.localizedTitle,
             let localizedPrice
         else {
             return nil
@@ -35,7 +60,7 @@ extension SKProduct {
     }
 }
 
-extension Set<StoreSubscription> {
+extension Set<LegacyStoreSubscription> {
     var productIdentifiersSet: Set<String> {
         Set<String>(map { $0.rawValue })
     }