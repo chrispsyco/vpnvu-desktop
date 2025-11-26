mullvad/mullvadvpn-app

Convert split tunneling test to route object model pattern

desktop/packages/mullvad-vpn/test/e2e/route-object-models/split-tunneling-settings/split-tunneling-settings-route-object-model.ts
@@ -1,4 +1,4 @@
-import { Page } from 'playwright';
+import { Locator, Page } from 'playwright';
 
 import { RoutePath } from '../../../../src/shared/routes';
 import { type TestUtils } from '../../utils';
@@ -7,12 +7,31 @@ import { createSelectors } from './selectors';
 export class SplitTunnelingSettingsRouteObjectModel {
   readonly page: Page;
   readonly utils: TestUtils;
-  readonly selectors: ReturnType<typeof createSelectors>;
+  public readonly selectors: ReturnType<typeof createSelectors>;
 
   constructor(page: Page, utils: TestUtils) {
     this.page = page;
     this.utils = utils;
     this.selectors = createSelectors(page);
+  }
+
+  async toggleSplitTunneling() {
+    await this.selectors.splitTunnelingSwitch().click();
+  }
+
+  async toggleApplication(application: Locator) {
+    await this.selectors.applicationToogle(application).click();
+  }
+
+  async numberOfSplitApplications() {
+    const list = this.selectors.splitApplicationsList();
+
+    const listHidden = await list.isHidden();
+    if (listHidden) {
+      return 0;
+    }
+
+    return this.selectors.applicationButtonsInList(list).count();
   }
 
   getLinuxApplication(applicationName: string) {

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/test/e2e/route-object-models/split-tunneling-settings/split-tunneling-settings-route-object-model.ts
@@ -1,4 +1,4 @@
-import { Page } from 'playwright';
+import { Locator, Page } from 'playwright';
 
 import { RoutePath } from '../../../../src/shared/routes';
 import { type TestUtils } from '../../utils';
@@ -7,12 +7,31 @@ import { createSelectors } from './selectors';
 export class SplitTunnelingSettingsRouteObjectModel {
   readonly page: Page;
   readonly utils: TestUtils;
-  readonly selectors: ReturnType<typeof createSelectors>;
+  public readonly selectors: ReturnType<typeof createSelectors>;
 
   constructor(page: Page, utils: TestUtils) {
     this.page = page;
     this.utils = utils;
     this.selectors = createSelectors(page);
+  }
+
+  async toggleSplitTunneling() {
+    await this.selectors.splitTunnelingSwitch().click();
+  }
+
+  async toggleApplication(application: Locator) {
+    await this.selectors.applicationToggle(application).click();
+  }
+
+  async numberOfSplitApplications() {
+    const list = this.selectors.splitApplicationsList();
+
+    const listHidden = await list.isHidden();
+    if (listHidden) {
+      return 0;
+    }
+
+    return this.selectors.applicationButtonsInList(list).count();
   }
 
   getLinuxApplication(applicationName: string) {