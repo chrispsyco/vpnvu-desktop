mullvad/mullvadvpn-app

Support blocking Ipv6 in E2E tests
If the device uses ipv6 to talk to RAAS we need to block
the ipv6 source address as well.

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/Networking.kt
@@ -1,23 +1,30 @@
 package net.mullvad.mullvadvpn.test.e2e.misc
 
 import java.net.Inet4Address
+import java.net.Inet6Address
 import java.net.NetworkInterface
-import org.junit.Assert.fail
+
+data class IpAddrs(val ipv4: String, val ipv6: List<String>)
 
 object Networking {
-    fun getDeviceIpv4Address(): String {
-        NetworkInterface.getNetworkInterfaces()!!.toList().map { networkInterface ->
-            val address =
+    fun getDeviceAddress(): IpAddrs {
+        NetworkInterface.getNetworkInterfaces()!!.toList().forEach { networkInterface ->
+            val v4 =
                 networkInterface.inetAddresses.toList().find {
                     !it.isLoopbackAddress && it is Inet4Address
                 }
 
-            if (address != null && address.hostAddress != null) {
-                return address.hostAddress!!
+            val v6 =
+                networkInterface.inetAddresses
+                    .toList()
+                    .filter { !it.isLoopbackAddress && it is Inet6Address }
+                    .map { it.hostAddress!! }
+
+            if (v4 != null && v4.hostAddress != null) {
+                return IpAddrs(v4.hostAddress!!, v6)
             }
         }
 
-        fail("Failed to get test device IP address")
-        return ""
+        error("Failed to get test device IP address")
     }
 }

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/router/firewall/DropRule.kt
@@ -19,26 +19,46 @@ data class DropRule(
     @SerialName("block_all_except_dst") val blockAllExceptDestination: Boolean = false,
 ) {
     companion object {
-        fun blockUDPTrafficRule(to: String): DropRule {
-            val testDeviceIpAddress = Networking.getDeviceIpv4Address()
-            return DropRule(
-                source = testDeviceIpAddress,
-                destination = to,
-                protocols = listOf(NetworkingProtocol.UDP),
-            )
-        }
 
-        fun blockWireGuardTrafficRule(to: String): DropRule =
-            blockUDPTrafficRule(to).copy(protocols = listOf(NetworkingProtocol.WireGuard))
+        fun blockUDPTrafficRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(protocols = listOf(NetworkingProtocol.UDP), toIpv4 = toIpv4)
 
-        fun blockAllTrafficExceptToDestinationRule(to: String): DropRule {
-            val testDeviceIpAddress = Networking.getDeviceIpv4Address()
-            return DropRule(
-                source = testDeviceIpAddress,
-                destination = to,
+        fun blockWireGuardTrafficRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(protocols = listOf(NetworkingProtocol.WireGuard), toIpv4 = toIpv4)
+
+        fun blockAllTrafficExceptToDestinationRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(
                 protocols = emptyList(),
+                toIpv4 = toIpv4,
                 blockAllExceptDestination = true,
             )
+
+        private fun blockTrafficToRule(
+            protocols: List<NetworkingProtocol>,
+            toIpv4: String,
+            blockAllExceptDestination: Boolean = false,
+        ): List<DropRule> {
+            val (sourceIpv4, sourceIpv6) = Networking.getDeviceAddress()
+
+            val ipv4Rule =
+                DropRule(
+                    source = sourceIpv4,
+                    destination = toIpv4,
+                    protocols = protocols,
+                    blockAllExceptDestination = blockAllExceptDestination,
+                )
+
+            val ipv6Rules =
+                sourceIpv6.map {
+                    DropRule(
+                        source = it.split('%')[0], // remove link-local suffix
+                        destination = toIpv4,
+                        protocols = protocols,
+                        blockAllExceptDestination = blockAllExceptDestination,
+                    )
+                }
+
+            return ipv6Rules + ipv4Rule
         }
     }
 }

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/router/packetCapture/PacketCapture.kt
@@ -19,7 +19,6 @@ import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import java.util.UUID
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.mullvad.mullvadvpn.test.e2e.constant.getRaasHost
 import net.mullvad.mullvadvpn.test.e2e.misc.Networking
@@ -107,7 +106,7 @@ class PacketCaptureClient(private val httpClient: HttpClient = defaultHttpClient
     }
 
     suspend fun sendGetCapturedPacketsRequest(session: PacketCaptureSession): HttpResponse {
-        val testDeviceIpAddress = Networking.getDeviceIpv4Address()
+        val testDeviceIpAddress = Networking.getDeviceAddress().ipv4
         return httpClient.put("parse-capture/${session.value}") {
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)

chrispsyco/vpnvu-desktop

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/misc/Networking.kt
@@ -1,23 +1,30 @@
 package net.mullvad.mullvadvpn.test.e2e.misc
 
 import java.net.Inet4Address
+import java.net.Inet6Address
 import java.net.NetworkInterface
-import org.junit.Assert.fail
+
+data class IpAddrs(val ipv4: String, val ipv6: List<String>)
 
 object Networking {
-    fun getDeviceIpv4Address(): String {
-        NetworkInterface.getNetworkInterfaces()!!.toList().map { networkInterface ->
-            val address =
+    fun getDeviceIpAddrs(): IpAddrs {
+        NetworkInterface.getNetworkInterfaces()!!.toList().forEach { networkInterface ->
+            val v4 =
                 networkInterface.inetAddresses.toList().find {
                     !it.isLoopbackAddress && it is Inet4Address
                 }
 
-            if (address != null && address.hostAddress != null) {
-                return address.hostAddress!!
+            val v6 =
+                networkInterface.inetAddresses
+                    .toList()
+                    .filter { !it.isLoopbackAddress && it is Inet6Address }
+                    .map { it.hostAddress!! }
+
+            if (v4 != null && v4.hostAddress != null) {
+                return IpAddrs(v4.hostAddress!!, v6)
             }
         }
 
-        fail("Failed to get test device IP address")
-        return ""
+        error("Failed to get test device IP address")
     }
 }

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/router/firewall/DropRule.kt
@@ -19,26 +19,46 @@ data class DropRule(
     @SerialName("block_all_except_dst") val blockAllExceptDestination: Boolean = false,
 ) {
     companion object {
-        fun blockUDPTrafficRule(to: String): DropRule {
-            val testDeviceIpAddress = Networking.getDeviceIpv4Address()
-            return DropRule(
-                source = testDeviceIpAddress,
-                destination = to,
-                protocols = listOf(NetworkingProtocol.UDP),
-            )
-        }
 
-        fun blockWireGuardTrafficRule(to: String): DropRule =
-            blockUDPTrafficRule(to).copy(protocols = listOf(NetworkingProtocol.WireGuard))
+        fun blockUDPTrafficRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(protocols = listOf(NetworkingProtocol.UDP), toIpv4 = toIpv4)
 
-        fun blockAllTrafficExceptToDestinationRule(to: String): DropRule {
-            val testDeviceIpAddress = Networking.getDeviceIpv4Address()
-            return DropRule(
-                source = testDeviceIpAddress,
-                destination = to,
+        fun blockWireGuardTrafficRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(protocols = listOf(NetworkingProtocol.WireGuard), toIpv4 = toIpv4)
+
+        fun blockAllTrafficExceptToDestinationRule(toIpv4: String): List<DropRule> =
+            blockTrafficToRule(
                 protocols = emptyList(),
+                toIpv4 = toIpv4,
                 blockAllExceptDestination = true,
             )
+
+        private fun blockTrafficToRule(
+            protocols: List<NetworkingProtocol>,
+            toIpv4: String,
+            blockAllExceptDestination: Boolean = false,
+        ): List<DropRule> {
+            val (sourceIpv4, sourceIpv6) = Networking.getDeviceIpAddrs()
+
+            val ipv4Rule =
+                DropRule(
+                    source = sourceIpv4,
+                    destination = toIpv4,
+                    protocols = protocols,
+                    blockAllExceptDestination = blockAllExceptDestination,
+                )
+
+            val ipv6Rules =
+                sourceIpv6.map {
+                    DropRule(
+                        source = it.split('%')[0], // remove link-local suffix
+                        destination = toIpv4,
+                        protocols = protocols,
+                        blockAllExceptDestination = blockAllExceptDestination,
+                    )
+                }
+
+            return ipv6Rules + ipv4Rule
         }
     }
 }

android/test/e2e/src/main/kotlin/net/mullvad/mullvadvpn/test/e2e/router/packetCapture/PacketCapture.kt
@@ -19,7 +19,6 @@ import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import java.util.UUID
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.mullvad.mullvadvpn.test.e2e.constant.getRaasHost
 import net.mullvad.mullvadvpn.test.e2e.misc.Networking
@@ -107,7 +106,7 @@ class PacketCaptureClient(private val httpClient: HttpClient = defaultHttpClient
     }
 
     suspend fun sendGetCapturedPacketsRequest(session: PacketCaptureSession): HttpResponse {
-        val testDeviceIpAddress = Networking.getDeviceIpv4Address()
+        val testDeviceIpAddress = Networking.getDeviceIpAddrs().ipv4
         return httpClient.put("parse-capture/${session.value}") {
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)