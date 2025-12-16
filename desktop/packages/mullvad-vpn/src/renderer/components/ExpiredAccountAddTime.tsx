anthunyy/thevadvpn-app

Use view components in expired account views

desktop/packages/mullvad-vpn/src/renderer/components/ExpiredAccountAddTime.tsx
@@ -12,6 +12,7 @@ import { useAppContext } from '../context';
 import useActions from '../lib/actionsHook';
 import { Button, Flex } from '../lib/components';
 import { FlexColumn } from '../lib/components/flex-column';
+import { View } from '../lib/components/view';
 import { colors } from '../lib/foundations';
 import { TransitionType, useHistory } from '../lib/history';
 import { IconBadge } from '../lib/icon-badge';
@@ -21,7 +22,7 @@ import { useSelector } from '../redux/store';
 import { AppMainHeader } from './app-main-header';
 import { hugeText, measurements, tinyText } from './common-styles';
 import CustomScrollbars from './CustomScrollbars';
-import { Container, Footer, Layout } from './Layout';
+import { Container } from './Layout';
 import {
   RedeemVoucherContainer,
   RedeemVoucherInput,
@@ -85,30 +86,35 @@ export function VoucherInput() {
   }, [history]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <RedeemVoucherContainer onSuccess={onSuccess}>
-            <StyledBody>
-              <StyledTitle>{messages.pgettext('connect-view', 'Redeem voucher')}</StyledTitle>
-              <StyledLabel>{messages.pgettext('connect-view', 'Enter voucher code')}</StyledLabel>
-              <StyledRedeemVoucherInput />
-              <RedeemVoucherResponse />
-            </StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            indent="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <RedeemVoucherContainer onSuccess={onSuccess}>
+              <FlexColumn>
+                <StyledTitle>{messages.pgettext('connect-view', 'Redeem voucher')}</StyledTitle>
+                <StyledLabel>{messages.pgettext('connect-view', 'Enter voucher code')}</StyledLabel>
+                <StyledRedeemVoucherInput />
+                <RedeemVoucherResponse />
+              </FlexColumn>
 
-            <Footer>
               <FlexColumn gap="medium">
                 <RedeemVoucherSubmitButton />
                 <Button onClick={navigateBack}>
                   <Button.Text>{messages.gettext('Cancel')}</Button.Text>
                 </Button>
               </FlexColumn>
-            </Footer>
-          </RedeemVoucherContainer>
-        </StyledContainer>
+            </RedeemVoucherContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 
@@ -160,40 +166,44 @@ export function TimeAdded(props: ITimeAddedProps) {
   }
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>
-            <Flex justifyContent="center" margin={{ bottom: 'medium' }}>
-              <IconBadge state="positive" />
-            </Flex>
-            <StyledTitle>
-              {props.title ?? messages.pgettext('connect-view', 'Time was successfully added')}
-            </StyledTitle>
-            <StyledLabel>
-              {duration
-                ? sprintf(
-                    messages.gettext('%(duration)s was added, account paid until %(expiry)s.'),
-                    {
-                      duration,
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            indent="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <FlexColumn>
+              <Flex justifyContent="center" margin={{ bottom: 'medium' }}>
+                <IconBadge state="positive" />
+              </Flex>
+              <StyledTitle>
+                {props.title ?? messages.pgettext('connect-view', 'Time was successfully added')}
+              </StyledTitle>
+              <StyledLabel>
+                {duration
+                  ? sprintf(
+                      messages.gettext('%(duration)s was added, account paid until %(expiry)s.'),
+                      {
+                        duration,
+                        expiry: newExpiry,
+                      },
+                    )
+                  : sprintf(messages.gettext('Account paid until %(expiry)s.'), {
                       expiry: newExpiry,
-                    },
-                  )
-                : sprintf(messages.gettext('Account paid until %(expiry)s.'), {
-                    expiry: newExpiry,
-                  })}
-            </StyledLabel>
-          </StyledBody>
-
-          <Footer>
+                    })}
+              </StyledLabel>
+            </FlexColumn>
             <Button onClick={navigateToSetupFinished}>
               <Button.Text>{messages.gettext('Next')}</Button.Text>
             </Button>
-          </Footer>
-        </StyledContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 
@@ -204,27 +214,32 @@ export function SetupFinished() {
   const openPrivacyLink = useCallback(() => openUrl(urls.privacyGuide), [openUrl]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>
-            <StyledTitle>{messages.pgettext('connect-view', 'You’re all set!')}</StyledTitle>
-            <StyledLabel>
-              {messages.pgettext(
-                'connect-view',
-                'Go ahead and start using the app to begin reclaiming your online privacy.',
-              )}
-            </StyledLabel>
-            <StyledLabel>
-              {messages.pgettext(
-                'connect-view',
-                'To continue your journey as a privacy ninja, visit our website to pick up other privacy-friendly habits and tools.',
-              )}
-            </StyledLabel>
-          </StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            indent="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <FlexColumn>
+              <StyledTitle>{messages.pgettext('connect-view', 'You’re all set!')}</StyledTitle>
+              <StyledLabel>
+                {messages.pgettext(
+                  'connect-view',
+                  'Go ahead and start using the app to begin reclaiming your online privacy.',
+                )}
+              </StyledLabel>
+              <StyledLabel>
+                {messages.pgettext(
+                  'connect-view',
+                  'To continue your journey as a privacy ninja, visit our website to pick up other privacy-friendly habits and tools.',
+                )}
+              </StyledLabel>
+            </FlexColumn>
 
-          <Footer>
             <FlexColumn gap="medium">
               <Button
                 onClick={openPrivacyLink}
@@ -246,10 +261,10 @@ export function SetupFinished() {
                 </Button.Text>
               </Button>
             </FlexColumn>
-          </Footer>
-        </StyledContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 

desktop/packages/mullvad-vpn/src/renderer/components/ExpiredAccountErrorView.tsx
@@ -10,6 +10,7 @@ import { useAppContext } from '../context';
 import { LockdownModeSwitch } from '../features/tunnel/components';
 import { Button, Flex } from '../lib/components';
 import { FlexColumn } from '../lib/components/flex-column';
+import { View } from '../lib/components/view';
 import { spacings } from '../lib/foundations';
 import { useHistory } from '../lib/history';
 import { useExclusiveTask } from '../lib/hooks/use-exclusive-task';
@@ -22,14 +23,11 @@ import {
   StyledAccountNumberContainer,
   StyledAccountNumberLabel,
   StyledAccountNumberMessage,
-  StyledBody,
-  StyledContainer,
   StyledCustomScrollbars,
   StyledDeviceLabel,
   StyledMessage,
   StyledTitle,
 } from './ExpiredAccountErrorViewStyles';
-import { Footer, Layout } from './Layout';
 import { ModalAlert, ModalAlertType, ModalMessage } from './Modal';
 import { SettingsListItem } from './settings-list-item';
 
@@ -72,7 +70,7 @@ function ExpiredAccountErrorViewComponent() {
   }, [push]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <AppMainHeader
         variant={isNewAccount ? 'default' : 'basedOnConnectionStatus'}
         size="basedOnLoginStatus">
@@ -80,10 +78,15 @@ function ExpiredAccountErrorViewComponent() {
         <AppMainHeader.SettingsButton />
       </AppMainHeader>
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>{isNewAccount ? <WelcomeView /> : <Content />}</StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            indent="large"
+            margin={{ top: 'large' }}
+            flexGrow={1}
+            justifyContent="space-between">
+            <FlexColumn>{isNewAccount ? <WelcomeView /> : <Content />}</FlexColumn>
 
-          <Footer>
             <FlexColumn gap="medium">
               {recoveryAction === RecoveryAction.disconnect && (
                 <Button variant="destructive" disabled={disconnecting} onClick={disconnect}>
@@ -107,12 +110,12 @@ function ExpiredAccountErrorViewComponent() {
                 </Button.Text>
               </Button>
             </FlexColumn>
-          </Footer>
 
-          <LockdownModeAlert />
-        </StyledContainer>
+            <LockdownModeAlert />
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/ExpiredAccountAddTime.tsx
@@ -12,6 +12,7 @@ import { useAppContext } from '../context';
 import useActions from '../lib/actionsHook';
 import { Button, Flex } from '../lib/components';
 import { FlexColumn } from '../lib/components/flex-column';
+import { View } from '../lib/components/view';
 import { colors } from '../lib/foundations';
 import { TransitionType, useHistory } from '../lib/history';
 import { IconBadge } from '../lib/icon-badge';
@@ -19,9 +20,8 @@ import { generateRoutePath } from '../lib/routeHelpers';
 import account from '../redux/account/actions';
 import { useSelector } from '../redux/store';
 import { AppMainHeader } from './app-main-header';
-import { hugeText, measurements, tinyText } from './common-styles';
+import { hugeText, tinyText } from './common-styles';
 import CustomScrollbars from './CustomScrollbars';
-import { Container, Footer, Layout } from './Layout';
 import {
   RedeemVoucherContainer,
   RedeemVoucherInput,
@@ -31,20 +31,6 @@ import {
 
 export const StyledCustomScrollbars = styled(CustomScrollbars)({
   flex: 1,
-});
-
-export const StyledContainer = styled(Container)({
-  paddingTop: '22px',
-  minHeight: '100%',
-  backgroundColor: colors.darkBlue,
-});
-
-export const StyledBody = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  flex: 1,
-  padding: `0 ${measurements.horizontalViewMargin}`,
-  paddingBottom: 'auto',
 });
 
 export const StyledTitle = styled.span(hugeText, {
@@ -60,13 +46,6 @@ export const StyledLabel = styled.span(tinyText, {
 
 export const StyledRedeemVoucherInput = styled(RedeemVoucherInput)({
   flex: 0,
-});
-
-export const StyledStatusIcon = styled.div({
-  alignSelf: 'center',
-  width: '60px',
-  height: '60px',
-  marginBottom: '18px',
 });
 
 export function VoucherInput() {
@@ -85,30 +64,35 @@ export function VoucherInput() {
   }, [history]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <RedeemVoucherContainer onSuccess={onSuccess}>
-            <StyledBody>
-              <StyledTitle>{messages.pgettext('connect-view', 'Redeem voucher')}</StyledTitle>
-              <StyledLabel>{messages.pgettext('connect-view', 'Enter voucher code')}</StyledLabel>
-              <StyledRedeemVoucherInput />
-              <RedeemVoucherResponse />
-            </StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            horizontalMargin="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <RedeemVoucherContainer onSuccess={onSuccess}>
+              <FlexColumn>
+                <StyledTitle>{messages.pgettext('connect-view', 'Redeem voucher')}</StyledTitle>
+                <StyledLabel>{messages.pgettext('connect-view', 'Enter voucher code')}</StyledLabel>
+                <StyledRedeemVoucherInput />
+                <RedeemVoucherResponse />
+              </FlexColumn>
 
-            <Footer>
               <FlexColumn gap="medium">
                 <RedeemVoucherSubmitButton />
                 <Button onClick={navigateBack}>
                   <Button.Text>{messages.gettext('Cancel')}</Button.Text>
                 </Button>
               </FlexColumn>
-            </Footer>
-          </RedeemVoucherContainer>
-        </StyledContainer>
+            </RedeemVoucherContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 
@@ -160,40 +144,44 @@ export function TimeAdded(props: ITimeAddedProps) {
   }
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>
-            <Flex justifyContent="center" margin={{ bottom: 'medium' }}>
-              <IconBadge state="positive" />
-            </Flex>
-            <StyledTitle>
-              {props.title ?? messages.pgettext('connect-view', 'Time was successfully added')}
-            </StyledTitle>
-            <StyledLabel>
-              {duration
-                ? sprintf(
-                    messages.gettext('%(duration)s was added, account paid until %(expiry)s.'),
-                    {
-                      duration,
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            horizontalMargin="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <FlexColumn>
+              <Flex justifyContent="center" margin={{ bottom: 'medium' }}>
+                <IconBadge state="positive" />
+              </Flex>
+              <StyledTitle>
+                {props.title ?? messages.pgettext('connect-view', 'Time was successfully added')}
+              </StyledTitle>
+              <StyledLabel>
+                {duration
+                  ? sprintf(
+                      messages.gettext('%(duration)s was added, account paid until %(expiry)s.'),
+                      {
+                        duration,
+                        expiry: newExpiry,
+                      },
+                    )
+                  : sprintf(messages.gettext('Account paid until %(expiry)s.'), {
                       expiry: newExpiry,
-                    },
-                  )
-                : sprintf(messages.gettext('Account paid until %(expiry)s.'), {
-                    expiry: newExpiry,
-                  })}
-            </StyledLabel>
-          </StyledBody>
-
-          <Footer>
+                    })}
+              </StyledLabel>
+            </FlexColumn>
             <Button onClick={navigateToSetupFinished}>
               <Button.Text>{messages.gettext('Next')}</Button.Text>
             </Button>
-          </Footer>
-        </StyledContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 
@@ -204,27 +192,32 @@ export function SetupFinished() {
   const openPrivacyLink = useCallback(() => openUrl(urls.privacyGuide), [openUrl]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <HeaderBar />
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>
-            <StyledTitle>{messages.pgettext('connect-view', 'You’re all set!')}</StyledTitle>
-            <StyledLabel>
-              {messages.pgettext(
-                'connect-view',
-                'Go ahead and start using the app to begin reclaiming your online privacy.',
-              )}
-            </StyledLabel>
-            <StyledLabel>
-              {messages.pgettext(
-                'connect-view',
-                'To continue your journey as a privacy ninja, visit our website to pick up other privacy-friendly habits and tools.',
-              )}
-            </StyledLabel>
-          </StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            horizontalMargin="large"
+            justifyContent="space-between"
+            margin={{ top: 'large' }}
+            flexGrow={1}>
+            <FlexColumn>
+              <StyledTitle>{messages.pgettext('connect-view', 'You’re all set!')}</StyledTitle>
+              <StyledLabel>
+                {messages.pgettext(
+                  'connect-view',
+                  'Go ahead and start using the app to begin reclaiming your online privacy.',
+                )}
+              </StyledLabel>
+              <StyledLabel>
+                {messages.pgettext(
+                  'connect-view',
+                  'To continue your journey as a privacy ninja, visit our website to pick up other privacy-friendly habits and tools.',
+                )}
+              </StyledLabel>
+            </FlexColumn>
 
-          <Footer>
             <FlexColumn gap="medium">
               <Button
                 onClick={openPrivacyLink}
@@ -246,10 +239,10 @@ export function SetupFinished() {
                 </Button.Text>
               </Button>
             </FlexColumn>
-          </Footer>
-        </StyledContainer>
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 

desktop/packages/mullvad-vpn/src/renderer/components/ExpiredAccountErrorView.tsx
@@ -10,6 +10,7 @@ import { useAppContext } from '../context';
 import { LockdownModeSwitch } from '../features/tunnel/components';
 import { Button, Flex } from '../lib/components';
 import { FlexColumn } from '../lib/components/flex-column';
+import { View } from '../lib/components/view';
 import { spacings } from '../lib/foundations';
 import { useHistory } from '../lib/history';
 import { useExclusiveTask } from '../lib/hooks/use-exclusive-task';
@@ -22,14 +23,11 @@ import {
   StyledAccountNumberContainer,
   StyledAccountNumberLabel,
   StyledAccountNumberMessage,
-  StyledBody,
-  StyledContainer,
   StyledCustomScrollbars,
   StyledDeviceLabel,
   StyledMessage,
   StyledTitle,
 } from './ExpiredAccountErrorViewStyles';
-import { Footer, Layout } from './Layout';
 import { ModalAlert, ModalAlertType, ModalMessage } from './Modal';
 import { SettingsListItem } from './settings-list-item';
 
@@ -72,7 +70,7 @@ function ExpiredAccountErrorViewComponent() {
   }, [push]);
 
   return (
-    <Layout>
+    <View backgroundColor="darkBlue">
       <AppMainHeader
         variant={isNewAccount ? 'default' : 'basedOnConnectionStatus'}
         size="basedOnLoginStatus">
@@ -80,10 +78,15 @@ function ExpiredAccountErrorViewComponent() {
         <AppMainHeader.SettingsButton />
       </AppMainHeader>
       <StyledCustomScrollbars fillContainer>
-        <StyledContainer>
-          <StyledBody>{isNewAccount ? <WelcomeView /> : <Content />}</StyledBody>
+        <View.Content>
+          <View.Container
+            flexDirection="column"
+            horizontalMargin="large"
+            margin={{ top: 'large' }}
+            flexGrow={1}
+            justifyContent="space-between">
+            <FlexColumn>{isNewAccount ? <WelcomeView /> : <Content />}</FlexColumn>
 
-          <Footer>
             <FlexColumn gap="medium">
               {recoveryAction === RecoveryAction.disconnect && (
                 <Button variant="destructive" disabled={disconnecting} onClick={disconnect}>
@@ -107,12 +110,12 @@ function ExpiredAccountErrorViewComponent() {
                 </Button.Text>
               </Button>
             </FlexColumn>
-          </Footer>
 
-          <LockdownModeAlert />
-        </StyledContainer>
+            <LockdownModeAlert />
+          </View.Container>
+        </View.Content>
       </StyledCustomScrollbars>
-    </Layout>
+    </View>
   );
 }
 