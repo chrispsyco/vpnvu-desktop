mullvad/mullvadvpn-app

Refactor vpn settings into feature module

android/app/src/androidTest/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreenTest.kt
@@ -14,8 +14,8 @@ import de.mannodermaus.junit5.compose.ComposeContext
 import io.mockk.MockKAnnotations
 import io.mockk.mockk
 import io.mockk.verify
-import net.mullvad.mullvadvpn.compose.state.CustomDnsItem
-import net.mullvad.mullvadvpn.compose.state.VpnSettingsUiState
+import net.mullvad.mullvadvpn.feaure.vpnsettings.impl.CustomDnsItem
+import net.mullvad.mullvadvpn.feaure.vpnsettings.impl.VpnSettingsUiState
 import net.mullvad.mullvadvpn.lib.common.Lc
 import net.mullvad.mullvadvpn.lib.common.toLc
 import net.mullvad.mullvadvpn.lib.model.Constraint

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/MullvadApp.kt
@@ -57,12 +57,23 @@ import com.ramcosta.composedestinations.generated.serveripoverride.destinations.
 import com.ramcosta.composedestinations.generated.serveripoverride.destinations.ServerIpOverridesDestination
 import com.ramcosta.composedestinations.generated.serveripoverride.destinations.ServerIpOverridesInfoDestination
 import com.ramcosta.composedestinations.generated.splittunneling.destinations.SplitTunnelingDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.ConnectOnStartupInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.ContentBlockersInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.CustomDnsInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.DeviceIpInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.DnsDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.Ipv6InfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.LocalNetworkSharingInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.MalwareInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.MtuDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.QuantumResistanceInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.VpnSettingsDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import net.mullvad.mullvadvpn.common.compose.accessibilityDataSensitive
-import net.mullvad.mullvadvpn.compose.util.BackstackObserver
+import net.mullvad.mullvadvpn.core.BackstackObserver
 import net.mullvad.mullvadvpn.viewmodel.DaemonScreenEvent
 import net.mullvad.mullvadvpn.viewmodel.MullvadAppViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -81,7 +92,10 @@ annotation class MainGraph {
     @ExternalDestination<AppearanceDestination>
     @ExternalDestination<AutoConnectAndLockdownModeDestination>
     @ExternalDestination<ChangelogDestination>
+    @ExternalDestination<ConnectOnStartupInfoDestination>
+    @ExternalDestination<ContentBlockersInfoDestination>
     @ExternalDestination<CreateCustomListDestination>
+    @ExternalDestination<CustomDnsInfoDestination>
     @ExternalDestination<CustomListLocationsDestination>
     @ExternalDestination<CustomListsDestination>
     @ExternalDestination<CustomPortDestination>
@@ -90,14 +104,21 @@ annotation class MainGraph {
     @ExternalDestination<DaitaDirectOnlyInfoDestination>
     @ExternalDestination<DeleteApiAccessMethodConfirmationDestination>
     @ExternalDestination<DeleteCustomListDestination>
+    @ExternalDestination<DeviceIpInfoDestination>
     @ExternalDestination<DiscardApiAccessChangesDestination>
     @ExternalDestination<DiscardChangesDestination>
+    @ExternalDestination<DnsDestination>
     @ExternalDestination<EditApiAccessMethodDestination>
     @ExternalDestination<EditCustomListDestination>
     @ExternalDestination<EncryptedDnsProxyInfoDestination>
     @ExternalDestination<FilterDestination>
     @ExternalDestination<ImportOverridesByTextDestination>
+    @ExternalDestination<Ipv6InfoDestination>
+    @ExternalDestination<LocalNetworkSharingInfoDestination>
+    @ExternalDestination<MalwareInfoDestination>
+    @ExternalDestination<MtuDestination>
     @ExternalDestination<MultihopDestination>
+    @ExternalDestination<QuantumResistanceInfoDestination>
     @ExternalDestination<RedeemVoucherDestination>
     @ExternalDestination<ReportProblemDestination>
     @ExternalDestination<ReportProblemNoEmailDestination>
@@ -109,6 +130,7 @@ annotation class MainGraph {
     @ExternalDestination<SplitTunnelingDestination>
     @ExternalDestination<VerificationPendingDestination>
     @ExternalDestination<ViewLogsDestination>
+    @ExternalDestination<VpnSettingsDestination>
     companion object Includes
 }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
@@ -18,7 +18,7 @@ import net.mullvad.mullvadvpn.appinfo.impl.AppInfoViewModel
 import net.mullvad.mullvadvpn.appinfo.impl.changelog.ChangelogViewModel
 import net.mullvad.mullvadvpn.compose.screen.location.LocationBottomSheetState
 import net.mullvad.mullvadvpn.compose.screen.location.RelayListScrollConnection
-import net.mullvad.mullvadvpn.compose.util.BackstackObserver
+import net.mullvad.mullvadvpn.core.BackstackObserver
 import net.mullvad.mullvadvpn.customlist.impl.screen.create.CreateCustomListDialogViewModel
 import net.mullvad.mullvadvpn.customlist.impl.screen.delete.DeleteCustomListConfirmationViewModel
 import net.mullvad.mullvadvpn.customlist.impl.screen.editlist.EditCustomListViewModel
@@ -33,6 +33,9 @@ import net.mullvad.mullvadvpn.feature.problemreport.impl.viewlogs.ViewLogsViewMo
 import net.mullvad.mullvadvpn.feature.redeemvoucher.impl.VoucherDialogViewModel
 import net.mullvad.mullvadvpn.feature.splittunneling.impl.SplitTunnelingViewModel
 import net.mullvad.mullvadvpn.feature.splittunneling.impl.applist.ApplicationsProvider
+import net.mullvad.mullvadvpn.feaure.vpnsettings.impl.VpnSettingsViewModel
+import net.mullvad.mullvadvpn.feaure.vpnsettings.impl.dns.DnsDialogViewModel
+import net.mullvad.mullvadvpn.feaure.vpnsettings.impl.mtu.MtuDialogViewModel
 import net.mullvad.mullvadvpn.filter.impl.FilterViewModel
 import net.mullvad.mullvadvpn.lib.common.constant.BillingTypes
 import net.mullvad.mullvadvpn.lib.common.constant.BuildTypes
@@ -98,17 +101,14 @@ import net.mullvad.mullvadvpn.viewmodel.ApiUnreachableViewModel
 import net.mullvad.mullvadvpn.viewmodel.ConnectViewModel
 import net.mullvad.mullvadvpn.viewmodel.DeviceListViewModel
 import net.mullvad.mullvadvpn.viewmodel.DeviceRevokedViewModel
-import net.mullvad.mullvadvpn.viewmodel.DnsDialogViewModel
 import net.mullvad.mullvadvpn.viewmodel.LoginViewModel
 import net.mullvad.mullvadvpn.viewmodel.ManageDevicesViewModel
-import net.mullvad.mullvadvpn.viewmodel.MtuDialogViewModel
 import net.mullvad.mullvadvpn.viewmodel.MullvadAppViewModel
 import net.mullvad.mullvadvpn.viewmodel.NotificationSettingsViewModel
 import net.mullvad.mullvadvpn.viewmodel.OutOfTimeViewModel
 import net.mullvad.mullvadvpn.viewmodel.PrivacyDisclaimerViewModel
 import net.mullvad.mullvadvpn.viewmodel.SettingsViewModel
 import net.mullvad.mullvadvpn.viewmodel.SplashViewModel
-import net.mullvad.mullvadvpn.viewmodel.VpnSettingsViewModel
 import net.mullvad.mullvadvpn.viewmodel.WelcomeViewModel
 import net.mullvad.mullvadvpn.viewmodel.location.LocationBottomSheetViewModel
 import net.mullvad.mullvadvpn.viewmodel.location.SearchLocationViewModel

chrispsyco/vpnvu-desktop

android/app/src/androidTest/kotlin/net/mullvad/mullvadvpn/compose/screen/VpnSettingsScreenTest.kt
@@ -14,8 +14,9 @@ import de.mannodermaus.junit5.compose.ComposeContext
 import io.mockk.MockKAnnotations
 import io.mockk.mockk
 import io.mockk.verify
-import net.mullvad.mullvadvpn.compose.state.CustomDnsItem
-import net.mullvad.mullvadvpn.compose.state.VpnSettingsUiState
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.CustomDnsItem
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.VpnSettingsScreen
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.VpnSettingsUiState
 import net.mullvad.mullvadvpn.lib.common.Lc
 import net.mullvad.mullvadvpn.lib.common.toLc
 import net.mullvad.mullvadvpn.lib.model.Constraint
@@ -58,7 +59,6 @@ class VpnSettingsScreenTest {
         isIpv6Enabled: Boolean = true,
         isContentBlockersExpanded: Boolean = false,
         isModal: Boolean = false,
-        isScrollToFeatureEnabled: Boolean = true,
     ) =
         VpnSettingsUiState.from(
             mtu = mtu,
@@ -74,7 +74,6 @@ class VpnSettingsScreenTest {
             isIpv6Enabled = isIpv6Enabled,
             isContentBlockersExpanded = isContentBlockersExpanded,
             isModal = isModal,
-            isScrollToFeatureEnabled = isScrollToFeatureEnabled,
         )
 
     private fun ComposeContext.initScreen(

android/app/src/main/kotlin/net/mullvad/mullvadvpn/compose/screen/MullvadApp.kt
@@ -60,12 +60,23 @@ import com.ramcosta.composedestinations.generated.serveripoverride.destinations.
 import com.ramcosta.composedestinations.generated.serveripoverride.destinations.ServerIpOverridesDestination
 import com.ramcosta.composedestinations.generated.serveripoverride.destinations.ServerIpOverridesInfoDestination
 import com.ramcosta.composedestinations.generated.splittunneling.destinations.SplitTunnelingDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.ConnectOnStartupInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.ContentBlockersInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.CustomDnsInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.DeviceIpInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.DnsDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.Ipv6InfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.LocalNetworkSharingInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.MalwareInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.MtuDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.QuantumResistanceInfoDestination
+import com.ramcosta.composedestinations.generated.vpnsettings.destinations.VpnSettingsDestination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.dependency
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import net.mullvad.mullvadvpn.common.compose.accessibilityDataSensitive
-import net.mullvad.mullvadvpn.compose.util.BackstackObserver
+import net.mullvad.mullvadvpn.util.BackstackObserver
 import net.mullvad.mullvadvpn.viewmodel.DaemonScreenEvent
 import net.mullvad.mullvadvpn.viewmodel.MullvadAppViewModel
 import org.koin.androidx.compose.koinViewModel
@@ -84,7 +95,10 @@ annotation class MainGraph {
     @ExternalDestination<AppearanceDestination>
     @ExternalDestination<AutoConnectAndLockdownModeDestination>
     @ExternalDestination<ChangelogDestination>
+    @ExternalDestination<ConnectOnStartupInfoDestination>
+    @ExternalDestination<ContentBlockersInfoDestination>
     @ExternalDestination<CreateCustomListDestination>
+    @ExternalDestination<CustomDnsInfoDestination>
     @ExternalDestination<CustomListLocationsDestination>
     @ExternalDestination<CustomListsDestination>
     @ExternalDestination<CustomPortDestination>
@@ -93,17 +107,24 @@ annotation class MainGraph {
     @ExternalDestination<DaitaDirectOnlyInfoDestination>
     @ExternalDestination<DeleteApiAccessMethodConfirmationDestination>
     @ExternalDestination<DeleteCustomListDestination>
+    @ExternalDestination<DeviceIpInfoDestination>
     @ExternalDestination<DiscardApiAccessChangesDestination>
     @ExternalDestination<DiscardChangesDestination>
+    @ExternalDestination<DnsDestination>
     @ExternalDestination<EditApiAccessMethodDestination>
     @ExternalDestination<EditCustomListDestination>
     @ExternalDestination<EncryptedDnsProxyInfoDestination>
     @ExternalDestination<FilterDestination>
     @ExternalDestination<ImportOverridesByTextDestination>
+    @ExternalDestination<Ipv6InfoDestination>
+    @ExternalDestination<LocalNetworkSharingInfoDestination>
+    @ExternalDestination<MalwareInfoDestination>
     @ExternalDestination<ManageDevicesDestination>
     @ExternalDestination<ManageDevicesRemoveConfirmationDestination>
+    @ExternalDestination<MtuDestination>
     @ExternalDestination<MultihopDestination>
     @ExternalDestination<NotificationSettingsDestination>
+    @ExternalDestination<QuantumResistanceInfoDestination>
     @ExternalDestination<RedeemVoucherDestination>
     @ExternalDestination<ReportProblemDestination>
     @ExternalDestination<ReportProblemNoEmailDestination>
@@ -115,6 +136,7 @@ annotation class MainGraph {
     @ExternalDestination<SplitTunnelingDestination>
     @ExternalDestination<VerificationPendingDestination>
     @ExternalDestination<ViewLogsDestination>
+    @ExternalDestination<VpnSettingsDestination>
     companion object Includes
 }
 

android/app/src/main/kotlin/net/mullvad/mullvadvpn/di/UiModule.kt
@@ -18,7 +18,6 @@ import net.mullvad.mullvadvpn.appinfo.impl.AppInfoViewModel
 import net.mullvad.mullvadvpn.appinfo.impl.changelog.ChangelogViewModel
 import net.mullvad.mullvadvpn.compose.screen.location.LocationBottomSheetState
 import net.mullvad.mullvadvpn.compose.screen.location.RelayListScrollConnection
-import net.mullvad.mullvadvpn.compose.util.BackstackObserver
 import net.mullvad.mullvadvpn.customlist.impl.screen.create.CreateCustomListDialogViewModel
 import net.mullvad.mullvadvpn.customlist.impl.screen.delete.DeleteCustomListConfirmationViewModel
 import net.mullvad.mullvadvpn.customlist.impl.screen.editlist.EditCustomListViewModel
@@ -35,6 +34,9 @@ import net.mullvad.mullvadvpn.feature.problemreport.impl.viewlogs.ViewLogsViewMo
 import net.mullvad.mullvadvpn.feature.redeemvoucher.impl.VoucherDialogViewModel
 import net.mullvad.mullvadvpn.feature.splittunneling.impl.SplitTunnelingViewModel
 import net.mullvad.mullvadvpn.feature.splittunneling.impl.applist.ApplicationsProvider
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.VpnSettingsViewModel
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.dns.DnsDialogViewModel
+import net.mullvad.mullvadvpn.feature.vpnsettings.impl.mtu.MtuDialogViewModel
 import net.mullvad.mullvadvpn.filter.impl.FilterViewModel
 import net.mullvad.mullvadvpn.lib.common.constant.BillingTypes
 import net.mullvad.mullvadvpn.lib.common.constant.BuildTypes
@@ -95,20 +97,18 @@ import net.mullvad.mullvadvpn.serveripoverride.impl.ServerIpOverridesViewModel
 import net.mullvad.mullvadvpn.serveripoverride.impl.reset.ResetServerIpOverridesConfirmationViewModel
 import net.mullvad.mullvadvpn.ui.MainActivity
 import net.mullvad.mullvadvpn.ui.serviceconnection.ServiceConnectionManager
+import net.mullvad.mullvadvpn.util.BackstackObserver
 import net.mullvad.mullvadvpn.viewmodel.AccountViewModel
 import net.mullvad.mullvadvpn.viewmodel.ApiUnreachableViewModel
 import net.mullvad.mullvadvpn.viewmodel.ConnectViewModel
 import net.mullvad.mullvadvpn.viewmodel.DeviceListViewModel
 import net.mullvad.mullvadvpn.viewmodel.DeviceRevokedViewModel
-import net.mullvad.mullvadvpn.viewmodel.DnsDialogViewModel
 import net.mullvad.mullvadvpn.viewmodel.LoginViewModel
-import net.mullvad.mullvadvpn.viewmodel.MtuDialogViewModel
 import net.mullvad.mullvadvpn.viewmodel.MullvadAppViewModel
 import net.mullvad.mullvadvpn.viewmodel.OutOfTimeViewModel
 import net.mullvad.mullvadvpn.viewmodel.PrivacyDisclaimerViewModel
 import net.mullvad.mullvadvpn.viewmodel.SettingsViewModel
 import net.mullvad.mullvadvpn.viewmodel.SplashViewModel
-import net.mullvad.mullvadvpn.viewmodel.VpnSettingsViewModel
 import net.mullvad.mullvadvpn.viewmodel.WelcomeViewModel
 import net.mullvad.mullvadvpn.viewmodel.location.LocationBottomSheetViewModel
 import net.mullvad.mullvadvpn.viewmodel.location.SearchLocationViewModel
@@ -312,7 +312,7 @@ val uiModule = module {
     viewModel { SettingsViewModel(get(), get(), get(), get(), IS_PLAY_BUILD) }
     viewModel { SplashViewModel(get(), get(), get(), get()) }
     viewModel { VoucherDialogViewModel(get(), get()) }
-    viewModel { VpnSettingsViewModel(get(), get(), get(), get(), get(), get()) }
+    viewModel { VpnSettingsViewModel(get(), get(), get(), get(), get()) }
     viewModel { AntiCensorshipSettingsViewModel(get(), get()) }
     viewModel { WelcomeViewModel(get(), get(), get(), get(), isPlayBuild = IS_PLAY_BUILD) }
     viewModel {