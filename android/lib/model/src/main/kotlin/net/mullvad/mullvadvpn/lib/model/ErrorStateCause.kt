mullvad/mullvadvpn-app

Set the app in a blocking state if invalid ipv6 config

android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/ErrorStateCause.kt
@@ -32,6 +32,12 @@ sealed class ErrorStateCause {
     data object OtherLegacyAlwaysOnApp : ErrorStateCause()
 
     data class NoRelaysMatchSelectedPort(val port: Port) : ErrorStateCause()
+
+    data class InvalidIpv6Config(
+        val addresses: List<InetAddress>,
+        val routes: List<InetAddress>,
+        val dnsServers: List<InetAddress>,
+    ) : ErrorStateCause()
 }
 
 sealed interface AuthFailedError {

chrispsyco/vpnvu-desktop

android/lib/model/src/main/kotlin/net/mullvad/mullvadvpn/lib/model/ErrorStateCause.kt
@@ -32,6 +32,12 @@ sealed class ErrorStateCause {
     data object OtherLegacyAlwaysOnApp : ErrorStateCause()
 
     data class NoRelaysMatchSelectedPort(val port: Port) : ErrorStateCause()
+
+    data class InvalidIpv6Config(
+        val addresses: List<String>,
+        val routes: List<String>,
+        val dnsServers: List<String>,
+    ) : ErrorStateCause()
 }
 
 sealed interface AuthFailedError {