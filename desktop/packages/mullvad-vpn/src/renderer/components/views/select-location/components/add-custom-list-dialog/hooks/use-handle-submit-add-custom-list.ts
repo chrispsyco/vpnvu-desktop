mullvad/mullvadvpn-app

Use dialog to create new custom list

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-dialog/hooks/use-handle-submit-add-custom-list.ts
@@ -2,31 +2,32 @@ import React from 'react';
 
 import { useCustomLists } from '../../../../../../features/location/hooks';
 import { useCustomListListContext } from '../../custom-list-location-list/CustomListLocationListContext';
-import { useAddCustomListFormContext } from '../AddCustomListFormContext';
+import { useAddCustomListDialogContext } from '../AddCustomListDialogContext';
 
 export function useHandleSubmitAddCustomList() {
   const { createCustomList: contextCreateCustomList } = useCustomLists();
-  const { hideAddForm, setAddingForm } = useCustomListListContext();
+  const { setAddingCustomList } = useCustomListListContext();
   const {
+    onOpenChange,
     form: {
       setError,
       customListTextField: { value, invalid, reset },
     },
-  } = useAddCustomListFormContext();
+  } = useAddCustomListDialogContext();
 
   const submitCustomList = React.useCallback(
     async (name: string) => {
-      setAddingForm(true);
+      setAddingCustomList(true);
       const result = await contextCreateCustomList(name);
       if (result) {
         setError(true);
       } else {
         reset();
-        hideAddForm();
+        onOpenChange?.(false);
       }
-      setAddingForm(false);
+      setAddingCustomList(false);
     },
-    [contextCreateCustomList, hideAddForm, reset, setAddingForm, setError],
+    [contextCreateCustomList, onOpenChange, reset, setAddingCustomList, setError],
   );
 
   return React.useCallback(

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/components/custom-lists-section-title/CustomListsSectionTitle.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import React from 'react';
 
 import { messages } from '../../../../../../../../shared/gettext';
 import {
@@ -9,16 +9,12 @@ import { useCustomListListContext } from '../../CustomListLocationListContext';
 
 export type CustomListsSectionTitleProps = SectionTitleProps;
 
-const StyledIconButton = styled(SectionTitle.IconButton)<{ $active: boolean }>`
-  transform: ${({ $active }) => (!$active ? 'rotate(45deg)' : 'rotate(0deg)')};
-  transition: transform 0.2s ease-in-out;
-`;
-
 export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitleProps) {
-  const { addFormVisible, hideAddForm, showAddForm } = useCustomListListContext();
-  const handleOnClick = addFormVisible ? hideAddForm : showAddForm;
-  const createAriaLabel = messages.pgettext('accessibility', 'Create new custom list');
-  const cancelAriaLabel = messages.pgettext('accessibility', 'Cancel creating new custom list');
+  const { setAddCustomListDialogOpen } = useCustomListListContext();
+
+  const handleOnClick = React.useCallback(() => {
+    setAddCustomListDialogOpen(true);
+  }, [setAddCustomListDialogOpen]);
 
   return (
     <SectionTitle {...props}>
@@ -26,12 +22,11 @@ export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitlePro
         {messages.pgettext('select-location-view', 'Custom lists')}
       </SectionTitle.Title>
       <SectionTitle.Divider />
-      <StyledIconButton
-        $active={addFormVisible}
+      <SectionTitle.IconButton
         onClick={handleOnClick}
-        aria-label={addFormVisible ? cancelAriaLabel : createAriaLabel}>
-        <SectionTitle.IconButton.Icon icon="cross" />
-      </StyledIconButton>
+        aria-label={messages.pgettext('accessibility', 'Add a new custom list')}>
+        <SectionTitle.IconButton.Icon icon="add" />
+      </SectionTitle.IconButton>
     </SectionTitle>
   );
 }

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-dialog/hooks/use-handle-submit-add-custom-list.ts
@@ -2,31 +2,32 @@ import React from 'react';
 
 import { useCustomLists } from '../../../../../../features/location/hooks';
 import { useCustomListLocationListContext } from '../../custom-list-location-list/CustomListLocationListContext';
-import { useAddCustomListFormContext } from '../AddCustomListFormContext';
+import { useAddCustomListDialogContext } from '../AddCustomListDialogContext';
 
 export function useHandleSubmitAddCustomList() {
   const { createCustomList: contextCreateCustomList } = useCustomLists();
-  const { hideAddForm, setAddingForm } = useCustomListLocationListContext();
+  const { setAddingCustomList } = useCustomListLocationListContext();
   const {
+    onOpenChange,
     form: {
       setError,
       customListTextField: { value, invalid, reset },
     },
-  } = useAddCustomListFormContext();
+  } = useAddCustomListDialogContext();
 
   const submitCustomList = React.useCallback(
     async (name: string) => {
-      setAddingForm(true);
+      setAddingCustomList(true);
       const result = await contextCreateCustomList(name);
       if (result) {
         setError(true);
       } else {
         reset();
-        hideAddForm();
+        onOpenChange?.(false);
       }
-      setAddingForm(false);
+      setAddingCustomList(false);
     },
-    [contextCreateCustomList, hideAddForm, reset, setAddingForm, setError],
+    [contextCreateCustomList, onOpenChange, reset, setAddingCustomList, setError],
   );
 
   return React.useCallback(

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/components/custom-lists-section-title/CustomListsSectionTitle.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import React from 'react';
 
 import { messages } from '../../../../../../../../shared/gettext';
 import {
@@ -9,16 +9,12 @@ import { useCustomListLocationListContext } from '../../CustomListLocationListCo
 
 export type CustomListsSectionTitleProps = SectionTitleProps;
 
-const StyledIconButton = styled(SectionTitle.IconButton)<{ $active: boolean }>`
-  transform: ${({ $active }) => (!$active ? 'rotate(45deg)' : 'rotate(0deg)')};
-  transition: transform 0.2s ease-in-out;
-`;
-
 export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitleProps) {
-  const { addFormVisible, hideAddForm, showAddForm } = useCustomListLocationListContext();
-  const handleOnClick = addFormVisible ? hideAddForm : showAddForm;
-  const createAriaLabel = messages.pgettext('accessibility', 'Create new custom list');
-  const cancelAriaLabel = messages.pgettext('accessibility', 'Cancel creating new custom list');
+  const { setAddCustomListDialogOpen } = useCustomListLocationListContext();
+
+  const handleOnClick = React.useCallback(() => {
+    setAddCustomListDialogOpen(true);
+  }, [setAddCustomListDialogOpen]);
 
   return (
     <SectionTitle {...props}>
@@ -26,12 +22,11 @@ export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitlePro
         {messages.pgettext('select-location-view', 'Custom lists')}
       </SectionTitle.Title>
       <SectionTitle.Divider />
-      <StyledIconButton
-        $active={addFormVisible}
+      <SectionTitle.IconButton
         onClick={handleOnClick}
-        aria-label={addFormVisible ? cancelAriaLabel : createAriaLabel}>
-        <SectionTitle.IconButton.Icon icon="cross" />
-      </StyledIconButton>
+        aria-label={messages.pgettext('accessibility', 'Add a new custom list')}>
+        <SectionTitle.IconButton.Icon icon="add" />
+      </SectionTitle.IconButton>
     </SectionTitle>
   );
 }