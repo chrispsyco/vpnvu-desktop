mullvad/mullvadvpn-app

Add and ListItemTrailingActions to layout several trailing actions

desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-actions/index.ts
@@ -1,0 +1,1 @@
+export * from './ListItemTralingActions';

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/list-item-trailing-actions/index.ts
@@ -1,0 +1,1 @@
+export * from './ListItemTrailingActions';