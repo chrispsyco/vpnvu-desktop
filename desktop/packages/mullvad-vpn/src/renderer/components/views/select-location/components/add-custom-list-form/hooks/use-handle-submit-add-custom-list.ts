mullvad/mullvadvpn-app

Update add custom list form states when submitting

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-form/hooks/use-handle-submit-add-custom-list.ts
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import log from '../../../../../../../shared/logging';
 import { useCustomLists } from '../../../../../../features/location/hooks';
 import { useCustomListListContext } from '../../custom-list-location-list/CustomListLocationListContext';
 import { useAddCustomListFormContext } from '../AddCustomListFormContext';
 
 export function useHandleSubmitAddCustomList() {
   const { createCustomList: contextCreateCustomList } = useCustomLists();
-  const { hideAddForm } = useCustomListListContext();
+  const { hideAddForm, setAddingForm } = useCustomListListContext();
   const {
     form: {
       setError,
@@ -17,20 +16,17 @@ export function useHandleSubmitAddCustomList() {
 
   const submitCustomList = React.useCallback(
     async (name: string) => {
-      try {
-        const result = await contextCreateCustomList(name);
-        if (result) {
-          setError(true);
-        } else {
-          reset();
-          hideAddForm();
-        }
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to create list:', error.message);
+      setAddingForm(true);
+      const result = await contextCreateCustomList(name);
+      if (result) {
+        setError(true);
+      } else {
+        reset();
+        hideAddForm();
       }
+      setAddingForm(false);
     },
-    [contextCreateCustomList, hideAddForm, reset, setError],
+    [contextCreateCustomList, hideAddForm, reset, setAddingForm, setError],
   );
 
   return React.useCallback(

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
@@ -43,7 +43,7 @@ function CustomListLocationListImpl({
   selectedElementRef,
 }: Pick<CustomListListProps, 'selectedElementRef'>) {
   const { customListLocations } = useCustomListLocationsContext();
-  const { addFormVisible } = useCustomListListContext();
+  const { addFormVisible, addingForm } = useCustomListListContext();
   const handleSelectCustomList = useHandleSelectCustomList();
   const hasNoCustomListsInSearchResult = useHasNoCustomListsInSearchResult();
   const hasCustomLists = useHasCustomLists();
@@ -77,7 +77,7 @@ function CustomListLocationListImpl({
           })}
         </StyledAnimatedList>
 
-        {!hasCustomLists && !addFormVisible && (
+        {!hasCustomLists && !addFormVisible && !addingForm && (
           <Text variant="labelTiny" color="whiteAlpha60">
             {messages.pgettext(
               'select-location-view',

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-form/hooks/use-handle-submit-add-custom-list.ts
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import log from '../../../../../../../shared/logging';
 import { useCustomLists } from '../../../../../../features/location/hooks';
 import { useCustomListLocationListContext } from '../../custom-list-location-list/CustomListLocationListContext';
 import { useAddCustomListFormContext } from '../AddCustomListFormContext';
 
 export function useHandleSubmitAddCustomList() {
   const { createCustomList: contextCreateCustomList } = useCustomLists();
-  const { hideAddForm } = useCustomListLocationListContext();
+  const { hideAddForm, setAddingForm } = useCustomListLocationListContext();
   const {
     form: {
       setError,
@@ -17,20 +16,17 @@ export function useHandleSubmitAddCustomList() {
 
   const submitCustomList = React.useCallback(
     async (name: string) => {
-      try {
-        const result = await contextCreateCustomList(name);
-        if (result) {
-          setError(true);
-        } else {
-          reset();
-          hideAddForm();
-        }
-      } catch (e) {
-        const error = e as Error;
-        log.error('Failed to create list:', error.message);
+      setAddingForm(true);
+      const result = await contextCreateCustomList(name);
+      if (result) {
+        setError(true);
+      } else {
+        reset();
+        hideAddForm();
       }
+      setAddingForm(false);
     },
-    [contextCreateCustomList, hideAddForm, reset, setError],
+    [contextCreateCustomList, hideAddForm, reset, setAddingForm, setError],
   );
 
   return React.useCallback(

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
@@ -43,7 +43,7 @@ function CustomListLocationListImpl({
   selectedElementRef,
 }: Pick<CustomListListProps, 'selectedElementRef'>) {
   const { customListLocations } = useCustomListLocationsContext();
-  const { addFormVisible } = useCustomListLocationListContext();
+  const { addFormVisible, addingForm } = useCustomListLocationListContext();
   const handleSelectCustomList = useHandleSelectCustomList();
   const hasNoCustomListsInSearchResult = useHasNoCustomListsInSearchResult();
   const hasCustomLists = useHasCustomLists();
@@ -77,7 +77,7 @@ function CustomListLocationListImpl({
           })}
         </StyledAnimatedList>
 
-        {!hasCustomLists && !addFormVisible && (
+        {!hasCustomLists && !addFormVisible && !addingForm && (
           <Text variant="labelTiny" color="whiteAlpha60">
             {messages.pgettext(
               'select-location-view',