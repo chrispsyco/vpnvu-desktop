mullvad/mullvadvpn-app

Refactor select location components to use consistent naming

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-form/hooks/use-handle-submit-add-custom-list.ts
@@ -1,0 +1,45 @@
+import React from 'react';
+
+import log from '../../../../../../../shared/logging';
+import { useCustomLists } from '../../../../../../features/location/hooks';
+import { useCustomListListContext } from '../../custom-list-location-list/CustomListLocationListContext';
+import { useAddCustomListFormContext } from '../AddCustomListFormContext';
+
+export function useHandleSubmitAddCustomList() {
+  const { createCustomList: contextCreateCustomList } = useCustomLists();
+  const { hideAddForm } = useCustomListListContext();
+  const {
+    form: {
+      setError,
+      customListTextField: { value, invalid, reset },
+    },
+  } = useAddCustomListFormContext();
+
+  const submitCustomList = React.useCallback(
+    async (name: string) => {
+      try {
+        const result = await contextCreateCustomList(name);
+        if (result) {
+          setError(true);
+        } else {
+          reset();
+          hideAddForm();
+        }
+      } catch (e) {
+        const error = e as Error;
+        log.error('Failed to create list:', error.message);
+      }
+    },
+    [contextCreateCustomList, hideAddForm, reset, setError],
+  );
+
+  return React.useCallback(
+    async (event: React.FormEvent) => {
+      event.preventDefault();
+      if (!invalid) {
+        await submitCustomList(value);
+      }
+    },
+    [invalid, submitCustomList, value],
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
@@ -1,0 +1,94 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../shared/gettext';
+import { Text } from '../../../../../lib/components';
+import { Accordion } from '../../../../../lib/components/accordion';
+import { FlexColumn } from '../../../../../lib/components/flex-column';
+import { spacings } from '../../../../../lib/foundations';
+import { useRelayListContext } from '../../RelayListContext';
+import { AddCustomListForm } from '../add-custom-list-form/AddCustomListForm';
+import { CustomListLocationListItem } from '../custom-list-location-list-item';
+import { CustomListsSectionTitle } from './components';
+import {
+  CustomListLocationListProvider,
+  useCustomListListContext,
+} from './CustomListLocationListContext';
+import { useHandleSelectCustomList, useHasCustomLists } from './hooks';
+import { useHasNoCustomListsInSearchResult } from './hooks/use-has-no-custom-lists-in-search-result';
+
+export type LocationSelection = 'entry' | 'exit';
+
+export type CustomListListProps = {
+  locationSelection: LocationSelection;
+  selectedElementRef: React.Ref<HTMLDivElement>;
+};
+
+const StyledLocationContainer = styled.div`
+  // If the container has children, add spacing between them
+  &:not(:last-child):has(> *) {
+    margin-bottom: ${spacings.small};
+  }
+`;
+
+function CustomListLocationListImpl({
+  selectedElementRef,
+}: Pick<CustomListListProps, 'selectedElementRef'>) {
+  const { customLists } = useRelayListContext();
+  const { addFormVisible } = useCustomListListContext();
+  const handleSelectCustomList = useHandleSelectCustomList();
+  const hasNoCustomListsInSearchResult = useHasNoCustomListsInSearchResult();
+  const hasCustomLists = useHasCustomLists();
+
+  if (hasNoCustomListsInSearchResult) {
+    return null;
+  }
+
+  return (
+    <FlexColumn gap="tiny">
+      <CustomListsSectionTitle />
+
+      <FlexColumn>
+        <Accordion expanded={addFormVisible}>
+          <Accordion.Content>
+            <AddCustomListForm />
+          </Accordion.Content>
+        </Accordion>
+
+        {hasCustomLists && (
+          <FlexColumn>
+            {customLists.map((customList) => {
+              return (
+                <StyledLocationContainer key={Object.values(customList.details).join('-')}>
+                  <CustomListLocationListItem
+                    location={customList}
+                    level={0}
+                    selectedElementRef={selectedElementRef}
+                    onSelect={handleSelectCustomList}
+                  />
+                </StyledLocationContainer>
+              );
+            })}
+          </FlexColumn>
+        )}
+
+        {!hasCustomLists && !addFormVisible && (
+          <Text variant="labelTiny" color="whiteAlpha60">
+            {messages.pgettext(
+              'select-location-view',
+              'Add a custom list by clicking the “+” icon ',
+            )}
+          </Text>
+        )}
+      </FlexColumn>
+    </FlexColumn>
+  );
+}
+
+export function CustomListLocationList({ locationSelection, ...props }: CustomListListProps) {
+  return (
+    <CustomListLocationListProvider locationSelection={locationSelection}>
+      <CustomListLocationListImpl {...props} />
+    </CustomListLocationListProvider>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationListContext.tsx
@@ -1,0 +1,51 @@
+import React from 'react';
+
+import { useBoolean } from '../../../../../lib/utility-hooks';
+import type { LocationSelection } from './CustomListLocationList';
+
+type CustomListLocationListContextProps = Omit<CustomListLocationListProviderProps, 'children'> & {
+  addFormVisible: boolean;
+  showAddForm: () => void;
+  hideAddForm: () => void;
+};
+
+const CustomListLocationListContext = React.createContext<
+  CustomListLocationListContextProps | undefined
+>(undefined);
+
+export const useCustomListListContext = (): CustomListLocationListContextProps => {
+  const context = React.useContext(CustomListLocationListContext);
+  if (!context) {
+    throw new Error(
+      'useCustomListListContext must be used within a CustomListLocationListProvider',
+    );
+  }
+  return context;
+};
+
+type CustomListLocationListProviderProps = React.PropsWithChildren & {
+  locationSelection: LocationSelection;
+};
+
+export function CustomListLocationListProvider({
+  children,
+  ...props
+}: CustomListLocationListProviderProps) {
+  const [addFormVisible, showAddForm, hideAddForm] = useBoolean(false);
+
+  const value = React.useMemo(
+    () => ({
+      addFormVisible,
+      showAddForm,
+      hideAddForm,
+      ...props,
+    }),
+    [addFormVisible, showAddForm, hideAddForm, props],
+  );
+
+  return (
+    <CustomListLocationListContext.Provider value={value}>
+      {children}
+    </CustomListLocationListContext.Provider>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/components/custom-lists-section-title/CustomListsSectionTitle.tsx
@@ -1,0 +1,37 @@
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../../../shared/gettext';
+import {
+  SectionTitle,
+  type SectionTitleProps,
+} from '../../../../../../../lib/components/section-title';
+import { useCustomListListContext } from '../../CustomListLocationListContext';
+
+export type CustomListsSectionTitleProps = SectionTitleProps;
+
+const StyledIconButton = styled(SectionTitle.IconButton)<{ $active: boolean }>`
+  transform: ${({ $active }) => (!$active ? 'rotate(45deg)' : 'rotate(0deg)')};
+  transition: transform 0.2s ease-in-out;
+`;
+
+export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitleProps) {
+  const { addFormVisible, hideAddForm, showAddForm } = useCustomListListContext();
+  const handleOnClick = addFormVisible ? hideAddForm : showAddForm;
+  const createAriaLabel = messages.pgettext('accessibility', 'Create new custom list');
+  const cancelAriaLabel = messages.pgettext('accessibility', 'Cancel creating new custom list');
+
+  return (
+    <SectionTitle {...props}>
+      <SectionTitle.Title>
+        {messages.pgettext('select-location-view', 'Custom lists')}
+      </SectionTitle.Title>
+      <SectionTitle.Divider />
+      <StyledIconButton
+        $active={addFormVisible}
+        onClick={handleOnClick}
+        aria-label={addFormVisible ? cancelAriaLabel : createAriaLabel}>
+        <SectionTitle.IconButton.Icon icon="cross" />
+      </StyledIconButton>
+    </SectionTitle>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/hooks/use-handle-select-custom-list.ts
@@ -1,0 +1,27 @@
+import React from 'react';
+
+import type { RelayLocation } from '../../../../../../../shared/daemon-rpc-types';
+import { useHandleSelectEntryLocation, useHandleSelectExitLocation } from '../../../hooks';
+import { useCustomListListContext } from '../CustomListLocationListContext';
+
+export function useHandleSelectCustomList() {
+  const handleSelectExitRelay = useHandleSelectExitLocation();
+  const handleSelectEntryRelay = useHandleSelectEntryLocation();
+  const { locationSelection } = useCustomListListContext();
+
+  return React.useCallback(
+    async (value: RelayLocation) => {
+      const location = { ...value };
+      if ('country' in location) {
+        // Only the geographical part should be sent to the daemon when setting a location.
+        delete location.customList;
+      }
+      if (locationSelection === 'entry') {
+        await handleSelectEntryRelay(location);
+      } else {
+        await handleSelectExitRelay(location);
+      }
+    },
+    [locationSelection, handleSelectEntryRelay, handleSelectExitRelay],
+  );
+}

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/add-custom-list-form/hooks/use-handle-submit-add-custom-list.ts
@@ -1,0 +1,45 @@
+import React from 'react';
+
+import log from '../../../../../../../shared/logging';
+import { useCustomLists } from '../../../../../../features/location/hooks';
+import { useCustomListLocationListContext } from '../../custom-list-location-list/CustomListLocationListContext';
+import { useAddCustomListFormContext } from '../AddCustomListFormContext';
+
+export function useHandleSubmitAddCustomList() {
+  const { createCustomList: contextCreateCustomList } = useCustomLists();
+  const { hideAddForm } = useCustomListLocationListContext();
+  const {
+    form: {
+      setError,
+      customListTextField: { value, invalid, reset },
+    },
+  } = useAddCustomListFormContext();
+
+  const submitCustomList = React.useCallback(
+    async (name: string) => {
+      try {
+        const result = await contextCreateCustomList(name);
+        if (result) {
+          setError(true);
+        } else {
+          reset();
+          hideAddForm();
+        }
+      } catch (e) {
+        const error = e as Error;
+        log.error('Failed to create list:', error.message);
+      }
+    },
+    [contextCreateCustomList, hideAddForm, reset, setError],
+  );
+
+  return React.useCallback(
+    async (event: React.FormEvent) => {
+      event.preventDefault();
+      if (!invalid) {
+        await submitCustomList(value);
+      }
+    },
+    [invalid, submitCustomList, value],
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationList.tsx
@@ -1,0 +1,94 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../shared/gettext';
+import { Text } from '../../../../../lib/components';
+import { Accordion } from '../../../../../lib/components/accordion';
+import { FlexColumn } from '../../../../../lib/components/flex-column';
+import { spacings } from '../../../../../lib/foundations';
+import { useRelayListContext } from '../../RelayListContext';
+import { AddCustomListForm } from '../add-custom-list-form/AddCustomListForm';
+import { CustomListLocationListItem } from '../custom-list-location-list-item';
+import { CustomListsSectionTitle } from './components';
+import {
+  CustomListLocationListProvider,
+  useCustomListLocationListContext,
+} from './CustomListLocationListContext';
+import { useHandleSelectCustomList, useHasCustomLists } from './hooks';
+import { useHasNoCustomListsInSearchResult } from './hooks/use-has-no-custom-lists-in-search-result';
+
+export type LocationSelection = 'entry' | 'exit';
+
+export type CustomListListProps = {
+  locationSelection: LocationSelection;
+  selectedElementRef: React.Ref<HTMLDivElement>;
+};
+
+const StyledLocationContainer = styled.div`
+  // If the container has children, add spacing between them
+  &:not(:last-child):has(> *) {
+    margin-bottom: ${spacings.small};
+  }
+`;
+
+function CustomListLocationListImpl({
+  selectedElementRef,
+}: Pick<CustomListListProps, 'selectedElementRef'>) {
+  const { customLists } = useRelayListContext();
+  const { addFormVisible } = useCustomListLocationListContext();
+  const handleSelectCustomList = useHandleSelectCustomList();
+  const hasNoCustomListsInSearchResult = useHasNoCustomListsInSearchResult();
+  const hasCustomLists = useHasCustomLists();
+
+  if (hasNoCustomListsInSearchResult) {
+    return null;
+  }
+
+  return (
+    <FlexColumn gap="tiny">
+      <CustomListsSectionTitle />
+
+      <FlexColumn>
+        <Accordion expanded={addFormVisible}>
+          <Accordion.Content>
+            <AddCustomListForm />
+          </Accordion.Content>
+        </Accordion>
+
+        {hasCustomLists && (
+          <FlexColumn>
+            {customLists.map((customList) => {
+              return (
+                <StyledLocationContainer key={Object.values(customList.details).join('-')}>
+                  <CustomListLocationListItem
+                    location={customList}
+                    level={0}
+                    selectedElementRef={selectedElementRef}
+                    onSelect={handleSelectCustomList}
+                  />
+                </StyledLocationContainer>
+              );
+            })}
+          </FlexColumn>
+        )}
+
+        {!hasCustomLists && !addFormVisible && (
+          <Text variant="labelTiny" color="whiteAlpha60">
+            {messages.pgettext(
+              'select-location-view',
+              'Add a custom list by clicking the “+” icon ',
+            )}
+          </Text>
+        )}
+      </FlexColumn>
+    </FlexColumn>
+  );
+}
+
+export function CustomListLocationList({ locationSelection, ...props }: CustomListListProps) {
+  return (
+    <CustomListLocationListProvider locationSelection={locationSelection}>
+      <CustomListLocationListImpl {...props} />
+    </CustomListLocationListProvider>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/CustomListLocationListContext.tsx
@@ -1,0 +1,51 @@
+import React from 'react';
+
+import { useBoolean } from '../../../../../lib/utility-hooks';
+import type { LocationSelection } from './CustomListLocationList';
+
+type CustomListLocationListContextProps = Omit<CustomListLocationListProviderProps, 'children'> & {
+  addFormVisible: boolean;
+  showAddForm: () => void;
+  hideAddForm: () => void;
+};
+
+const CustomListLocationListContext = React.createContext<
+  CustomListLocationListContextProps | undefined
+>(undefined);
+
+export const useCustomListLocationListContext = (): CustomListLocationListContextProps => {
+  const context = React.useContext(CustomListLocationListContext);
+  if (!context) {
+    throw new Error(
+      'useCustomListLocationListContext must be used within a CustomListLocationListProvider',
+    );
+  }
+  return context;
+};
+
+type CustomListLocationListProviderProps = React.PropsWithChildren & {
+  locationSelection: LocationSelection;
+};
+
+export function CustomListLocationListProvider({
+  children,
+  ...props
+}: CustomListLocationListProviderProps) {
+  const [addFormVisible, showAddForm, hideAddForm] = useBoolean(false);
+
+  const value = React.useMemo(
+    () => ({
+      addFormVisible,
+      showAddForm,
+      hideAddForm,
+      ...props,
+    }),
+    [addFormVisible, showAddForm, hideAddForm, props],
+  );
+
+  return (
+    <CustomListLocationListContext.Provider value={value}>
+      {children}
+    </CustomListLocationListContext.Provider>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/components/custom-lists-section-title/CustomListsSectionTitle.tsx
@@ -1,0 +1,37 @@
+import styled from 'styled-components';
+
+import { messages } from '../../../../../../../../shared/gettext';
+import {
+  SectionTitle,
+  type SectionTitleProps,
+} from '../../../../../../../lib/components/section-title';
+import { useCustomListLocationListContext } from '../../CustomListLocationListContext';
+
+export type CustomListsSectionTitleProps = SectionTitleProps;
+
+const StyledIconButton = styled(SectionTitle.IconButton)<{ $active: boolean }>`
+  transform: ${({ $active }) => (!$active ? 'rotate(45deg)' : 'rotate(0deg)')};
+  transition: transform 0.2s ease-in-out;
+`;
+
+export function CustomListsSectionTitle({ ...props }: CustomListsSectionTitleProps) {
+  const { addFormVisible, hideAddForm, showAddForm } = useCustomListLocationListContext();
+  const handleOnClick = addFormVisible ? hideAddForm : showAddForm;
+  const createAriaLabel = messages.pgettext('accessibility', 'Create new custom list');
+  const cancelAriaLabel = messages.pgettext('accessibility', 'Cancel creating new custom list');
+
+  return (
+    <SectionTitle {...props}>
+      <SectionTitle.Title>
+        {messages.pgettext('select-location-view', 'Custom lists')}
+      </SectionTitle.Title>
+      <SectionTitle.Divider />
+      <StyledIconButton
+        $active={addFormVisible}
+        onClick={handleOnClick}
+        aria-label={addFormVisible ? cancelAriaLabel : createAriaLabel}>
+        <SectionTitle.IconButton.Icon icon="cross" />
+      </StyledIconButton>
+    </SectionTitle>
+  );
+}

desktop/packages/mullvad-vpn/src/renderer/components/views/select-location/components/custom-list-location-list/hooks/use-handle-select-custom-list.ts
@@ -1,0 +1,27 @@
+import React from 'react';
+
+import type { RelayLocation } from '../../../../../../../shared/daemon-rpc-types';
+import { useHandleSelectEntryLocation, useHandleSelectExitLocation } from '../../../hooks';
+import { useCustomListLocationListContext } from '../CustomListLocationListContext';
+
+export function useHandleSelectCustomList() {
+  const handleSelectExitRelay = useHandleSelectExitLocation();
+  const handleSelectEntryRelay = useHandleSelectEntryLocation();
+  const { locationSelection } = useCustomListLocationListContext();
+
+  return React.useCallback(
+    async (value: RelayLocation) => {
+      const location = { ...value };
+      if ('country' in location) {
+        // Only the geographical part should be sent to the daemon when setting a location.
+        delete location.customList;
+      }
+      if (locationSelection === 'entry') {
+        await handleSelectEntryRelay(location);
+      } else {
+        await handleSelectExitRelay(location);
+      }
+    },
+    [locationSelection, handleSelectEntryRelay, handleSelectExitRelay],
+  );
+}