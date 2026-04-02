mullvad/mullvadvpn-app

Fix incorrect usage of play purchase payment token
Previously the PlayPurchasePaymentToken type was used when initializing
a google play purchase. This is not correct, as the API returns an
external obfuscated account id.

android/lib/billing/src/main/kotlin/net/mullvad/mullvadvpn/lib/billing/BillingRepository.kt
@@ -28,6 +28,7 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import net.mullvad.mullvadvpn.lib.billing.model.BillingException
 import net.mullvad.mullvadvpn.lib.billing.model.PurchaseEvent
+import net.mullvad.mullvadvpn.lib.model.PlayObfuscatedExternalAccountId
 
 class BillingRepository(context: Context) {
 
@@ -118,13 +119,13 @@ class BillingRepository(context: Context) {
 
     suspend fun startPurchaseFlow(
         productDetails: ProductDetails,
-        obfuscatedId: String,
+        obfuscatedId: PlayObfuscatedExternalAccountId,
         activityProvider: () -> Activity,
     ): BillingResult {
         return try {
             ensureConnected()
 
-            if (obfuscatedId.isEmpty()) {
+            if (obfuscatedId.value.isEmpty()) {
                 Logger.e("Obfuscated id is empty")
                 return BillingResult.newBuilder().setResponseCode(BillingResponseCode.ERROR).build()
             }
@@ -139,7 +140,7 @@ class BillingRepository(context: Context) {
             val billingFlowParams =
                 BillingFlowParams.newBuilder()
                     .setProductDetailsParamsList(productDetailsParamsList)
-                    .setObfuscatedAccountId(obfuscatedId)
+                    .setObfuscatedAccountId(obfuscatedId.value)
                     .build()
 
             val activity = activityProvider()

android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/ManagementService.kt
@@ -93,9 +93,9 @@ import net.mullvad.mullvadvpn.lib.model.NewAccessMethodSetting
 import net.mullvad.mullvadvpn.lib.model.ObfuscationMode
 import net.mullvad.mullvadvpn.lib.model.ObfuscationSettings
 import net.mullvad.mullvadvpn.lib.model.Ownership as ModelOwnership
+import net.mullvad.mullvadvpn.lib.model.PlayObfuscatedExternalAccountId
 import net.mullvad.mullvadvpn.lib.model.PlayPurchase
 import net.mullvad.mullvadvpn.lib.model.PlayPurchaseInitError
-import net.mullvad.mullvadvpn.lib.model.PlayPurchasePaymentToken
 import net.mullvad.mullvadvpn.lib.model.PlayPurchaseVerifyError
 import net.mullvad.mullvadvpn.lib.model.Port
 import net.mullvad.mullvadvpn.lib.model.Providers
@@ -796,7 +796,8 @@ class ManagementService(
                 }
             }
 
-    suspend fun initializePlayPurchase(): Either<PlayPurchaseInitError, PlayPurchasePaymentToken> =
+    suspend fun initializePlayPurchase():
+        Either<PlayPurchaseInitError, PlayObfuscatedExternalAccountId> =
         Either.catch { grpc.initPlayPurchase(Empty.getDefaultInstance()).toDomain() }
             .onLeft { Logger.e("Initialize play purchase error") }
             .mapLeft { PlayPurchaseInitError.OtherError }

android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/ToDomain.kt
@@ -55,7 +55,7 @@ import net.mullvad.mullvadvpn.lib.model.ObfuscationSettings
 import net.mullvad.mullvadvpn.lib.model.ObfuscationType
 import net.mullvad.mullvadvpn.lib.model.Ownership
 import net.mullvad.mullvadvpn.lib.model.ParameterGenerationError
-import net.mullvad.mullvadvpn.lib.model.PlayPurchasePaymentToken
+import net.mullvad.mullvadvpn.lib.model.PlayObfuscatedExternalAccountId
 import net.mullvad.mullvadvpn.lib.model.Port
 import net.mullvad.mullvadvpn.lib.model.PortRange
 import net.mullvad.mullvadvpn.lib.model.ProviderId
@@ -670,8 +670,8 @@ internal fun ManagementInterface.SplitTunnelSettings.toDomain(): SplitTunnelSett
         excludedApps = appsList.map { AppId(it) }.toSet(),
     )
 
-internal fun ManagementInterface.PlayPurchasePaymentToken.toDomain(): PlayPurchasePaymentToken =
-    PlayPurchasePaymentToken(value = token)
+internal fun ManagementInterface.PlayExternalObfuscatedAccountId.toDomain():
+    PlayObfuscatedExternalAccountId = PlayObfuscatedExternalAccountId(value = id)
 
 internal fun ManagementInterface.ApiAccessMethodSettings.toDomain(): List<ApiAccessMethodSetting> =
     buildList {

mullvad-api/src/lib.rs
@@ -5,7 +5,7 @@ use futures::channel::mpsc;
 use hyper::body::Incoming;
 use mullvad_types::account::{AccountData, AccountNumber, VoucherSubmission};
 #[cfg(target_os = "android")]
-use mullvad_types::account::{PlayPurchase, PlayPurchasePaymentToken};
+use mullvad_types::account::{PlayPurchase, PlayExternalObfuscatedAccountId };
 use proxy::{ApiConnectionMode, ConnectionModeProvider};
 use std::{collections::BTreeMap, future::Future, io, net::SocketAddr, path::Path, sync::Arc};
 use talpid_types::ErrorExt;
@@ -642,7 +642,7 @@ impl AccountsProxy {
     pub fn init_play_purchase(
         &mut self,
         account: AccountNumber,
-    ) -> impl Future<Output = Result<PlayPurchasePaymentToken, rest::Error>> + use<> {
+    ) -> impl Future<Output = Result<PlayExternalObfuscatedAccountId, rest::Error>> + use<> {
         #[derive(serde::Deserialize)]
         struct PlayPurchaseInitResponse {
             obfuscated_id: String,

mullvad-daemon/src/lib.rs
@@ -49,7 +49,7 @@ use mullvad_api::{
 use mullvad_encrypted_dns_proxy::state::EncryptedDnsProxyState;
 use mullvad_relay_selector::{RelaySelector, SelectorConfig};
 #[cfg(target_os = "android")]
-use mullvad_types::account::{PlayPurchase, PlayPurchasePaymentToken};
+use mullvad_types::account::{PlayPurchase, PlayExternalObfuscatedAccountId};
 #[cfg(any(target_os = "windows", target_os = "android", target_os = "macos"))]
 use mullvad_types::settings::SplitApp;
 #[cfg(daita)]
@@ -416,7 +416,7 @@ pub enum DaemonCommand {
     BypassSocket(RawFd, oneshot::Sender<()>),
     /// Initialize a google play purchase through the API.
     #[cfg(target_os = "android")]
-    InitPlayPurchase(ResponseTx<PlayPurchasePaymentToken, Error>),
+    InitPlayPurchase(ResponseTx<PlayExternalObfuscatedAccountId, Error>),
     /// Verify that a google play payment was successful through the API.
     #[cfg(target_os = "android")]
     VerifyPlayPurchase(ResponseTx<(), Error>, PlayPurchase),
@@ -3361,7 +3361,7 @@ impl Daemon {
     }
 
     #[cfg(target_os = "android")]
-    fn on_init_play_purchase(&mut self, tx: ResponseTx<PlayPurchasePaymentToken, Error>) {
+    fn on_init_play_purchase(&mut self, tx: ResponseTx<PlayExternalObfuscatedAccountId, Error>) {
         let manager = self.account_manager.clone();
         tokio::spawn(async move {
             Self::oneshot_send(

chrispsyco/vpnvu-desktop

android/lib/billing/src/main/kotlin/net/mullvad/mullvadvpn/lib/billing/BillingRepository.kt
@@ -28,6 +28,7 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import net.mullvad.mullvadvpn.lib.billing.model.BillingException
 import net.mullvad.mullvadvpn.lib.billing.model.PurchaseEvent
+import net.mullvad.mullvadvpn.lib.model.PlayExternalObfuscatedAccountId
 
 class BillingRepository(context: Context) {
 
@@ -118,13 +119,13 @@ class BillingRepository(context: Context) {
 
     suspend fun startPurchaseFlow(
         productDetails: ProductDetails,
-        obfuscatedId: String,
+        obfuscatedId: PlayExternalObfuscatedAccountId,
         activityProvider: () -> Activity,
     ): BillingResult {
         return try {
             ensureConnected()
 
-            if (obfuscatedId.isEmpty()) {
+            if (obfuscatedId.value.isEmpty()) {
                 Logger.e("Obfuscated id is empty")
                 return BillingResult.newBuilder().setResponseCode(BillingResponseCode.ERROR).build()
             }
@@ -139,7 +140,7 @@ class BillingRepository(context: Context) {
             val billingFlowParams =
                 BillingFlowParams.newBuilder()
                     .setProductDetailsParamsList(productDetailsParamsList)
-                    .setObfuscatedAccountId(obfuscatedId)
+                    .setObfuscatedAccountId(obfuscatedId.value)
                     .build()
 
             val activity = activityProvider()

android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/ManagementService.kt
@@ -93,9 +93,9 @@ import net.mullvad.mullvadvpn.lib.model.NewAccessMethodSetting
 import net.mullvad.mullvadvpn.lib.model.ObfuscationMode
 import net.mullvad.mullvadvpn.lib.model.ObfuscationSettings
 import net.mullvad.mullvadvpn.lib.model.Ownership as ModelOwnership
+import net.mullvad.mullvadvpn.lib.model.PlayExternalObfuscatedAccountId
 import net.mullvad.mullvadvpn.lib.model.PlayPurchase
 import net.mullvad.mullvadvpn.lib.model.PlayPurchaseInitError
-import net.mullvad.mullvadvpn.lib.model.PlayPurchasePaymentToken
 import net.mullvad.mullvadvpn.lib.model.PlayPurchaseVerifyError
 import net.mullvad.mullvadvpn.lib.model.Port
 import net.mullvad.mullvadvpn.lib.model.Providers
@@ -796,7 +796,8 @@ class ManagementService(
                 }
             }
 
-    suspend fun initializePlayPurchase(): Either<PlayPurchaseInitError, PlayPurchasePaymentToken> =
+    suspend fun initializePlayPurchase():
+        Either<PlayPurchaseInitError, PlayExternalObfuscatedAccountId> =
         Either.catch { grpc.initPlayPurchase(Empty.getDefaultInstance()).toDomain() }
             .onLeft { Logger.e("Initialize play purchase error") }
             .mapLeft { PlayPurchaseInitError.OtherError }

android/lib/grpc/src/main/kotlin/net/mullvad/mullvadvpn/lib/grpc/mapper/ToDomain.kt
@@ -55,7 +55,7 @@ import net.mullvad.mullvadvpn.lib.model.ObfuscationSettings
 import net.mullvad.mullvadvpn.lib.model.ObfuscationType
 import net.mullvad.mullvadvpn.lib.model.Ownership
 import net.mullvad.mullvadvpn.lib.model.ParameterGenerationError
-import net.mullvad.mullvadvpn.lib.model.PlayPurchasePaymentToken
+import net.mullvad.mullvadvpn.lib.model.PlayExternalObfuscatedAccountId
 import net.mullvad.mullvadvpn.lib.model.Port
 import net.mullvad.mullvadvpn.lib.model.PortRange
 import net.mullvad.mullvadvpn.lib.model.ProviderId
@@ -670,8 +670,8 @@ internal fun ManagementInterface.SplitTunnelSettings.toDomain(): SplitTunnelSett
         excludedApps = appsList.map { AppId(it) }.toSet(),
     )
 
-internal fun ManagementInterface.PlayPurchasePaymentToken.toDomain(): PlayPurchasePaymentToken =
-    PlayPurchasePaymentToken(value = token)
+internal fun ManagementInterface.PlayExternalObfuscatedAccountId.toDomain():
+    PlayExternalObfuscatedAccountId = PlayExternalObfuscatedAccountId(value = id)
 
 internal fun ManagementInterface.ApiAccessMethodSettings.toDomain(): List<ApiAccessMethodSetting> =
     buildList {

mullvad-api/src/lib.rs
@@ -5,7 +5,7 @@ use futures::channel::mpsc;
 use hyper::body::Incoming;
 use mullvad_types::account::{AccountData, AccountNumber, VoucherSubmission};
 #[cfg(target_os = "android")]
-use mullvad_types::account::{PlayPurchase, PlayPurchasePaymentToken};
+use mullvad_types::account::{PlayExternalObfuscatedAccountId, PlayPurchase};
 use proxy::{ApiConnectionMode, ConnectionModeProvider};
 use std::{collections::BTreeMap, future::Future, io, net::SocketAddr, path::Path, sync::Arc};
 use talpid_types::ErrorExt;
@@ -642,7 +642,7 @@ impl AccountsProxy {
     pub fn init_play_purchase(
         &mut self,
         account: AccountNumber,
-    ) -> impl Future<Output = Result<PlayPurchasePaymentToken, rest::Error>> + use<> {
+    ) -> impl Future<Output = Result<PlayExternalObfuscatedAccountId, rest::Error>> + use<> {
         #[derive(serde::Deserialize)]
         struct PlayPurchaseInitResponse {
             obfuscated_id: String,

mullvad-daemon/src/lib.rs
@@ -49,7 +49,7 @@ use mullvad_api::{
 use mullvad_encrypted_dns_proxy::state::EncryptedDnsProxyState;
 use mullvad_relay_selector::{Config, RelaySelector};
 #[cfg(target_os = "android")]
-use mullvad_types::account::{PlayPurchase, PlayPurchasePaymentToken};
+use mullvad_types::account::{PlayExternalObfuscatedAccountId, PlayPurchase};
 #[cfg(any(target_os = "windows", target_os = "android", target_os = "macos"))]
 use mullvad_types::settings::SplitApp;
 #[cfg(daita)]
@@ -418,7 +418,7 @@ pub enum DaemonCommand {
     BypassSocket(RawFd, oneshot::Sender<()>),
     /// Initialize a google play purchase through the API.
     #[cfg(target_os = "android")]
-    InitPlayPurchase(ResponseTx<PlayPurchasePaymentToken, Error>),
+    InitPlayPurchase(ResponseTx<PlayExternalObfuscatedAccountId, Error>),
     /// Verify that a google play payment was successful through the API.
     #[cfg(target_os = "android")]
     VerifyPlayPurchase(ResponseTx<(), Error>, PlayPurchase),
@@ -3392,7 +3392,7 @@ impl Daemon {
     }
 
     #[cfg(target_os = "android")]
-    fn on_init_play_purchase(&mut self, tx: ResponseTx<PlayPurchasePaymentToken, Error>) {
+    fn on_init_play_purchase(&mut self, tx: ResponseTx<PlayExternalObfuscatedAccountId, Error>) {
         let manager = self.account_manager.clone();
         tokio::spawn(async move {
             Self::oneshot_send(