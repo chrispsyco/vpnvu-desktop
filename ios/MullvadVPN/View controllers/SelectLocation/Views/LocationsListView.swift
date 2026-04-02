mullvad/mullvadvpn-app

Create automatic location view

ios/MullvadVPN/View controllers/SelectLocation/Views/LocationsListView.swift
@@ -47,4 +47,7 @@ struct LocationsListView<ContextMenu>: View where ContextMenu: View {
         }
     }
     .background(Color.mullvadBackground)
+    .onAppear {
+        viewModel.exitContext.customLists.insert(AutomaticLocationNode(), at: 0)
+    }
 }

chrispsyco/vpnvu-desktop

ios/MullvadVPN/View controllers/SelectLocation/Views/LocationsListView.swift
@@ -47,4 +47,7 @@ struct LocationsListView<ContextMenu>: View where ContextMenu: View {
         }
     }
     .background(Color.mullvadBackground)
+    .onAppear {
+        viewModel.exitContext.recents.insert(AutomaticLocationNode(), at: 0)
+    }
 }