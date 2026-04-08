mullvad/mullvadvpn-app

Use Nav3 for bottom sheet navigation

android/lib/feature/location/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/location/impl/bottomsheet/LocationBottomSheetViewModel.kt
@@ -4,21 +4,31 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.touchlab.kermit.Logger
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.WhileSubscribed
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
+import net.mullvad.mullvadvpn.feature.location.api.LocationBottomSheetState
 import net.mullvad.mullvadvpn.feature.location.impl.UndoChangeMultihopAction
+import net.mullvad.mullvadvpn.feature.location.impl.addLocationToCustomList
+import net.mullvad.mullvadvpn.feature.location.impl.removeLocationFromCustomList
 import net.mullvad.mullvadvpn.lib.common.Lc
 import net.mullvad.mullvadvpn.lib.common.constant.VIEW_MODEL_STOP_TIMEOUT
 import net.mullvad.mullvadvpn.lib.common.util.relaylist.withDescendants
+import net.mullvad.mullvadvpn.lib.model.CustomListId
 import net.mullvad.mullvadvpn.lib.model.CustomListName
 import net.mullvad.mullvadvpn.lib.model.GeoLocationId
 import net.mullvad.mullvadvpn.lib.model.RelayItem
+import net.mullvad.mullvadvpn.lib.model.communication.CustomListAction
+import net.mullvad.mullvadvpn.lib.model.communication.CustomListActionResultData
+import net.mullvad.mullvadvpn.lib.repository.CustomListsRepository
+import net.mullvad.mullvadvpn.lib.repository.RelayListRepository
 import net.mullvad.mullvadvpn.lib.repository.WireguardConstraintsRepository
 import net.mullvad.mullvadvpn.lib.usecase.HopSelectionUseCase
 import net.mullvad.mullvadvpn.lib.usecase.ModifyAndEnableMultihopUseCase
@@ -29,10 +39,15 @@ import net.mullvad.mullvadvpn.lib.usecase.RelayItemCanBeSelectedUseCase
 import net.mullvad.mullvadvpn.lib.usecase.SelectAndEnableMultihopUseCase
 import net.mullvad.mullvadvpn.lib.usecase.SelectRelayItemError
 import net.mullvad.mullvadvpn.lib.usecase.SelectedLocationUseCase
+import net.mullvad.mullvadvpn.lib.usecase.customlists.CustomListActionUseCase
 import net.mullvad.mullvadvpn.lib.usecase.customlists.CustomListsRelayItemUseCase
 
+@Suppress("TooManyFunctions", "LongParameterList")
 class LocationBottomSheetViewModel(
     private val locationBottomSheetState: LocationBottomSheetState,
+    private val customListActionUseCase: CustomListActionUseCase,
+    private val customListsRepository: CustomListsRepository,
+    private val relayListRepository: RelayListRepository,
     private val hopSelectionUseCase: HopSelectionUseCase,
     private val modifyMultihopUseCase: ModifyMultihopUseCase,
     private val modifyAndEnableMultihopUseCase: ModifyAndEnableMultihopUseCase,
@@ -115,6 +130,9 @@ class LocationBottomSheetViewModel(
                 started = SharingStarted.WhileSubscribed(VIEW_MODEL_STOP_TIMEOUT),
                 Lc.Loading(Unit),
             )
+
+    private val _uiSideEffect = Channel<LocationBottomSheetSideEffect>()
+    val uiSideEffect = _uiSideEffect.receiveAsFlow()
 
     fun setAsEntry(
         item: RelayItem,
@@ -202,6 +220,107 @@ class LocationBottomSheetViewModel(
         }
     }
 
+    fun performAction(action: CustomListAction) {
+        viewModelScope.launch { customListActionUseCase(action) }
+    }
+
+    fun addLocationToList(item: RelayItem.Location, customList: RelayItem.CustomList) {
+        viewModelScope.launch {
+            val result =
+                addLocationToCustomList(
+                    item = item,
+                    customList = customList,
+                    update = customListActionUseCase::invoke,
+                )
+            _uiSideEffect.send(LocationBottomSheetSideEffect.CustomListActionToast(result))
+        }
+    }
+
+    fun removeLocationFromList(item: RelayItem.Location, customListId: CustomListId) {
+        viewModelScope.launch {
+            val result =
+                removeLocationFromCustomList(
+                    item = item,
+                    customListId = customListId,
+                    getCustomListById = customListsRepository::getCustomListById,
+                    update = customListActionUseCase::invoke,
+                )
+            _uiSideEffect.trySend(LocationBottomSheetSideEffect.CustomListActionToast(result))
+        }
+    }
+
+    fun onModifyMultihopError(
+        modifyMultihopError: ModifyMultihopError,
+        multihopChange: MultihopChange,
+    ) {
+        viewModelScope.launch {
+            _uiSideEffect.send(modifyMultihopError.toSideEffect(multihopChange))
+        }
+    }
+
+    fun onSelectRelayItemError(selectRelayItemError: SelectRelayItemError) {
+        viewModelScope.launch { _uiSideEffect.send(selectRelayItemError.toSideEffect()) }
+    }
+
+    fun onMultihopChanged(undoChangeMultihopAction: UndoChangeMultihopAction) {
+        viewModelScope.launch {
+            _uiSideEffect.send(
+                LocationBottomSheetSideEffect.MultihopChanged(undoChangeMultihopAction)
+            )
+        }
+    }
+
+    fun undoMultihopAction(undoChangeMultihopAction: UndoChangeMultihopAction) {
+        viewModelScope.launch {
+            when (undoChangeMultihopAction) {
+                UndoChangeMultihopAction.Enable ->
+                    wireguardConstraintsRepository.setMultihop(true).onLeft {
+                        _uiSideEffect.send(LocationBottomSheetSideEffect.GenericError)
+                    }
+                UndoChangeMultihopAction.Disable ->
+                    wireguardConstraintsRepository.setMultihop(false).onLeft {
+                        _uiSideEffect.send(LocationBottomSheetSideEffect.GenericError)
+                    }
+                is UndoChangeMultihopAction.DisableAndSetEntry ->
+                    wireguardConstraintsRepository
+                        .setMultihopAndEntryLocation(false, undoChangeMultihopAction.relayItemId)
+                        .onLeft { _uiSideEffect.send(LocationBottomSheetSideEffect.GenericError) }
+                is UndoChangeMultihopAction.DisableAndSetExit ->
+                    relayListRepository
+                        .updateExitRelayLocationMultihop(
+                            false,
+                            undoChangeMultihopAction.relayItemId,
+                        )
+                        .onLeft { _uiSideEffect.send(LocationBottomSheetSideEffect.GenericError) }
+            }
+        }
+    }
+
+    private fun ModifyMultihopError.toSideEffect(
+        multihopChange: MultihopChange
+    ): LocationBottomSheetSideEffect =
+        when (this) {
+            is ModifyMultihopError.EntrySameAsExit ->
+                when (multihopChange) {
+                    is MultihopChange.Entry ->
+                        LocationBottomSheetSideEffect.ExitAlreadySelected(relayItem = relayItem)
+                    is MultihopChange.Exit ->
+                        LocationBottomSheetSideEffect.EntryAlreadySelected(relayItem = relayItem)
+                }
+            ModifyMultihopError.GenericError -> LocationBottomSheetSideEffect.GenericError
+            is ModifyMultihopError.RelayItemInactive ->
+                LocationBottomSheetSideEffect.RelayItemInactive(relayItem = relayItem)
+        }
+
+    private fun SelectRelayItemError.toSideEffect(): LocationBottomSheetSideEffect =
+        when (this) {
+            SelectRelayItemError.GenericError -> LocationBottomSheetSideEffect.GenericError
+            is SelectRelayItemError.RelayInactive ->
+                LocationBottomSheetSideEffect.RelayItemInactive(relayItem = relayItem)
+            SelectRelayItemError.EntryAndExitSame ->
+                LocationBottomSheetSideEffect.EntryAndExitAreSame
+        }
+
     private fun isMultihopEnabled() =
         wireguardConstraintsRepository.wireguardConstraints.value?.isMultihopEnabled ?: false
 
@@ -217,4 +336,23 @@ class LocationBottomSheetViewModel(
         } else {
             SetAsState.DISABLED
         }
+}
+
+sealed interface LocationBottomSheetSideEffect {
+
+    data class CustomListActionToast(val resultData: CustomListActionResultData) :
+        LocationBottomSheetSideEffect
+
+    data object GenericError : LocationBottomSheetSideEffect
+
+    data class RelayItemInactive(val relayItem: RelayItem) : LocationBottomSheetSideEffect
+
+    data class EntryAlreadySelected(val relayItem: RelayItem) : LocationBottomSheetSideEffect
+
+    data class ExitAlreadySelected(val relayItem: RelayItem) : LocationBottomSheetSideEffect
+
+    data object EntryAndExitAreSame : LocationBottomSheetSideEffect
+
+    data class MultihopChanged(val undoChangeMultihopAction: UndoChangeMultihopAction) :
+        LocationBottomSheetSideEffect
 }

android/lib/feature/serveripoverride/impl/src/main/java/net/mullvad/mullvadvpn/feature/serveripoverride/impl/ImportServerIpOverridesBottomSheet.kt
@@ -1,0 +1,138 @@
+package net.mullvad.mullvadvpn.feature.serveripoverride.impl
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.TextFields
+import androidx.compose.material.icons.rounded.UploadFile
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.lifecycle.compose.dropUnlessResumed
+import net.mullvad.mullvadvpn.common.compose.closeBottomSheet
+import net.mullvad.mullvadvpn.core.NavKey2
+import net.mullvad.mullvadvpn.core.Navigator
+import net.mullvad.mullvadvpn.feature.serveripoverride.api.ImportOverrideByFileNavResult
+import net.mullvad.mullvadvpn.feature.serveripoverride.api.ImportOverrideByTextNavKey
+import net.mullvad.mullvadvpn.lib.ui.component.MullvadModalBottomSheet
+import net.mullvad.mullvadvpn.lib.ui.component.listitem.BottomSheetListItem
+import net.mullvad.mullvadvpn.lib.ui.component.listitem.IconListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.ListItemDefaults
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+import net.mullvad.mullvadvpn.lib.ui.resource.R
+import net.mullvad.mullvadvpn.lib.ui.tag.SERVER_IP_OVERRIDES_IMPORT_BY_FILE_TEST_TAG
+import net.mullvad.mullvadvpn.lib.ui.tag.SERVER_IP_OVERRIDES_IMPORT_BY_TEXT_TEST_TAG
+import net.mullvad.mullvadvpn.lib.ui.theme.Dimens
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ImportOverridesBottomSheet(navigator: Navigator, overridesActive: Boolean) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
+
+    val backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainer
+    val onBackgroundColor: Color = MaterialTheme.colorScheme.onSurface
+
+    fun closeBottomSheet(animate: Boolean, goTo: NavKey2? = null) =
+        closeBottomSheet(
+            animate = animate,
+            goTo = goTo,
+            sheetState = sheetState,
+            scope = scope,
+            navigator = navigator,
+        )
+
+    MullvadModalBottomSheet(
+        sheetState = sheetState,
+        backgroundColor = backgroundColor,
+        onBackgroundColor = onBackgroundColor,
+        onDismissRequest = { closeBottomSheet(animate = false) },
+    ) {
+        BottomSheetContent(
+            backgroundColor = backgroundColor,
+            onBackgroundColor = onBackgroundColor,
+            overridesActive = overridesActive,
+            onImportByFile =
+                dropUnlessResumed { navigator.goBack(result = ImportOverrideByFileNavResult) },
+            onImportByText =
+                dropUnlessResumed {
+                    closeBottomSheet(animate = true, goTo = ImportOverrideByTextNavKey)
+                },
+        )
+    }
+}
+
+@Composable
+private fun BottomSheetContent(
+    backgroundColor: Color,
+    onBackgroundColor: Color,
+    overridesActive: Boolean,
+    onImportByFile: () -> Unit,
+    onImportByText: () -> Unit,
+) {
+    BottomSheetListItem(
+        title = stringResource(id = R.string.server_ip_overrides_import_by),
+        backgroundColor = backgroundColor,
+        onBackgroundColor = onBackgroundColor,
+    )
+    HorizontalDivider(color = onBackgroundColor)
+    IconListItem(
+        leadingIcon = Icons.Rounded.UploadFile,
+        title = stringResource(id = R.string.server_ip_overrides_import_by_file),
+        modifier = Modifier.testTag(SERVER_IP_OVERRIDES_IMPORT_BY_FILE_TEST_TAG),
+        position = Position.Middle,
+        onClick = { onImportByFile() },
+        colors =
+            ListItemDefaults.colors(
+                containerColorParent = backgroundColor,
+                headlineColor = onBackgroundColor,
+            ),
+    )
+    IconListItem(
+        leadingIcon = Icons.Rounded.TextFields,
+        title = stringResource(id = R.string.server_ip_overrides_import_by_text),
+        position = Position.Middle,
+        modifier = Modifier.testTag(SERVER_IP_OVERRIDES_IMPORT_BY_TEXT_TEST_TAG),
+        onClick = { onImportByText() },
+        colors =
+            ListItemDefaults.colors(
+                containerColorParent = backgroundColor,
+                headlineColor = onBackgroundColor,
+            ),
+    )
+    if (overridesActive) {
+        HorizontalDivider(color = onBackgroundColor)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.padding(Dimens.mediumPadding),
+                imageVector = Icons.Rounded.Info,
+                tint = MaterialTheme.colorScheme.error,
+                contentDescription = null,
+            )
+            Text(
+                modifier =
+                    Modifier.padding(
+                        top = Dimens.smallPadding,
+                        end = Dimens.mediumPadding,
+                        bottom = Dimens.smallPadding,
+                    ),
+                text = stringResource(R.string.import_overrides_bottom_sheet_override_warning),
+                maxLines = 2,
+                style = MaterialTheme.typography.labelLarge,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}

chrispsyco/vpnvu-desktop

android/lib/feature/location/impl/src/main/kotlin/net/mullvad/mullvadvpn/feature/location/impl/bottomsheet/LocationBottomSheetViewModel.kt
@@ -4,21 +4,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import co.touchlab.kermit.Logger
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.WhileSubscribed
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
-import net.mullvad.mullvadvpn.feature.location.impl.UndoChangeMultihopAction
+import net.mullvad.mullvadvpn.feature.location.api.LocationBottomSheetNavResult
+import net.mullvad.mullvadvpn.feature.location.api.LocationBottomSheetState
+import net.mullvad.mullvadvpn.feature.location.api.UndoChangeMultihopAction
+import net.mullvad.mullvadvpn.feature.location.impl.addLocationToCustomList
+import net.mullvad.mullvadvpn.feature.location.impl.removeLocationFromCustomList
 import net.mullvad.mullvadvpn.lib.common.Lc
 import net.mullvad.mullvadvpn.lib.common.constant.VIEW_MODEL_STOP_TIMEOUT
 import net.mullvad.mullvadvpn.lib.common.util.relaylist.withDescendants
+import net.mullvad.mullvadvpn.lib.model.CustomListId
 import net.mullvad.mullvadvpn.lib.model.CustomListName
 import net.mullvad.mullvadvpn.lib.model.GeoLocationId
 import net.mullvad.mullvadvpn.lib.model.RelayItem
+import net.mullvad.mullvadvpn.lib.repository.CustomListsRepository
 import net.mullvad.mullvadvpn.lib.repository.WireguardConstraintsRepository
 import net.mullvad.mullvadvpn.lib.usecase.HopSelectionUseCase
 import net.mullvad.mullvadvpn.lib.usecase.ModifyAndEnableMultihopUseCase
@@ -29,10 +37,14 @@ import net.mullvad.mullvadvpn.lib.usecase.RelayItemCanBeSelectedUseCase
 import net.mullvad.mullvadvpn.lib.usecase.SelectAndEnableMultihopUseCase
 import net.mullvad.mullvadvpn.lib.usecase.SelectRelayItemError
 import net.mullvad.mullvadvpn.lib.usecase.SelectedLocationUseCase
+import net.mullvad.mullvadvpn.lib.usecase.customlists.CustomListActionUseCase
 import net.mullvad.mullvadvpn.lib.usecase.customlists.CustomListsRelayItemUseCase
 
+@Suppress("TooManyFunctions", "LongParameterList")
 class LocationBottomSheetViewModel(
     private val locationBottomSheetState: LocationBottomSheetState,
+    private val customListActionUseCase: CustomListActionUseCase,
+    private val customListsRepository: CustomListsRepository,
     private val hopSelectionUseCase: HopSelectionUseCase,
     private val modifyMultihopUseCase: ModifyMultihopUseCase,
     private val modifyAndEnableMultihopUseCase: ModifyAndEnableMultihopUseCase,
@@ -115,6 +127,9 @@ class LocationBottomSheetViewModel(
                 started = SharingStarted.WhileSubscribed(VIEW_MODEL_STOP_TIMEOUT),
                 Lc.Loading(Unit),
             )
+
+    private val _uiSideEffect = Channel<LocationBottomSheetNavResult>()
+    val uiSideEffect = _uiSideEffect.receiveAsFlow()
 
     fun setAsEntry(
         item: RelayItem,
@@ -201,6 +216,77 @@ class LocationBottomSheetViewModel(
                 )
         }
     }
+
+    fun addLocationToList(item: RelayItem.Location, customList: RelayItem.CustomList) {
+        viewModelScope.launch {
+            val result =
+                addLocationToCustomList(
+                    item = item,
+                    customList = customList,
+                    update = customListActionUseCase::invoke,
+                )
+            _uiSideEffect.send(LocationBottomSheetNavResult.CustomListActionToast(result))
+        }
+    }
+
+    fun removeLocationFromList(item: RelayItem.Location, customListId: CustomListId) {
+        viewModelScope.launch {
+            val result =
+                removeLocationFromCustomList(
+                    item = item,
+                    customListId = customListId,
+                    getCustomListById = customListsRepository::getCustomListById,
+                    update = customListActionUseCase::invoke,
+                )
+            _uiSideEffect.trySend(LocationBottomSheetNavResult.CustomListActionToast(result))
+        }
+    }
+
+    fun onModifyMultihopError(
+        modifyMultihopError: ModifyMultihopError,
+        multihopChange: MultihopChange,
+    ) {
+        viewModelScope.launch {
+            _uiSideEffect.send(modifyMultihopError.toSideEffect(multihopChange))
+        }
+    }
+
+    fun onSelectRelayItemError(selectRelayItemError: SelectRelayItemError) {
+        viewModelScope.launch { _uiSideEffect.send(selectRelayItemError.toSideEffect()) }
+    }
+
+    fun onMultihopChanged(undoChangeMultihopAction: UndoChangeMultihopAction) {
+        viewModelScope.launch {
+            _uiSideEffect.send(
+                LocationBottomSheetNavResult.MultihopChanged(undoChangeMultihopAction)
+            )
+        }
+    }
+
+    private fun ModifyMultihopError.toSideEffect(
+        multihopChange: MultihopChange
+    ): LocationBottomSheetNavResult =
+        when (this) {
+            is ModifyMultihopError.EntrySameAsExit ->
+                when (multihopChange) {
+                    is MultihopChange.Entry ->
+                        LocationBottomSheetNavResult.ExitAlreadySelected(relayItem = relayItem)
+                    is MultihopChange.Exit ->
+                        LocationBottomSheetNavResult.EntryAlreadySelected(relayItem = relayItem)
+                }
+            ModifyMultihopError.GenericError -> LocationBottomSheetNavResult.GenericError
+            is ModifyMultihopError.RelayItemInactive ->
+                LocationBottomSheetNavResult.RelayItemInactive(relayItem = relayItem)
+        }
+
+    private fun SelectRelayItemError.toSideEffect(): LocationBottomSheetNavResult =
+        when (this) {
+            SelectRelayItemError.GenericError -> LocationBottomSheetNavResult.GenericError
+            is SelectRelayItemError.RelayInactive ->
+                LocationBottomSheetNavResult.RelayItemInactive(relayItem = relayItem)
+            SelectRelayItemError.EntryAndExitSame ->
+                LocationBottomSheetNavResult.EntryAndExitAreSame
+        }
 
     private fun isMultihopEnabled() =
         wireguardConstraintsRepository.wireguardConstraints.value?.isMultihopEnabled ?: false

android/lib/feature/serveripoverride/impl/src/main/java/net/mullvad/mullvadvpn/feature/serveripoverride/impl/ImportServerIpOverridesBottomSheet.kt
@@ -1,0 +1,130 @@
+package net.mullvad.mullvadvpn.feature.serveripoverride.impl
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.TextFields
+import androidx.compose.material.icons.rounded.UploadFile
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.lifecycle.compose.dropUnlessResumed
+import net.mullvad.mullvadvpn.common.compose.animateClose
+import net.mullvad.mullvadvpn.core.Navigator
+import net.mullvad.mullvadvpn.feature.serveripoverride.api.ImportOverrideByFileNavResult
+import net.mullvad.mullvadvpn.feature.serveripoverride.api.ImportOverrideByTextNavKey
+import net.mullvad.mullvadvpn.lib.ui.component.MullvadModalBottomSheet
+import net.mullvad.mullvadvpn.lib.ui.component.listitem.BottomSheetListItem
+import net.mullvad.mullvadvpn.lib.ui.component.listitem.IconListItem
+import net.mullvad.mullvadvpn.lib.ui.designsystem.ListItemDefaults
+import net.mullvad.mullvadvpn.lib.ui.designsystem.Position
+import net.mullvad.mullvadvpn.lib.ui.resource.R
+import net.mullvad.mullvadvpn.lib.ui.tag.SERVER_IP_OVERRIDES_IMPORT_BY_FILE_TEST_TAG
+import net.mullvad.mullvadvpn.lib.ui.tag.SERVER_IP_OVERRIDES_IMPORT_BY_TEXT_TEST_TAG
+import net.mullvad.mullvadvpn.lib.ui.theme.Dimens
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ImportOverridesBottomSheet(navigator: Navigator, overridesActive: Boolean) {
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
+
+    val backgroundColor: Color = MaterialTheme.colorScheme.surfaceContainer
+    val onBackgroundColor: Color = MaterialTheme.colorScheme.onSurface
+
+    MullvadModalBottomSheet(
+        sheetState = sheetState,
+        backgroundColor = backgroundColor,
+        onBackgroundColor = onBackgroundColor,
+        onDismissRequest = { navigator.goBack() },
+    ) {
+        BottomSheetContent(
+            backgroundColor = backgroundColor,
+            onBackgroundColor = onBackgroundColor,
+            overridesActive = overridesActive,
+            onImportByFile =
+                dropUnlessResumed { navigator.goBack(result = ImportOverrideByFileNavResult) },
+            onImportByText =
+                dropUnlessResumed {
+                    sheetState.animateClose(scope) {
+                        navigator.navigateReplaceTop(ImportOverrideByTextNavKey)
+                    }
+                },
+        )
+    }
+}
+
+@Composable
+private fun BottomSheetContent(
+    backgroundColor: Color,
+    onBackgroundColor: Color,
+    overridesActive: Boolean,
+    onImportByFile: () -> Unit,
+    onImportByText: () -> Unit,
+) {
+    BottomSheetListItem(
+        title = stringResource(id = R.string.server_ip_overrides_import_by),
+        backgroundColor = backgroundColor,
+        onBackgroundColor = onBackgroundColor,
+    )
+    HorizontalDivider(color = onBackgroundColor)
+    IconListItem(
+        leadingIcon = Icons.Rounded.UploadFile,
+        title = stringResource(id = R.string.server_ip_overrides_import_by_file),
+        modifier = Modifier.testTag(SERVER_IP_OVERRIDES_IMPORT_BY_FILE_TEST_TAG),
+        position = Position.Middle,
+        onClick = { onImportByFile() },
+        colors =
+            ListItemDefaults.colors(
+                containerColorParent = backgroundColor,
+                headlineColor = onBackgroundColor,
+            ),
+    )
+    IconListItem(
+        leadingIcon = Icons.Rounded.TextFields,
+        title = stringResource(id = R.string.server_ip_overrides_import_by_text),
+        position = Position.Middle,
+        modifier = Modifier.testTag(SERVER_IP_OVERRIDES_IMPORT_BY_TEXT_TEST_TAG),
+        onClick = { onImportByText() },
+        colors =
+            ListItemDefaults.colors(
+                containerColorParent = backgroundColor,
+                headlineColor = onBackgroundColor,
+            ),
+    )
+    if (overridesActive) {
+        HorizontalDivider(color = onBackgroundColor)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.padding(Dimens.mediumPadding),
+                imageVector = Icons.Rounded.Info,
+                tint = MaterialTheme.colorScheme.error,
+                contentDescription = null,
+            )
+            Text(
+                modifier =
+                    Modifier.padding(
+                        top = Dimens.smallPadding,
+                        end = Dimens.mediumPadding,
+                        bottom = Dimens.smallPadding,
+                    ),
+                text = stringResource(R.string.import_overrides_bottom_sheet_override_warning),
+                maxLines = 2,
+                style = MaterialTheme.typography.labelLarge,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}