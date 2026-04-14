mullvad/mullvadvpn-app

Add ability to set LWO port
This mirrors the existing WireGuard port setting

desktop/packages/mullvad-vpn/src/renderer/components/views/index.ts
@@ -30,3 +30,4 @@ export * from './vpn-settings';
 export * from './user-interface-settings';
 export * from './anti-censorship';
 export * from './wireguard-port';
+export * from './lwo-port';

desktop/packages/mullvad-vpn/test/e2e/route-object-models/anti-censorship/anti-censorship-route-object-model.ts
@@ -19,6 +19,11 @@ export class AntiCensorshipRouteObjectModel extends NavigationObjectModel {
     await this.utils.expectRoute(RoutePath.wireguardPort);
   }
 
+  async gotoLwoPort() {
+    await this.selectors.lwoPortButton().click();
+    await this.utils.expectRoute(RoutePath.lwoPort);
+  }
+
   async gotoUdpOverTcpSettings() {
     await this.selectors.udpOverTcpSettingsButton().click();
     await this.utils.expectRoute(RoutePath.udpOverTcp);

desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
@@ -9,6 +9,7 @@ import { ExpiredRouteObjectModel } from './expired';
 import { FilterRouteObjectModel } from './filter';
 import { LaunchRouteObjectModel } from './launch';
 import { LoginRouteObjectModel } from './login';
+import { LwoPortRouteObjectModel } from './lwo-port';
 import { MainRouteObjectModel } from './main';
 import { ManageDevicesRouteObjectModel } from './manage-devices';
 import { MultihopSettingsRouteObjectModel } from './multihop-settings';
@@ -53,6 +54,7 @@ export class RoutesObjectModel {
   readonly account: AccountRouteObjectModel;
   readonly manageDevices: ManageDevicesRouteObjectModel;
   readonly wireguardPort: WireGuardPortRouteObjectModel;
+  readonly lwoPort: LwoPortRouteObjectModel;
 
   constructor(page: Page, utils: TestUtils) {
     this.selectLanguage = new SelectLanguageRouteObjectModel(page, utils);
@@ -80,5 +82,6 @@ export class RoutesObjectModel {
     this.account = new AccountRouteObjectModel(page, utils);
     this.manageDevices = new ManageDevicesRouteObjectModel(page, utils);
     this.wireguardPort = new WireGuardPortRouteObjectModel(page, utils);
+    this.lwoPort = new LwoPortRouteObjectModel(page, utils);
   }
 }

mullvad-management-interface/src/types/conversions/relay_constraints.rs
@@ -150,6 +150,7 @@ impl From<&mullvad_types::relay_constraints::ObfuscationSettings> for proto::Obf
             wireguard_port: Some(proto::obfuscation_settings::WireguardPort::from(
                 &settings.wireguard_port,
             )),
+            lwo: Some(proto::obfuscation_settings::Lwo::from(&settings.lwo)),
         }
     }
 }
@@ -186,6 +187,14 @@ impl From<&mullvad_types::relay_constraints::WireguardPortSettings>
     fn from(port: &mullvad_types::relay_constraints::WireguardPortSettings) -> Self {
         Self {
             port: port.get().map(u32::from).option(),
+        }
+    }
+}
+
+impl From<&mullvad_types::relay_constraints::LwoSettings> for proto::obfuscation_settings::Lwo {
+    fn from(settings: &mullvad_types::relay_constraints::LwoSettings) -> Self {
+        Self {
+            port: settings.port.map(u32::from).option(),
         }
     }
 }
@@ -397,11 +406,21 @@ impl TryFrom<proto::ObfuscationSettings> for mullvad_types::relay_constraints::O
             }
         };
 
+        let lwo = match settings.lwo {
+            Some(s) => mullvad_types::relay_constraints::LwoSettings::try_from(&s)?,
+            None => {
+                return Err(FromProtobufTypeError::InvalidArgument(
+                    "invalid LWO settings",
+                ));
+            }
+        };
+
         Ok(Self {
             selected_obfuscation,
             udp2tcp,
             shadowsocks,
             wireguard_port,
+            lwo,
         })
     }
 }
@@ -442,6 +461,16 @@ impl TryFrom<&proto::obfuscation_settings::WireguardPort>
     ) -> Result<Self, Self::Error> {
         let port = settings.port.map(|port| port as u16);
         Ok(Self::from(port))
+    }
+}
+
+impl TryFrom<&proto::obfuscation_settings::Lwo> for mullvad_types::relay_constraints::LwoSettings {
+    type Error = FromProtobufTypeError;
+
+    fn try_from(settings: &proto::obfuscation_settings::Lwo) -> Result<Self, Self::Error> {
+        Ok(Self {
+            port: Constraint::from(settings.port.map(|port| port as u16)),
+        })
     }
 }
 

chrispsyco/vpnvu-desktop

desktop/packages/mullvad-vpn/src/renderer/components/views/index.ts
@@ -30,3 +30,4 @@ export * from './vpn-settings';
 export * from './user-interface-settings';
 export * from './anti-censorship';
 export * from './wireguard-port';
+export * from './lwo-settings';

desktop/packages/mullvad-vpn/test/e2e/route-object-models/anti-censorship/anti-censorship-route-object-model.ts
@@ -19,6 +19,11 @@ export class AntiCensorshipRouteObjectModel extends NavigationObjectModel {
     await this.utils.expectRoute(RoutePath.wireguardPort);
   }
 
+  async gotoLwoSettings() {
+    await this.selectors.lwoSettingsButton().click();
+    await this.utils.expectRoute(RoutePath.lwo);
+  }
+
   async gotoUdpOverTcpSettings() {
     await this.selectors.udpOverTcpSettingsButton().click();
     await this.utils.expectRoute(RoutePath.udpOverTcp);

desktop/packages/mullvad-vpn/test/e2e/route-object-models/routes-object-model.ts
@@ -9,6 +9,7 @@ import { ExpiredRouteObjectModel } from './expired';
 import { FilterRouteObjectModel } from './filter';
 import { LaunchRouteObjectModel } from './launch';
 import { LoginRouteObjectModel } from './login';
+import { LwoSettingsRouteObjectModel } from './lwo-settings';
 import { MainRouteObjectModel } from './main';
 import { ManageDevicesRouteObjectModel } from './manage-devices';
 import { MultihopSettingsRouteObjectModel } from './multihop-settings';
@@ -53,6 +54,7 @@ export class RoutesObjectModel {
   readonly account: AccountRouteObjectModel;
   readonly manageDevices: ManageDevicesRouteObjectModel;
   readonly wireguardPort: WireGuardPortRouteObjectModel;
+  readonly lwoSettings: LwoSettingsRouteObjectModel;
 
   constructor(page: Page, utils: TestUtils) {
     this.selectLanguage = new SelectLanguageRouteObjectModel(page, utils);
@@ -80,5 +82,6 @@ export class RoutesObjectModel {
     this.account = new AccountRouteObjectModel(page, utils);
     this.manageDevices = new ManageDevicesRouteObjectModel(page, utils);
     this.wireguardPort = new WireGuardPortRouteObjectModel(page, utils);
+    this.lwoSettings = new LwoSettingsRouteObjectModel(page, utils);
   }
 }

mullvad-management-interface/src/types/conversions/relay_constraints.rs
@@ -152,6 +152,7 @@ impl From<&mullvad_types::relay_constraints::ObfuscationSettings> for proto::Obf
             wireguard_port: Some(proto::obfuscation_settings::WireguardPort::from(
                 &settings.wireguard_port,
             )),
+            lwo: Some(proto::obfuscation_settings::Lwo::from(&settings.lwo)),
         }
     }
 }
@@ -188,6 +189,14 @@ impl From<&mullvad_types::relay_constraints::WireguardPortSettings>
     fn from(port: &mullvad_types::relay_constraints::WireguardPortSettings) -> Self {
         Self {
             port: port.get().map(u32::from).option(),
+        }
+    }
+}
+
+impl From<&mullvad_types::relay_constraints::LwoSettings> for proto::obfuscation_settings::Lwo {
+    fn from(settings: &mullvad_types::relay_constraints::LwoSettings) -> Self {
+        Self {
+            port: settings.port.map(u32::from).option(),
         }
     }
 }
@@ -399,11 +408,21 @@ impl TryFrom<proto::ObfuscationSettings> for mullvad_types::relay_constraints::O
             }
         };
 
+        let lwo = match settings.lwo {
+            Some(s) => mullvad_types::relay_constraints::LwoSettings::try_from(&s)?,
+            None => {
+                return Err(FromProtobufTypeError::invalid_argument(
+                    "invalid LWO settings",
+                ));
+            }
+        };
+
         Ok(Self {
             selected_obfuscation,
             udp2tcp,
             shadowsocks,
             wireguard_port,
+            lwo,
         })
     }
 }
@@ -444,6 +463,16 @@ impl TryFrom<&proto::obfuscation_settings::WireguardPort>
     ) -> Result<Self, Self::Error> {
         let port = settings.port.map(|port| port as u16);
         Ok(Self::from(port))
+    }
+}
+
+impl TryFrom<&proto::obfuscation_settings::Lwo> for mullvad_types::relay_constraints::LwoSettings {
+    type Error = FromProtobufTypeError;
+
+    fn try_from(settings: &proto::obfuscation_settings::Lwo) -> Result<Self, Self::Error> {
+        Ok(Self {
+            port: Constraint::from(settings.port.map(|port| port as u16)),
+        })
     }
 }
 